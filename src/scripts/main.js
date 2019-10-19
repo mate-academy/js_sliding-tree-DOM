35a8b3b7a83a41c99ad8e0223916f6e5f595a3fd
VadimK <djvadya16@gmail.com>
2019-10-19 21:16:16 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-console.log('hello mate academy!');
+const mainList = document.querySelector('.tree');
+
+mainList.addEventListener('click', (e) => {
+  const ulChild = e.target.parentElement.querySelector('ul');
+  ulChild.hidden = !ulChild.hidden;
+});
