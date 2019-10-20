04971a1ede6e1dbf2f7e379c4adc2924ac59d745
Viktor Lukovets <nuerio63@gmail.com>
2019-10-20 16:12:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+const hideChildren = (e) => {
+  if (!e.target.matches('.title')) {
+    return;
+  }
+  const children = e.target.parentElement.children;
+  for (const child of children) {
+    if (!child.matches('.title') && child.hidden === false) {
+      child.hidden = true;
+      return;
+    } else {
+      child.hidden = false;
+    }
+  }
+};
+
+tree.addEventListener('click', hideChildren);
