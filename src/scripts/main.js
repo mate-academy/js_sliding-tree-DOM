f707169ed15c57367a2c51325a5a783f6f353120
Dmitriymush <powder322@gmail.com>
2019-10-22 16:42:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
+const treeHeaders = document.querySelector('.tree');
 
-console.log('hello mate academy!');
+treeHeaders.addEventListener('click', (action) => {
+  if (action.target.tagName !== 'SPAN') {
+    return;
+  }
+  const innerInfo = action.target.parentNode.childNodes[2];
+  if (!innerInfo) {
+    alert('SORRY NO ITEMS MORE!');
+  }
+  innerInfo.hidden = !innerInfo.hidden;
+});
+// const span = document.createElement('span');
+// const li = document.getElementsByTagName('li');
+// for (const text of li) {
+//   text.prepend(span);
+//   console.log(text.textContent);
+// } не получилось ;(
