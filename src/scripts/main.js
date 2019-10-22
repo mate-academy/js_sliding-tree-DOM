7e9a33e8e1564da1ee7e2302ab3c3910a8dac9ba
Veronika D <donets.veronika@yahoo.com>
2019-10-22 21:48:00 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,8 @@
 'use strict';
 
 document.querySelector('.tree').addEventListener('click', (targetEvent) => {
-  if (targetEvent.target.tagName !== 'SPAN') { return; }
-
   const child = targetEvent.target.parentElement.querySelector('ul');
-  if (!child) { return; }
+  if (!child || targetEvent.target.tagName !== 'SPAN') { return; }
 
   child.hidden = !child.hidden;
 });
