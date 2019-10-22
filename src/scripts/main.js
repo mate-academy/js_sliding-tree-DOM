de16483e576bebf6773b7dae724135b4bb600cae
VadimK <djvadya16@gmail.com>
2019-10-22 17:51:45 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,9 @@
 const mainList = document.querySelector('.tree');
 
 mainList.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
   const ulChild = e.target.parentElement.querySelector('ul');
   ulChild.hidden = !ulChild.hidden;
 });
