f51b6916fc220726e1084c95cb46931ccd760d65
Viktor Lukovets <nuerio63@gmail.com>
2019-10-22 09:35:18 +0300

    spans have no classes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,13 @@
 const tree = document.querySelector('.tree');
 
 const hideChildren = (e) => {
-  if (!e.target.matches('.title')) {
+  if (e.target.tagName !== 'SPAN') {
     return;
   }
   const children = e.target.parentElement.children;
   for (const child of children) {
-    if (!child.matches('.title') && child.hidden === false) {
-      child.hidden = true;
+    if (child.tagName !== e.target.tagName && !child.hidden) {
+      child.hidden = !child.hidden;
       return;
     } else {
       child.hidden = false;
