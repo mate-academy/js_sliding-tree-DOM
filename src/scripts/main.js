96d3e26b884c7dd144feb1399aa387130cb28697
olesyamogil <olesya.mogil@gmail.com>
2019-10-22 14:15:03 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('hello mate academy!');
+const menu = document.querySelector('.tree');
+menu.querySelectorAll('li').forEach((element) => {
+  if (element.children.length) {
+    element.classList.add('closed');
+  }
+});
+
+menu.addEventListener('click', (e) => {
+  const currentItem = e.target;
+  if (currentItem.tagName !== 'LI') {
+    return;
+  }
+  if (currentItem.children.length) {
+    currentItem.classList.toggle('closed');
+    currentItem.classList.toggle('open');
+  }
+});
