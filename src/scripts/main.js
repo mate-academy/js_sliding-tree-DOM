6044c3a23d70f95522737d433afe651965d12541
Leon <FeelineKing@gmail.com>
2019-10-24 02:23:37 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+const liArray = document.querySelectorAll('li');
+
+for (const el of liArray) {
+  if (el.children.length) {
+    el.className = 'closed';
+  }
+}
+
+tree.addEventListener('click', function(event) {
+  const requiredItem = event.target;
+  if (requiredItem.tagName !== 'LI') {
+    return;
+  }
+  if (requiredItem.children.length) {
+    requiredItem.classList.toggle('closed');
+    requiredItem.classList.toggle('open');
+  }
+});
