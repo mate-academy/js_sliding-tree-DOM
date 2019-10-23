58c92ee70267f3d65fd0854b0de0827d3b040dc4
Viktoriia <jhunt@ukr.net>
2019-10-23 12:00:14 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', (element) => {
+  const listItem = element.target.parentElement.querySelector('ul');
+
+  if (element.target.tagName === 'SPAN') {
+    listItem.hidden = !listItem.hidden;
+  }
+});
