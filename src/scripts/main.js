b0fc68bc5db7b6f5ab5cacd037d5d014979277e0
lubremets <lubremets@gmail.com>
2019-10-25 01:20:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
+const liList = document.querySelectorAll('.tree li');
+for (const li of liList) {
+  const span = document.createElement('span');
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
 
-console.log('hello mate academy!');
+document.querySelector('.tree').addEventListener('click', () => {
+  if (event.target.tagName === 'SPAN') {
+    const childrenContainer = event.target.parentNode.querySelector('ul');
+    if (childrenContainer) {
+      childrenContainer.hidden = !childrenContainer.hidden;
+    }
+  }
+});
