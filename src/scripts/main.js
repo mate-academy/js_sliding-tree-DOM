f741b5534c3c20161d3a148fb9d0228fcb55e63a
lubremets <lubremets@gmail.com>
2019-10-25 14:32:16 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,14 @@
 'use strict';
 const liList = document.querySelectorAll('.tree li');
+
 for (const li of liList) {
   const span = document.createElement('span');
   li.prepend(span);
   span.append(span.nextSibling);
 }
 
-document.querySelector('.tree').addEventListener('click', () => {
+const ul = document.getElementById('tree');
+ul.addEventListener('click', () => {
   if (event.target.tagName === 'SPAN') {
     const childrenContainer = event.target.parentNode.querySelector('ul');
     if (childrenContainer) {
