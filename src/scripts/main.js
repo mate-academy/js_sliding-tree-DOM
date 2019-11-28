d0ddc4a909a44d8d1a88c19938ff990c5018652b
Denys <denys.cheporniuk@gmail.com>
2019-11-28 15:45:56 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.getElementsByClassName('tree')[0];
+const ul = document.querySelectorAll('ul');
+
+[...ul].forEach(item => {
+  if (item.parentNode.tagName === 'LI') {
+    const span = document.createElement('span');
+
+    item.parentNode.prepend(span);
+    span.append(span.nextSibling);
+  };
+});
+
+tree.addEventListener('click', () => {
+  if (event.target.tagName !== 'SPAN') {
+    return;
+  };
+
+  [...event.target.parentNode.children].map((item) => {
+    if (item.tagName === 'SPAN') {
+      return;
+    };
+
+    if (item.children && item.style.display !== 'none') {
+      item.style.display = 'none';
+    } else if (item.children) {
+      item.style.display = 'block';
+    };
+  });
+});
