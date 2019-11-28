ebf3d0e8d20e7d2532006df06436cd18f4c698c3
Oles Marola <oles_pw@ukr.net>
2019-11-28 15:49:06 +0200

    tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-console.log('hello mate academy!');
+// eslint-disable-next-line no-shadow
+function hideList(event) {
+  const item = event.target.closest('span');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  const itemList = item.parentNode.querySelector('ul');
+
+  if (!itemList) {
+    return;
+  }
+
+  itemList.hidden = !itemList.hidden;
+}
+
+// eslint-disable-next-line no-shadow
+function listWrapper(list) {
+  for (const li of list.querySelectorAll('li')) {
+    const span = document.createElement('span');
+
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', hideList);
+
+listWrapper(list);
