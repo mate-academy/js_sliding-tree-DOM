9a1b94c4e119dc9040ecb54a60755ebf71c6874b
YuliiaChupryna <julia.chuprina2907@gmail.com>
2019-11-29 18:51:34 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+tree.querySelectorAll('li').forEach(li => {
+  const span = document.createElement('span');
+
+  span.className += 'list-item';
+  li.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.onclick = function(action) {
+  if (action.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const children = action.target.parentNode.querySelector('ul');
+
+  if (children) {
+    children.hidden = !children.hidden;
+  }
+};
