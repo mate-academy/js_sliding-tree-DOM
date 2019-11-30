2d2ca476b8fa5e41aea165f87243ca9d112ee354
IsaacKlark <isaackeklarke@gmail.com>
2019-11-30 12:29:30 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('hello mate academy!');
+for (const li of document.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+function hideList() {
+  if (event.target.nextSibling !== null) {
+    if (!event.target.nextSibling.isHidden) {
+      event.target.nextSibling.style.display = 'none';
+      event.target.nextSibling.isHidden = true;
+    } else if (event.target.nextSibling.isHidden) {
+      event.target.nextSibling.style.display = 'block';
+      event.target.nextSibling.isHidden = false;
+    }
+  }
+}
+
+for (const span of document.querySelectorAll('span')) {
+  span.addEventListener('click', hideList);
+}
