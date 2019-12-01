7c4e4698ae57dbc76f85db503e732afca2cd17ef
bandura <banduraweb@ukr.net>
2019-12-01 23:21:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+for (const li of tree.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  span.classList.add('show');
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.onclick = function(e) {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const childrenContainer = e.target.parentNode.querySelector('ul');
+
+  if (!childrenContainer) {
+    return;
+  }
+
+  childrenContainer.hidden = !childrenContainer.hidden;
+
+  if (childrenContainer.hidden) {
+    e.target.classList.toggle('show');
+  } else {
+    e.target.classList.toggle('show');
+  }
+};
