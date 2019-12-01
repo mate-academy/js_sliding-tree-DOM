891ef193ef63ae797706544cc74baed8b8211f12
YuliiaChupryna <julia.chuprina2907@gmail.com>
2019-12-01 22:59:09 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,22 +2,23 @@
 
 const tree = document.querySelector('.tree');
 
-tree.querySelectorAll('li').forEach(li => {
+for (const li of tree.querySelectorAll('li')) {
   const span = document.createElement('span');
 
-  span.className += 'list-item';
   li.prepend(span);
   span.append(span.nextSibling);
-});
+}
 
 tree.onclick = function(action) {
   if (action.target.tagName !== 'SPAN') {
     return;
   }
 
-  const children = action.target.parentNode.querySelector('ul');
+  const childrenContainer = action.target.parentNode.querySelector('ul');
 
-  if (children) {
-    children.hidden = !children.hidden;
+  if (!childrenContainer) {
+    return undefined;
   }
+
+  childrenContainer.hidden = !childrenContainer.hidden;
 };
