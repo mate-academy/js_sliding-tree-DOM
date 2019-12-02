3985b34b84837d6c1ee3f8cd6679291a0c4a6428
PavloPantus <mr.pantus@gmail.com>
2019-12-02 20:06:49 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('hello mate academy!');
+const menu = document.querySelector('.tree');
+
+function putInSpan(element) {
+  for (const li of element.querySelectorAll('li')) {
+    const span = document.createElement('span');
+
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+putInSpan(menu);
+
+menu.addEventListener('click', (myEvent) => {
+  if (myEvent.target.tagName === 'SPAN') {
+    const children
+    = Array.from(myEvent.target.closest('li').children).slice(1,);
+
+    for (const child of children) {
+      child.classList.toggle('hide');
+    }
+  }
+});
