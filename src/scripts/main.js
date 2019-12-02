82eadb30f17971cafd75b1427bc224c8cad19411
OlehTereshchuk <olehtereshchuk@ukr.net>
2019-12-02 16:27:46 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('hello mate academy!');
+const ul = document.querySelectorAll('ul');
+const tree = document.querySelector('.tree');
+
+[...ul].forEach(elem => {
+  if (elem.parentElement.tagName === 'LI') {
+    const span = document.createElement('span');
+
+    elem.parentNode.prepend(span);
+    span.append(span.nextSibling);
+  }
+});
+
+const hideList = function() {
+  const item = event.target.closest('span');
+
+  if (!item) {
+    return;
+  }
+
+  const itemList = item.parentNode.querySelector('ul');
+
+  itemList.hidden = !itemList.hidden;
+};
+
+tree.addEventListener('click', hideList);
