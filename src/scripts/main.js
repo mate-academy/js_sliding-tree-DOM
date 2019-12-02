cf6f569de7361f287753e065e64f5e006d28baa9
Alexandra Boyko <Alexboyko04@gmail.com>
2019-12-02 13:54:28 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+for (const li of tree.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.className = 'header';
+  span.append(span.nextSibling);
+}
+
+tree.onclick = function(action) {
+  if (action.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const childrenContainer = action.target.parentNode.querySelector('ul');
+
+  if (!childrenContainer) {
+    return;
+  }
+  childrenContainer.hidden = !childrenContainer.hidden;
+};
