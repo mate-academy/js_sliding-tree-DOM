3ec5c2d8718487074ab339993603b8f2f398e4ea
helmax-y <max_rad@ukr.net>
2019-12-02 16:45:27 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('hello mate academy!');
+document.querySelectorAll('li').forEach(li => {
+  if (li.children.length) {
+    const span = document.createElement('span');
+
+    span.classList.add('list-name');
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+});
+
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', action => {
+  if (action.target.tagName === 'SPAN') {
+    const ulClasses = action.target.nextSibling.classList;
+
+    if (ulClasses.contains('hide')) {
+      ulClasses.remove('hide');
+    } else {
+      ulClasses.add('hide');
+    }
+  }
+});
