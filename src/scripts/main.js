ee18dd28cfb4ba697ebc356ab08c541c9e840240
Anna <anna.lyrynenko@gmail.com>
2020-02-06 00:06:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+const listElements = tree.querySelectorAll('li');
+
+for (const li of listElements) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.onclick = function(action) {
+  if (action.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const clidrenList = action.target.parentNode.querySelector('ul');
+
+  if (clidrenList) {
+    clidrenList.hidden = !clidrenList.hidden;
+  }
+};
