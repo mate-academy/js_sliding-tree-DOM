fa4543e43526ec330f3bb2cc6ebc6ead4287d8a3
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-02-05 16:48:25 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', function(ev) {
+  const item = ev.target;
+
+  if (item.tagName !== 'LI') {
+    return;
+  }
+
+  if (item.children.length !== 0) {
+    item.children[0].classList.toggle('tree__hidden');
+  };
+});
