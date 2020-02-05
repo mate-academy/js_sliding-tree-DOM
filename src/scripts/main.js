9af1ba956e8ac5537b418894092fa3275b459c46
Viacheslav <slavic.lavrenyuk@gmail.com>
2020-02-05 23:00:04 +0200

    Sliding DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+const li = tree.querySelectorAll('li');
+
+for (const item of li) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+}
+
+const toggleList = function(e) {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const childUl = e.target.parentNode.querySelector('ul');
+
+  if (!childUl) {
+    return;
+  }
+
+  childUl.hidden = !childUl.hidden;
+};
+
+tree.addEventListener('click', toggleList);
