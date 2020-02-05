ce13c8157981fc81fd391e0012d67b0422dd65ee
Olexander <kravchenko.sania@gmail.com>
2020-02-05 23:36:53 +0200

    attempt 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+tree.querySelectorAll('li').forEach(item => {
+  const node = item.firstChild;
+
+  item.insertAdjacentHTML('afterBegin', `<span>${node.textContent}</span>`);
+  item.removeChild(node);
+});
+
+function handler(event1) {
+  if (event1.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const innerUl = event1.target.parentElement.querySelector('ul');
+
+  if (innerUl) {
+    innerUl.hidden = !innerUl.hidden;
+  }
+}
+
+tree.addEventListener('click', handler);
