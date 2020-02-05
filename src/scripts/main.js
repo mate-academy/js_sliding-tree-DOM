d94f9d4fbce13d26fc2ac288b3d13703d44f70f4
Vitalii <vitaliybondarenko1982@gmail.com>
2020-02-05 23:08:53 +0200

    on review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+const liList = tree.querySelectorAll('li');
+
+for (const li of liList) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+const onClickHandler = function(evt) {
+  const item = evt.target.parentNode.querySelector('ul');
+
+  if (evt.target.tagName !== 'SPAN' || !item) {
+    return;
+  }
+
+  item.classList.toggle('hidden');
+};
+
+tree.addEventListener('click', onClickHandler);
