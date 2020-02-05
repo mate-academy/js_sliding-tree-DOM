e93bd6e064c768c11568df1e323c58757641a0e6
masha <mariafil2107@gmail.com>
2020-02-05 17:17:13 +0200

    list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.tree');
+
+const handleListClick = ({ target }) => {
+  if (target.nodeName === 'LI' && target.childElementCount > 0) {
+    const nextLi = target.children[0].children;
+
+    for (let i = 0; i < nextLi.length; i++) {
+      nextLi[i].classList.toggle('displaied');
+    }
+  }
+};
+
+list.addEventListener('click', handleListClick);
