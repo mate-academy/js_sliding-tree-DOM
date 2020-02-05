ee6033822e501352c7c9cd0ef950d229956c418e
Natalia Lypovyk <cardinnata@gmail.com>
2020-02-06 00:13:56 +0200

    Sliding Tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+
+for (const li of listItems) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+};
+
+tree.addEventListener('click', function(item) {
+  if (item.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const ul = item.target.parentElement.querySelector('ul');
+
+  if (!ul) {
+    return;
+  }
+
+  ul.classList.toggle('hidden');
+});
