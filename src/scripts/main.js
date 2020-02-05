3264a40a3d985ea514558ca385cebaa073c72fab
Senya Volkov <formobileprima@gmail.com>
2020-02-05 15:40:36 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.body.querySelector('.tree');
+
+function showAndHide(e) {
+  if (e.target.children.length) {
+    [...e.target.children][0].classList.toggle('HideShow');
+  }
+}
+
+tree.addEventListener('click', showAndHide);
+
+function boldText(e) {
+  if (e.target.tagName === 'LI' && e.target.children.length) {
+    e.target.classList.add('boldText');
+
+    for (const elem of [...e.target.children]) {
+      elem.classList.add('normalText');
+    }
+  }
+}
+
+tree.addEventListener('mouseover', boldText);
+
+function normalText(e) {
+  if (e.target.tagName === 'LI' && e.target.children.length) {
+    e.target.classList.remove('boldText');
+  }
+}
+
+tree.addEventListener('mouseout', normalText);
