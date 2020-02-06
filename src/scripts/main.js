1a7cb2cb9bea5160e182705d33ce09b773a7ddfe
Roma <romakoder@gmail.com>
2020-02-06 20:34:36 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+const list = tree.querySelectorAll('li');
+
+for (const li of list) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.onclick = function(e) {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const ulContainer = e.target.parentNode.querySelector('ul');
+
+  if (!ulContainer) {
+    return;
+  }
+
+  ulContainer.hidden = !ulContainer.hidden;
+};
