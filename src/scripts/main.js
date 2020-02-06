a4069c5433d4fecce3137b36a0ccf2e67a890091
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-02-06 17:19:38 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+for (const li of tree.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+};
+
+tree.addEventListener('click', ({ target }) => {
+  if (target.tagName !== 'SPAN') {
+    return;
+  };
+
+  const children = target.parentNode.querySelector('ul');
+
+  if (!children) {
+    return;
+  };
+
+  children.hidden = !children.hidden;
+});
