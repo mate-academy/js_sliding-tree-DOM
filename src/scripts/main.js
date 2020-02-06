f25ea30702e96eed8a6462f73387a1b325e09d31
Stanislav Mochyr <delenjer@gmail.com>
2020-02-06 17:26:23 +0200

    js_sliding-tree-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', (e) => {
+  const item = e.target;
+
+  if (item.children.length !== 0) {
+    item.children[0].classList.toggle('open');
+  }
+});
