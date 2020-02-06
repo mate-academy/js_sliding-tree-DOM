a35a5e24927d8a026fd25b253ecff2cbf36affff
Actireon <eheieiei37@gmail.com>
2020-02-06 11:23:04 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+function removeChildrenHandler() {
+  const children = event.target.children[0].children;
+
+  for (const key of children) {
+    if (key.nodeName === 'LI') {
+      key.classList.toggle('tree__toggle');
+    }
+  }
+}
+
+tree.addEventListener('click', removeChildrenHandler);
