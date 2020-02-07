34ab74e16a2673c8e8c1b3016686475d6bfd0ba6
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-02-07 16:54:37 +0200

    sliding tree DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+for (const li of tree.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.onclick = function(e) {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const childrenBox = e.target.parentNode.querySelector('ul');
+
+  if (!childrenBox) {
+    return;
+  }
+
+  childrenBox.hidden = !childrenBox.hidden;
+};
