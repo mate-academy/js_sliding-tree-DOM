32ed8905ee52a4b502027d8355734ddaf2747dcc
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-02-08 17:59:33 +0200

    add span with js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const tree = document.querySelector('.tree');
+const li = document.querySelectorAll('li');
+
+for (const element of li) {
+  const span = document.createElement('span');
+
+  element.prepend(span);
+  span.prepend(span.nextSibling);
+}
 
 tree.addEventListener('click', () => {
   const listItem = event.target;
