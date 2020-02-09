534b7d1fb96c6aee8fbed96c34b4b713024adcef
SahaPylypyuk <sashapulupyk@gmail.com>
2020-02-09 14:34:49 +0200

    Task implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+const list = document.querySelectorAll('li');
+
+for (const li of list) {
+  const p = document.createElement('p');
+
+  li.prepend(p);
+  p.append(p.nextSibling);
+}
+
+tree.addEventListener('click', () => {
+  if (event.target.tagName !== 'P') {
+    return;
+  }
+
+  const item = event.target.parentNode.querySelector('ul');
+
+  if (!item) {
+    return;
+  }
+
+  item.hidden = !item.hidden;
+});
