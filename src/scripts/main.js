6ccb342c02f1dc1a40cc7985b564f76c99f1e695
Iurii Petrov <yurapetrov91@gmail.com>
2020-04-15 15:55:05 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+const listOfLi = document.querySelectorAll('li');
+
+for (const li of listOfLi) {
+  const span = document.createElement('span');
+
+  span.classList.add('bold-text');
+  span.append(li.firstChild);
+  li.prepend(span);
+}
+
+tree.addEventListener('click', (arg) => {
+  const ul = arg.target.parentNode.querySelector('ul');
+
+  if (!ul) {
+    return;
+  }
+
+  ul.hidden = !ul.hidden;
+});
