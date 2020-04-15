3767bc0857d19c8b236eccacb17384bb72380ad9
Rustam Khananov <antmine2017@gmail.com>
2020-04-15 18:37:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+function sublistHider(ev) {
+  if (ev.target.tagName === 'SPAN') {
+    ev.target
+      .parentElement
+      .children[1].hidden = !ev.target.parentElement.children[1].hidden;
+  }
+}
+
+const lies = tree.querySelectorAll('li');
+
+for (const item of lies) {
+  const a = item.childNodes[0];
+
+  item.insertAdjacentHTML(
+    'afterbegin',
+    `<span>${item.childNodes[0].data}</span>`
+  );
+  a.data = '';
+}
+
+tree.addEventListener('click', sublistHider);
