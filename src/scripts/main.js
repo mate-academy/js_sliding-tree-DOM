e6ff458125d7a7d1325eceefefca3483e8830c30
Nick <nikolyaoooyartchuk@gmail.com>
2020-04-15 16:41:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('hello mate academy!');
+function onClick(value) {
+  const item = value.target;
+  const elem = item.firstElementChild;
+
+  if (elem.matches('ul')) {
+    elem.hidden = !elem.hidden;
+  }
+}
+
+const list = document.querySelector('.tree');
+
+for (const child of list.children) {
+  child.classList.add('head');
+}
+
+list.addEventListener('click', onClick);
