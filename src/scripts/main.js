6a5a7717256eca90ea7c0f80186fcd3b49880457
Tetiana <ishchuktr@gmail.com>
2020-04-15 16:45:25 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (event) => {
+  const li = event.target.closest('li');
+
+  if (!li) {
+    return;
+  }
+
+  if (li.children.length) {
+    const children = li.children[0];
+    
+    children.hidden = !children.hidden;
+  }
+});
