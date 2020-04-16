5e158fefab2448f27429d478192ea11aa956ab6c
Aleksandr-Tyagun <alexander.tiagun@gmail.com>
2020-04-16 17:46:44 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('hello mate academy!');
+const liList = document.querySelectorAll('li');
+const ul = document.querySelector('ul');
+
+for (const li of liList) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+ul.addEventListener('click', function() {
+  if (event.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  if (event.target.nextSibling) {
+    event.target.nextSibling.hidden = !event.target.nextSibling.hidden;
+  }
+});
