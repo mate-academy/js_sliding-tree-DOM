03cc54a6d424a176b8aeb6e4a0dc871ff5214e83
romarios1987 <romios047@gmail.com>
2020-04-16 09:13:38 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+const lis = document.querySelectorAll('li');
+
+lis.forEach(item => {
+  const span = document.createElement('span');
+
+  span.innerHTML = item.firstChild.data.trim();
+  item.replaceChild(span, item.firstChild);
+});
+
+tree.addEventListener('click', e => {
+  const ul = e.target.nextElementSibling;
+
+  if (ul) {
+    ul.classList.toggle('hide');
+  }
+});
