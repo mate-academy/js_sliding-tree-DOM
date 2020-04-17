7147db83c1224fa06d71954f58a463a48f10ec38
Kira Marks <serafita22@gmail.com>
2020-04-17 22:41:33 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+const lis = document.querySelectorAll('li');
+
+for (const li of lis) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+function hideList(e) {
+  const target = e.target;
+
+  if (target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const childrenContainer = target.parentNode.querySelector('ul');
+
+  if (!childrenContainer) {
+    return;
+  }
+
+  childrenContainer.hidden = !childrenContainer.hidden;
+}
+
+tree.addEventListener('click', hideList);
