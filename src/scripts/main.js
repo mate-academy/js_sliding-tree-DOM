1824fb44f1a7ea4dfa20efce8ebd7e146bc6cf54
Vladimir-Beliy <vovaazick@gmail.com>
2020-04-18 14:17:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+for (const li of tree.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', hideList);
+
+function hideList(e) {
+  const ul = e.target.nextElementSibling;
+
+  if (!ul || ul.tagName !== 'UL' || e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  ul.hidden = !ul.hidden;
+}
