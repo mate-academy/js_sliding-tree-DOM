fb16bf02c19917814d141de2ca56e9884355d821
Vlad <oneviddi@gmail.com>
2020-04-20 11:31:25 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
+/* eslint-disable no-useless-return */
 'use strict';
 
-console.log('hello mate academy!');
+for (const li of document.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const parentUL = e.target.parentElement.querySelector('ul');
+
+  if (parentUL) {
+    parentUL.hidden = !parentUL.hidden;
+  }
+});
