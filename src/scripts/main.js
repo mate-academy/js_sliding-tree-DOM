5b9684b3eb81c717f7c4f2941ed64b1b86953a0c
ShapovalDenys <denys.igorevich@gmail.com>
2020-04-21 16:32:42 +0300

    solution v0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('hello mate academy!');
+const ul = document.querySelector('.tree');
+const arrayOfLi = document.querySelectorAll('li');
+
+for (const li of arrayOfLi) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(li.childNodes[1]);
+}
+
+ul.addEventListener('click', onClick);
+
+function onClick(e) {
+  const target = e.target;
+  const child = target.parentNode.querySelector('ul');
+
+  if (target.tagName === 'SPAN') {
+    child.hidden = !child.hidden;
+  } else {
+    return;
+  };
+};
