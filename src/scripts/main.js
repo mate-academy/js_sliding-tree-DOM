3c1d61f79fb2894907fa137a0ad432797609ef4a
aleksey-10 <alexcare1337@gmail.com>
2020-06-11 15:38:57 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('hello mate academy!');
+document.querySelector('.tree').addEventListener('click', function(e) {
+  const title = e.target.firstChild;
+  const list = title.nextElementSibling;
+
+  if (e.target.tagName !== 'LI' || !list) {
+    return;
+  }
+
+  list.classList.toggle('hidden');
+});
