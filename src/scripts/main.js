c3c8386f4e5392146a3102252062ae613fe25282
aleksey-10 <alexcare1337@gmail.com>
2020-06-11 21:56:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.querySelector('.tree').addEventListener('click', function(e) {
-  const list = e.target.firstChild.nextElementSibling;
+  const list = e.target.firstElementChild;
 
   if (e.target.tagName === 'LI' && list) {
     list.classList.toggle('hidden');
