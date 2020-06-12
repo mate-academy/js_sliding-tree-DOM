777325eac84559cb2bc397d7c4e279b46b7e20e1
Liliia <chuiko1112@gmail.com>
2020-06-12 21:43:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
+/* eslint-disable no-console */
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', function(ev) {
+  const listInside = Array.from(ev.target.childNodes);
+
+  if (checker(ev.target)) {
+    for (const item of listInside) {
+      if (item.tagName === 'UL') {
+        item.classList.toggle('hide');
+      }
+    }
+  }
+});
+
+list.addEventListener('mouseover', function(ev) {
+  if (checker(ev.target)) {
+    ev.target.style.fontWeight = 'bold';
+    ev.target.style.cursor = 'pointer';
+    ev.target.firstElementChild.style.fontWeight = 'normal';
+    ev.target.firstElementChild.style.cursor = 'auto';
+  }
+});
+
+list.addEventListener('mouseout', function(ev) {
+  if (checker(ev.target)) {
+    ev.target.style.fontWeight = 'normal';
+  }
+});
+
+function checker(element) {
+  const checkIsList = Array.from(element.childNodes).some(
+    tag => tag.tagName === 'UL');
+
+  return checkIsList;
+}
