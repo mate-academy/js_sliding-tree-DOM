a6f8bf7fcd2b866635852771fcee34d4775da787
solaryasha <solaryasha@gmail.com>
2020-06-15 17:30:34 +0300

    Add hide on click functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', e => {
+  const children = [ ...e.target.children ];
+
+  if (children.length) {
+    const [title] = children;
+
+    [ ...title.children ].forEach(item => item.classList.toggle('hide'));
+  }
+});
