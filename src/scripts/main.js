23d33b5904331411c242fd81bb32c4b2d8f2aec7
DimaDP <dimadp95@gmail.com>
2020-06-16 19:58:23 +0300

    dimadp__solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+const liItems = document.querySelectorAll('li');
+
+for (const item of liItems) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.prepend(span.nextSibling);
+}
+
+function hideItems(e) {
+  const item = e.target.closest('li');
+
+  if (!item) {
+    return;
+  }
+
+  if (item.children.length) {
+    const children = item.children[1];
+
+    children.hidden = !children.hidden;
+  }
+};
+tree.addEventListener('click', hideItems);
