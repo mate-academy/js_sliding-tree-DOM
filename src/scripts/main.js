539c94355d6b2a342a13b617180df2c77f69c9d0
Campari229 <KennyKoin@gmail.com>
2020-06-16 19:18:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.tree');
+
+function targetCatcher(ev) {
+  if (ev.target.tagName === 'LI' && ev.target.children.length !== 0) {
+    return true;
+  }
+
+  return false;
+}
+
+list.addEventListener('click', function(ev) {
+  const item = ev.target;
+
+  if (targetCatcher(ev)) {
+    if (item.children[0].style.display === 'none') {
+      item.children[0].style.display = 'block';
+    } else {
+      item.children[0].style.display = 'none';
+    }
+  }
+});
+
+list.addEventListener('mouseover', function(ev) {
+  const item = ev.target;
+
+  if (targetCatcher(ev)) {
+    item.style.fontWeight = 'bold';
+    item.children[0].style.fontWeight = 'normal';
+  }
+});
+
+list.addEventListener('mouseout', function(ev) {
+  const item = ev.target;
+
+  if (targetCatcher(ev)) {
+    item.style.fontWeight = 'normal';
+  }
+});
