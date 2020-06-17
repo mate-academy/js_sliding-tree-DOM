d28d668acfa1469454adc25189eed00df60ce783
Stanislav <stasusss07@gmail.com>
2020-06-18 01:50:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+function closer(action) {
+  const element = action.target.firstElementChild;
+
+  if (element) {
+    element.classList.toggle('closed-tree');
+  }
+}
+
+function hover(action) {
+  if (action.target.firstElementChild) {
+    action.target.classList.toggle('hovered-text');
+  }
+}
+
+tree.addEventListener('click', closer);
+tree.addEventListener('mouseover', hover);
+tree.addEventListener('mouseout', hover);
