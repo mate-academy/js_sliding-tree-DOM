88e5c4c0708eeba8013f3d83069ffa1b41f81469
DTR <t.terry.rice@gmail.com>
2020-06-17 19:21:38 +0300

    final 1.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,25 @@
 'use strict';
 
-const generalTree = document.querySelector('.tree');
+const tree = document.querySelector('.tree');
+
+function check(element) {
+  return (element || tree.contains(element));
+}
 
 function hide(hideEvent) {
   const targetElement = hideEvent.target.firstElementChild;
 
-  if (!targetElement || !generalTree.contains(targetElement)) {
-    return;
-  };
+  if (check(targetElement)) {
+    if (targetElement.tagName === ('UL')) {
+      if (targetElement.style.display !== 'none') {
+        targetElement.style.display = 'none';
 
-  if (targetElement.tagName === ('UL')) {
-    if (targetElement.style.display !== 'none') {
-      targetElement.style.display = 'none';
+        return;
+      };
 
-      return;
-    };
-
-    if (targetElement.style.display === 'none') {
-      targetElement.style.display = 'block';
+      if (targetElement.style.display === 'none') {
+        targetElement.style.display = 'block';
+      }
     }
   }
 }
@@ -25,28 +27,24 @@ function hide(hideEvent) {
 function over(overEvent) {
   const targetElement = overEvent.target.firstElementChild;
 
-  if (!targetElement || !generalTree.contains(targetElement)) {
-    return;
-  }
-
-  if (targetElement.tagName === ('UL')) {
-    overEvent.target.style.fontWeight = 'bold';
-    targetElement.style.fontWeight = 'normal';
+  if (check(targetElement)) {
+    if (targetElement.tagName === ('UL')) {
+      overEvent.target.style.fontWeight = 'bold';
+      targetElement.style.fontWeight = 'normal';
+    }
   }
 }
 
 function out(outEvent) {
   const targetElement = outEvent.target.firstElementChild;
 
-  if (!targetElement || !generalTree.contains(targetElement)) {
-    return;
-  }
-
-  if (targetElement.tagName === ('UL')) {
-    outEvent.target.style.fontWeight = 'normal';
+  if (check(targetElement)) {
+    if (targetElement.tagName === ('UL')) {
+      outEvent.target.style.fontWeight = 'normal';
+    }
   }
 }
 
-generalTree.addEventListener('click', hide);
-generalTree.addEventListener('mouseover', over);
-generalTree.addEventListener('mouseout', out);
+tree.addEventListener('click', hide);
+tree.addEventListener('mouseover', over);
+tree.addEventListener('mouseout', out);
