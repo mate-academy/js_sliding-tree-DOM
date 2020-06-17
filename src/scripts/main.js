1bc75174f5134414c1aa99ae56477f2f72b5b9e7
Vovasef <vovasef@gmail.com>
2020-06-17 21:35:27 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('hello mate academy!');
+let selectedElement;
+
+document.addEventListener('mouseover', e => {
+  if (e.target.tagName === 'UL' || e.target.tagName === 'LI') {
+    e.target.style.fontWeight = 'bold';
+
+    [...e.target.children].forEach(child => {
+      child.style.fontWeight = 'normal';
+    });
+  };
+});
+
+document.addEventListener('mouseout', e => {
+  e.target.style.fontWeight = 'normal';
+});
+
+document.addEventListener('click', e => {
+  selectedElement = e.target;
+
+  if (selectedElement.firstElementChild) {
+    if (selectedElement.firstElementChild.tagName === 'UL') {
+      selectedElement.firstElementChild.classList.toggle('display-none');
+    }
+  }
+});
