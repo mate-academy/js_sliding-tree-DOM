6d3107eb34dab65169ad02ba176ac408b316d1be
tyooma <flying.alien.godzilla@gmail.com>
2020-06-17 15:54:43 +0300

    sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.tree');
+
+let count = 0;
+
+function listHider() {
+  count++;
+
+  const item = event.target;
+  const ul = item.querySelector('UL');
+
+  if (!item || !item.contains(ul)) {
+    return;
+  }
+
+  if (count % 2 === 0) {
+    ul.style.display = 'none';
+  } else {
+    ul.style.display = '';
+  }
+}
+
+function boldText() {
+  const item = event.target;
+  const ul = item.querySelector('UL');
+
+  if (!item || !item.contains(ul)) {
+    return;
+  }
+
+  item.style.fontWeight = 'bold';
+  item.firstElementChild.style.fontWeight = 'normal';
+}
+
+function normalizeText() {
+  const item = event.target;
+  const ul = item.querySelector('UL');
+
+  if (!item || !item.contains(ul)) {
+    return;
+  }
+
+  item.style.fontWeight = 'normal';
+}
+
+list.addEventListener('click', listHider);
+list.addEventListener('mouseover', boldText);
+list.addEventListener('mouseout', normalizeText);
