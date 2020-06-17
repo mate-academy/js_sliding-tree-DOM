9ef22f589e8b264832f6c6fa7d22dee790f80c99
DTR <t.terry.rice@gmail.com>
2020-06-17 18:43:47 +0300

    final 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,52 @@
 'use strict';
 
-console.log('hello mate academy!');
+const generalTree = document.querySelector('.tree');
+
+function hide(hideEvent) {
+  const targetElement = hideEvent.target.firstElementChild;
+
+  if (!targetElement || !generalTree.contains(targetElement)) {
+    return;
+  };
+
+  if (targetElement.tagName === ('UL')) {
+    if (targetElement.style.display !== 'none') {
+      targetElement.style.display = 'none';
+
+      return;
+    };
+
+    if (targetElement.style.display === 'none') {
+      targetElement.style.display = 'block';
+    }
+  }
+}
+
+function over(overEvent) {
+  const targetElement = overEvent.target.firstElementChild;
+
+  if (!targetElement || !generalTree.contains(targetElement)) {
+    return;
+  }
+
+  if (targetElement.tagName === ('UL')) {
+    overEvent.target.style.fontWeight = 'bold';
+    targetElement.style.fontWeight = 'normal';
+  }
+}
+
+function out(outEvent) {
+  const targetElement = outEvent.target.firstElementChild;
+
+  if (!targetElement || !generalTree.contains(targetElement)) {
+    return;
+  }
+
+  if (targetElement.tagName === ('UL')) {
+    outEvent.target.style.fontWeight = 'normal';
+  }
+}
+
+generalTree.addEventListener('click', hide);
+generalTree.addEventListener('mouseover', over);
+generalTree.addEventListener('mouseout', out);
