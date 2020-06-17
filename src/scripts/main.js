e542fb0755dc93a3bdc699f7f4a052a39759d7fe
tyooma <flying.alien.godzilla@gmail.com>
2020-06-17 16:39:37 +0300

    sliding tree version 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,48 +2,20 @@
 
 const list = document.querySelector('.tree');
 
-let count = 0;
-
 function listHider() {
-  count++;
+  const item = event.target.firstElementChild;
 
-  const item = event.target;
-  const ul = item.querySelector('UL');
-
-  if (!item || !item.contains(ul)) {
-    return;
-  }
-
-  if (count % 2 === 0) {
-    ul.style.display = 'none';
-  } else {
-    ul.style.display = '';
+  if (item) {
+    item.classList.toggle('hidden');
   }
 }
 
-function boldText() {
-  const item = event.target;
-  const ul = item.querySelector('UL');
-
-  if (!item || !item.contains(ul)) {
-    return;
+function fontWeightChanger() {
+  if (event.target.firstElementChild) {
+    event.target.classList.toggle('hover');
   }
-
-  item.style.fontWeight = 'bold';
-  item.firstElementChild.style.fontWeight = 'normal';
-}
-
-function normalizeText() {
-  const item = event.target;
-  const ul = item.querySelector('UL');
-
-  if (!item || !item.contains(ul)) {
-    return;
-  }
-
-  item.style.fontWeight = 'normal';
 }
 
 list.addEventListener('click', listHider);
-list.addEventListener('mouseover', boldText);
-list.addEventListener('mouseout', normalizeText);
+list.addEventListener('mouseover', fontWeightChanger);
+list.addEventListener('mouseout', fontWeightChanger);
