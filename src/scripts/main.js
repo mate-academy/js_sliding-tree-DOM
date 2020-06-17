58dd4c7c474c64c78f57c177f843c2d524dfb28f
Denys <gryshko_denys@gmail.com>
2020-06-17 10:08:34 +0300

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-console.log('hello mate academy!');
+const mainList = document.querySelector('.tree');
+
+mainList.addEventListener('click', toggleList);
+
+function toggleList(e) {
+  const currentList = e.target;
+
+  if (e.target.tagName !== 'LI' || !currentList.children[0]) {
+    return;
+  }
+
+  if (currentList.children[0].hidden) {
+    currentList.children[0].hidden = false;
+  } else {
+    currentList.children[0].hidden = true;
+  }
+}
+
+mainList.addEventListener('mousemove', makeBold);
+
+function makeBold(e) {
+  const currentItem = e.target;
+
+  if (e.target.tagName !== 'LI') {
+    return;
+  }
+
+  currentItem.style.fontWeight = 'bold';
+}
+
+mainList.addEventListener('mouseout', makeNormal);
+
+function makeNormal(e) {
+  const currentItem = e.target;
+
+  if (e.target.tagName !== 'LI') {
+    return;
+  }
+
+  currentItem.style.fontWeight = 'normal';
+}
