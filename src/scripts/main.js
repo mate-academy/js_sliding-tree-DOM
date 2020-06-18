5f33379ec17d9c26f08692667fdb2089d71c7755
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-18 11:09:51 +0300

    scripted list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,18 @@
+/* eslint-disable no-console */
 'use strict';
 
-// eslint-disable-next-line no-console
 console.log('hello mate academy!');
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', click => {
+  const ul = click.target.querySelector('ul');
+
+  if (ul) {
+    if (ul.style.display !== 'none') {
+      ul.style.display = 'none';
+    } else {
+      ul.style.display = '';
+    }
+  }
+});
