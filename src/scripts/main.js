95b8f540465b7a12d0b1d97b66d5a9d410d704b3
Slava Polishchuk <dizerllc@gmail.com>
2020-06-18 20:26:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const isHidden = e.target.firstElementChild.getAttribute('hidden') || false;
+
+  if (e.target.firstElementChild.tagName === 'UL') {
+    const branch = e.target.firstElementChild;
+
+    isHidden === false
+      ? branch.setAttribute('hidden', true)
+      : branch.removeAttribute('hidden');
+  };
+});
