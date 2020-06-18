3e085464947583bb15344f1f8d516aadeb965ab6
Andrii Mishtal <mishtal.andriy@gmail.com>
2020-06-18 09:16:07 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tree = document.querySelector('.tree');
+
+function slidingTree(e) {
+  const item = e.target.firstElementChild;
+
+  if (item) {
+    item.classList.toggle('hide');
+  }
+}
+
+function hover(e) {
+  if (e.target.firstElementChild) {
+    e.target.classList.toggle('hovered');
+  }
+}
+
+tree.addEventListener('click', slidingTree);
+tree.addEventListener('mouseover', hover);
+tree.addEventListener('mouseout', hover);
