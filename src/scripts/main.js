e2c3b4d4d2f2a4f1ba65cf267b82bdd028a4e6c5
Vitalii Kuzmichov <43987305+Kussmich@users.noreply.github.com>
2020-06-18 10:28:27 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('hello mate academy!');
+const mainList = document.querySelector('.tree');
+
+function hoverTitle(e) {
+  if (e.target.firstElementChild) {
+    e.target.classList.toggle('hover');
+  }
+}
+
+function listItemToggle(e) {
+  const listItem = e.target.firstElementChild;
+
+  if (listItem) {
+    listItem.classList.toggle('hidden');
+  }
+}
+
+mainList.addEventListener('click', listItemToggle);
+mainList.addEventListener('mouseover', hoverTitle);
+mainList.addEventListener('mouseout', hoverTitle);
