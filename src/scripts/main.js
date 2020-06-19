34ee683f909bb9217f520a2dc0db83e5afa715f9
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-06-19 20:45:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', item => {
+  item.target.firstElementChild.classList.toggle('hidden');
+});
+
+function hoverAdd(item) {
+  item.target.style.fontWeight = 'bold';
+  item.target.child.style.fontWeight = 'normal';
+}
+
+function hoverRemove(item) {
+  item.target.style.fontWeight = 'normal';
+}
+
+list.addEventListener('mouseover', hoverAdd);
+list.addEventListener('mouseout', hoverRemove);
