7c9f418827530e9c19d73ea77a9061ce7675cb6b
Anatoliy Pokhylko <pokhilko.av@gmail.com>
2020-07-14 13:28:08 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const treeLi = tree.querySelectorAll('li');
+
+[...treeLi].map(item => {
+  if (item.childNodes.length > 1) {
+    return item.prepend(document.createElement('span'));
+  }
+});
+
+const spanLi = tree.querySelectorAll('span');
+
+[...spanLi].map(item => item.append(item.nextSibling));
+
+tree.addEventListener('click', event => {
+  const point = event.target;
+  const pointFirstChild = point.nextSibling;
+
+  if (point.tagName !== 'SPAN' || !pointFirstChild) {
+    return;
+  }
+
+  if (pointFirstChild.hidden) {
+    pointFirstChild.hidden = false;
+  } else {
+    pointFirstChild.hidden = true;
+  }
+});
