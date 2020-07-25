37c9fc477e2c69aa2cb78df76ca350715e0365b1
Ihor Hranetskyi <graniecki20@gmail.com>
2020-07-25 08:14:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const items = list.querySelectorAll('li');
+
+[...items].forEach(item => {
+  if (item.querySelector('ul')) {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+
+    const header = item.querySelector('span');
+
+    header.append(item.childNodes[1]);
+
+    header.addEventListener('click', () => {
+      const ul = item.querySelector('ul');
+
+      if (document.getComputedStyle(ul).display === 'none') {
+        ul.style.display = 'block';
+
+        return;
+      }
+
+      ul.style.display = 'none';
+    });
+  }
+});
