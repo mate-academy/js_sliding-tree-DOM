b5de95dce4b654ac58a075034003bd3f8aeb8e1b
Halynka Lozinska <galinkalozinska96@gmail.com>
2020-08-06 16:43:50 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write code here
+
+const li = document.querySelectorAll('li');
+const span = document.createElement('span');
+
+const handler = (event) => {
+  const item = event.target.parentElement;
+
+  if (item.children.length === 1) {
+    return;
+  }
+
+  if (item.children[1].hidden === false) {
+    item.children[1].hidden = true;
+  } else {
+    item.children[1].hidden = false;
+  }
+};
+
+for (const element of [...li]) {
+  element.prepend(span.cloneNode(true));
+
+  const wantedSpan = element.querySelector('span');
+
+  wantedSpan.append(element.childNodes[1]);
+
+  wantedSpan.addEventListener('click', handler);
+}
