46815abd0ed3d4ec50c270f70cc02b362fc68f77
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-19 15:12:02 +0300

    +

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('li').forEach((li) => {
+  li.style.cursor = 'pointer';
+  li.parentNode.style.transition = '1s';
+  li.parentNode.style.overflow = 'hidden';
+
+  li.onclick = (event) => {
+    event.stopPropagation();
+
+    const ul = li.querySelector('ul');
+
+    if (!ul) {
+      return;
+    }
+
+    if (ul.style.opacity !== '0') {
+      ul.style.opacity = '0';
+      ul.style.transform = 'scaleY(0)';
+      ul.style.maxHeight = '0';
+    } else {
+      ul.style.opacity = '1';
+      ul.style.transform = 'none';
+      ul.style.maxHeight = '';
+    }
+  };
+});
