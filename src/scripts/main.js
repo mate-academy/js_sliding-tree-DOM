14681aa787b252d27b91aa15f2e46a9cdd58526f
Vlad Savustianenko <vladsavustianenko@gmail.com>
2020-08-20 18:25:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', event => {
+  event.target.firstElementChild.classList.toggle('hidden');
+});
+
+tree.addEventListener('mouseover', event => {
+  const subList = event.target.firstElementChild;
+
+  if (event.target.tagName === 'LI' && subList) {
+    event.target.style.fontWeight = 'bold';
+    subList.style.fontWeight = 'normal';
+  };
+});
+
+tree.addEventListener('mouseout', event => {
+  const subList = event.target.firstElementChild;
+
+  if (event.target.tagName === 'LI' && subList) {
+    event.target.style.fontWeight = 'normal';
+  };
+});
