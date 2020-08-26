1c33423b590666a489e197847ca8126852199fb6
nznmvs <nznmvs@gmail.com>
2020-08-26 23:50:14 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listElements = [...document.querySelectorAll('li')];
+
+listElements.map(li => {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.addEventListener('click', (event) => {
+  const ul = event.target.nextElementSibling;
+
+  if (ul.tagName === 'UL') {
+    ul.hidden = !ul.hidden;
+  }
+});
