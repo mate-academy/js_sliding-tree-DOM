c06bcfd5a2bd511372642b4cad29b4d55c5a2536
Roman <romikkus@gmail.com>
2020-08-26 13:29:36 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const liList = document.querySelectorAll('li');
+
+for (let i = 0; i < [...liList].length; i++) {
+  if (liList[i].children.length > 0) {
+    const span = document.createElement('span');
+
+    liList[i].prepend(span);
+
+    span.classList = 'toggler';
+
+    span.innerText = `${span.nextSibling.textContent.trim()}`;
+
+    span.nextSibling.remove();
+  }
+}
+
+document.addEventListener('click', e => {
+  const item = e.target;
+
+  if (!item.classList.contains('toggler')) {
+    return;
+  }
+
+  item.nextSibling.style.display = item.nextSibling.style.display === 'none'
+    ? 'block'
+    : 'none';
+});
