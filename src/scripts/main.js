5e5fbe815671eba772d104c4bdac2073a8e38b63
nanccyy <lidiapriymak@gmail.com>
2020-08-26 13:01:42 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liAll = document.querySelectorAll('li');
+
+const headers = [...liAll].filter(li => li.children.length > 0);
+
+[...headers].map(li => {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.prepend(span.nextSibling.textContent);
+  span.nextSibling.textContent = '';
+});
+
+tree.addEventListener('click', (event) => {
+  if (event.target.tagName === 'SPAN') {
+    if (event.target.nextElementSibling.style.display !== 'none') {
+      event.target.nextElementSibling.style.display = 'none';
+    } else {
+      event.target.nextElementSibling.style.display = 'block';
+    }
+  }
+});
