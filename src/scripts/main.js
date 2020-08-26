9d583996060f55492ffbc1b53631991c405aeacc
DmJSCourse <dimonua@gmail.com>
2020-08-26 20:16:27 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const headers = [...document.querySelectorAll('li')]
+  .filter(element => element.children.length > 0);
+
+headers.forEach(header => {
+  const wrapper = document.createElement('span');
+
+  header.prepend(wrapper);
+  wrapper.append(wrapper.nextSibling);
+
+  wrapper.addEventListener('click', event => {
+    event.stopPropagation();
+
+    const child = header.querySelector('ul');
+
+    if (child.style.display !== 'none') {
+      child.style.display = 'none';
+    } else {
+      child.style.display = 'block';
+    }
+  });
+});
