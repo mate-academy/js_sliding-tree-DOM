abe733d0c1ee204bb25868da283b86c9be2855a2
Vitalii Arseniuk <arsenyuk.v.v@gmail.com>
2020-08-26 15:50:54 +0300

    refactored

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,13 @@
 'use strict';
 
-const liArray = [...document.querySelectorAll('li')];
+const listItems = [...document.querySelectorAll('li')];
 
-for (let i = liArray.length - 1; i >= 0; i--) {
-  if (liArray[i].querySelector('ul')) {
-    const heading = liArray[i].firstChild.textContent.trim();
+for (const item of listItems) {
+  if (item.querySelector('ul')) {
+    const span = document.createElement('span');
 
-    liArray[i].firstChild.remove();
-
-    liArray[i].insertAdjacentHTML('afterbegin', `
-      <span>${heading}</span>
-    `);
+    item.prepend(span);
+    span.append(span.nextSibling);
   }
 }
 
