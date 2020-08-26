a0af55a5f2dedb7da4c3fe8d7aef6789d6561ca9
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-26 13:17:58 +0300

    -_-

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,17 @@
 document.body.style.justifyContent = 'flex-start';
 document.body.style.paddingTop = '200px';
 
-
 document.querySelectorAll('li').forEach((li) => {
-  li.style.cursor = 'pointer';
+  const span = document.createElement('span');
+
+  span.prepend(li.firstChild);
+  li.prepend(span);
+
   li.parentNode.style.transition = '1s';
   li.parentNode.style.overflow = 'hidden';
+  span.style.userSelect = 'none';
 
-  li.onclick = (event) => {
+  span.onclick = (event) => {
     event.stopPropagation();
 
     const ul = li.querySelector('ul');
