86f0136ddfba2fe0c5759bbbcb55b913a47f77e6
mpone <m.parphenjuk@gmail.com>
2020-08-26 16:48:19 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listElements = tree.querySelectorAll('li');
+
+for (const li of listElements) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.textContent = span.nextSibling.textContent.trim();
+  span.nextSibling.remove();
+}
+
+tree.addEventListener('click', event => {
+  const sibling = event.target.nextElementSibling;
+
+  if (sibling) {
+    if (sibling.tagName === 'UL' && sibling.hidden === false) {
+      sibling.hidden = true;
+    } else
+    if (sibling.tagName === 'UL' && sibling.hidden === true) {
+      sibling.hidden = false;
+    }
+  }
+});
