1f56f1f0dcc721111cac966fab7b0e87c316ddb7
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-08-26 14:55:44 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
+/* eslint-disable no-undef */
+// getComputedStyle is not defined they said
 'use strict';
 
-// write code here
+const points = document.querySelectorAll('li');
+
+points.forEach(li => {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.prepend(span.nextSibling);
+
+  span.addEventListener('click', (event) => {
+    const ul = span.parentElement.children[1];
+
+    if (!ul) {
+      return;
+      // I need it because of next sybbling or current variant will throw
+      // error in console if I touch Cherry or other childrenless element
+    }
+
+    if (getComputedStyle(ul).display !== 'none') {
+      ul.style.display = 'none';
+    } else {
+      ul.style.display = 'block';
+    }
+  });
+});
