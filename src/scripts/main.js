78533192a832f3532da14cb82c2b7af8142fddc7
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-26 10:39:03 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,8 @@
 'use strict';
 
-const list = document.querySelector('.tree');
-
-list.addEventListener('click', item => {
-  item.target.firstElementChild.classList.toggle('hidden');
+document.addEventListener('click', event => {
+  if (event.target.children[0]
+    && event.target.children[0].className !== 'tree') {
+    event.target.children[0].classList.toggle('hidden');
+  }
 });
-
-function hoverAdd(item) {
-  item.target.style.fontWeight = 'bold';
-  item.target.child.style.fontWeight = 'normal';
-}
-
-function hoverRemove(item) {
-  item.target.style.fontWeight = 'normal';
-}
-
-list.addEventListener('mouseover', hoverAdd);
-list.addEventListener('mouseout', hoverRemove);
