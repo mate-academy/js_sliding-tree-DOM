d4b93491db78f6417da5a2d7e444ba051e869499
Anastasiia Lukina <anlukina24@gmail.com>
2020-08-26 22:24:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+
+[...items]
+  .filter(item => item.children.length !== 0)
+  .forEach(item => {
+    const textNode = item.firstChild;
+
+    textNode.parentElement.insertAdjacentHTML('afterbegin', `
+      <span>${textNode.textContent.trim()}</span>
+    `);
+    textNode.remove();
+  });
+
+const spans = document.querySelectorAll('span');
+let isVisible = true;
+
+[...spans].forEach(span => {
+  span.addEventListener('click', event => {
+    if (isVisible) {
+      event.target.nextElementSibling.style.display = 'none';
+      isVisible = false;
+    } else {
+      event.target.nextElementSibling.style.display = 'block';
+      isVisible = true;
+    }
+  });
+});
