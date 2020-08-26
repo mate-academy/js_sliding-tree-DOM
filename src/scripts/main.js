78fe297f352a1b62f421d9673d558f822fab1e2e
nanccyy <lidiapriymak@gmail.com>
2020-08-26 15:03:57 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,22 +3,17 @@
 const tree = document.querySelector('.tree');
 const liAll = document.querySelectorAll('li');
 
-const headers = [...liAll].filter(li => li.children.length > 0);
-
-[...headers].map(li => {
+[...liAll].map(li => {
   const span = document.createElement('span');
 
   li.prepend(span);
-  span.prepend(span.nextSibling.textContent.trim());
-  span.nextSibling.textContent = '';
+  span.append(span.nextSibling);
 });
 
 tree.addEventListener('click', (event) => {
-  if (event.target.nextElementSibling.tagName === 'UL') {
-    if (event.target.nextElementSibling.style.display !== 'none') {
-      event.target.nextElementSibling.style.display = 'none';
-    } else {
-      event.target.nextElementSibling.style.display = 'block';
-    }
+  const ul = event.target.nextElementSibling;
+
+  if (ul.tagName === 'UL') {
+    ul.hidden = !ul.hidden;
   }
 });
