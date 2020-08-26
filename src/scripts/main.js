c52e7bc102552e5ea0d7353de88d194fc28312b1
Vitalii Arseniuk <arsenyuk.v.v@gmail.com>
2020-08-26 14:51:58 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const liArray = [...document.querySelectorAll('li')];
+
+for (let i = liArray.length - 1; i >= 0; i--) {
+  if (liArray[i].querySelector('ul')) {
+    const heading = liArray[i].firstChild.textContent.trim();
+
+    liArray[i].firstChild.remove();
+
+    liArray[i].insertAdjacentHTML('afterbegin', `
+      <span>${heading}</span>
+    `);
+  }
+}
+
+document.addEventListener('click', (event) => {
+  const item = event.target;
+
+  if (item.nextElementSibling.tagName === 'UL') {
+    item.nextElementSibling.hidden = !item.nextElementSibling.hidden;
+  }
+});
