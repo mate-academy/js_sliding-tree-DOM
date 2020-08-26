baf2110b012d48eb5f4497804f40d15d4fbb323b
Maxym Chernyavskiy <masyatya@gmail.com>
2020-08-26 16:37:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const lists = document.querySelectorAll('ul');
+
+for (const list of lists) {
+  for (const li of list.children) {
+    if (li.children.length) {
+      const span = document.createElement('span');
+
+      span.prepend(li.firstChild);
+      span.className = 'toggler';
+      li.prepend(span);
+    }
+  }
+}
+
+let flagToogler = true;
+
+document.querySelectorAll('li').forEach(li => {
+  li.addEventListener('click', (event) => {
+    event.stopPropagation();
+
+    if (event.target.className !== 'toggler') {
+      return;
+    }
+
+    if (flagToogler) {
+      li.children[1].style.cssText = `
+        opacity: 0;
+      `;
+      flagToogler = false;
+    } else {
+      li.children[1].style.cssText = `
+        opacity: 1;
+      `;
+      flagToogler = true;
+    }
+  });
+});
