35db6a6f23bff3b9b69bd7e9a6e5fbe42e616c52
Mikle25 <mikle2593@gmail.com>
2020-08-26 16:13:14 +0300

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+(function() {
+  const list = document.querySelectorAll('li');
+
+  for (const elem of [...list]) {
+    const textUl = elem.firstChild.textContent;
+
+    elem.firstChild.remove();
+
+    elem.insertAdjacentHTML('afterbegin', `
+    <span>${textUl}</span>
+    `);
+  }
+
+  document.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.nextElementSibling.tagName === 'UL') {
+      if (target.nextElementSibling.style.display !== 'none') {
+        target.nextElementSibling.style.display = 'none';
+      } else {
+        target.nextElementSibling.style.display = 'block';
+      }
+    }
+  });
+}());
