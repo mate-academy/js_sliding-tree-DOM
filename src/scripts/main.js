c184375a7321735c40b57493d56e095304572812
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-08-27 14:39:21 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+
+tree.style.position = 'absolute';
+tree.style.top = `${tree.getBoundingClientRect().top}px`;
+tree.style.left = `${tree.getBoundingClientRect().left}px`;
+
+[...listItems].map(item => {
+  const itemSpan = document.createElement('span');
+
+  itemSpan.className = 'toggle';
+  item.prepend(itemSpan);
+  itemSpan.append(itemSpan.nextSibling);
+});
+
+tree.addEventListener('click', (event) => {
+  const ul = event.target.nextElementSibling;
+
+  if (ul.tagName === 'UL') {
+    ul.hidden = !ul.hidden;
+  }
+});
