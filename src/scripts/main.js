67ba5617f0da7705470f69fd36cf5c3f8c2eb1c1
martakupka <marta-k2008@ukr.net>
2020-08-27 12:35:45 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const textlist = document.querySelector('.tree').innerText;
+const itemList = document.querySelectorAll('li');
+
+const textListArray = textlist.split('\n');
+
+for (const item of itemList) {
+  item.insertAdjacentHTML('afterbegin', `
+  <span>${textListArray[0]}</span>
+  `);
+
+  textListArray.shift();
+}
+
+const spanList = document.querySelectorAll('span');
+
+for (const item of spanList) {
+  const siblingElement = item.nextSibling;
+
+  siblingElement.nextSibling.remove();
+
+  item.addEventListener('click', (event) => {
+    const targetElement = event.target;
+
+    if (targetElement.nextElementSibling.hidden === false) {
+      targetElement.nextElementSibling.setAttribute('hidden', true);
+    } else {
+      targetElement.nextElementSibling.removeAttribute('hidden');
+    }
+  });
+}
