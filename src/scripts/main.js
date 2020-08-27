97a681fe54984ab4fa3c8f05c070df18e35c5b5b
Zarva <zarva6596@gmail.com>
2020-08-27 11:58:37 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+for (const a of document.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  a.prepend(span);
+  span.innerText = span.nextSibling.textContent;
+  span.nextSibling.remove();
+}
+
+const spans = document.querySelectorAll('span');
+
+for (const a of spans) {
+  a.addEventListener('click', (event) => {
+    const ul = event.target;
+
+    if (ul.nextElementSibling.tagName === 'UL') {
+      ul.nextElementSibling.hidden = !ul.nextElementSibling.hidden;
+    }
+  });
+}
