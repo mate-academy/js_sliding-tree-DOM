cc7f9b4d7715d85c478cd2004b46107d5833f298
Gabbasova Oksana <oksana.gabba@gmail.com>
2020-08-27 22:10:45 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = tree.querySelectorAll('li');
+
+listItems.forEach(li => {
+  if (li.children.length > 0) {
+    const span = document.createElement('span');
+
+    li.prepend(span);
+    span.textContent = span.nextSibling.textContent;
+    span.nextSibling.remove();
+  }
+});
+
+tree.addEventListener('click', (event) => {
+  if (event.target.tagName === 'SPAN') {
+    const li = event.target;
+    const ul = li.nextSibling;
+
+    ul.hidden = !ul.hidden;
+  }
+});
