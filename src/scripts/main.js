267f8fb4a5bf50286d1aa4eb5921fe334b12ae2d
unknown <pavlo.com.5@gmail.com>
2020-08-27 15:36:41 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liList = [...document.querySelectorAll('li')];
+
+tree.style.position = 'absolute';
+tree.style.top = `${tree.getBoundingClientRect().top}px`;
+tree.style.left = `${tree.getBoundingClientRect().left}px`;
+
+for (const li of liList) {
+  if (li.querySelector('ul')) {
+    const span = document.createElement('span');
+
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+tree.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if (!target.nextSibling) {
+    return;
+  }
+
+  target.nextSibling.hidden = !target.nextSibling.hidden;
+});
