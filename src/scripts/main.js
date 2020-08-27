83fef2da4b81c961404ffd7325c39c0c5abd78e1
karinaKudatska <karina.kudatska@gmail.com>
2020-08-27 15:09:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const elements = document.querySelectorAll('li');
+
+[...elements].forEach(el => {
+  const span = document.createElement('span');
+
+  el.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.addEventListener('click', (event) => {
+  const sibling = event.target.nextElementSibling;
+
+  if (sibling.tagName === 'UL') {
+    sibling.hidden = !sibling.hidden;
+  }
+});
