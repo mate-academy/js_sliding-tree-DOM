bd7e88ec6d7459cc358f4fe9f6287a5d59affce0
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-28 00:31:44 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+[...document.querySelectorAll('li')].forEach(li => {
+  const text = li.firstChild.textContent.trim();
+
+  li.firstChild.textContent = '';
+  li.insertAdjacentHTML('afterbegin', `<span>${text}</span>`);
+});
+
+document.querySelector('.tree').addEventListener('click', (event) => {
+  if (event.target.nextSibling.nextSibling.nodeName === 'UL') {
+    if (event.target.nextSibling.nextSibling.style.display !== 'none') {
+      event.target.nextSibling.nextSibling.style.display = 'none';
+    } else {
+      event.target.nextSibling.nextSibling.style.display = 'block';
+    }
+  }
+});
