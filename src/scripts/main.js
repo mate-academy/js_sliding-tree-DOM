53297789493d1073f4790ab7388218af7ff08d1e
Alisa <cheshyr.alisa@gmail.com>
2020-08-27 14:06:15 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const elements = tree.querySelectorAll('li');
+
+[...elements].forEach(elem => {
+  const span = document.createElement('span');
+
+  span.prepend(elem.firstChild);
+  elem.prepend(span);
+});
+
+function callback(event) {
+  const target = event.target;
+
+  if (target.nextElementSibling.tagName === 'UL') {
+    target.nextElementSibling.hidden !== true
+      ? target.nextElementSibling.hidden = true
+      : target.nextElementSibling.hidden = false;
+  };
+}
+
+document.addEventListener('click', callback);
