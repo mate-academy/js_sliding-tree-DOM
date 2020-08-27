bc8909b52512a1af9e77e3546fa021a933fecf0f
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-28 00:42:47 +0300

    changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,26 @@
 'use strict';
 
-[...document.querySelectorAll('li')].forEach(li => {
-  const text = li.firstChild.textContent.trim();
+function hideList() {
+  function spanWrapper(elementSelector) {
+    [...document.querySelectorAll(elementSelector)].forEach(li => {
+      const text = li.firstChild.textContent.trim();
 
-  li.firstChild.textContent = '';
-  li.insertAdjacentHTML('afterbegin', `<span>${text}</span>`);
-});
-
-document.querySelector('.tree').addEventListener('click', (event) => {
-  if (event.target.nextSibling.nextSibling.nodeName === 'UL') {
-    if (event.target.nextSibling.nextSibling.style.display !== 'none') {
-      event.target.nextSibling.nextSibling.style.display = 'none';
-    } else {
-      event.target.nextSibling.nextSibling.style.display = 'block';
-    }
+      li.firstChild.textContent = '';
+      li.insertAdjacentHTML('afterbegin', `<span>${text}</span>`);
+    });
   }
-});
+
+  spanWrapper('li');
+
+  document.querySelector('.tree').addEventListener('click', (event) => {
+    if (event.target.nextSibling.nextSibling.nodeName === 'UL') {
+      if (event.target.nextSibling.nextSibling.style.display !== 'none') {
+        event.target.nextSibling.nextSibling.style.display = 'none';
+      } else {
+        event.target.nextSibling.nextSibling.style.display = 'block';
+      }
+    }
+  });
+}
+
+hideList();
