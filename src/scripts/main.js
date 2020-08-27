18ae09effc280a72b5a98b0b4ba03f931ee79380
karinaKudatska <karina.kudatska@gmail.com>
2020-08-27 18:59:35 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,10 @@
 const tree = document.querySelector('.tree');
 const elements = document.querySelectorAll('li');
 
+tree.style.position = 'absolute';
+tree.style.top = `${tree.getBoundingClientRect().top}px`;
+tree.style.left = `${tree.getBoundingClientRect().left}px`;
+
 [...elements].forEach(el => {
   const span = document.createElement('span');
 
