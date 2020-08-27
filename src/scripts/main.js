790520c8a10585014f6f5c5d8a7a6a1d005e60e1
Anton <a.v.sorokin95@gmail.com>
2020-08-27 16:22:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const fullList = document.querySelector('.tree');
+
+for (const item of document.querySelectorAll('li')) {
+  if (item.children.length > 0) {
+    const name = item.firstChild.textContent;
+
+    item.firstChild.textContent = '';
+
+    item.insertAdjacentHTML('afterbegin', `
+      <span class='list'>${name}</span>
+  `);
+  }
+};
+
+const hideList = (event) => {
+  const item = event.target.nextElementSibling;
+
+  if (item.tagName !== 'UL') {
+    return;
+  }
+
+  if (item.style.visibility === 'hidden') {
+    item.style.visibility = 'visible';
+    item.style.position = 'static';
+    item.style.opacity = '1';
+  } else {
+    item.style.visibility = 'hidden';
+    item.style.position = 'absolute';
+    item.style.opacity = '0';
+  }
+};
+
+fullList.addEventListener('click', hideList);
