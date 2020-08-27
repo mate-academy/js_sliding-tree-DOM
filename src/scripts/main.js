cf0148d01829ff9e93026ed0530d776556f42b76
artsashko <artyomsashko@gmail.com>
2020-08-27 16:34:53 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const liElements = document.querySelectorAll('li');
+const span = document.createElement('span');
+
+for (const li of [...liElements]) {
+  li.prepend(span.cloneNode());
+  li.childNodes[0].append(li.childNodes[1]);
+}
+
+list.addEventListener('click', (event) => {
+  const li = event.target.parentElement;
+  const innerList = li.querySelector('ul');
+  const tagName = event.target.tagName;
+
+  if (tagName === 'UL' || tagName === 'LI') {
+    return;
+  }
+
+  if (innerList !== null) {
+    innerList.hidden = !innerList.hidden;
+  }
+});
