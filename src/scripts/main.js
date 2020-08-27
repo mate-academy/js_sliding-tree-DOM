6f6e54480b418ddb135937cc3647e55e8b6eef4c
nznmvs <nznmvs@gmail.com>
2020-08-27 16:50:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,13 @@
 
 const tree = document.querySelector('.tree');
 const listElements = [...document.querySelectorAll('li')];
+const coords = tree.getBoundingClientRect();
+
+tree.style.cssText = `
+  position: absolute;
+  top: ${coords.top}px;
+  left: ${coords.left}px;
+`;
 
 listElements.map(li => {
   const span = document.createElement('span');
