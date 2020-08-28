b556ce6bf85df53ac265ef485b492b07801c1f24
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-08-28 12:36:37 +0300

    new

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,6 +12,7 @@ const tree = document.querySelector('.tree');
 
 tree.style.position = 'absolute';
 tree.style.top = '50px';
+tree.style.left = '40%';
 
 tree.addEventListener('click', (event) => {
   const point = event.target;
