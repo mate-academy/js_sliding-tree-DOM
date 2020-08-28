cf0b036c044eaecba2f231170994962fc7c2acdf
ChrisZinch <chrispakulyak@gmail.com>
2020-08-28 14:01:16 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const li = tree.querySelectorAll('li');
+
+tree.style.position = 'absolute';
+tree.style.top = `${tree.getBoundingClientRect().top}px`;
+tree.style.left = `${tree.getBoundingClientRect().left}px`;
+
+for (const item of li) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.textContent = span.nextSibling.textContent.trim();
+  span.nextSibling.remove();
+}
+
+tree.addEventListener('click', (event) => {
+  const list = event.target.nextElementSibling;
+
+  if (list.tagName === 'UL') {
+    list.hidden = !list.hidden;
+  }
+});
