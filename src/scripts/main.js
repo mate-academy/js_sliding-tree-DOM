8454e994c368c0033acae7138c87213eca362a2d
Vasyl_Zarichnyi <vasylzarichnyi@gmail.com>
2020-08-29 14:47:24 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const tree = document.querySelectorAll('li');
+
+for (const item of tree) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+
+  span.textContent = span.nextSibling.textContent;
+  span.nextSibling.remove();
+}
+
+const list = document.querySelector('.tree');
+
+list.style.position = 'absolute';
+list.style.top = 20 + '%';
+list.style.left = 40 + '%';
+
+list.addEventListener('click', (event) => {
+  const element = event.target;
+
+  if (element.nextElementSibling.tagName === 'UL') {
+    element.nextElementSibling.hidden = !element.nextElementSibling.hidden;
+  }
+});
