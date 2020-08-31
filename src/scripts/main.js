c1864b0304e5461e6730928790c01c6e7e334380
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-31 13:33:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
+[...document.querySelectorAll('li')].map((li) => {
+  const span = document.createElement('span');
+
+  if (li.parentNode.querySelector('ul')) {
+    span.prepend(li.firstChild);
+    li.prepend(span);
+  }
+});
+
 document.addEventListener('click', event => {
-  if (event.target.children[0]
-    && event.target.children[0].className !== 'tree') {
-    event.target.children[0].classList.toggle('hidden');
+  if (event.target.tagName === 'SPAN') {
+    event.target.parentNode.childNodes[1].hidden
+    = !event.target.parentNode.childNodes[1].hidden;
   }
 });
