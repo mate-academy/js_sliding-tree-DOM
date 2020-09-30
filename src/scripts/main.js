04eff2276790006519f147a53d5f7fac6006ae84
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-09-30 20:52:36 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write code here
+const theTree = document.querySelector('.tree');
+
+for (const liElement of document.querySelectorAll('li')) {
+  if (liElement.children.length > 0) {
+    const content = liElement.firstChild.textContent;
+
+    liElement.firstChild.textContent = '';
+
+    liElement.insertAdjacentHTML('afterbegin', `
+      <span class='list'>${content}</span>`);
+  }
+};
+
+const hideEvent = (event) => {
+  const item = event.target.nextElementSibling;
+
+  if (item.tagName !== 'UL') {
+    return;
+  }
+
+  item.hidden = !item.hidden;
+};
+
+theTree.addEventListener('click', hideEvent);
