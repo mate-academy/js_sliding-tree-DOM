67f7f906e5a9cac0c5e2524161a9267a69ca0e0b
Petro <petdavy@1991gmail.com>
2020-10-12 02:46:23 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const treeLists = tree.querySelectorAll('ul');
+
+for (const list of treeLists) {
+  const listName = list.previousSibling.data.trim();
+
+  list.previousSibling.remove();
+
+  list.insertAdjacentHTML('beforebegin', `<span>${listName}</span>`);
+  list.opened = true;
+}
+
+tree.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.tagName === 'SPAN') {
+    const list = target.nextElementSibling;
+
+    if (list.opened) {
+      list.style.display = 'none';
+      list.opened = false;
+    } else {
+      list.style.display = 'block';
+      list.opened = true;
+    }
+  }
+});
