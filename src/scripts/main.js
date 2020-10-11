d3ebedffc3741478d57665b94dc4d550921e1757
Yazheviks <vicsergeyeva@gmail.com>
2020-10-11 20:40:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach(item => {
+  if (item.children.length > 0 && item.firstElementChild.tagName === 'UL') {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+    span.append(item.childNodes[1]);
+  }
+});
+
+tree.addEventListener('click', event => {
+  if (event.target.tagName === 'SPAN') {
+    if (event.target.parentElement.lastElementChild.style.display === 'none') {
+      event.target.parentElement.lastElementChild.style.display = 'block';
+    } else {
+      event.target.parentElement.lastElementChild.style.display = 'none';
+    }
+  }
+});
