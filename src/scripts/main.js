79a3e3793e91d5aee67b71d53f799ef3008ed4ec
Alina <alina90565@gmail.com>
2020-10-15 21:01:45 +0300

    added span for titles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,25 @@
 'use strict';
 
 const tree = document.querySelector('.tree');
+const items = document.querySelectorAll('li');
 
-tree.addEventListener('click', ({target}) => {
+const titles = [...items].filter(item => item.firstElementChild);
+
+titles.forEach(({ firstChild }) => {
+  const span = document.createElement('span');
+
+  firstChild.after(span);
+  span.prepend(firstChild);
+})
+
+tree.addEventListener('click', ({ target }) => {
   if (target.tagName !== 'LI' || !target.firstElementChild) {
     return;
   }
 
   const list = target.firstElementChild;
 
-  list.style.display = getComputedStyle(list).display === 'block'
+  list.style.display = window.getComputedStyle(list).display === 'block'
     ? 'none'
-    : 'block'
-})
+    : 'block';
+});
