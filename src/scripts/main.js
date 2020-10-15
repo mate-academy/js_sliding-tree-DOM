163ccb25bc6b20d5216c9341c1fd024c6aa7de8b
Alina <alina90565@gmail.com>
2020-10-15 20:40:02 +0300

    done sliding tree task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', ({target}) => {
+  if (target.tagName !== 'LI' || !target.firstElementChild) {
+    return;
+  }
+
+  const list = target.firstElementChild;
+
+  list.style.display = getComputedStyle(list).display === 'block'
+    ? 'none'
+    : 'block'
+})
