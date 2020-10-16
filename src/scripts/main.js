5567ca82e0d98c105f626edbf7b9e2172c1ffe30
OleksandrMykoliuk <69759907+OleksandrMykoliuk@users.noreply.github.com>
2020-10-16 12:34:47 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const listElement = document.querySelectorAll('li');
+
+for (const i of listElement) {
+  const addSpan = document.createElement('span');
+
+  addSpan.className = 'active';
+
+  if (i.firstElementChild) {
+    i.prepend(addSpan);
+    addSpan.append(i.childNodes[1]);
+  }
+}
+
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('active')) {
+    const hidden = event.target.parentElement.lastElementChild;
+
+    hidden.style.display = (hidden.style.display === 'none') ? '' : 'none';
+  }
+});
