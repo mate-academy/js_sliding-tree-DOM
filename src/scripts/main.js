d7201d312bd60c3754aaf133c27641d22033d835
goncharukop <goncharukop@gmail.com>
2020-10-17 22:57:10 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', event => {
+  const listTarget = event.target.closest('li');
+  let display = listTarget.firstElementChild.style.display;
+
+  if (!listTarget || !listTarget.firstElementChild.tagName === 'UL') {
+    return;
+  }
+
+  if (listTarget.firstElementChild.tagName === 'UL') {
+    display = display === 'none' ? 'block' : 'none';
+    listTarget.firstElementChild.style.display = display;
+  }
+});
