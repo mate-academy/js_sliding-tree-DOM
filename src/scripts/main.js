20b137b20c836d1ba8afc12a65ffbea425a8343a
andlap24 <andrew.laptev24@gmail.com>
2020-10-17 15:15:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const list = document.querySelectorAll('li');
+
+for (const item of list) {
+  if (item.children.length > 0 && item.firstElementChild.tagName === 'UL') {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+    span.append(item.childNodes[1]);
+  }
+}
+
+tree.addEventListener('click', event => {
+  if (event.target.tagName === 'SPAN') {
+    const styles = event.target.parentElement.lastElementChild.style;
+
+    if (styles.display === 'none') {
+      styles.display = 'block';
+    } else {
+      styles.display = 'none';
+    }
+  }
+});
