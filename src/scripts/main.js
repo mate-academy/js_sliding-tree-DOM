863a7954828a2c2fae08d109038bed6a83e7f8df
Tania Omelko <tanyaomelko39@gmail.com>
2020-10-18 22:45:31 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.style.position = 'absolute';
+tree.style.top = tree.getBoundingClientRect().top + 'px';
+tree.style.left = tree.getBoundingClientRect().left + 'px';
+
+const liList = document.querySelectorAll('li');
+
+for (const item of liList) {
+  if (item.children.length) {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+document.addEventListener('click', event => {
+  const header = event.target;
+
+  if (header.nextElementSibling.tagName === 'UL') {
+    header.nextElementSibling.hidden = !header.nextElementSibling.hidden;
+  }
+});
