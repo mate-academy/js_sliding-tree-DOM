0c37b85ccaae22a15ecf6f71d7df6e49cdfca373
Anton <office.sklyarov@gmail.com>
2020-10-19 16:37:38 +0300

    added sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+for (const listItem of list) {
+  if (listItem.children.length > 0) {
+    const span = document.createElement('span');
+
+    listItem.prepend(span);
+    span.append(listItem.childNodes[1]);
+  }
+}
+
+tree.addEventListener('click', event => {
+  if (event.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const element = event.target;
+  const elementDisplay = element.nextSibling.style.display;
+
+  if (elementDisplay !== 'none') {
+    element.nextSibling.style.display = 'none';
+  }
+
+  if (elementDisplay === 'none') {
+    element.nextSibling.style.display = 'block';
+  }
+});
