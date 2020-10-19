95f2519e74b23f24a7560958720175553b6b11e5
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-10-19 12:48:03 +0300

    sliding

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const treeList = list.querySelectorAll('ul');
+
+for (const tree of treeList) {
+  const listName = tree.previousSibling.data.trim();
+
+  tree.previousSibling.remove();
+
+  tree.insertAdjacentHTML('beforebegin', `<span>${listName}</span>`);
+}
+
+list.addEventListener('click', event => {
+  const cheked = event.target;
+
+  if (cheked.tagName === 'SPAN') {
+    const name = cheked.nextElementSibling;
+
+    if (name.style.display === 'block') {
+      name.style.display = 'none';
+    } else {
+      name.style.display = 'block';
+    }
+  }
+});
