529651349068256ed127ad1ac92aca4a092a0ad6
Liza <elizabethhoncharenko@gmail.com>
2020-10-19 15:34:57 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (event) => {
+  for (const element of [...event.target.children]) {
+    if (element.style.display === 'block') {
+      element.style.display = 'none';
+    } else {
+      element.style.display = 'block';
+    }
+  }
+});
