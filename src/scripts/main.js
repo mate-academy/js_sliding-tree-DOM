f907afad283ce0b1591b0da0dc50deca86a11701
Liza <elizabethhoncharenko@gmail.com>
2020-10-19 16:46:58 +0300

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,19 @@
 'use strict';
 
 const tree = document.querySelector('.tree');
+const arrayOfLi = document.querySelectorAll('li');
+
+for (const li of arrayOfLi) {
+  const span = document.createElement('SPAN');
+
+  li.prepend(span);
+  span.append(li.childNodes[1]);
+}
 
 tree.addEventListener('click', (event) => {
-  for (const element of [...event.target.children]) {
+  if (event.target.tagName === 'SPAN') {
+    const element = event.target.nextSibling;
+
     if (element.style.display === 'block') {
       element.style.display = 'none';
     } else {
