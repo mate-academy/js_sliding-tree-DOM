a5855fa842cfc7020077c527f0d22214d2775828
38063 <xeilen1112@gmail.com>
2020-10-19 19:44:11 +0300

    -solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItem = document.querySelectorAll('li');
+
+listItem.forEach(li => {
+  if (li.children.length > 0 && li.children[0].tagName === 'UL') {
+    const span = document.createElement('span');
+
+    li.prepend(span);
+    span.append(li.childNodes[1]);
+  }
+});
+
+tree.addEventListener('click', event => {
+  const { target } = event;
+
+  if (target.tagName !== 'SPAN' || !target.nextElementSibling) {
+    return;
+  }
+  target.nextElementSibling.hidden = !target.nextElementSibling.hidden;
+});
