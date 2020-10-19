ed4ba77f3c56b355b0323b3ce2fdf40a9111603a
martakupka <marta-k2008@ukr.net>
2020-10-20 01:23:47 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const treeList = document.querySelector('.tree');
+const treeElements = document.querySelectorAll('li');
+const elementsText = treeList.innerText.split('\n');
+
+treeList.style.position = 'absolute';
+treeList.style.top = treeList.getBoundingClientRect().top + 'px';
+treeList.style.left = treeList.getBoundingClientRect().left + 'px';
+
+treeElements.forEach((element, i) => {
+  element.firstChild.remove();
+
+  element.insertAdjacentHTML('afterbegin', `
+  <span>${elementsText[i]}</span>
+  `);
+});
+
+const spanElements = document.querySelectorAll('span');
+
+spanElements.forEach(element => {
+  element.addEventListener('click', (event) => {
+    const selectedElementSibling = event.target.nextElementSibling;
+
+    if (selectedElementSibling) {
+      const isHidden = selectedElementSibling.hidden;
+
+      selectedElementSibling.hidden = !isHidden;
+    }
+  });
+});
