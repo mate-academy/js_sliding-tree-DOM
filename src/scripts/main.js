16335212ff39f9e77eb3b4201c42f066059533cd
milla-romankova <milla-romankova@gmail.com>
2020-10-19 16:31:45 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const treeElements = document.querySelectorAll('li');
+
+for (const element of treeElements) {
+  const elementWrapper = document.createElement('span');
+
+  element.prepend(elementWrapper);
+  elementWrapper.append(elementWrapper.nextSibling);
+}
+
+tree.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.tagName !== 'SPAN') {
+    return;
+  }
+
+  target.nextElementSibling.hidden = !target.nextElementSibling.hidden;
+});
