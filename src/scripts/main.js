6fa816518275a30872461ee3070cf79b13f0dfdd
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-19 17:00:01 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', function(event) {
+  const li = event.target;
+
+  if (li.children.length > 0) {
+    const liChildren = li.children;
+
+    for (const child of liChildren) {
+      if (child.style.display !== 'none') {
+        child.style.display = 'none';
+      } else {
+        child.style.display = 'block';
+      }
+    }
+  }
+});
