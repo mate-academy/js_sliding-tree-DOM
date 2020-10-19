d304cf5cd6d4ce6a89af4dd4c663e8ab4da60a4d
KirillLutsenko <mnrovski68@gmail.com>
2020-10-19 21:12:14 +0300

    Sliding Tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const lists = tree.querySelectorAll('ul');
+
+lists.forEach(list => {
+  const listHeader = list.previousSibling.data;
+
+  list.previousSibling.remove();
+  list.insertAdjacentHTML('beforebegin', `<span>${listHeader}</span>`);
+});
+
+tree.addEventListener('click', ({ target }) => {
+  const list = target.nextSibling;
+
+  if (target.tagName !== 'SPAN' || !list) {
+    return;
+  }
+
+  const currentDisplay = window.getComputedStyle(list).display;
+
+  list.style.display = (currentDisplay === 'block')
+    ? 'none'
+    : 'block';
+});
