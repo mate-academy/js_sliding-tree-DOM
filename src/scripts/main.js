0e3a13aea237dade3ea3598f853990a4a32d92f6
Eugene <evolokhin@gmail.com>
2020-10-19 21:21:14 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const treeHeaders = tree.querySelectorAll('ul');
+
+treeHeaders.forEach(item => {
+  const span = document.createElement('span');
+
+  item.closest('li').prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.addEventListener('click', (event) => {
+  if (event.target.tagName === 'SPAN') {
+    const list = event.target.nextSibling;
+
+    (list.style.display !== 'none')
+      ? list.style.display = 'none'
+      : list.style.display = 'block';
+  }
+});
