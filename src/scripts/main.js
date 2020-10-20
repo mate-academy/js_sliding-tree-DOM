22dda2ecdddb1789aec548d4cee4634844a0bc09
TishchenkoSergey <tishchenko.seriy@gmail.com>
2020-10-21 00:05:15 +0300

    Sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = tree.querySelectorAll('li');
+const listWithInsetr = [...listItems]
+  .filter((listItem) => {
+    const childList = listItem.querySelector('ul');
+
+    return !!childList;
+  });
+
+listWithInsetr.forEach((listItem) => {
+  const textNode = listItem.childNodes[0];
+  const textContainer = document.createElement('span');
+
+  listItem.prepend(textContainer);
+  textContainer.append(textNode);
+});
+
+tree.addEventListener('click', (event) => {
+  if (event.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const childList = event.target.nextElementSibling;
+
+  childList.hidden = !childList.hidden;
+});
