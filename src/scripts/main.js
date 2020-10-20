72725e8cb0231d71e4bd8eda4796dd1b9d4dbdfa
Vitalik <ztx25sss@gmail.com>
2020-10-20 15:37:53 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const list = tree.querySelectorAll('ul');
+
+tree.style.cssText = 'position: absolute; top: 200px; left: 600px;';
+
+for (const elem of list) {
+  const listName = elem.previousSibling.data.trim();
+
+  elem.previousSibling.remove();
+
+  elem.insertAdjacentHTML('beforebegin', `<span>${listName}</span>`);
+};
+
+tree.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if (target.tagName === 'SPAN') {
+    const element = target.nextElementSibling;
+
+    element.hidden = !element.hidden;
+  }
+});
