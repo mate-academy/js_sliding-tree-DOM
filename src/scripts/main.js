17d1b80699cf4c4da1fc9a5ba7ad5f98bc050a63
andlap24 <andrew.laptev24@gmail.com>
2020-10-20 11:48:29 +0300

    add functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,30 @@
 const tree = document.querySelector('.tree');
 const list = document.querySelectorAll('li');
 
-for (const item of list) {
-  if (item.firstElementChild && item.firstElementChild.tagName === 'UL') {
-    const span = document.createElement('span');
+function initHeaderList(items) {
+  for (const item of items) {
+    const itemFirstChild = item.firstElementChild;
 
-    item.prepend(span);
-    span.append(item.childNodes[1]);
+    if (itemFirstChild && itemFirstChild.tagName === 'UL') {
+      const span = document.createElement('span');
+
+      item.prepend(span);
+      span.append(item.childNodes[1]);
+    }
   }
 }
 
-tree.addEventListener('click', event => {
-  if (event.target.tagName !== 'SPAN') {
-    return;
-  }
+function clickListener(element) {
+  element.addEventListener('click', event => {
+    if (event.target.tagName !== 'SPAN') {
+      return;
+    }
 
-  const childList = event.target.nextElementSibling;
+    const childList = event.target.nextElementSibling;
 
-  childList.hidden = !childList.hidden;
-});
+    childList.hidden = !childList.hidden;
+  });
+}
+
+initHeaderList(list);
+clickListener(tree);
