22e8fa1dd5170d5b65c787943131fdaaa030144a
Marina <m.gerasymova29@gmail.com>
2020-10-20 14:41:30 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const lists = [...document.querySelectorAll('li')];
+
+lists.forEach(list => {
+  const titleWrap = document.createElement('span');
+
+  titleWrap.textContent = list.firstChild.textContent;
+  list.firstChild.remove();
+  list.prepend(titleWrap);
+
+  titleWrap.addEventListener('click', (event) => {
+    if (event.target === titleWrap) {
+      const nestedList = list.querySelector('ul');
+
+      if (nestedList) {
+        if (nestedList.style.display === 'none') {
+          nestedList.style.display = 'block';
+        } else {
+          nestedList.style.display = 'none';
+        }
+      }
+    }
+  });
+});
