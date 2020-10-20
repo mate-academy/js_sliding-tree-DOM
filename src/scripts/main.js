8631fad1cb4e800dd972b4fc9f6342e8211caff3
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-20 10:49:38 +0300

    use Yura's hints

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,27 @@
 'use strict';
 
 const list = document.querySelector('.tree');
+const allLi = document.querySelectorAll('li');
+
+for (const li of allLi) {
+  const childList = li.querySelector('ul');
+
+  if (childList) {
+    const span = document.createElement('span');
+    const textNode = li.firstChild;
+
+    span.append(textNode);
+    li.prepend(span);
+  }
+}
 
 list.addEventListener('click', function(event) {
-  const li = event.target;
-
-  if (li.children.length > 0) {
-    const liChildren = li.children;
-
-    for (const child of liChildren) {
-      if (child.style.display !== 'none') {
-        child.style.display = 'none';
-      } else {
-        child.style.display = 'block';
-      }
-    }
+  if (event.target.tagName !== 'SPAN') {
+    return;
   }
+
+  const ul = event.target.nextElementSibling;
+
+  ul.hidden = !ul.hidden;
+  event.target.style.fontWeight = ul.hidden ? '700' : '400';
 });
