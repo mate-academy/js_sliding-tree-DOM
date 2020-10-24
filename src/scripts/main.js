331c3657d45b1939c9cc4cab3a9fe3de9a7f877f
dmitry <akulinind@gmail.com>
2020-10-24 13:52:18 +0300

    add implementation of siding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const treeList = document.querySelector('.tree');
+const lists = document.querySelectorAll('li');
+
+lists.forEach(list => {
+  const spanElement = document.createElement('span');
+
+  list.prepend(spanElement);
+  spanElement.append(spanElement.nextSibling);
+});
+
+treeList.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.tagName !== 'SPAN' || !target.nextSibling) {
+    return;
+  }
+
+  target.nextSibling.hidden = !target.nextSibling.hidden;
+});
