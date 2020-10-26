319ea6bbb33c6628a18f19729be7679403ddad7d
AntonChetveryk <chetverykanton92@gmail.com>
2020-10-26 22:54:51 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const li = document.querySelectorAll('li');
+const headlines = [...li].filter((item) => {
+  if (item.firstElementChild) {
+    return item.firstElementChild.nodeName === 'UL';
+  }
+});
+
+headlines.forEach((item) => {
+  const headlineText = item.firstChild.textContent;
+  const span = document.createElement('span');
+
+  item.firstChild.remove();
+  span.innerText = headlineText;
+  span.className = 'headline';
+  item.prepend(span);
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.className === 'headline') {
+    e.target.nextSibling.hidden = !e.target.nextSibling.hidden;
+  }
+});
