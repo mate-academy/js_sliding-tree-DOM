a7c61651644026ea40e08ca7e7612f3409d10df3
Taras Sidak <tarasidak1990@gmail.com>
2020-11-12 16:12:43 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const fruitList = document.querySelector('.tree');
+const li = document.querySelectorAll('li');
+
+fruitList.style.position = 'absolute';
+fruitList.style.top = fruitList.getBoundingClientRect().top + 'px';
+fruitList.style.left = fruitList.getBoundingClientRect().left + 'px';
+
+[...li].map(item => {
+  const span = document.createElement('span');
+
+  span.setAttribute('class', 'title');
+  item.prepend(span);
+  span.append(span.nextSibling);
+});
+
+fruitList.addEventListener('click', (event) => {
+  const item = event.target.nextElementSibling;
+
+  if (item.nodeName === 'UL') {
+    item.hidden = !item.hidden;
+  }
+});
