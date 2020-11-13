77aa1a244d94dbbb117248b1d7ae8da089c488d1
Anna Uska <annauska95@gmail.com>
2020-11-13 13:34:11 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+
+let visible = true;
+
+for (const item of [...listItems]) {
+  const span = document.createElement('span');
+
+  span.append(item.firstChild);
+  item.prepend(span);
+}
+
+tree.addEventListener('click', e => {
+  const item = e.target.closest('li');
+
+  if (item) {
+    if (visible) {
+      item.children[1].style.display = 'none';
+      visible = false;
+    } else {
+      item.children[1].style.display = 'block';
+      visible = true;
+    }
+  }
+});
