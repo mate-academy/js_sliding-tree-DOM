e9462085296b415f6c34a95d9d9315f3197b1c56
Anna Uska <annauska95@gmail.com>
2020-11-16 20:15:08 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,25 +3,15 @@
 const tree = document.querySelector('.tree');
 const listItems = document.querySelectorAll('li');
 
-let visible = true;
-
-for (const item of [...listItems]) {
+for (const list of [...listItems]) {
   const span = document.createElement('span');
 
-  span.append(item.firstChild);
-  item.prepend(span);
+  span.append(list.firstChild);
+  list.prepend(span);
+
+  tree.addEventListener('click', e => {
+    if (e.target.nextElementSibling.tagName === 'UL') {
+      e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+    };
+  });
 }
-
-tree.addEventListener('click', e => {
-  const item = e.target.closest('li');
-
-  if (item) {
-    if (visible) {
-      item.children[1].style.display = 'none';
-      visible = false;
-    } else {
-      item.children[1].style.display = 'block';
-      visible = true;
-    }
-  }
-});
