2d294651dc70c8359ab72127124441f29a05f121
Olga Rubalska <olga.anikeieva@gmail.com>
2020-11-22 21:13:30 +0200

    Clicking on the headline hide/show element

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const addSpan = function(element) {
+  if (element.children.length === 0) {
+    return;
+  }
+
+  if (element.tagName === 'LI') {
+    const text = element.childNodes[0];
+    const span = document.createElement('span');
+
+    span.innerHTML = text.textContent;
+    element.prepend(span);
+    text.remove();
+  }
+
+  for (const prop of element.children) {
+    addSpan(prop);
+  }
+};
+
+const handler = function(event) {
+  const clicked = event.target;
+
+  if (clicked.closest('span')) {
+    clicked.nextElementSibling.hidden = !clicked.nextElementSibling.hidden;
+  }
+};
+
+addSpan(tree);
+document.addEventListener('click', handler);
