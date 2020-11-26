f4c7a3e162af93895c117a11d77826a4eb2c4122
y.kostenko <y.kostenko@mate.academy>
2020-11-26 20:01:56 +0200

    tests changed using jquery

--- cypress/integration/slidingTreeDom.spec.js
+++ cypress/integration/slidingTreeDom.spec.js
@@ -1,53 +1,36 @@
 'use strict';
 
+Cypress.Commands.add('hideTree', (selector) => {
+  cy.get('ul').contains(selector).click();
+
+  cy.get('ul').contains(selector).parent().should(($ul) => {
+    const ul = $ul.find('ul').is(':hidden');
+
+    expect(ul).to.equal(true);
+  });
+  cy.get('ul').contains(selector).click();
+
+  cy.get('ul').contains(selector).parent().should(($ul) => {
+    const ul = $ul.find('ul').is(':visible');
+
+    expect(ul).to.equal(true);
+  });
+});
+
 describe(('Sliding tree'), () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.get('.tree > :nth-child(1) > ul').as('fruits');
-    cy.get('.tree > :nth-child(2) > ul').as('fishes');
   });
 
-  it(`Click on the headline should hide/show first level <ul> elements`, () => {
-    cy.contains('Fruit').click();
-    cy.get('@fruits').should('not.be.visible');
-
-    cy.contains('Fruit').click();
-    cy.get('@fruits').should('be.visible');
+  it(`should hide/show first level <ul> elements by click `, () => {
+    cy.hideTree('Fruit');
+    cy.hideTree('Fish');
   });
 
-  it(`Click on the headline should hide/show second level ul elements`, () => {
-    cy.contains('Red').click();
-    cy.get('@fruits').find(':nth-child(1) > ul').should('not.be.visible');
-
-    cy.contains('Yellow').click();
-    cy.get('@fruits').find(':nth-child(2) > ul').should('not.be.visible');
-
-    cy.contains('Red').click();
-    cy.get('@fruits').find(':nth-child(1) > ul').should('be.visible');
-
-    cy.contains('Yellow').click();
-    cy.get('@fruits').find(':nth-child(2) > ul').should('be.visible');
-  });
-
-  it(`Click on the headline should hide/show first level ul elements`, () => {
-    cy.contains('Fish').click();
-    cy.get('@fishes').should('not.be.visible');
-
-    cy.contains('Fish').click();
-    cy.get('@fishes').should('be.visible');
-  });
-
-  it(`Click on the headline should hide/show second level <ul> elements`, () => {
-    cy.contains('Ocean fishes').click();
-    cy.get('@fishes').find(':nth-child(1) > ul').should('not.be.visible');
-
-    cy.contains('Freshwater fish').click();
-    cy.get('@fishes').find(':nth-child(2) > ul').should('not.be.visible');
-
-    cy.contains('Ocean fishes').click();
-    cy.get('@fishes').find(':nth-child(1) > ul').should('be.visible');
-
-    cy.contains('Freshwater fish').click();
-    cy.get('@fishes').find(':nth-child(2) > ul').should('be.visible');
+  it(`should hide/show second level <ul> elements by click`, () => {
+    cy.hideTree('Red');
+    cy.hideTree('Yellow');
+    cy.hideTree('Ocean fishes');
+    cy.hideTree('Freshwater fish');
   });
 });
