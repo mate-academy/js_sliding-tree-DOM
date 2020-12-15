1d2ddf3ea2de55db05951c16cde9c663bd9733f4
AndrewKreshchenko <mail2andyk@gmail.com>
2020-12-15 20:01:36 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const toggleHeadline = (e) => {
+  if (e.target.closest('span')) {
+    e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  [...tree.querySelectorAll('li')].forEach((li) => {
+    if (li.tagName !== 'LI' || !li.children.length) {
+      return;
+    }
+
+    const headline = li.firstChild;
+    const span = document.createElement('span');
+
+    span.innerHTML = headline.textContent;
+
+    headline.remove();
+    li.prepend(span);
+  });
+
+  tree.addEventListener('click', toggleHeadline);
+});
