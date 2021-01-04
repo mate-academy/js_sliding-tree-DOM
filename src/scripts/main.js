f946c3f63d3b5b6a9f2b2a5c8d4fb9cb50224633
Serhii Alieksieievych <kajot1863@gmail.com>
2021-01-04 23:15:48 +0200

    Completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+const headers = [...document.querySelectorAll('li')];
+
+for (const header of headers) {
+  const text = header.firstChild.data;
+
+  header.insertAdjacentHTML('afterbegin', `<span>${text}</span>`);
+
+  if (header.firstChild.nextSibling.nodeName === '#text') {
+    header.firstChild.nextSibling.data = '';
+  };
+}
+
+const spans = [...document.querySelectorAll('span')];
+const lists = [...document.querySelectorAll('ul')];
+
+for (const item of lists) {
+  item.classList.add('visible');
+}
+
+for (const span of spans) {
+  span.addEventListener('click', (evvnt) => {
+    if (evvnt.target.parentElement.querySelector('ul') !== null) {
+      evvnt.target.parentElement
+        .querySelector('ul')
+        .classList.toggle('hidden');
+
+      evvnt.target.parentElement
+        .querySelector('ul')
+        .classList.toggle('visible');
+
+      if (evvnt.target.parentElement.querySelector('.hidden')) {
+        evvnt.target.parentElement
+          .querySelector('.hidden')
+          .style.cssText = 'display:none';
+      }
+
+      if (evvnt.target.parentElement.querySelector('.visible')) {
+        evvnt.target.parentElement
+          .querySelector('.visible')
+          .style.cssText = 'display:block';
+      }
+    }
+  });
+}
