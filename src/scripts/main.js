3e7190c93e2cc74747544c3859b082b3e9914770
Alex Lemeshchuk <alexyskr@gmail.com>
2021-01-06 20:04:17 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const liElements = [...document.querySelectorAll('li')];
+const itemsSpan = liElements.filter(li => li.children.length > 0);
+const list = document.querySelector('.tree');
+
+for (const item of itemsSpan) {
+  const tagContent = item.firstChild.textContent;
+
+  item.firstChild.remove();
+
+  item.insertAdjacentHTML('afterbegin', `
+    <span>${tagContent}</span>
+  `);
+}
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+  }
+});
