c8f99a06bebd7c15be69c4bee77d29c3f9b71fd8
vovador <vovadorko@gmail.com>
2021-01-06 13:58:28 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const liHeaders = document.querySelectorAll('.tree > li');
+const subLiHeaders = document.querySelectorAll('.tree > li > ul > li');
+const list = document.querySelector('.tree');
+
+liHeaders.forEach((el) => {
+  const headerText = el.firstChild.textContent.trim();
+  const span = document.createElement('span');
+
+  el.firstChild.remove();
+  span.innerText = headerText;
+  el.prepend(span);
+});
+
+subLiHeaders.forEach((el) => {
+  const headerText = el.firstChild.textContent.trim();
+  const span = document.createElement('span');
+
+  el.firstChild.remove();
+  span.innerText = headerText;
+  el.prepend(span);
+});
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    if (!e.target.nextSibling.style.display) {
+      e.target.nextSibling.style.display = 'none';
+
+      return;
+    }
+
+    e.target.nextSibling.style.display = '';
+  }
+});
