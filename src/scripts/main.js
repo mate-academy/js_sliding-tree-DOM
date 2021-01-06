a59f18ff6bbe59f74fb56e2d4399f15bb6ba6196
Valentina Vorfolomeeva <vorfolomeevav@gmail.com>
2021-01-06 15:42:39 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const listElements = document.querySelectorAll('li');
+
+for (const element of [...listElements]) {
+  const content = element.firstChild.textContent.trim();
+
+  if (element.children.length !== 0) {
+    element.childNodes[0].remove();
+    element.insertAdjacentHTML('afterbegin', `<span>${content}</span>`);
+  }
+
+  element.onclick = (e) => {
+    e.stopPropagation();
+    element.children[1].hidden = !element.children[1].hidden;
+  };
+}
