539b76ed168fd4eeb91e0fe3f738770d456d9ab6
Lora Levitska <larakad94@gmail.com>
2021-01-07 20:59:07 +0200

    made tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+[...list.children].forEach(element => {
+  headerWrapper(element);
+
+  [...element.children[1].children].forEach(item => {
+    headerWrapper(item);
+  });
+});
+
+list.addEventListener('click', (fact) => {
+  if (fact.target.classList.contains('open')) {
+    fact.target.classList.toggle('open');
+    fact.target.nextSibling.style.display = 'block';
+  } else if (fact.target.classList.contains('wrap')) {
+    fact.target.classList.add('open');
+    fact.target.nextSibling.style.display = 'none';
+  }
+});
+
+function headerWrapper(element) {
+  const text = element.childNodes[0].data;
+  const wrap = document.createElement('span');
+
+  wrap.classList.add('wrap');
+  wrap.innerText = text.trim();
+  element.childNodes[0].replaceWith(wrap);
+}
