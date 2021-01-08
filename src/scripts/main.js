82e3ea6955c2b6e4f05886affe6a5df31074b0df
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-01-08 18:12:46 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const headlines = document.querySelector('.tree');
+
+document.querySelectorAll('li').forEach(li => {
+  if (li.children.length > 0) {
+    li.insertAdjacentHTML('afterbegin',
+      `<span>${li.firstChild.textContent}</span>`);
+
+    li.firstChild.nextSibling.remove();
+  };
+});
+
+headlines.addEventListener('click', (e) => {
+  const list = e.target.nextSibling;
+
+  if (e.target.tagName === 'SPAN') {
+    if (list.style.display === 'none') {
+      list.style.display = '';
+    } else {
+      list.style.display = 'none';
+    }
+  }
+});
