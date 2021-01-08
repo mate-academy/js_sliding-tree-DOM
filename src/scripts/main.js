0dd658e119220ec5506ad1e3bcf72cde8b233465
Andrii Haidai <haidai.andriy@gmail.com>
2021-01-08 16:19:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const focusElement = document.querySelectorAll('li ul');
+
+focusElement.forEach(element => {
+  const span = document.createElement('span');
+
+  span.innerHTML = element.previousSibling.textContent.trim();
+  element.previousSibling.replaceWith(span);
+
+  span.onclick = () => {
+    span.nextElementSibling.hidden = !span.nextElementSibling.hidden;
+  };
+});
