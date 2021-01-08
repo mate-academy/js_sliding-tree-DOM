146b89fb8402b3e45308b799606a5d9cb9c325e3
5Mountains <V.Piatyhor@gmail.com>
2021-01-08 13:22:53 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const root = document.querySelector('.tree');
+
+root.querySelectorAll('ul').forEach(item => creating(item));
+root.addEventListener('click', e => hiding(e));
+
+function creating(item) {
+  const elem = item.previousSibling;
+
+  elem.parentNode.insertAdjacentHTML('afterbegin',
+    `<span>${elem.textContent.trim()}</span>`);
+
+  elem.textContent = '';
+}
+
+function hiding(e) {
+  if (e.target.matches('span')) {
+    const child = e.target.nextElementSibling;
+
+    child.hidden = !child.hidden;
+  }
+}
