e56b02d914ff851532e790265a788c1e17c31643
Khrulenko Vitalii <khrulenko.v@gmail.com>
2021-01-08 23:10:02 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('LI')];
+
+list.forEach(el => {
+  if (el.children.length > 0) {
+    const savedText = el.firstChild.data;
+
+    el.firstChild.data = '';
+    el.insertAdjacentHTML('afterbegin', `<span>${savedText}</span>`);
+  }
+});
+
+function hider(e) {
+  if (e.target.tagName === 'SPAN') {
+    if (e.target.nextElementSibling.hidden === false) {
+      e.target.nextElementSibling.hidden = true;
+    } else {
+      e.target.nextElementSibling.hidden = false;
+    }
+  }
+}
+
+document.addEventListener('click', hider);
