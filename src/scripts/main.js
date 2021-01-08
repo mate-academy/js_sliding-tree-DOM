466f8633d6cf74e0c905758c219981be651cf94b
KirillLutsenko <mnrovski68@gmail.com>
2021-01-08 16:31:16 +0200

    Sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+const listItemsWithChildren = [...listItems]
+  .filter((listItem) => {
+    const childList = listItem.querySelector('ul');
+
+    return Boolean(childList);
+  });
+
+const hideAndShowHandling = (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const childList = e.target.nextElementSibling;
+
+  childList.hidden = !childList.hidden;
+};
+
+listItemsWithChildren.forEach((listItem) => {
+  const textNode = listItem.firstChild;
+  const textContainer = document.createElement('span');
+
+  textContainer.append(textNode);
+  listItem.prepend(textContainer);
+});
+
+tree.addEventListener('click', hideAndShowHandling);
