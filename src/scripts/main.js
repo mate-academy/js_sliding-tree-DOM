c6d48664abe9f4649607625376965e340e04e431
Vitalii Siedykh <siedykh.v@gmail.com>
2021-01-08 14:05:25 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const items = document.querySelectorAll('.tree > li');
+const subItems = document.querySelectorAll('.tree > li > ul > li');
+
+function addSpan(elements) {
+  for (const item of elements) {
+    item.insertAdjacentHTML('afterbegin',
+      `<span>${item.firstChild.textContent}</span>`);
+    item.firstChild.nextSibling.remove();
+  }
+}
+
+const hide = (ev) => {
+  if (!ev.target.closest('span')) {
+    return;
+  }
+
+  const elem = ev.target.closest('span').nextSibling;
+
+  elem.style.display === 'none'
+    ? elem.style.display = 'block'
+    : elem.style.display = 'none';
+};
+
+list.addEventListener('click', hide);
+addSpan(items);
+addSpan(subItems);
