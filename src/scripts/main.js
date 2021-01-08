1e87fe7126daabe66a530a8b470c7c655cf0f74a
Pavlo Terletskyi <pavloterleckij2@gmail.com>
2021-01-08 22:52:40 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const allLi = document.querySelectorAll('ul li');
+const list = document.querySelector('.tree');
+
+for (let item of [...allLi]) {
+  const spanContent = item.firstChild.textContent.trim();
+
+  item.firstChild.remove();
+
+  item.insertAdjacentHTML('afterbegin', `
+    <span>${spanContent}</span>
+  `);
+}
+
+
+list.addEventListener('click', (event) => {
+  event.target.nextElementSibling.hidden = !event.target.nextElementSibling.hidden;
+});
