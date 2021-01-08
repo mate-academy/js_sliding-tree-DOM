80b026e343bf06208511059c5162e5482fb895c4
Serhii Alieksieievych <kajot1863@gmail.com>
2021-01-08 19:24:46 +0200

    Code was optimized

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,35 +11,32 @@ for (const header of headers) {
   }
 }
 
-const spans = [...document.querySelectorAll('span')];
 const lists = [...document.querySelectorAll('ul')];
 
 for (const item of lists) {
   item.classList.add('visible');
 }
 
-for (const span of spans) {
-  span.addEventListener('click', (evvnt) => {
-    if (evvnt.target.parentElement.querySelector('ul') !== null) {
+document.querySelector('.tree').addEventListener('click', (evvnt) => {
+  if (evvnt.target.parentElement.querySelector('ul') !== null) {
+    evvnt.target.parentElement
+      .querySelector('ul')
+      .classList.toggle('hidden');
+
+    evvnt.target.parentElement
+      .querySelector('ul')
+      .classList.toggle('visible');
+
+    if (evvnt.target.parentElement.querySelector('.hidden')) {
       evvnt.target.parentElement
-        .querySelector('ul')
-        .classList.toggle('hidden');
-
-      evvnt.target.parentElement
-        .querySelector('ul')
-        .classList.toggle('visible');
-
-      if (evvnt.target.parentElement.querySelector('.hidden')) {
-        evvnt.target.parentElement
-          .querySelector('.hidden')
-          .style.cssText = 'display:none';
-      }
-
-      if (evvnt.target.parentElement.querySelector('.visible')) {
-        evvnt.target.parentElement
-          .querySelector('.visible')
-          .style.cssText = 'display:block';
-      }
+        .querySelector('.hidden')
+        .style.cssText = 'display:none';
     }
-  });
-}
+
+    if (evvnt.target.parentElement.querySelector('.visible')) {
+      evvnt.target.parentElement
+        .querySelector('.visible')
+        .style.cssText = 'display:block';
+    }
+  }
+});
