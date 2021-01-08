24f352573e9dd81a3bf44a078c5462eb3d2d323a
vovador <vovadorko@gmail.com>
2021-01-08 14:52:11 +0200

    The code was corrected according to DRY principle

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,21 @@
 'use strict';
 
+function spanner(el) {
+  const headerText = el.firstChild.textContent.trim();
+  const span = document.createElement('span');
+
+  el.firstChild.remove();
+  span.innerText = headerText;
+  el.prepend(span);
+}
+
 const liHeaders = document.querySelectorAll('.tree > li');
 const subLiHeaders = document.querySelectorAll('.tree > li > ul > li');
 const list = document.querySelector('.tree');
 
-liHeaders.forEach((el) => {
-  const headerText = el.firstChild.textContent.trim();
-  const span = document.createElement('span');
+liHeaders.forEach(spanner);
 
-  el.firstChild.remove();
-  span.innerText = headerText;
-  el.prepend(span);
-});
-
-subLiHeaders.forEach((el) => {
-  const headerText = el.firstChild.textContent.trim();
-  const span = document.createElement('span');
-
-  el.firstChild.remove();
-  span.innerText = headerText;
-  el.prepend(span);
-});
+subLiHeaders.forEach(spanner);
 
 list.addEventListener('click', (e) => {
   if (e.target.tagName === 'SPAN') {
