fea6fb6cea4476662557716f51e0d0401a818750
Roman Matsuk <the.show.of.ice.man@gmail.com>
2021-01-09 13:49:39 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+const li = [...document.querySelectorAll('li')];
+
+li.map(item => {
+  item.insertAdjacentHTML('afterbegin', `
+    <span>${item.firstChild.textContent.trim()}</span>
+  `);
+  item.childNodes[3].remove();
+});
+
+for (const item of li) {
+  item.addEventListener('click', (e) => {
+    if (e.target.tagName === 'SPAN') {
+      e.stopPropagation();
+      e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+    }
+  });
+}
