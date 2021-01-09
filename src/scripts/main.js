02d615a2276009c1c427bca544fa6e0606b2897a
Serhii Alieksieievych <kajot1863@gmail.com>
2021-01-09 20:10:37 +0200

    Code was optimized

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,32 +11,7 @@ for (const header of headers) {
   }
 }
 
-const lists = [...document.querySelectorAll('ul')];
-
-for (const item of lists) {
-  item.classList.add('visible');
-}
-
-document.querySelector('.tree').addEventListener('click', (evvnt) => {
-  if (evvnt.target.parentElement.querySelector('ul') !== null) {
-    evvnt.target.parentElement
-      .querySelector('ul')
-      .classList.toggle('hidden');
-
-    evvnt.target.parentElement
-      .querySelector('ul')
-      .classList.toggle('visible');
-
-    if (evvnt.target.parentElement.querySelector('.hidden')) {
-      evvnt.target.parentElement
-        .querySelector('.hidden')
-        .style.cssText = 'display:none';
-    }
-
-    if (evvnt.target.parentElement.querySelector('.visible')) {
-      evvnt.target.parentElement
-        .querySelector('.visible')
-        .style.cssText = 'display:block';
-    }
-  }
+document.querySelector('.tree').addEventListener('click', (evnt) => {
+  evnt.target.nextElementSibling.hidden
+    = !evnt.target.nextElementSibling.hidden;
 });
