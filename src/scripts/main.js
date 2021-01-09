08a8d857d85c387c7c1f251952906f5ba757497e
Zhenya <e.mezhueva@gmail.com>
2021-01-09 20:08:05 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+for (const item of items) {
+  if (item.querySelector('ul')) {
+    const listHeaderSpan = `<span>${item.childNodes[0].nodeValue}</span>`;
+
+    item.removeChild(item.firstChild);
+    item.insertAdjacentHTML('afterbegin', listHeaderSpan);
+  }
+}
+
+tree.addEventListener('click', evt => {
+  if (evt.target.tagName === 'SPAN') {
+    const nestedList = evt.target.nextElementSibling;
+
+    nestedList.hidden = !nestedList.hidden;
+  }
+});
