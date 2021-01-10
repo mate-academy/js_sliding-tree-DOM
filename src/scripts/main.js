bc140fe47dddbe7b0c62e87e1334bd44615cf803
Your Name <mogolgogol.90@gmail.com>
2021-01-10 10:37:35 +0200

    removed unnecessary ifs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,30 +6,16 @@ const subList = document.querySelectorAll('.tree > li > ul > li');
 
 for (const item of list) {
   item.insertAdjacentHTML('afterbegin', `
-  <span class="list">${item.firstChild.textContent.trim()}</span>`);
+  <span>${item.firstChild.textContent.trim()}</span>`);
   item.firstElementChild.nextSibling.remove();
 }
 
 for (const item of subList) {
   item.insertAdjacentHTML('afterbegin', `
-  <span class="sub-list">${item.firstChild.textContent.trim()}</span>`);
+  <span>${item.firstChild.textContent.trim()}</span>`);
   item.firstElementChild.nextSibling.remove();
 }
 
 tree.addEventListener('click', (e) => {
-  if (e.target.classList.contains('list')) {
-    if (e.target.nextElementSibling.style.display === 'none') {
-      e.target.nextElementSibling.style.display = 'block';
-    } else {
-      e.target.nextElementSibling.style.display = 'none';
-    }
-  }
-
-  if (e.target.classList.contains('sub-list')) {
-    if (e.target.nextElementSibling.style.display === 'none') {
-      e.target.nextElementSibling.style.display = 'block';
-    } else {
-      e.target.nextElementSibling.style.display = 'none';
-    }
-  }
+  e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
 });
