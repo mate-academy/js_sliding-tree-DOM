5eddcf88a62a4c72f25f512de75f7e77cc2c3d8e
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-10 14:52:09 +0200

    created sliding-tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const mainList = document.querySelector('.tree');
+const list = [...document.querySelectorAll('li')];
+
+list.forEach(item => {
+  if (item.firstElementChild) {
+    const titleText = item.firstChild.textContent;
+
+    item.firstChild.remove();
+
+    item.insertAdjacentHTML('afterbegin', `
+    <span>${titleText}</span>`);
+  }
+});
+
+mainList.addEventListener('click', (e) => {
+  if (!mainList.contains(e.target.closest('.tree'))) {
+    return;
+  }
+
+  if (e.target.tagName === 'SPAN') {
+    e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+  }
+});
