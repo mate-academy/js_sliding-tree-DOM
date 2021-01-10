aa1ad1b61ae3aab5173e09b1c819785c5e37130e
Alex <alexmogwaiii@gmail.com>
2021-01-10 16:44:17 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const root = document.querySelector('.tree');
+const heads = document.querySelectorAll('.tree > li');
+const subHeads = document.querySelectorAll('.tree > li > ul > li');
+
+const insertTag = nodes => {
+  for (const elem of nodes) {
+    const span = document.createElement('span');
+
+    span.textContent = elem.firstChild.textContent.trim();
+
+    elem.firstChild.replaceWith(span);
+  }
+};
+
+insertTag(heads);
+insertTag(subHeads);
+
+root.addEventListener('click', e => {
+  if (e.target.tagName === 'SPAN') {
+    if (!e.target.nextElementSibling.hidden) {
+      e.target.nextElementSibling.hidden = true;
+    } else {
+      e.target.nextElementSibling.hidden = false;
+    }
+  }
+});
