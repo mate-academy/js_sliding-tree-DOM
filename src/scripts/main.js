f2e919a6939f93f8e27a2c308f263d9f0ae402c3
Iryna Kozhura <ivladimirovna713@gmail.com>
2021-01-10 19:25:50 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+list.forEach(item => {
+  const text = item.firstChild.textContent;
+
+  if (item.children.length > 0) {
+    const span = document.createElement('span');
+    span.textContent = text;
+
+    item.firstChild.replaceWith(span);
+
+    item.addEventListener('click', (ev) => {
+      ev.target.nextSibling.hidden = !ev.target.nextSibling.hidden;
+      ev.stopPropagation();
+    });
+  }
+});
