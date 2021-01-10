bbc470aaed974e3740e2d6ce6ee4ed11bc76b77b
Dima Bondarenko <73847068+dima-bond@users.noreply.github.com>
2021-01-10 18:50:10 +0200

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,5 @@
 'use strict';
 
-// write code here
-
-// const list = document.querySelector('.tree');
-// const items = document.querySelectorAll('.tree > li');
-// const subItems = document.querySelectorAll('.tree > li > ul > li');
-
-// function addSpan(elements) {
-//   for (const item of elements) {
-//     item.insertAdjacentHTML('afterbegin',
-//       `<span>${item.firstChild.textContent}</span>`);
-//     item.firstChild.nextSibling.remove();
-//   }
-// }
-
-// const hide = (ev) => {
-//   if (!ev.target.closest('span')) {
-//     return;
-//   }
-
-//   const elem = ev.target.closest('span').nextSibling;
-
-//   elem.style.display === 'none'
-//     ? elem.style.display = 'block'
-//     : elem.style.display = 'none';
-
-//   };
-
-//   list.addEventListener('click', hide);
-//   addSpan(items);
-//   addSpan(subItems);
-
 const list = document.querySelector('.tree');
 const treeLi = list.querySelectorAll('li');
 
