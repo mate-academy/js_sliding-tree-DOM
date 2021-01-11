7038e1295afa1f7ebc61d0dce25f3b0014a62044
ShyianO <ol.shyian@gmacil.com>
2021-01-11 15:10:47 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const items = [...document.querySelectorAll('li')];
+
+items.forEach(item => {
+  const list = item.querySelector('ul');
+
+  if (!list) {
+    return;
+  }
+
+  const textNode = item.firstChild;
+  const span = document.createElement('span');
+
+  textNode.replaceWith(span);
+  span.textContent = textNode.data.trim();
+
+  span.addEventListener('click', () => {
+    list.hidden = !list.hidden;
+  });
+});
