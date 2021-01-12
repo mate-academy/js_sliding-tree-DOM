9895463d04f8333444c4db2a2d4f4a82a49c90cf
ShyianO <ol.shyian@gmacil.com>
2021-01-12 12:43:42 +0200

    Fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,21 @@
 'use strict';
 
 const items = [...document.querySelectorAll('li')];
+const tree = document.querySelector('.tree');
 
 items.forEach(item => {
   const list = item.querySelector('ul');
+  const textNode = item.firstChild;
+  const span = document.createElement('span');
 
   if (!list) {
     return;
   }
 
-  const textNode = item.firstChild;
-  const span = document.createElement('span');
-
   textNode.replaceWith(span);
   span.textContent = textNode.data.trim();
+});
 
-  span.addEventListener('click', () => {
-    list.hidden = !list.hidden;
-  });
+tree.addEventListener('click', (e) => {
+  e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
 });
