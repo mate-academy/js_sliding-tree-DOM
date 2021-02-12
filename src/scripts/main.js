6009906678d186faf1f973d29c112fbdcff55be8
DominoSl <69808518+DominoSl@users.noreply.github.com>
2021-02-12 19:02:15 +0200

    solution?

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = [...document.querySelectorAll('LI')];
+
+listItems.forEach(item => {
+  const span = document.createElement('SPAN');
+
+  span.innerText = item.firstChild.textContent;
+  item.firstChild.remove();
+  item.insertAdjacentElement('afterbegin', span);
+});
+
+const hide = (evento) => {
+  if (evento.target.children.length !== 0) {
+    evento.target.nextSibling.hidden = !evento.target.nextSibling.hidden;
+  }
+};
+
+tree.addEventListener('click', hide);
