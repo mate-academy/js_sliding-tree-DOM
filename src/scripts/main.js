94e54443913c04db3d0c0d65c3a27499b4a0aeca
Artem Fedoseev <afedoseev359@gmail.com>
2021-02-16 11:44:30 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write code here
+const list = document.querySelector('.tree');
+const items = document.querySelectorAll('li');
+
+[...items].forEach(item => {
+  if (item.children.length > 0) {
+    const container = document.createElement('span');
+
+    container.append(item.childNodes[0]);
+    item.prepend(container);
+  }
+});
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN' || !list.contains(e.target)) {
+    return;
+  }
+
+  const item = e.target.closest('li');
+
+  item.children[1].hidden = !item.children[1].hidden;
+});
