cf8ae3c36101db5ea124fe26f60bab9c4268d29d
chri911 <delightsome@bigmir.net>
2021-02-27 12:31:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,9 @@ liTitles.map(li => {
   const span = document.createElement('span');
 
   span.innerText = li.firstChild.textContent;
-  span.nextElementSibling.style.display = 'block';
 
   li.firstChild.replaceWith(span);
+  span.nextElementSibling.style.display = 'block';
 
   span.addEventListener('click', () => {
     span.nextElementSibling.style.display !== 'block'
