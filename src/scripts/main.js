25fc0466cc168c6ee31b774779b7d3e6aa0c6901
chri911 <delightsome@bigmir.net>
2021-02-27 12:22:20 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const liAll = document.querySelectorAll('li');
+const liTitles = [...liAll].filter(li => li.children.length > 0);
+
+liTitles.map(li => {
+  const span = document.createElement('span');
+
+  span.innerText = li.firstChild.textContent;
+  span.nextElementSibling.style.display = 'block';
+
+  li.firstChild.replaceWith(span);
+
+  span.addEventListener('click', () => {
+    span.nextElementSibling.style.display !== 'block'
+      ? span.nextElementSibling.style.display = 'block'
+      : span.nextElementSibling.style.display = 'none';
+  });
+});
