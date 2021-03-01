1263d2d67a4c82733db20a54e131ad8fd8a78323
morozoleh <olegmoroz93@gmail.com>
2021-03-01 14:21:30 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const items = document.querySelectorAll('li');
+
+items.forEach(item => {
+  const span = document.createElement('span');
+
+  if (item.children.length) {
+    span.innerText = item.childNodes[0].data.trim();
+    item.childNodes[0].data = '';
+    item.insertAdjacentElement('afterbegin', span);
+  }
+});
+
+function hide(e) {
+  if (e.target.closest('li')) {
+    if (e.target.nextElementSibling.hidden) {
+      e.target.nextElementSibling.hidden = false;
+    } else {
+      e.target.nextElementSibling.hidden = true;
+    }
+  }
+};
+
+list.addEventListener('click', hide);
