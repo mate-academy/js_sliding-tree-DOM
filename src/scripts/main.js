4026f26a9c7fb28264aecdb02e3c343300754f5f
Taras <seman6745@gmail.com>
2021-03-02 21:07:35 +0200

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+const lis = document.querySelectorAll('li');
+
+[...lis].filter(li => li.children.length > 0)
+  .forEach(li => {
+    const span = document.createElement('span');
+
+    span.innerText = li.firstChild.textContent;
+
+    li.firstChild.replaceWith(span);
+  });
+
+list.addEventListener('click', (e) => {
+  const item = e.target.closest('li').querySelector('ul');
+
+  if (item.isClicked) {
+    item.style.display = 'block';
+    item.isClicked = false;
+  } else {
+    item.style.display = 'none';
+    item.isClicked = true;
+  }
+});
