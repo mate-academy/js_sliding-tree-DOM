3eb621bcf45ffbdfa86d27bfc1ac7f00d68b3d82
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-03-10 16:02:22 +0200

    Implemented Sliding Tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const items = [...tree.querySelectorAll('li')];
+
+items.forEach(item => {
+  const list = item.querySelector('ul');
+
+  if (!list) {
+    return;
+  }
+
+  const textNode = item.firstChild;
+  const span = document.createElement('span');
+
+  textNode.replaceWith(span);
+  span.textContent = textNode.data.trim();
+
+  span.addEventListener('click', () => {
+    list.hidden = !list.hidden;
+  });
+});
