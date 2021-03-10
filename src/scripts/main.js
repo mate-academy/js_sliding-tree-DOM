edaace91c968fc8de4548c2191cf96c6ef6012e7
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-10 16:57:39 +0200

    solution tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const elementsOfList = [...tree.querySelectorAll('li')];
+
+elementsOfList.forEach(pointOfList => {
+  const list = pointOfList.querySelector('ul');
+
+  if (!list) {
+    return;
+  }
+
+  const newNode = pointOfList.firstChild;
+
+  const spanElement = document.createElement('span');
+
+  newNode.replaceWith(spanElement);
+
+  spanElement.textContent = newNode.data;
+
+  spanElement.addEventListener('click', (e) => {
+    list.hidden = !list.hidden;
+  });
+});
