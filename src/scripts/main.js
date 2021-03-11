3e8302bc90fe8358a529db031fcfe9e3c8b5b8e8
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-11 15:26:30 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const branches = document.querySelectorAll('li');
+
+for (const branch of branches) {
+  const wrap = document.createElement('span');
+
+  wrap.innerText = branch.firstChild.textContent;
+
+  branch.firstChild.replaceWith(wrap);
+}
+
+tree.addEventListener('click', e => {
+  const list = e.target.closest('li').querySelector('ul');
+
+  if (list === null) {
+    return;
+  }
+
+  if (list.style.display === 'none') {
+    list.style.display = 'block';
+  } else {
+    list.style.display = 'none';
+  }
+});
