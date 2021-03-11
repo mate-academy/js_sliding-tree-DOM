8745d03937bf662aa9fe94a91568365efccf6759
Olha Briukhanova <oaleksandrovva@gmail.com>
2021-03-11 17:33:35 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
+/* eslint-disable no-useless-return */
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const elementsOfTree = tree.querySelectorAll('li');
+const listOfTree = [...elementsOfTree].filter(element => (
+  element.children.length > 0
+));
+
+listOfTree.forEach(element => {
+  const span = document.createElement('span');
+  const firstChildOfElement = element.firstChild;
+
+  span.textContent = firstChildOfElement.textContent;
+
+  firstChildOfElement.replaceWith(span);
+});
+
+tree.addEventListener('click', clickEvent => {
+  const element = clickEvent.target;
+  const childrenListOfElement = element.parentNode.querySelector('ul');
+
+  if (element.tagName !== 'SPAN') {
+    return;
+  }
+
+  childrenListOfElement.hidden = !childrenListOfElement.hidden;
+});
