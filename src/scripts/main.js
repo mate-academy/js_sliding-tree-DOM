145938e703392e2787414139f4277d98b041e56e
Tetiana Zotova <zotova.tetiana@gmail.com>
2021-03-11 14:49:24 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const listItems = [...document.querySelectorAll('li')];
+
+listItems.forEach(item => {
+  const listOfGoods = item.querySelector('ul');
+
+  if (!listOfGoods) {
+    return;
+  }
+
+  const textNode = item.firstChild;
+  const span = document.createElement('span');
+
+  textNode.replaceWith(span);
+
+  span.textContent = textNode.data.trim();
+
+  span.addEventListener('click', () => {
+    listOfGoods.hidden = !listOfGoods.hidden;
+  });
+});
