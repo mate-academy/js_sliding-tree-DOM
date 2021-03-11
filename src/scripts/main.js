fe21c761910a0c205c572c99d95cbc73cb00d7a1
Serhii Ivanyk <youdex06@gmail.com>
2021-03-11 11:05:41 +0200

    Task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const colection = document.querySelectorAll('li');
+
+colection.forEach(el => {
+  const span = document.createElement('span');
+
+  span.innerText = el.firstChild.textContent;
+  el.replaceChild(span, el.firstChild);
+});
+
+tree.addEventListener('click', (tap) => {
+  if (tap.target.matches('span')) {
+    const ul = tap.target.parentNode.querySelector('ul');
+
+    ul.toggleAttribute('hidden');
+  }
+});
