89500c89685fa43e8db6e2f0a8b2d6641973960d
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-03-11 16:20:10 +0200

    added toggle of visibility

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach(item => {
+  const span = document.createElement('span');
+
+  if (item.children.length) {
+    span.textContent = item.firstChild.textContent;
+    item.firstChild.replaceWith(span);
+  }
+});
+
+function visibilityToggler(e) {
+  const toggledElement = e.target.closest('li').querySelector('ul');
+
+  toggledElement.hidden = !toggledElement.hidden;
+};
+
+list.addEventListener('click', visibilityToggler);
