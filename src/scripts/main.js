ad14964d36b65f1689934b49eacba0f83f855923
Taras Chirkov <chirkovtaras@gmail.com>
2021-03-11 13:29:46 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const lists = [...document.querySelectorAll('li')]
+  .filter(li => li.children.length > 0);
+
+for (const list of lists) {
+  const span = document.createElement('span');
+
+  span.classList.add('hidden');
+  span.textContent = list.firstChild.textContent.trim();
+
+  list.firstChild.replaceWith(span);
+}
+
+tree.addEventListener('click', clickEvent => {
+  if (clickEvent.target.className === 'hidden') {
+    clickEvent.target.nextElementSibling.hidden
+    = !clickEvent.target.nextElementSibling.hidden;
+  }
+});
