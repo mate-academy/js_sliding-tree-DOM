c6ea98c2a4effa9d9b966cdb53978856275956c6
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-11 16:26:43 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const generallTree = document.querySelector('.tree');
+
+const liElement = [...document.querySelectorAll('li')];
+
+for (let i = 0; i < liElement.length; i++) {
+  const spanElement = document.createElement('span');
+
+  spanElement.innerText = liElement[i].firstChild.nodeValue;
+  liElement[i].prepend(spanElement);
+  liElement[i].childNodes[1].remove();
+}
+
+const closeTree = (clickHandler) => {
+  if (clickHandler.target.tagName === 'SPAN') {
+    clickHandler.target.nextSibling.hidden = !clickHandler
+      .target.nextSibling.hidden;
+  };
+};
+
+generallTree.addEventListener('click', closeTree);
