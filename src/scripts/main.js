320679deacb4db215ac15f6ae1c1ad59b2eca363
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-03-11 17:49:50 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write code here
+const li = document.body.querySelectorAll('li');
+const list = document.body.querySelector('.tree');
+
+li.forEach(element => {
+  if (element.children.length !== 0) {
+    const wrapElement = document.createElement('span');
+
+    wrapElement.textContent = element.firstChild.textContent;
+    element.firstChild.replaceWith(wrapElement);
+  }
+});
+
+list.addEventListener('click', (eventClick) => {
+  const elementToHidden = eventClick.target.nextSibling;
+
+  if (!elementToHidden) {
+    return;
+  }
+
+  if (eventClick.target.tagName === 'SPAN'
+    && elementToHidden.hidden === false) {
+    elementToHidden.hidden = true;
+
+    return;
+  }
+
+  if (eventClick.target.tagName === 'SPAN' && elementToHidden.hidden === true) {
+    elementToHidden.hidden = false;
+  }
+});
