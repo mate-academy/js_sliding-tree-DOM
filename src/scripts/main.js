34f81bfb22d631eddb1d865012a0cefdcdebc520
Oleg Bezriadin <muhachov86@gmail.com>
2021-03-11 17:34:13 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listCollection = document.querySelectorAll('li');
+
+tree.addEventListener('click', (clickEvent) => {
+  const selectedElement = clickEvent.target;
+  const ul = selectedElement.closest('li').querySelector('ul');
+
+  if (ul.style.display === 'none') {
+    ul.style.display = '';
+  } else {
+    ul.style.display = 'none';
+  }
+});
+
+for (const li of listCollection) {
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent;
+  li.firstChild.replaceWith(span);
+}
