c1d5f62bf00340230457783478352aa614f66ca6
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-12 14:48:31 +0200

    after mentor review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,24 @@
 'use strict';
 
-const generallTree = document.querySelector('.tree');
+const tree = document.querySelector('.tree');
 
-const liElement = [...document.querySelectorAll('li')];
+const liElements = [...document.querySelectorAll('li')];
 
-for (let i = 0; i < liElement.length; i++) {
+for (let i = 0; i < liElements.length; i++) {
   const spanElement = document.createElement('span');
 
-  if (liElement[i].children.length > 0) {
-    spanElement.innerText = liElement[i].firstChild.nodeValue;
-    liElement[i].prepend(spanElement);
-    liElement[i].childNodes[1].remove();
+  if (liElements[i].children.length > 0) {
+    spanElement.innerText = liElements[i].firstChild.nodeValue;
+    liElements[i].prepend(spanElement);
+    liElements[i].childNodes[1].remove();
   }
 }
 
 const closeTree = (clickHandler) => {
-  if (clickHandler.target.tagName === 'SPAN') {
+  if (clickHandler.closest('span')) {
     clickHandler.target.nextSibling.hidden = !clickHandler
       .target.nextSibling.hidden;
   };
 };
 
-generallTree.addEventListener('click', closeTree);
+tree.addEventListener('click', closeTree);
