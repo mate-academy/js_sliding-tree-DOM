ec45847920f43c9ddbb302350f465fabb3d0c497
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-03-12 15:07:02 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,15 +7,20 @@ listItems.forEach(item => {
   const span = document.createElement('span');
 
   if (item.children.length) {
+    span.classList.add('tree__span');
     span.textContent = item.firstChild.textContent;
     item.firstChild.replaceWith(span);
   }
 });
 
 function visibilityToggler(e) {
-  const toggledElement = e.target.closest('li').querySelector('ul');
+  const toggledElement = e.target.closest('.tree__span');
 
-  toggledElement.hidden = !toggledElement.hidden;
+  if (!toggledElement) {
+    return;
+  }
+
+  toggledElement.nextElementSibling.hidden = !toggledElement.nextElementSibling.hidden;
 };
 
 list.addEventListener('click', visibilityToggler);
