b09f82595f702c0311906212f1d0eaf5daeb5925
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-12 09:35:57 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const elementsList = [...document.querySelectorAll('li')];
+
+elementsList.forEach(li => {
+  if (li.children.length > 0) {
+    const span = document.createElement('span');
+
+    span.innerText = li.firstChild.textContent;
+
+    li.firstChild.replaceWith(span);
+  }
+});
+
+tree.addEventListener('click', (clickEvent) => {
+  const list = clickEvent.target;
+
+  if (list.tagName === 'SPAN') {
+    (list.nextSibling.hidden = !list.nextSibling.hidden);
+  }
+});
