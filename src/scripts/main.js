da94fe3e0f2a7fcd21785bd849816931ff34d831
Artur Shpontak <ashpontak97@gmail.com>
2021-03-14 19:21:23 +0200

    Solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+class Span {
+  constructor(item) {
+    const newSpan = document.createElement('span');
+
+    newSpan.innerText = item.textContent;
+    newSpan.className = '.tree__span';
+
+    return newSpan;
+  }
+}
+
+for (const item of list) {
+  if (item.children.length === 0) {
+    continue;
+  }
+
+  const newElement = new Span(item.firstChild);
+
+  item.firstChild.replaceWith(newElement);
+}
+
+function hideElement(element) {
+  const nearestElement = element.target.nextSibling;
+
+  if (!nearestElement) {
+    return;
+  }
+
+  nearestElement.hidden = !nearestElement.hidden;
+}
+
+document.addEventListener('click', hideElement);
