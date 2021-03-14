8ad21bb3f20b710ba2b10ec28569b07e5ae5c5e0
Anna <garsinaanna32778@gmail.com>
2021-03-15 00:57:09 +0200

    sliding tree was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liElements = document.querySelectorAll('li');
+
+[...liElements].forEach(element => {
+  if (element.children.length > 0) {
+    const span = document.createElement('span');
+
+    span.innerText = element.firstChild.textContent;
+    span.className = 'spanElement';
+    element.firstChild.replaceWith(span);
+  }
+});
+
+tree.addEventListener('click', () => {
+  const list = event.target.closest('.spanElement');
+
+  if (!list || !tree.contains(list)) {
+    return;
+  }
+
+  list.nextElementSibling.hidden = !list.nextElementSibling.hidden;
+});
