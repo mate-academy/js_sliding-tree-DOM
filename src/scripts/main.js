8e3cdfe2051f232d32298f2add5c53b5fd44d672
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-03-14 13:14:09 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const elements = document.querySelectorAll('li');
+
+for (const element of elements) {
+  if (element.firstElementChild) {
+    const span = document.createElement('span');
+
+    span.textContent = element.firstChild.textContent;
+
+    element.firstChild.replaceWith(span);
+  }
+}
+
+list.addEventListener('click', (action) => {
+  action.preventDefault();
+
+  const target = action.target.nextSibling;
+
+  if (target) {
+    target.hidden = !target.hidden;
+  }
+});
