c1001a4fa62d812b4ee3fe507002b689ddc3b199
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-03-15 16:33:40 +0200

    solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,18 +4,17 @@ const list = document.querySelector('.tree');
 const elements = document.querySelectorAll('li');
 
 for (const element of elements) {
-  if (element.firstElementChild) {
-    const span = document.createElement('span');
-
-    span.textContent = element.firstChild.textContent;
-
-    element.firstChild.replaceWith(span);
+  if (!element.firstElementChild) {
+    continue;
   }
+
+  const span = document.createElement('span');
+
+  span.textContent = element.firstChild.textContent;
+  element.firstChild.replaceWith(span);
 }
 
 list.addEventListener('click', (action) => {
-  action.preventDefault();
-
   const target = action.target.nextSibling;
 
   if (target) {
