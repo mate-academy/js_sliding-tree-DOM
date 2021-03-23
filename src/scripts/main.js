9f448a9e5c4b52d4562309f60d35f9c23013dfbb
Ivan Bilozerov <vaniabelozerov@gmail.com>
2021-03-23 17:07:53 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const elements = document.querySelectorAll('.tree ul');
+
+elements.forEach(element => {
+  const span = document.createElement('span');
+  const listName = element.previousSibling.textContent;
+
+  span.innerText = listName;
+  element.previousSibling.replaceWith(span);
+
+  span.onclick = () => {
+    const nextSibling = span.nextElementSibling;
+
+    if (nextSibling.style.display === 'none') {
+      nextSibling.style.display = 'block';
+    } else {
+      nextSibling.style.display = 'none';
+    }
+  };
+});
