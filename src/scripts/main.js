ff8713eae1e915ded2446b9e46b820f88c2db952
Ihor Drul <druleger@gmail.com>
2021-03-25 11:31:18 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const lists = document.querySelectorAll('ul');
+let show = true;
+
+for (let i = 0; i < lists.length; i++) {
+  const span = document.createElement('span');
+
+  span.textContent = lists[i].previousSibling.textContent;
+  lists[i].previousSibling.replaceWith(span);
+
+  span.addEventListener('click', () => {
+    show
+      ? span.nextElementSibling.style.display = 'none'
+      : span.nextElementSibling.style.display = 'block';
+    show = !show;
+  });
+}
