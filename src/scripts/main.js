220e78f2d99d6735ab4dd2ce36acd219d771716c
Artem Maksimikhin <uanov86@gmail.com>
2021-03-25 17:02:02 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const lists = document.querySelectorAll('.tree ul');
+
+lists.forEach(element => {
+  const span = document.createElement('span');
+
+  span.innerText = element.previousSibling.textContent;
+  element.previousSibling.replaceWith(span);
+
+  const nextElement = span.nextElementSibling;
+
+  span.onclick = () => {
+    nextElement.hidden = nextElement.hidden === false;
+  };
+});
