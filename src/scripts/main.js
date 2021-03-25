c2008d2e37eac3575295a308cc1eee46db65643c
Onishchuk <onishchuk.danil@gmail.com>
2021-03-25 13:30:02 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const changer = function(e) {
+  const target = e.target;
+
+  e.preventDefault();
+
+  if (target.nextElementSibling.style.display !== 'none') {
+    target.nextElementSibling.style.display = 'none';
+  } else {
+    target.nextElementSibling.style.display = 'block';
+  }
+};
+
+const list = document.querySelectorAll('ul');
+
+for (let i = 0; i < list.length; i++) {
+  const span = document.createElement('span');
+
+  span.textContent = list[i].previousSibling.textContent;
+  list[i].previousSibling.replaceWith(span);
+  span.addEventListener('click', changer);
+}
