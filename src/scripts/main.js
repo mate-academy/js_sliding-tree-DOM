5945033b012542d75703be4cb4831084c4aebd63
Inna Sorochynska <innasoro@gmail.com>
2021-03-25 20:27:43 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const lists = document.querySelectorAll('ul');
+const tree = document.querySelector('.tree');
+
+for (const list of lists) {
+  if (list.className !== 'tree') {
+    const item = list.closest('li');
+    const wrapper = document.createElement('span');
+
+    item.prepend(wrapper);
+    wrapper.append(wrapper.nextSibling);
+  }
+}
+
+tree.addEventListener('click', (e) => {
+  if (e.target.matches('span')) {
+    const toHide = e.target.nextSibling;
+
+    if (toHide.style.display === 'none') {
+      toHide.style.display = '';
+    } else {
+      toHide.style.display = 'none';
+    }
+  }
+});
