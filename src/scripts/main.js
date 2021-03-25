6afb236df6c09fe9cb65d6967840a4ba231e9e82
Julia <mjulia1794@gmail.com>
2021-03-26 01:10:27 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write code here
+const tree = document.querySelectorAll('.tree ul');
+
+for (const item of tree) {
+  const span = document.createElement('span');
+
+  span.textContent = item.previousSibling.textContent;
+  item.previousSibling.replaceWith(span);
+
+  span.onclick = () => {
+    if (span.nextSibling.style.display === 'none') {
+      span.nextSibling.style.display = 'block';
+    } else {
+      span.nextSibling.style.display = 'none';
+    }
+  };
+}
