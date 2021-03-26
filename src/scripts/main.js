a571abec7f4d0c450f204c05b16504dc7b2c845d
Svyat Krit <krit.mgmn@gmail.com>
2021-03-26 12:20:41 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const lists = [...document.querySelectorAll('ul')];
+
+for (let i = 0; i < lists.length; i++) {
+  if (lists[i].className === 'tree') {
+    continue;
+  }
+
+  const title = lists[i].previousSibling;
+  const titleText = lists[i].previousSibling.textContent.trim();
+
+  const newTitle = document.createElement('span');
+
+  newTitle.textContent = titleText;
+
+  lists[i].parentElement.removeChild(title);
+  lists[i].parentElement.prepend(newTitle);
+}
+
+document.addEventListener('click', (eventOne) => {
+  const item = eventOne.target;
+
+  if (item.tagName !== 'SPAN') {
+    return;
+  }
+
+  const display = item.nextSibling.style.display;
+
+  item.nextSibling.style.display = display === 'none' ? 'block' : 'none';
+});
