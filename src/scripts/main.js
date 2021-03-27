6a4452608c37494cf132fd615fe7bcae16de7b28
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-03-27 20:22:47 +0200

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+function slidingTree() {
+  const fruits = document.querySelectorAll('li');
+  const tree = document.querySelector('.tree');
+
+  for (const li of fruits) {
+    const elementSpan = document.createElement('span');
+
+    li.prepend(elementSpan);
+    elementSpan.append(elementSpan.nextSibling);
+  };
+
+  tree.onclick = function(click) {
+    if (click.target.tagName !== 'SPAN') {
+      return null;
+    }
+
+    const childrenContainer = click.target.parentNode.querySelector('ul');
+
+    if (!childrenContainer) {
+      return null;
+    }
+
+    childrenContainer.hidden = !childrenContainer.hidden;
+  };
+}
+
+slidingTree();
