c1f49006cf9cac1cb8b4e7003de25adb43e77b1a
Makhmut <grupry@gmail.com>
2021-03-28 20:42:06 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const activeBranch = document.querySelectorAll('li ul');
+
+activeBranch.forEach(element => {
+  const listTitle = document.createElement('span');
+
+  listTitle.textContent = element.previousSibling.textContent.trim();
+  element.previousSibling.replaceWith(listTitle);
+
+  listTitle.addEventListener('click', function() {
+    listTitle.nextElementSibling.hidden = !listTitle.nextElementSibling.hidden;
+  });
+});
