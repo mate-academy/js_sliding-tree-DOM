4ea6d458b0c45270034008d2897445c7e711eed7
Taras Chaban <tarekchaban@gmail.com>
2021-03-28 16:25:36 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const elements = document.querySelectorAll('.tree ul');
+
+for (const element of elements) {
+  const span = document.createElement('span');
+
+  span.textContent = element.previousSibling.textContent;
+  element.previousSibling.replaceWith(span);
+
+  span.onclick = () => {
+    span.nextElementSibling.hidden = !span.nextElementSibling.hidden;
+  };
+};
