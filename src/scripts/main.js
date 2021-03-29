f39a80d339a24ac8236a40597c7aef13f560e93b
Ihor Drul <druleger@gmail.com>
2021-03-29 15:44:48 +0300

    Changed for loop to for-of loop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,11 @@
 
 const lists = document.querySelectorAll('ul');
 
-for (let i = 0; i < lists.length; i++) {
+for (const item of lists) {
   const span = document.createElement('span');
 
-  span.textContent = lists[i].previousSibling.textContent;
-  lists[i].previousSibling.replaceWith(span);
+  span.textContent = item.previousSibling.textContent;
+  item.previousSibling.replaceWith(span);
 
   span.addEventListener('click', () => {
     span.nextElementSibling.hidden = !span.nextElementSibling.hidden;
