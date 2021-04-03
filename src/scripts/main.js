4a5d5a0984524dabeb0e02edf17bd6deaf9e10c1
dinexpod <work.toxa@gmail.com>
2021-04-04 00:28:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liElements = tree.querySelectorAll('li');
+
+liElements.forEach(li => {
+  if (li.childElementCount > 0) {
+    const newNode = document.createElement('span');
+
+    newNode.textContent = li.firstChild.data.toString();
+
+    li.replaceChild(newNode, li.childNodes[0]);
+  }
+});
+
+tree.addEventListener('click', e => {
+  const ulElement = e.target.parentNode.querySelector('ul');
+
+  for (const child of ulElement.children) {
+    child.hidden = !child.hidden;
+  }
+});
