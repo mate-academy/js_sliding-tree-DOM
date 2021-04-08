ca51ec5acbb77f83614719a900290d0614869a88
Alex Fedorchenko <mralex.fed@gmail.com>
2021-04-08 13:14:28 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', e => {
+  const item = e.target;
+
+  if (item.tagName !== 'LI') {
+    return;
+  }
+  item.children[0].hidden = !item.children[0].hidden;
+});
