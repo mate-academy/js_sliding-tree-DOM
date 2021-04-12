c94434862b23de0695101ecd537a6a007db8ce2a
Alex Fedorchenko <mralex.fed@gmail.com>
2021-04-12 22:56:59 +0300

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,36 @@
 
 const list = document.querySelector('.tree');
 
+const spanIn = (node) => {
+  const liArr = node.querySelectorAll('li');
+
+  for (let i = 0; i < liArr.length; i++) {
+    const span = document.createElement('span');
+
+    liArr[i].prepend(span);
+    span.append(span.nextSibling);
+
+    if (liArr[i].children.length > 0) {
+      spanIn(liArr[i].firstChild);
+    } else {
+      const text = liArr[i].innerText;
+
+      liArr[i].innerText = null;
+
+      liArr[i].insertAdjacentHTML('beforeend', `
+  <span>${text}</span>
+  `);
+    }
+  }
+};
+
+spanIn(list);
+
 list.addEventListener('click', e => {
   const item = e.target;
 
-  if (item.tagName !== 'LI') {
+  if (item.tagName !== 'SPAN') {
     return;
   }
-  item.children[0].hidden = !item.children[0].hidden;
+  item.nextSibling.hidden = !item.nextSibling.hidden;
 });
