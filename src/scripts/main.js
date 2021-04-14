8deef979e9a9886afa5865eb4463aced4433fe27
Mariia Kornieva <mariiakornieva@gmail.com>
2021-04-14 08:42:14 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const treeItems = tree.querySelectorAll('li');
+
+treeItems.forEach(item => {
+  if (item.firstChild.nextElementSibling) {
+    const span = document.createElement('span');
+
+    span.textContent = item.firstChild.textContent.trim();
+    item.firstChild.replaceWith(span);
+  }
+});
+
+tree.addEventListener('click', ev => {
+  const headline = ev.target.closest('span');
+
+  if (!headline || !tree.contains(headline)) {
+    return;
+  }
+
+  headline.nextElementSibling.hidden = !headline.nextElementSibling.hidden;
+});
