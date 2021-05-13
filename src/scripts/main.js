6c1bdbfc8d40f9daf0d6ced6986381d71a5963cc
Vlad Trundenko <vlad.trundenko@gmail.com>
2021-05-14 00:07:24 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+for (let i = 0; i < li.length; i++) {
+  if (li[i].children.length > 0) {
+    const span = document.createElement('span');
+
+    span.innerText = li[i].firstChild.textContent.trim();
+
+    span.addEventListener('click', e => {
+      span.nextSibling.style.display = 'none';
+    });
+
+    li[i].replaceChild(span, li[i].firstChild);
+  }
+}
