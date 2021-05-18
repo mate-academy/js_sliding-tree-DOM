35201c8147a81f013f8a70a19897eafaffe51211
romandubovskyi <rdubowskij@gmail.com>
2021-05-18 16:10:27 +0300

    created sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listPoints = document.querySelectorAll('li');
+
+wrapInSpan(listPoints);
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName.toLowerCase() !== 'span') {
+    return;
+  }
+
+  const innerList = e.target.parentNode.querySelector('ul');
+
+  if (innerList) {
+    innerList.style.display = innerList.style.display === 'none'
+      ? 'block'
+      : 'none';
+  }
+});
+
+function wrapInSpan(items) {
+  items.forEach((element) => {
+    const newNode = document.createElement('span');
+
+    newNode.innerText = element.firstChild.nodeValue.trim();
+    element.removeChild(element.firstChild);
+    element.insertAdjacentElement('afterbegin', newNode);
+  });
+}
