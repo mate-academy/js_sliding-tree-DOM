4ca05a9943d37286f11a91ddbb91e08089d6ccc1
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-05-26 12:50:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', e => {
+  if (e.target.tagName === 'LI') {
+    for (const child of e.target.children) {
+      child.style.display = (child.style.display === 'none') ? '' : 'none';
+    }
+  }
+});
