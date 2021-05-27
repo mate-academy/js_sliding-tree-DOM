d7125a7d71bf02c1dee0b402ee20a2aadbadeeac
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-05-27 14:10:30 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,20 @@
 'use strict';
 
+const list = document.querySelectorAll('li');
+
+list.forEach((item) => {
+  const span = document.createElement('span');
+
+  span.innerText = item.firstChild.textContent.trim();
+  item.replaceChild(span, item.firstChild);
+});
+
 document.addEventListener('click', e => {
-  if (e.target.tagName === 'LI') {
-    for (const child of e.target.children) {
-      child.style.display = (child.style.display === 'none') ? '' : 'none';
+  if (e.target.tagName === 'SPAN') {
+    const hide = e.target.parentNode.querySelector('ul');
+
+    if (hide) {
+      hide.style.display = (hide.style.display === 'none') ? '' : 'none';
     }
   }
 });
