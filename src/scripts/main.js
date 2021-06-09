4f3487ceadcfa7f1325cb9d0b36eafdbd7f67bbf
Eugene Khablenko <eugene.khablenko@gmail.com>
2021-06-09 13:21:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const list = document.querySelectorAll('li');
+
+for (const item of [...list]) {
+  const element = document.createElement('span'); // created empty element span
+
+  const [ text ] = item.childNodes; // text from the list of the childNodes;
+
+  element.append(text);
+  item.prepend(element);
+}
+
+tree.addEventListener('click', (evnt) => {
+  const target = evnt.target;
+  const check = document.elementFromPoint(evnt.clientX, evnt.clientY);
+
+  if (check.tagName === 'SPAN') {
+    if (target.nextElementSibling === null) {
+      return;
+    }
+
+    if (target.nextElementSibling.nodeName === 'UL') {
+      // console.log(target.nextElementSibling.style.display = 'none')
+      target.nextElementSibling.style.display
+      = (target.nextElementSibling.style.display === '')
+          ? target.nextElementSibling.style.display = 'none'
+          : target.nextElementSibling.style.display = '';
+    }
+  }
+});
