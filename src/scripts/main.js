1164d0b95a30febde1c8ef57b0e5fc17c3d59ca4
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-05 18:05:08 +0300

    Implemented sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const headers = document.querySelectorAll('li');
+
+for (const listItem of headers) {
+  if (listItem.childElementCount > 0) {
+    const span = document.createElement('span');
+
+    listItem.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+tree.addEventListener('click', e => {
+  const span = e.target.closest('span');
+
+  if (!span) {
+    return;
+  }
+
+  span.nextElementSibling.hidden = !span.nextElementSibling.hidden;
+});
