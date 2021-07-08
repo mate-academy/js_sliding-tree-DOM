6698b488bc2e9e9e5b68074096527e3f563e3e17
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-07-08 19:34:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach(item => {
+  if (item.children.length > 0) {
+    const textInfo = item.childNodes[0];
+    const restOfList = item.childNodes[1];
+    const wraper = document.createElement('span');
+
+    wraper.classList.add('heading');
+    wraper.append(textInfo);
+
+    item.innerHTML = '';
+    item.append(wraper);
+    item.append(restOfList);
+  }
+});
+
+list.addEventListener('click', (e) => {
+  const head = e.target.closest('.heading');
+
+  if (!head || !list.contains(head)) {
+    return;
+  }
+
+  head.nextSibling.hidden = !head.nextSibling.hidden;
+});
