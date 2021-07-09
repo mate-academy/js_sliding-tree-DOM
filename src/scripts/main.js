18980868dece35325698fdd1b79503384138d7c3
korzh <korzh.alexey@yahoo.com>
2021-07-09 12:40:00 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+
+for (const item of listItems) {
+  if (item.children.length) {
+    const span = document.createElement('span');
+    const rest = item.childNodes[1];
+
+    span.append(item.childNodes[0]);
+    item.append(span, rest);
+  };
+};
+
+tree.addEventListener('click', e => {
+  const heading = e.target.closest('span');
+
+  if (!heading || !tree.contains(heading)) {
+    return false;
+  } else {
+    heading.nextSibling.hidden = !heading.nextSibling.hidden;
+  };
+});
