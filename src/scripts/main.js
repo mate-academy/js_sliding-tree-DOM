eead92127b1b781528bb083da65ba73ee319437f
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2021-07-13 05:07:38 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', () => {
+  if (event.target.children.length === 1) {
+    for (const li of event.target.children) {
+      if (li.style.display !== 'none') {
+        li.style.display = 'none';
+      } else {
+        li.style.display = 'block';
+      }
+    }
+  }
+});
