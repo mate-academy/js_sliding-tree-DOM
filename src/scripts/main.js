f75f57521f5457b70f90cdeda19fd998a00b4871
herkoss <herkodmytro@gmail.com>
2021-07-15 13:38:25 +0300

    Slider created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+
+const a = 10;
