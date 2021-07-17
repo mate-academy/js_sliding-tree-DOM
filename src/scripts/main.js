84acc9af73752e88a7c500e1e86032d6bad28fb5
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2021-07-17 16:04:12 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,24 @@
 'use strict';
 
 const tree = document.querySelector('.tree');
+const listItmes = tree.querySelectorAll('li');
+
+for (const item of listItmes) {
+  const span = document.createElement('span');
+
+  span.innerText = item.firstChild.textContent;
+  item.replaceChild(span, item.firstChild);
+}
 
 tree.addEventListener('click', () => {
-  if (event.target.children.length === 1) {
-    for (const li of event.target.children) {
-      if (li.style.display !== 'none') {
-        li.style.display = 'none';
+  if (event.target.tagName === 'SPAN') {
+    const ul = event.target.parentNode.querySelector('ul');
+
+    if (ul) {
+      if (ul.style.display !== 'none') {
+        ul.style.display = 'none';
       } else {
-        li.style.display = 'block';
+        ul.style.display = 'block';
       }
     }
   }
