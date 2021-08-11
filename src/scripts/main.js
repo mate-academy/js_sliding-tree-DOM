4da7e5412de137892882edd6601987c9fc2e12ef
Raz-Dva <my.poshta.goncharov@gmail.com>
2021-08-11 23:35:03 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const UL = document.querySelector('.tree');
+const LI = document.querySelectorAll('li');
+
+const headersLI = [...LI].filter((el) => {
+  return [...el.children].some((child) => {
+    return child.tagName === 'UL';
+  });
+});
+
+headersLI.forEach((li) => {
+  const SPAN = document.createElement('span');
+
+  SPAN.textContent = li.firstChild.textContent;
+
+  // console.log(spanHeight);
+  li.firstChild.remove();
+  li.prepend(SPAN);
+});
+
+UL.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN' && e.target.nextElementSibling !== null) {
+    e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+  }
+});
