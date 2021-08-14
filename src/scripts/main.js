b47ef4ef1755b821fada23797027461c60df8dd0
Larin Dmytro <dimiklarin420@gmail.com>
2021-08-14 12:42:22 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
-'use strict';
+'use strict'
 
-// write code here
+const treeList = document.querySelector('.tree');
+const treeItems = treeList.querySelectorAll('li');
+
+for (const element of treeItems) {
+  const span = document.createElement('span');
+
+  if (element.childElementCount > 0) {
+    element.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+treeList.onclick = function(e) {
+  if (e.target.tagName === 'SPAN') {
+    e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+  }
+}
