92d8009d1c9749404c0fd7dced9ac0cf07da5224
Oleksandr Hrachov <ol.grachov@gmail.com>
2021-08-31 12:43:47 +0300

    SOLUTION

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+const itemsLi = document.querySelectorAll('li');
+
+const itemsUl = [...itemsLi].filter(item => {
+  return [...item.children].some((child) => {
+    return child.tagName === 'UL';
+  });
+});
+
+itemsUl.forEach(item => {
+  const headline = item.firstChild;
+  const spanElem = document.createElement('span');
+
+  item.prepend(spanElem);
+  spanElem.append(headline);
+});
+
+list.addEventListener('click', e => {
+  const targetElem = e.target.closest('span');
+
+  if (targetElem) {
+    const targetElemNextSiblingStyle
+      = getComputedStyle(targetElem.nextElementSibling);
+
+    targetElem.nextElementSibling.style.display
+      = (targetElemNextSiblingStyle.display === 'block') ? 'none' : 'block';
+  }
+});
