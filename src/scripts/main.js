406981dec1df7555ee3b9c39c9cab6265456cc65
Oleksandr Hrachov <ol.grachov@gmail.com>
2021-09-02 10:44:40 +0300

    SOLUTION - 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,28 +4,21 @@ const list = document.querySelector('.tree');
 
 const itemsLi = document.querySelectorAll('li');
 
-const itemsUl = [...itemsLi].filter(item => {
-  return [...item.children].some((child) => {
-    return child.tagName === 'UL';
-  });
-});
+[...itemsLi].forEach(item => {
+  if (item.children.length > 0) {
+    const headline = item.firstChild;
+    const spanElem = document.createElement('span');
 
-itemsUl.forEach(item => {
-  const headline = item.firstChild;
-  const spanElem = document.createElement('span');
-
-  item.prepend(spanElem);
-  spanElem.append(headline);
+    item.prepend(spanElem);
+    spanElem.append(headline);
+  }
 });
 
 list.addEventListener('click', e => {
   const targetElem = e.target.closest('span');
 
   if (targetElem) {
-    const targetElemNextSiblingStyle
-      = getComputedStyle(targetElem.nextElementSibling);
-
-    targetElem.nextElementSibling.style.display
-      = (targetElemNextSiblingStyle.display === 'block') ? 'none' : 'block';
+    targetElem.nextElementSibling.hidden
+      = !targetElem.nextElementSibling.hidden;
   }
 });
