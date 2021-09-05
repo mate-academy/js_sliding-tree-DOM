9abb1f82cbf33b276557e34d838e3ef7c4c20cbe
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-09-05 11:04:45 +0300

    Task Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('li')];
+
+list.forEach(element => {
+  const nodes = [...element.childNodes];
+
+  nodes.forEach(item => {
+    if (item.tagName === 'UL') {
+      const span = document.createElement('span');
+      element.prepend(span);
+      span.append(span.nextSibling)
+    }
+  });
+
+  document.addEventListener('click', event => {
+    if (event.target.tagName === 'SPAN') {
+      const node = [...event.target.parentNode.childNodes];
+      for (let i = 0; i < node.length; i++) {
+        if (node[i].tagName === 'UL' && node[i].hidden === false) {
+          node[i].hidden = true;
+        } else {
+          node[i].hidden = false;
+        }
+      }
+    } else {
+      return;
+    }
+  })
+});
