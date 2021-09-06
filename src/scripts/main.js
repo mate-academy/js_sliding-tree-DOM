7bdc1c7512791be401e2cc9dd148ff2e27320915
Alex <alex.krynytskyi@gmail.com>
2021-09-06 22:50:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const elementUl = [...document.querySelectorAll('li')];
+
+for (const elem of elementUl) {
+  const span = document.createElement('span');
+
+  elem.prepend(span);
+  span.append(span.nextSibling);
+
+  span.addEventListener('click', click => {
+    const item = click.target.closest('span');
+
+    if (item) {
+      const title = click.target.parentElement.children[1];
+
+      title.hidden = !title.hidden;
+    };
+  });
+};
