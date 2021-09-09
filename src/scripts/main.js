c476f16097d426abd15c83a2faf804f3fdd60147
Olesya Parfylo <olesya.parfylo@gmail.com>
2021-09-09 14:46:43 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const listOfLi = document.querySelectorAll('li');
+
+listOfLi.forEach(item => {
+  if (item.children[0] !== undefined) {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+    span.append(span.nextSibling);
+
+    span.addEventListener('click', () => {
+      const itemLi = item.querySelectorAll('li');
+
+      itemLi.forEach(li => {
+        if (li.style.display === 'none') {
+          li.style.display = 'block';
+        } else {
+          li.style.display = 'none';
+        }
+      });
+    });
+  }
+});
