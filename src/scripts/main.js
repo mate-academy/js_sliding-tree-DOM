c72b33f276eb93990fe379c66bdc60c1a98f3ad4
Tykhon Shuliak <tiandzh2013@gmail.com>
2021-09-29 15:13:42 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+for (const li of tree.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', e => {
+  const item = e.target;
+
+  if (item.tagName === 'SPAN') {
+    item.nextSibling.hidden = !item.nextSibling.hidden;
+  }
+});
