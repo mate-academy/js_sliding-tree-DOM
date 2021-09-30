e9a1022caea0c8c4c8cfd25a67251dcce979bcf1
MaxStets <weawer458@gmail.com>
2021-09-30 23:42:15 +0300

    Solution -- 'Sliding-tree-DOM'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const subList = tree.querySelectorAll('ul');
+
+subList.forEach(el => {
+  const span = document.createElement('span');
+
+  el.before(span);
+  span.append(span.previousSibling);
+  el.style.display = 'none';
+
+  span.addEventListener('click', () => {
+    el.style.display === 'none'
+      ? el.style.display = 'block'
+      : el.style.display = 'none';
+  });
+});
