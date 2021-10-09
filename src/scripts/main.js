9f8e1ad819160bbe4cdc3f160c9132874cd31294
Roman Prokopiv <rprokopiv@gmail.com>
2021-10-09 10:12:11 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const liRows = document.querySelectorAll('li');
+
+liRows.forEach((row) => {
+  const names = row.childNodes[0].textContent;
+  const spanTeg = document.createElement('span');
+
+  row.childNodes[0].textContent = '';
+  spanTeg.textContent = names;
+
+  if (row.children.length === 0) {
+    row.append(spanTeg);
+  } else {
+
+  }
+  row.children[0].before(spanTeg);
+});
+
+const namesSpan = document.querySelectorAll('span');
+
+namesSpan.forEach((nameSpan) => {
+  nameSpan.addEventListener('click', (events) => {
+    if (nameSpan.parentElement.children.length > 1) {
+      nameSpan.parentElement.lastElementChild.hidden
+        = !nameSpan.parentElement.lastElementChild.hidden;
+    }
+  });
+});
