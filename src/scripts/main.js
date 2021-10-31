6e5db5bd31c4baf3fe75d89d719a767874e91434
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-10-31 12:50:18 +0200

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (event) => {
+  const item = event.target;
+
+  if (!item || !tree.contains(item)) {
+    return;
+  }
+
+  if (!item.classList.contains('hidden')) {
+    [...item.children].map(elem => elem.hidden = true);
+    item.classList.add('hidden');
+  } else {
+    [...item.children].map(elem => elem.hidden = false);
+    item.classList.remove('hidden');
+  };
+
+});
