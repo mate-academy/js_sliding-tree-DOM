e0d048f920660a7127ad5d1be0e73fcd187cdf54
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-15 21:05:47 +0200

    Create a table that closes itself

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+
+function addHeadlines() {
+  for (const listItem of listItems) {
+    if (listItem.childElementCount > 0) {
+      const spanHeadline = document.createElement('span');
+
+      spanHeadline.textContent = listItem.firstChild.textContent;
+      spanHeadline.className = 'headline';
+
+      listItem.firstChild.replaceWith(spanHeadline);
+    }
+  }
+}
+
+addHeadlines();
+
+tree.addEventListener('click', (e) => {
+  const selectedItem = e.target;
+
+  if (!selectedItem.classList.contains('headline')) {
+    return;
+  }
+
+  const list = selectedItem.nextElementSibling;
+
+  list.hidden = !list.hidden;
+});
