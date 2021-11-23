8e5cfc1c8c3464cef3ef4f343a4d73b90f520f94
Andriy Shevtsiv <andriy.shevtsiv@gmail.com>
2021-11-23 10:57:07 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
 // write code here
+
+const elements = document.querySelectorAll('li');
+
+for (const element of elements) {
+  if (element.childElementCount < 1) {
+    continue;
+  }
+
+  element.insertAdjacentHTML('afterbegin',
+    `<span>
+      ${element.childNodes[0].textContent}
+    </span>`
+  );
+
+  element.childNodes[1].textContent = '';
+}
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const clickPoint = event.target.closest('span');
+
+  if (!clickPoint) {
+    return;
+  }
+
+  const nestedList = clickPoint.parentElement.children[1].children;
+
+  for (const element of nestedList) {
+    if (element.hidden === false) {
+      element.hidden = true;
+    } else {
+      element.hidden = false;
+    }
+  }
+});
