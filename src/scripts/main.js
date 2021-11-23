f12a8058ec0a29f7a577a7b7b6949c2889c32d0f
FocusHuK <kirill919191kirill@gmail.com>
2021-11-23 16:19:56 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liAll = document.querySelectorAll('li');
+
+for (const li of liAll) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+};
+
+tree.onclick = function(events) {
+  const childrenContainer = events.target.parentNode.querySelector('ul');
+
+  if (!childrenContainer) {
+    return;
+  }
+
+  childrenContainer.hidden = !childrenContainer.hidden;
+};
