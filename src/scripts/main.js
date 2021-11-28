de3736468ee231bc50969f4228efc9bbf3ce4b7d
Dina Samoilova <s13.dina@gmail.com>
2021-11-28 22:36:14 +0200

    sliding tree solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const ulList = document.querySelectorAll('ul');
+
+for (let i = 0; i < ulList.length; i++) {
+  if (ulList[i].parentNode.tagName.toLowerCase() === 'li') {
+    ulList[i].parentNode.innerHTML = ulList[i].parentNode.innerHTML.replace(
+      /(\w+)\s(\w+)(\n+)|(\w+)(\n+)/gm, '<span class="title">$&</span>'
+    );
+  }
+}
+
+document.body.addEventListener('click', (click) => {
+  const item = click.target.closest('.title');
+
+  if (!item) {
+    return;
+  }
+
+  if (item.nextElementSibling.getAttribute('hidden') === null
+    || item.nextElementSibling.getAttribute('hidden') === true) {
+    item.nextElementSibling.setAttribute('hidden', true);
+  } else {
+    item.nextElementSibling.removeAttribute('hidden');
+  }
+});
