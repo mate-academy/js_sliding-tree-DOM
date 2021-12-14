70442c6e63d163ab3bec3d2f1e5bb5e2d6e123a6
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-12-14 15:51:43 +0200

    Solution js_sliding-tree-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+for (const item of li) {
+  if (item.children.length > 0) {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const item = e.target.nextSibling;
+
+  if (item.hidden === true) {
+    item.hidden = false;
+  } else {
+    item.hidden = true;
+  }
+});
