0860ab333b6b06a110b1b497a4562104f87b1903
Marina Gerasimenko <marigera1917@gmail.com>
2021-12-23 21:30:57 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const list = document.querySelector('.tree');
+
+for (const li of list.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+list.addEventListener('click', (e) => {
+  const container = e.target.parentNode.querySelector('ul');
+
+  container.hidden = !container.hidden;
+});
