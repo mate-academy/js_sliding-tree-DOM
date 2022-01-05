cf7aa5ed2f02afc69245718062ef4a815cab45f5
E-Sharpilo <evgeniy.sharpilo@gmail.com>
2022-01-05 12:47:52 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const list = document.querySelectorAll('li');
+
+list.forEach(item => {
+  if (item.children.length) {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+    span.append(span.nextSibling);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  const element = e.target.closest('span').nextSibling;
+
+  element.hidden = !element.hidden;
+});
