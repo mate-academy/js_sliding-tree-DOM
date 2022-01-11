21dafd7f49a40e96f8f001052aba21c0cb0fa52f
Pavlo Khashchevskyi <PikiMapo92@gmail.com>
2022-01-11 15:25:00 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', e => {
+  const item = e.target.closest('li');
+
+  if (!item.children.length || e.target !== item) {
+    return;
+  }
+
+  if (!item.children[0].hasAttribute('hidden')) {
+    item.children[0].setAttribute('hidden', 'true');
+  } else {
+    item.children[0].removeAttribute('hidden');
+  }
+});
