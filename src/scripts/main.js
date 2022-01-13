1abf66f96c32574c824d3a7f94bb138950382706
Dmytro Brushnivskyi <dimalvivatc@gmail.com>
2022-01-13 19:08:43 +0200

    Solution I Guess))

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const li = document.querySelectorAll('li');
+
+for (const item of li) {
+  if (item.children.length !== 0) {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+tree.addEventListener('click', function hideAndShow(action) {
+  const target = action.target.closest('span');
+
+  if (target.nextSibling.style.display === 'none') {
+    target.nextSibling.style.display = 'block';
+  } else {
+    target.nextSibling.style.display = 'none';
+  }
+});
