4a44dd027520011fbde70611bb19136174f92585
Ivan Shulhan <92580512+IvanShulhan@users.noreply.github.com>
2022-02-03 20:21:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+
+for (const item of items) {
+  const span = document.createElement('span');
+  const text = item.firstChild.textContent.trim();
+
+  span.innerText = text;
+  item.firstChild.textContent = null;
+
+  item.prepend(span);
+}
+
+document.body.addEventListener('click', listEvent => {
+  const listItem = listEvent.target.closest('span');
+
+  if (!listItem || !listItem.nextElementSibling) {
+    return;
+  }
+
+  const unList = listItem.nextElementSibling;
+
+  unList.hidden ? unList.hidden = false : unList.hidden = true;
+});
