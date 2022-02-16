a740574340a2cad9950a0fb75f39670972a275a7
Roman Kovalchuk <roma220702@gmail.com>
2022-02-16 18:01:15 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', (e) => {
+  const item = e.target.closest('li');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  for (const i of item.children) {
+
+    if (i.hidden === true) {
+      i.hidden = false;
+    } else {
+      i.hidden = true;
+    }
+  }
+});
