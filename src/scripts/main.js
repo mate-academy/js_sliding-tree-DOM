03925c6010812bff0e5431f45dc6090dce4b8031
Dmytro Bychkivskyi <dbychkivskyi@cccis.com>
2022-03-24 19:09:18 +0200

    Finish hw.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('li').forEach(item => {
+  if (item.children.length) {
+    const title = item.childNodes[0].textContent;
+
+    item.childNodes[0].textContent = '';
+    item.insertAdjacentHTML('afterbegin', `<span>${title}</span>`);
+  }
+});
+
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', (e) => {
+  const item = e.target;
+
+  if (item.tagName !== 'SPAN') {
+    return;
+  }
+
+  item.closest('li').querySelector('ul').hidden
+    = !item.closest('li').querySelector('ul').hidden;
+});
