cb555632f88a89bf43a056746dd12e2f633df597
Mykhailo Ianush <yanushmisha@gmail.com>
2022-03-27 00:23:08 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', e => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  e.target.clicked = !e.target.clicked;
+
+  if (e.target.clicked) {
+    e.target.nextElementSibling.style.display = 'none';
+    e.target.style.fontWeight = 'bold';
+  } else {
+    e.target.nextElementSibling.style.display = '';
+    e.target.style.fontWeight = 'normal';
+  }
+});
