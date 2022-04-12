525673ba0d105570675804997d36f12373d6ef68
Alex Zhidkov <sashazudkov.@gmail.com>
2022-04-12 17:55:50 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('ul');
+
+for (const item of list) {
+  const span = document.createElement('span');
+
+  span.innerHTML = item.previousSibling.textContent.trim();
+  item.previousSibling.replaceWith(span);
+
+  span.addEventListener('click', () => {
+    span.nextElementSibling.hidden = !span.nextElementSibling.hidden;
+  });
+}
