bb200edcfd4cf296bf571664112cb4228b6f9f99
Max Shylenko <maxshylenko@gmail.com>
2022-04-20 18:10:39 +0200

    sliding tree DOM is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+
+for (const item of listItems) {
+  const text = item.firstChild;
+  const header = document.createElement('span');
+
+  header.innerText = `${text.nodeValue.trim()}`;
+  item.prepend(header);
+
+  text.remove();
+}
+
+list.addEventListener('click', e => {
+  const headline = e.target.closest('span');
+
+  if (!headline) {
+    return;
+  }
+
+  const firstList = headline.parentElement.querySelector('ul');
+
+  if (!firstList.hidden) {
+    firstList.setAttribute('hidden', true);
+  } else {
+    firstList.removeAttribute('hidden');
+  }
+});
