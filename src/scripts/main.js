3c7af4cb7257358eb618f742cd82124ef8376f90
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-04-22 19:15:38 +0300

    js-sliding-tree-dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const itemsList = document.querySelectorAll('li');
+
+for (const item of itemsList) {
+  const span = document.createElement('span');
+
+  if (item.querySelector('ul')) {
+    span.classList.add('span__collapse');
+    span.textContent = item.firstChild.textContent;
+    item.firstChild.replaceWith(span);
+  }
+}
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', e => {
+  const item = e.target.closest('.span__collapse');
+
+  if (!item) {
+    return;
+  }
+
+  item.nextElementSibling.hidden = !item.nextElementSibling.hidden;
+});
