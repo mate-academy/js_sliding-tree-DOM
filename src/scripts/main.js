772d674c8977f2411325e9831faf56d85a5a30f7
Denys Lipin <lipin.transstream@gmail.com>
2022-04-28 21:56:17 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('ul');
+
+for (const item of items) {
+  if (item.className !== 'tree') {
+    const text = item.previousSibling.textContent.trim();
+    const span = document.createElement('span');
+
+    span.textContent = text;
+    item.previousSibling.replaceWith(span);
+
+    span.addEventListener('click', () => {
+      span.nextElementSibling.hidden = !span.nextElementSibling.hidden;
+    });
+  }
+}
