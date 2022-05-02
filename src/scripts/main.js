ef3949348c0cb8b517f3da7170d4129a42cce0e7
Lilia Mazur <liliamazur2508@gmail.com>
2022-05-02 16:00:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const headers = list.querySelectorAll('li');
+
+headers.forEach(li => {
+  const span = document.createElement('span');
+
+  span.append(li.firstChild);
+  li.prepend(span);
+});
+
+list.addEventListener('click', e => {
+  const item = e.target;
+  const insideList = item.parentElement.querySelector('ul');
+
+  if (!insideList || item.tagName !== 'SPAN') {
+    return;
+  }
+
+  if (insideList.hidden) {
+    insideList.removeAttribute('hidden');
+  } else {
+    insideList.hidden = `true`;
+  }
+});
