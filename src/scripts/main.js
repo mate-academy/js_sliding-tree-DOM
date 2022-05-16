afeafec5d407fc888ec0fc49e90784bc664c040c
Roman Nikitenko <Lomkaaa@ukr.net>
2022-05-16 14:36:57 +0300

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,48 +17,8 @@ itemList.forEach(item => {
       const targetUl = e.currentTarget.parentElement.querySelector('ul');
 
       targetUl.style.display = targetUl.style.display === 'none'
-      ? 'block'
-      : 'none';
+        ? 'block'
+        : 'none';
     });
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const listItems = document.querySelectorAll('li');
-//
-// listItems.forEach(item => {
-//   const list = item.querySelector('ul');
-//
-//   if (list) {
-//     const span = document.createElement('span');
-//
-//     list.style.display = 'none';
-//
-//     span.innerText = item.firstChild.textContent;
-//     item.firstChild.remove();
-//     item.prepend(span);
-//
-//     span.addEventListener('click', e => {
-//       const targetUl = e.currentTarget.parentElement.querySelector('ul');
-//
-//       targetUl.style.display = targetUl.style.display === 'none'
-//         ? 'block'
-//         : 'none';
-//     });
-//   }
-// });
