6b06f07b217500e32f19ddb4345e0139a5a3d113
Anastasiia Sydorchuk <nastia.sydorchuk@gmail.com>
2022-05-24 23:33:36 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+for (const item of items) {
+  if (item.children.length > 0) {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+    span.append(span.nextSibling);
+  }
+};
+
+tree.addEventListener('click', (browserEvent) => {
+  const item = browserEvent.target.closest('span');
+
+  if (item.nextElementSibling.style.display === 'none') {
+    item.nextElementSibling.style.display = 'block';
+  } else {
+    item.nextElementSibling.style.display = 'none';
+  }
+});
