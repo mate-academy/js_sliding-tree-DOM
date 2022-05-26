6bde3c7df55fcec6984ea4b540cfef924ab1e549
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-05-26 16:15:52 +0300

    tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write code here
+const lists = document.querySelectorAll('ul');
+
+for (const list of lists) {
+  if (list.className !== 'tree') {
+    const text = list.previousSibling.textContent.trim();
+    const span = document.createElement('span');
+
+    span.textContent = text;
+    list.previousSibling.replaceWith(span);
+
+    span.addEventListener('click', () => {
+      span.nextElementSibling.hidden = !span.nextElementSibling.hidden;
+    });
+  }
+}
