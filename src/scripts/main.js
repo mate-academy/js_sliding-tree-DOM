32b5f1388769240b27ede93571270e50b6fb777a
Dmytro Semeniuk <allesdmytro@gmail.com>
2022-06-12 10:32:24 +0300

    It works as expected, but all tests fails

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const liElements = document.querySelectorAll('li');
+
+for (const li of liElements) {
+  li.addEventListener('click', e => {
+    e.stopPropagation();
+
+    if (li.children.length > 0) {
+      const ul = li.querySelector('ul');
+
+      if (ul.style.display === 'none') {
+        ul.style.display = 'block';
+        li.style.fontWeight = 'normal';
+      } else {
+        ul.style.display = 'none';
+        li.style.fontWeight = 'bold';
+      }
+    }
+  });
+}
