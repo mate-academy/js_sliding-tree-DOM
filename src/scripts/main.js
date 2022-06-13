e0f63a92a1aa6164a728da0b0bfa4c0160346981
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-06-13 13:36:00 +0300

    first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('ul');
+
+for (const el of list) {
+  if (el.className !== 'tree') {
+    const txt = el.previousSibling.textContent.trim();
+    const span = document.createElement('span');
+
+    span.textContent = txt;
+    el.previousSibling.replaceWith(span);
+
+    span.addEventListener('click', () => {
+      span.nextElementSibling.hidden = !span.nextElementSibling.hidden;
+    });
+  }
+}
