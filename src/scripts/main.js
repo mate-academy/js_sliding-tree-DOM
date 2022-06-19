8dc30568f0804a15a310db078ad8fdef0aa6e43d
Igor Stasiv <igorstsv@gmail.com>
2022-06-19 14:47:10 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+
+[...listItems].forEach(li => {
+  if (li.firstElementChild) {
+    const title = document.createElement('span');
+
+    title.innerText = li.firstChild.textContent;
+    li.firstChild.replaceWith(title);
+
+    title.addEventListener('click', e => {
+      e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+    });
+  }
+});
