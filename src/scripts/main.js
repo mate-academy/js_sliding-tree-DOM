10f36eb88713a6f720a10a3e8f6488bd5f0fd538
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-06-20 14:52:35 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write code here
+const li = document.getElementsByTagName('li');
+
+for (let i = 0; i < li.length; i++) {
+  li[i].addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    for (let j = 0; j < li[i].children.length; j++) {
+      if (li[i].children[j].style.display === 'none') {
+        li[i].children[j].style.display = 'block';
+      } else {
+        li[i].children[j].style.display = 'none';
+      }
+    }
+  });
+}
