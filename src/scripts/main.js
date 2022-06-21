179b88b96b5682b82483830ac9e72402d04840bf
Vitalii-Heiko <papus@ukr.net.com>
2022-06-21 10:03:14 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+const lists = document.querySelector('.tree');
+const items = document.querySelectorAll('li');
+
+for (const item of items) {
+  const span = document.createElement('span');
+
+  span.textContent = item.firstChild.textContent.trim();
+  item.firstChild.replaceWith(span);
+}
+
+lists.addEventListener('click', e => {
+  if (e.target.nextSibling.hidden) {
+    e.target.nextSibling.hidden = false;
+
+    return;
+  }
+
+  e.target.nextSibling.hidden = true;
+});
