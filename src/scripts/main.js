c748b93f1873bc714e001bd6efef4bdf6ffe0599
Mikhail Shchuka <mikhail.shchuka@gmail.com>
2022-06-24 23:34:26 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const items = [...tree.querySelectorAll('li')];
+
+const createSpan = (item) => {
+  const span = document.createElement('span');
+
+  span.innerText = item.firstChild.textContent;
+
+  if (span.children.length > 0) {
+    span.className = 'item';
+  }
+
+  item.firstChild.replaceWith(span);
+};
+
+const moveList = (click) => {
+  const item = click.target.matches('.item');
+
+  if (!item) {
+    return false;
+  }
+
+  const nextList = click.target.nextElementSibling;
+
+  switch (nextList.style.display) {
+    case '':
+      nextList.style.display = 'none';
+      break;
+    case 'none':
+      nextList.style.display = '';
+      break;
+  }
+};
+
+items.map(createSpan);
+
+tree.addEventListener('click', moveList);
