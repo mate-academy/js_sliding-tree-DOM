312f11de1fd1671b09f4fe4352144ff268b358e8
Olexandr Istomin <gn.53@ukr.net>
2022-06-28 17:04:44 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', e => {
+  const targetElment = e.target.firstElementChild;
+
+  if (targetElment.tagName === 'UL') {
+    targetElment.hidden = !targetElment.hidden;
+  }
+});
