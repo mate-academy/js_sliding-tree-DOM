2ef9afeb9fab719115ad466cde6cabcff4fc25c8
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-06-29 13:16:44 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const links = document.querySelectorAll('li');
+links.forEach(link => {
+  let wrap = document.createElement("span");
+  link.insertBefore(wrap, link.firstChild);
+  wrap.appendChild(link.childNodes[1]);
+});
+
+tree.addEventListener('click', e => {
+    const clickTarget = e.target;
+
+    if (clickTarget.matches('span')) {
+        const currLi = clickTarget.closest('li');
+        const forHide = currLi.querySelector('ul');
+        if (forHide.style.display === 'none') {
+            forHide.style.display = 'block';
+        } else {
+        forHide.style.display = 'none';
+        }
+    }
+
+})
