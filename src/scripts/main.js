c785a4411e81f4405c39d3df559744e55ec35cae
Tania Troshchuk <troschuk95@gmail.com>
2022-06-30 23:25:18 +0300

    Solution Task: Sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const items = document.querySelectorAll('li');
+
+[...items].forEach(li => {
+  if (li.children.length > 0) {
+    const text = li.firstChild;
+    const span = document.createElement('span');
+
+    span.append(text);
+    li.prepend(span);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  const list = e.target;
+
+  if (list.tagName === 'SPAN') {
+    list.nextElementSibling.hidden = !list.nextElementSibling.hidden;
+  }
+});
