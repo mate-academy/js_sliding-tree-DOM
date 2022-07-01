89a027db32b9b136ce2f9cbe0a8c089207f2fd20
Anton Ustavshchykov <unton84@gmail.com>
2022-07-01 13:00:47 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const main = document.querySelector('.tree');
+const list = document.querySelectorAll('li');
+
+for (const element of list) {
+  if (element.children.length > 0) {
+    const span = document.createElement('span');
+
+    span.appendChild(element.firstChild.cloneNode(true));
+    element.replaceChild(span, element.firstChild);
+  }
+}
+
+main.addEventListener('click', (e) => {
+  const item = e.target;
+
+  if (item.tagName === 'SPAN') {
+    if (item.nextSibling.hidden === true) {
+      item.nextSibling.hidden = false;
+    } else {
+      item.nextSibling.hidden = true;
+    }
+  }
+});
