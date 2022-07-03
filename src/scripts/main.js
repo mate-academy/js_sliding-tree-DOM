f8e7e488195fa8eaaa6884fb7ead37a6161b5f36
Dmytro Lebedchenko <lebedchenkodmytro@gmail.com>
2022-07-03 20:14:55 +0300

    Sliding tree list was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const listItem = document.querySelectorAll('li');
+
+listItem.forEach(item => {
+  const spanWrapper = document.createElement('span');
+
+  item.insertAdjacentElement('afterbegin', spanWrapper);
+  spanWrapper.prepend(spanWrapper.nextSibling);
+});
+
+list.addEventListener('click', (e) => {
+  const element = e.target.closest('span');
+  const elementChildren = element.nextSibling;
+
+  if (!element || !list.contains(element)) {
+    return;
+  }
+
+  if (!elementChildren.hidden) {
+    elementChildren.hidden = true;
+  } else {
+    elementChildren.hidden = false;
+  }
+});
