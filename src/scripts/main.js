2b3e7fa1a753bd62683a272105610a7a2f3cc36e
Konstantin <ivanchenkomypost@gmail.com>
2022-07-05 19:26:26 +0300

    Task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+list.querySelectorAll('li').forEach(el => {
+  const span = document.createElement('span');
+
+  el.prepend(span);
+  span.append(span.nextSibling);
+});
+
+list.addEventListener('click', (e) => {
+  const title = e.target.closest('span');
+
+  if (!title) {
+    return;
+  }
+
+  const liContent = title.parentNode.querySelector('ul');
+
+  liContent.hidden = !liContent.hidden;
+});
