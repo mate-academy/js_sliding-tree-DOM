edc12260b48643d782e5ebe21940a19e8c2e0436
silchyn <ivan.silchin@gmail.com>
2022-07-06 23:28:44 +0300

    created sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+for (const item of document.querySelectorAll('li')) {
+  if (!item.children.length) {
+    continue;
+  }
+
+  const span = document.createElement('span');
+
+  span.innerText = item.childNodes[0].textContent.trim();
+  item.childNodes[0].remove();
+  item.prepend(span);
+}
+
+document.querySelector('.tree').addEventListener('click', e => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const list = e.target.nextElementSibling;
+
+  if (!list.style.display) {
+    list.style.display = 'none';
+  } else {
+    list.style.display = '';
+  }
+});
