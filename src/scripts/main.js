aaa38891fd012def9a612305453d175cdde0a161
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-07-07 15:23:24 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const elements = document.querySelectorAll('li');
+
+for (const element of elements) {
+  if (element.childElementCount > 0) {
+    const span = document.createElement('span');
+
+    span.innerText = element.firstChild.data;
+    element.firstChild.replaceWith(span);
+
+    span.addEventListener('click', (el) => {
+      const item = el.target.closest('span').nextSibling;
+
+      item.hidden = !item.hidden;
+    });
+  }
+}
