56bbb7f1ba03faa619a78252d3c55bbf359bcda2
fledith <barateon023@gmail.com>
2022-07-11 19:52:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const li = document.querySelectorAll('li');
+const copy = [...li].filter(el => el.firstElementChild !== null);
+
+for (let i = 0; i < copy.length; i++) {
+  const item = copy[i];
+  const itemText = item.firstChild.textContent.trim();
+  const span = document.createElement('span');
+
+  span.dataset.wrapper = `${itemText[0].toLowerCase() + itemText.slice(1)}`;
+  span.append(document.createTextNode(itemText));
+
+  item.replaceChild(span, copy[i].firstChild);
+}
+
+list.addEventListener('click', (eventOnclick) => {
+  if (eventOnclick.target.dataset.wrapper) {
+    eventOnclick
+      .target.nextSibling.hidden = !eventOnclick.target.nextSibling.hidden;
+  }
+});
