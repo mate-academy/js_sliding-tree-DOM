b612e95845257737a3a9e33067a3df0521a47573
Oleksandr Kotliarov <de.fr09n@gmail.com>
2022-07-11 12:33:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+for (const item of document.querySelectorAll('li')) {
+  if (!item.children.length) {
+    continue;
+  }
+
+  const span = document.createElement('span');
+
+  span.innerText = item.childNodes[0].textContent;
+  item.childNodes[0].remove();
+  item.prepend(span);
+
+  item.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    e.target.nextSibling.hidden = !e.target.nextSibling.hidden;
+  });
+}
