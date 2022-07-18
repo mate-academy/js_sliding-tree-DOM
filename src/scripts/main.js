0498723423ad90056c3a85c6fd61ed0229b77ca9
Vladimir Yurkov <vladimir.yurkov.91@gmail.com>
2022-07-18 16:14:00 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const listElements = document.querySelectorAll('li');
+
+for (const item of listElements) {
+  if (item.children.length) {
+    const spanWrapper = document.createElement('span');
+
+    item.prepend(spanWrapper);
+    spanWrapper.append(spanWrapper.nextSibling);
+  }
+}
+
+list.addEventListener('click', e => {
+  const targetItem = e.target.closest('span').nextSibling;
+
+  targetItem.hidden = !targetItem.hidden;
+});
