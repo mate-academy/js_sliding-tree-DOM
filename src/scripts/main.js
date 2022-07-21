4cbf6e5079335b09ec62f7ba0d846df2c6fc7b10
lentyaeev <daniilshaliuk@gmail.com>
2022-07-22 00:46:12 +0300

    Finnaly

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const allLi = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+for (const element of allLi) {
+  if (element.children.length) {
+    const spanElement = document.createElement('span');
+
+    spanElement.innerText = element.childNodes[0].textContent.trim();
+    spanElement.style.userSelect = 'none';
+    // header of each list
+    element.childNodes[0].remove();
+    element.prepend(spanElement);
+    // deleting the original text node and replacing it with the span node
+  }
+}
+
+tree.addEventListener('click', (e) => {
+  if (e.target.nodeName === 'SPAN') {
+    const childUl = e.target.nextElementSibling;
+
+    if (childUl.style.display === '') {
+      childUl.style.display = 'none';
+    } else {
+      childUl.style.display = '';
+    }
+  }
+});
