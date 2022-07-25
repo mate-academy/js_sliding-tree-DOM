2eb450878c41958c974726dd299b2c5be8312658
dasha <dashasv76@gmail.com>
2022-07-25 10:42:48 +0300

    sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const items = document.querySelectorAll('li');
+
+items.forEach(item => {
+  const elem = document.createElement('span');
+
+  elem.innerHTML = item.childNodes[0].textContent;
+
+  item.childNodes[0].remove();
+  item.prepend(elem);
+});
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+  }
+});
