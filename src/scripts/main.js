bbf6086e93edaa2ca41129eac545efd2b23197e9
Natalya Tsybulko <tsybulko.natalya395@gmail.com>
2022-07-28 19:25:49 +0300

    solve the slidingTreeDOM problem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+for (const header of tree.children) {
+  header.innerHTML = `
+    <span>
+      ${header.childNodes[0].data}
+    </span>
+  ` + `
+    <ul>
+      ${header.firstElementChild.innerHTML}
+    </ul>`;
+
+  const nextLayer = header.children[1];
+
+  for (const child of nextLayer.children) {
+    child.innerHTML = `
+    <span>
+      ${child.childNodes[0].data}
+    </span>
+  ` + `
+    <ul>
+      ${child.firstElementChild.innerHTML}
+    </ul>`;
+  }
+}
+
+tree.addEventListener('click', (e) => {
+  const target = e.target.closest('span');
+
+  if (!target) {
+    return;
+  }
+
+  const headerContent = target.parentNode.querySelector('ul');
+
+  headerContent.hidden = !headerContent.hidden;
+});
