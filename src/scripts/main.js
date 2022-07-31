45882034b4dde3e163de1138345db9592e8d2dbb
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-07-31 09:51:27 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+
+const hideShow = (e) => {
+  const item = e.target;
+  const element = item.firstElementChild;
+
+  if (!element.hidden) {
+    element.hidden = true;
+  } else {
+    element.hidden = false;
+  }
+};
+
+tree.addEventListener('click', hideShow);
