d5fb81b6094ab86e4ff1b2e2679bc9e2a1b4b6d4
Kostya Berehoviy <kostyaber3@gmail.com>
2022-08-13 00:31:05 +0300

    Sliding Tree Solution v03

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,14 @@
 const tree = document.querySelector('.tree');
 
 tree.addEventListener('click', (el) => {
+  const lists = document.querySelectorAll('ul');
+
+  for (const list of lists) {
+    if (el.target === list) {
+      return;
+    }
+  }
+
   const item = el.target;
   const element = item.firstElementChild;
 
