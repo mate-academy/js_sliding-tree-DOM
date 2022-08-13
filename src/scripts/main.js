03a128b8937bc98103c2d51bfb2d3fe15661a5e3
Vlad Skuratovskyi <vladskur0369@gmail.com>
2022-08-13 23:41:11 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+
+function trees(item) {
+  for (const layer of item.children) {
+    if (layer.firstElementChild !== null && layer.tagName === 'LI') {
+      layer.insertAdjacentHTML('afterbegin', `
+      <span></span>`);
+    }
+    trees(layer);
+  }
+}
+trees(tree);
+
+tree.addEventListener('click', (events) => {
+  const goin = events.target.children[0].tagName === 'SPAN';
+  const hide = events.target.children[1];
+
+  if (goin) {
+    hide.hidden = hide.hidden === false;
+  }
+});
