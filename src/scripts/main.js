214362e394944dff9d8db2680bc83dfa93b02e35
Kostya Konev <kosgoh.214@gmail.com>
2022-08-14 19:51:33 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const allLi = document.querySelectorAll('li');
+
+[...allLi].forEach(li => {
+  if (li.children) {
+    const span = document.createElement('span');
+
+    span.className = 'header';
+    span.textContent = li.firstChild.textContent;
+    li.firstChild.remove();
+    li.prepend(span);
+  }
+});
+
+list.addEventListener('click', (e) => {
+  const nextElement = e.target.parentElement.children[1];
+
+  if (e.target.className === 'header') {
+    nextElement.hidden = !nextElement.hidden;
+  }
+});
