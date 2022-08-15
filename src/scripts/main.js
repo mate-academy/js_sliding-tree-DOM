e5f3468f908d42d34ac804b8a8a483ebe460a7b3
Andrii Melnyk <andriimelnyq@gmail.com>
2022-08-15 13:11:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+
+for (const item of listItems) {
+  if (item.querySelector('ul')) {
+    const span = document.createElement('span');
+
+    span.className = 'header-text';
+    span.append(item.childNodes[0]);
+    item.prepend(span);
+  }
+}
+
+document.addEventListener('click', e => {
+  if (e.target.classList.contains('header-text')) {
+    e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+  }
+});
