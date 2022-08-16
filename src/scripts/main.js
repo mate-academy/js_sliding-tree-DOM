de15323efdf7ab0b283baadab9fbb01ab336de1f
Nazar Kulchevych <gamebandit38@gmail.com>
2022-08-16 17:08:22 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const list = [ ...document.querySelectorAll('li > ul') ];
+
+list.map(element => {
+  const li = element.parentNode;
+  const liTitle = li.firstChild;
+
+  const span = document.createElement('span');
+
+  span.prepend(liTitle);
+  li.prepend(span);
+
+  span.addEventListener('click', () => {
+    span.nextSibling.toggleAttribute('hidden');
+  });
+});
