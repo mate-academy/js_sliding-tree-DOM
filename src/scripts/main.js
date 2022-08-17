8947c87c812da5901ff4cd5dc78f59478d1beae5
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-08-17 21:47:55 +0300

    First commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const liArr = [...document.querySelectorAll('li')];
+
+liArr.forEach(liItem => liItem.insertAdjacentHTML('afterbegin', `
+  <span>${liItem.innerHTML.split(' ')[0]}</span>
+`));
+
+liArr.forEach(liItem => liItem.childNodes[3].remove());
+
+const list = document.querySelector('.tree');
+const handler = e => {
+  const targetItem = e.target.closest('span');
+
+  if (!targetItem || !list.contains(targetItem)) {
+    return;
+  }
+
+  const siblingForItem = targetItem.nextElementSibling;
+
+  if (siblingForItem) {
+    if (siblingForItem.hidden === true) {
+      siblingForItem.hidden = false;
+    } else {
+      siblingForItem.hidden = true;
+    }
+  }
+};
+
+list.addEventListener('click', handler);
