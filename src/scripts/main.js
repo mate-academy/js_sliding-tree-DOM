bb0599938af2bf3ff4eadf93af1dc47ee30186c4
Hovera-Liubomyr <ljusuk@gmail.com>
2022-08-18 19:14:10 +0300

    sliding_tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const items = tree.querySelectorAll('li');
+
+for (const key of items) {
+  const span = document.createElement('span');
+
+  key.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', el => {
+  el.target.nextElementSibling.hidden = !el.target.nextElementSibling.hidden;
+});
