051922eb42f703e50451af7e3c71cd3610033b43
Andriy Yurkiv <88943977+andriy-yurkiv@users.noreply.github.com>
2022-08-23 18:06:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.querySelectorAll('li').forEach(title => {
+  const span = document.createElement('span');
+
+  title.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.addEventListener('click', (el) => {
+  const list = el.target.parentNode.querySelector('ul');
+
+  if (el.target.tagName === 'SPAN') {
+    list.hidden = !list.hidden;
+  };
+});
