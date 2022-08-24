060ff35e3c2406d1bf1cfc7c49863c4fbb80a857
Yurii Kozlov <yurka22870@gmail.com>
2022-08-24 23:22:35 +0300

    sliding tree was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const treeItems = document.querySelectorAll('li');
+
+for (const item of treeItems) {
+  if (item.children.length > 0) {
+    const span = document.createElement('span');
+
+    span.append(item.childNodes[0]);
+    item.prepend(span);
+  }
+
+  tree.addEventListener('click', (e) => {
+    if (e.target.tagName === 'SPAN') {
+      // eslint-disable-next-line max-len
+      e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+    }
+  });
+}
