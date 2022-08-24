6ab10f57ad776777443c3cb955ff24cf89c88679
Grebenkina <grebenkina.2490@gmail.com>
2022-08-25 01:47:30 +0700

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const items = document.querySelectorAll('li');
+
+[...items].forEach(li => {
+  if (li.children) {
+    const span = document.createElement('span');
+
+    span.className = 'header';
+    span.textContent = li.firstChild.textContent;
+    li.firstChild.remove();
+    li.prepend(span);
+  }
+});
+
+list.addEventListener('click', (e) => {
+  const nextElement = e.target.parentElement.children[1];
+
+  if (e.target.className === 'header') {
+    nextElement.hidden = !nextElement.hidden;
+  }
+});
