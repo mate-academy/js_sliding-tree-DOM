8a1f3da18bc85f0bfd74e4b8a3f07e4a54cc0531
Tetiana Koval <tetiana.koval.bit@gmail.com>
2022-08-27 07:53:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const liItems = document.querySelectorAll('li');
+const liItemsArr = [...liItems];
+const list = document.querySelector('.tree');
+
+for (let i = 0; i < liItemsArr.length; i++) {
+  if (liItemsArr[i].querySelector('ul')) {
+    const span = document.createElement('SPAN');
+
+    span.textContent = liItemsArr[i].firstChild.textContent;
+    liItemsArr[i].prepend(span);
+    liItemsArr[i].childNodes[1].remove();
+  }
+}
+
+const switchDisplay = (e) => {
+  if (e.srcElement.tagName === 'SPAN') {
+    if (e.target.nextElementSibling.style.display === 'none') {
+      e.target.nextElementSibling.style.display = 'block';
+    } else {
+      const childCollection = e.target.parentElement.children;
+      const childArr = [...childCollection];
+
+      childArr.find(x => x.tagName === 'UL').style.display = 'none';
+    }
+  }
+};
+
+list.addEventListener('click', switchDisplay);
