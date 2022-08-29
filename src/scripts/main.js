fd341cd404955953b2bd5a5756af265fba0e95cf
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2022-08-29 20:49:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,52 @@
 'use strict';
 
 // write code here
+const mainlist = document.querySelector('.tree');
+const strCollection = mainlist.innerHTML
+  .trim()
+  .split('\n')
+  .map(item => item.trim());
+
+/* Create <span></span> tag around header list */
+function addSpanToHeader(stringCollection) {
+  const arrNewCollection = [];
+
+  for (let i = 0; i < stringCollection.length; i++) {
+    let currentElem = stringCollection[i];
+    const nextElem = stringCollection[i + 1];
+
+    if (nextElem === '<ul>') {
+      let headerLi = currentElem
+        .replace('<li>', '<li><span class="header-list">');
+
+      headerLi += '</span>';
+      currentElem = headerLi;
+    }
+
+    arrNewCollection.push(currentElem);
+  }
+
+  const collectionWithSpan = arrNewCollection.join('');
+
+  mainlist.innerHTML = collectionWithSpan;
+};
+
+addSpanToHeader(strCollection);
+
+/* Event delegation */
+mainlist.addEventListener('click', (e) => {
+  const item = e.target.closest('.header-list');
+
+  if (!item) {
+    return;
+  }
+
+  const itemParent = item.parentElement;
+  const currentListOfItem = itemParent.querySelector('ul');
+
+  if (currentListOfItem.style.display === 'none') {
+    currentListOfItem.style.display = 'block';
+  } else {
+    currentListOfItem.style.display = 'none';
+  }
+});
