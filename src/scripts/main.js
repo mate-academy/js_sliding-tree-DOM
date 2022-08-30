21a9237cf58c536aaa2b1ec72fe44c46597eeb4b
Dolhov Andrey <andrej.dolgov.042110@gmail.com>
2022-08-31 00:29:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liArr = [...document.querySelectorAll('ul')];
+
+liArr.slice(1, liArr.length).forEach(item => {
+  const span = document.createElement('span');
+
+  span.className = 'header';
+  span.innerText = item.parentElement.innerText;
+
+  item.parentElement.className = 'head';
+});
+
+tree.addEventListener('click', e => {
+  const item = e.target;
+
+  if (item.className !== 'head') {
+    return;
+  }
+
+  item.children[0].hidden = !item.children[0].hidden;
+});
