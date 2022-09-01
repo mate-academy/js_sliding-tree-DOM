f3ef592651e9a9f9a6c56c39e935a6ff7213cf1d
Kostya Berehoviy <kostyaber3@gmail.com>
2022-09-01 11:42:48 +0300

    Sliding Tree Solution v04

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,9 +14,5 @@ tree.addEventListener('click', (el) => {
   const item = el.target;
   const element = item.firstElementChild;
 
-  if (element.hidden === false) {
-    element.hidden = true;
-  } else {
-    element.hidden = false;
-  }
+  element.hidden = !element.hidden;
 });
