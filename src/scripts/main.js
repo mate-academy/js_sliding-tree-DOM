c91a7064ef89c62d4dfda4b1bea18a7bff4ab073
Oleksii <oleksii@it.com>
2022-09-11 20:01:20 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+for (const item of li) {
+  if (item.querySelector('ul')) {
+    const span = document.createElement('span');
+
+    span.className = 'move';
+    span.append(item.childNodes[0]);
+    item.prepend(span);
+  }
+}
+
+document.addEventListener('click', (el) => {
+  if (el.target.classList.contains('move')) {
+    el.target.nextElementSibling.hidden
+    = !el.target.nextElementSibling.hidden;
+  }
+});
