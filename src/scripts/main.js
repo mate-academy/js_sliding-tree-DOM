80c3b468d4c5e53925e540cf83c664696b71f59b
Max Bovtun <maxbovtun@gmail.com>
2022-09-20 19:16:49 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const allLiItems = document.querySelectorAll('li');
+
+for (const liItem of allLiItems) {
+  if (liItem.childNodes.length > 1) {
+    const span = document.createElement('span');
+
+    span.dataset.clickable = true;
+    span.innerText = liItem.firstChild.textContent;
+    liItem.firstChild.remove();
+    liItem.prepend(span);
+  }
+}
+
+tree.addEventListener('click', (e) => {
+  if (!e.target.dataset.clickable) {
+    return;
+  }
+
+  const item = e.target.closest('li');
+  const listsToHide = item.querySelectorAll('ul');
+
+  listsToHide.forEach((list) => {
+    if (list.style.display === 'none') {
+      list.style.display = 'block';
+    } else {
+      list.style.display = 'none';
+    }
+  });
+});
