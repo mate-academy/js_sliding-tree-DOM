e5249f1244628e7b6c46c183be152edf2c0f2e94
Paul Voronin <voron.paul@gmail.com>
2022-09-21 16:22:39 +0300

    ver.1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+
+for (const li of listItems) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.prepend(span.nextSibling);
+}
+
+document.addEventListener('click', e => {
+  const header = e.target.nextSibling;
+
+  if (header) {
+    header.hidden = !header.hidden;
+  }
+});
