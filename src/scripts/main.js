c48e675dc8ae655bfc3fe2c8607ba8052d81ca49
Volodymir Tymtsias <vovanuch2007@gmail.com>
2022-09-23 15:39:27 +0300

    Solution Sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = tree.querySelectorAll('li');
+
+for (const item of listItems) {
+  if (!item.querySelector('ul')) {
+    continue;
+  }
+
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+}
+
+function toggleVisibility(e) {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const childOfHeadline = e.target.parentNode.querySelector('ul');
+
+  childOfHeadline.hidden = !childOfHeadline.hidden;
+}
+
+tree.addEventListener('click', toggleVisibility);
