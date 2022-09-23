81aa9b2bf9a50258cee14afe8134987a08e2b90a
Sheremet Oleksandra <sandasheremet@gmail.com>
2022-09-23 13:52:40 +0300

    added sliding

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+const lists = document.querySelectorAll('li');
+
+for (const li of lists) {
+  const text = li.firstChild;
+
+  li.insertAdjacentHTML('afterbegin',
+    `<span class='title'>
+    ${li.firstChild.nodeValue.trim()}
+    </span>`);
+  text.remove();
+}
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', action => {
+  if (action.target.className !== 'title') {
+    return;
+  }
+
+  const toHide = action.target.nextElementSibling;
+
+  if (toHide) {
+    toHide.hidden = !toHide.hidden;
+  }
+});
