d9fde48a976ebf4b991478492583d917fa5548cf
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-09-25 18:56:43 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const sections
+    = [...document.querySelectorAll('li')]
+    .filter(el => el.innerHTML.includes('ul'));
+
+for (const section of sections) {
+  const headline = document.createElement('span');
+
+  headline.innerText = section.firstChild.textContent;
+
+  section.firstChild.remove();
+
+  section.prepend(headline);
+
+  section.querySelector('ul').style.display = 'none';
+}
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', () => {
+  const list = event.target.parentElement.querySelector('ul');
+
+  if (!event.target.matches('span')) {
+    return;
+  }
+
+  list.style.display === 'none'
+    ? list.style.display = '' : list.style.display = 'none';
+});
