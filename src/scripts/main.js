ed42463393e18c75cb8fc761fac9883c87309972
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-10-03 14:25:50 +0300

    done!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', e => {
+  if (!(e.target.tagName === 'LI' && e.target.children.length !== 0)) {
+    return;
+  }
+
+  const displayValue = e.target.children[0].style.display;
+
+  if (!displayValue) {
+    e.target.children[0].style.display = 'none';
+  } else {
+    e.target.children[0].style.display = null;
+  }
+});
