ab455c80e7ea3976024f6846f827efe7ca9a97bc
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-10-06 21:39:51 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,24 @@
 'use strict';
 
+const liItems = document.querySelectorAll('li');
+
+liItems.forEach(li => {
+  if (li.children.length !== 0) {
+    const textNode = li.firstChild.textContent.trim();
+
+    li.firstChild.remove();
+    li.insertAdjacentHTML('afterbegin', `<span>${textNode}</span>`);
+  }
+});
+
 document.addEventListener('click', e => {
-  if (!(e.target.tagName === 'LI' && e.target.children.length !== 0)) {
+  if (!(e.target.tagName === 'SPAN')) {
     return;
   }
 
-  const displayValue = e.target.children[0].style.display;
+  const displayValue = e.target.parentElement.children[1].style.display;
 
-  if (!displayValue) {
-    e.target.children[0].style.display = 'none';
-  } else {
-    e.target.children[0].style.display = null;
-  }
+  displayValue
+    ? e.target.parentElement.children[1].style.display = null
+    : e.target.parentElement.children[1].style.display = 'none';
 });
