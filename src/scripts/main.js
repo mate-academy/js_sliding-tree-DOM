bd9b65544dfc869cd273ed41b14b238616ef47b5
Danil <dasha8854567@gmail.com>
2022-10-07 16:35:52 +0300

    commit1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+document.querySelector('.tree').addEventListener('click', (e) => {
+  if (e.target.children[0] === undefined || e.target.localName === 'ul') {
+    return;
+  }
+
+  if (e.target.children[0].style.display === 'none') {
+    e.target.children[0].style.display = 'block';
+  } else {
+    e.target.children[0].style.display = 'none';
+  }
+});
