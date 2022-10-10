06fa8b8f843c0d9d8557c941fcbd1619e56b7827
Oleksandr Naumchak <naumchak@ukr.net>
2022-10-10 18:08:35 +0300

    JS sliding tree DOM (first solution)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+for (const row of list) {
+  const span = document.createElement('span');
+
+  row.prepend(span);
+  span.append(span.nextSibling);
+}
+
+document.addEventListener('click', elem => {
+  const element = elem.target.nextSibling;
+
+  if (elem.target.tagName === 'SPAN') {
+    element.hidden = !element.hidden;
+  }
+});
