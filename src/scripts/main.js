f7b5732e47c5347bdbc9cb0ec7b40ec7e114d430
Oleksiy Uchaev <corob.dir@gmail.com>
2022-10-15 11:08:36 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+[...document.querySelectorAll('li')]
+  .filter(li => li.childElementCount > 0)
+  .map(li => {
+    const span = document.createElement('span');
+    const text = (li.firstChild);
+
+    li.prepend(span);
+    span.append(text);
+  });
+
+document.querySelector('.tree')
+  .addEventListener('click', (e) => {
+    e.target.closest(':not(ul)').nextSibling.hidden
+    = !e.target.closest(':not(ul)').nextSibling.hidden;
+  });
