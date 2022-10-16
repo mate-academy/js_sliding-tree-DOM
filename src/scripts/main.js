eb56cae63ae18fc064192b046e0fe6e764816a0e
Anton Martyniuk <antonmartyniuk@gmail.com>
2022-10-16 16:29:32 +0300

    Solution js_sliding_tree_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+console.log(list.style.display);
+
+list.addEventListener('click', (e) => {
+  const item = e.target;
+
+  if (!item.matches('li')) {
+    return;
+  }
+
+  if (item.firstElementChild.style.display === 'none') {
+    item.firstElementChild.style.display = 'block';
+  } else {
+    item.firstElementChild.style.display = 'none';
+  }
+});
