15f8c4211ccbb6343d71d1cc733d4d642f97028f
Vlad Kurachenko <x0674016244@gmail.com>
2022-10-18 17:08:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const allLi = document.querySelectorAll('li');
+
+for (const item of allLi) {
+  if (item.querySelector('ul')) {
+    const span = document.createElement('span');
+
+    span.className = 'headerName';
+    span.append(item.childNodes[0]);
+    item.prepend(span);
+  }
+}
+
+document.addEventListener('click', e => {
+  if (e.target.classList.contains('headerName')) {
+    e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+  }
+});
