a1d109093d5debf2d6a4187656e14ef10bfb6405
illia_kots <illyahennadievich@gmail.com>
2022-10-18 22:36:35 +0300

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const list = document.querySelectorAll('li');
+
+[...list].map((el) => {
+  const span = document.createElement('span');
+
+  el.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.addEventListener('click', ev => {
+  const title = ev.target.nextSibling;
+
+  if (title.hidden === false) {
+    title.hidden = true;
+  } else {
+    title.hidden = false;
+  }
+});
