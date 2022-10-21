6d1bb91683c345fe89d5a651eef5426220284a9a
John Rust <johnrust12@gmail.com>
2022-10-21 13:05:31 +0300

    Sulution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write code here
+const tree = document.querySelectorAll('li');
+
+for (const item of tree) {
+  const header = item.firstChild.textContent;
+
+  item.firstChild.textContent = '';
+
+  item.insertAdjacentHTML(
+    'afterbegin',
+    `<span>${header}</span>`
+  );
+
+  item.firstChild.addEventListener('click', () => {
+    if (item.children[1].style.display !== 'none') {
+      item.children[1].style.display = 'none';
+
+      return;
+    }
+
+    item.children[1].style.display = 'initial';
+  });
+}
