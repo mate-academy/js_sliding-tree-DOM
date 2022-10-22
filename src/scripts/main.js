86caf400e5cfe8bdd799d876b982a360a417615b
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2022-10-22 15:48:22 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const mainList = document.querySelector('.tree');
+
+mainList.addEventListener('click', (e) => {
+  const allUl = document.querySelectorAll('ul');
+
+  for (const someUl of allUl) {
+    if (someUl === e.target) {
+      return;
+    }
+  }
+
+  const parentLi = e.target;
+  const childLi = parentLi.children[0];
+
+  childLi.hidden = (!childLi.hidden)? true: false;
+});
