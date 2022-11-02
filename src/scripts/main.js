48d3351fb00ecd81a5f69de33ba8fcfa10955641
Kolya Movchan <kmovchan261@gmail.com>
2022-11-02 18:05:04 +0200

    js_sliding-tree-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const headlines = document.querySelectorAll('li');
+
+const createSpan = () => {
+  return document.createElement('span');
+};
+
+[...headlines].map(headline => {
+  if (!headline.children.length) {
+    return;
+  }
+
+  headline.prepend(createSpan());
+
+  const span = headline.firstChild;
+
+  span.prepend(headline.firstChild.nextSibling);
+
+  span.addEventListener('click', (e) => {
+    const hidden = span.nextSibling.style;
+
+    if (hidden.display === 'none') {
+      hidden.display = '';
+
+      return;
+    };
+
+    hidden.display = 'none';
+  });
+});
