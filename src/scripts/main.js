dbd18060c019046d8ba76e6677c661a9f52fe89f
Mykyta Krainik <nikinta2003@gmail.com>
2022-11-08 17:57:17 +0200

    Expandable tree was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', function(e) {
+  const title = e.target;
+
+  if (title.tagName !== 'SPAN') {
+    return;
+  }
+
+  const ul = title.parentNode.querySelector('ul');
+
+  if (!ul) {
+    return;
+  }
+
+  ul.hidden = !ul.hidden;
+});
