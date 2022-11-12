140e0b78c4d8d6391f1569d8ea2183c1ed9696b7
July Terekhova <sky206380@gmail.com>
2022-11-12 22:06:06 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
+
 'use strict';
 
-// write code here
+const titles = document.querySelectorAll('li');
+
+titles.forEach(el => {
+  el.insertAdjacentHTML('afterbegin', `
+  <span> ${el.innerHTML.split('  ')[0]} </span>
+  `);
+  el.childNodes[3].remove();
+});
+
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', (ev) => {
+  const item = ev.target;
+
+  if (item.tagName !== 'SPAN') {
+    return;
+  }
+
+  if (item.nextElementSibling) {
+    item.nextElementSibling.hidden
+  = !item.nextElementSibling.hidden;
+  }
+});
