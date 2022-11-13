5acee4fd0f498f670d5a5c5460d7059a7abbf8e1
Oleksandr <Khabailiuk@gmail.com>
2022-11-13 16:23:06 +0100

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const items = document.querySelectorAll('li');
+
+items.forEach(item => {
+  if (item.children.length) {
+    const text = item.firstChild.textContent;
+
+    item.firstChild.remove();
+    item.insertAdjacentHTML('afterbegin', `<span> ${text} </span>`);
+  }
+});
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+  e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+});
