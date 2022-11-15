ade88824677c10dab4833edb18485f5bf2d28215
Denis Cherevaschenko <deniska.cherevaschenko@gmail.com>
2022-11-15 17:48:33 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+function f(listss) {
+  for (const key of [...listss.children]) {
+    const span = document.createElement('span');
+    const str = key.childNodes[0].textContent.trim();
+
+    span.innerText = str;
+    key.replaceChild(span, key.childNodes[0]);
+
+    if ([...key.children][1]) {
+      f(key.children[1]);
+    }
+  }
+}
+
+f(list);
+
+document.body.addEventListener('click', e => {
+  const item = e.target.closest('span');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  if (e.target.parentElement.children[1]) {
+    if (e.target.parentElement.children[1].hidden) {
+      e.target.parentElement.children[1].hidden = false;
+    } else {
+      e.target.parentElement.children[1].hidden = true;
+    }
+  }
+});
