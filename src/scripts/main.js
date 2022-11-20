62e4377641917c593f09e520c2b838d3df879214
Anastasiia Horidieieva <gordeevaisgreat@gmail.com>
2022-11-20 18:20:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('li').forEach(li => {
+  const text = li.childNodes[0];
+  const span = document.createElement('span');
+
+  li.replaceChild(span, text);
+  span.appendChild(text);
+});
+
+document.addEventListener('click', function(e) {
+  const clicked = e.target.closest('span');
+
+  if (!clicked) {
+    return;
+  }
+
+  const ul = clicked.closest('li').children[1];
+
+  if (!ul) {
+    return;
+  }
+
+  ul.hidden = !ul.hidden;
+});
