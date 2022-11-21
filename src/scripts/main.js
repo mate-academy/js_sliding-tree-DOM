4c4c06821cfa6004747e659f176dbea2275e1845
Kate Riazantseva <katyarezvova96@gmail.com>
2022-11-21 17:54:27 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+const liColection = document.querySelectorAll('li');
+const ulColection = document.querySelectorAll('ul');
+
+for (const ul of ulColection) {
+  ul.style.display = 'block';
+}
+
+for (const li of liColection) {
+  if (li.children.length > 0) {
+    const span = document.createElement('span');
+
+    span.className = 'title';
+
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+list.addEventListener('click', (e) => {
+  const item = e.target;
+
+  if (item.classList.contains('title')) {
+    if (item.nextSibling.style.display === 'block') {
+      item.nextSibling.style.display = 'none';
+    } else {
+      item.nextSibling.style.display = 'block';
+    }
+  }
+});
