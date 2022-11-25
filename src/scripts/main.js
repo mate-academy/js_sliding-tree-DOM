c98ae4ecd8b93a0cb01a129b2a136f2d06e34128
Eugene Farafonov <eu.farafonov@gmail.com>
2022-11-25 10:51:27 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+insertSpan(list);
+
+function insertSpan(node) {
+  const children = node.children;
+
+  for (let i = 0; i < children.length; i++) {
+    const firstChildren = children[i].firstElementChild;
+
+    if (firstChildren && firstChildren.nodeName === 'UL') {
+      const firstNode = children[i].firstChild;
+      const textNode = firstNode.textContent.trim();
+
+      const span = document.createElement('span');
+
+      span.textContent = textNode;
+      firstNode.replaceWith(span);
+
+      insertSpan(firstChildren);
+    }
+  }
+}
+
+list.addEventListener('click', (e) => {
+  if (e.target.nodeName === 'SPAN') {
+    const ul = e.target.nextElementSibling;
+
+    if (ul.style.display === '') {
+      ul.style.display = 'none';
+    } else {
+      ul.style.display = '';
+    }
+  }
+});
