2235842aeb5086b10e9c01ef9bdbb4db2144b4cf
Taras Kovalchuk <taras.kovalcuk93@gmail.com>
2022-11-27 12:35:34 +0200

    Fixed after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,16 @@
 'use strict';
 
 const list = document.querySelector('.tree');
-const fruit = document.querySelector('.tree').firstElementChild;
-const fruitWrap = document.createElement('span');
-const fruitRed = fruit.firstElementChild.children[0];
-const fruitYellow = fruit.firstElementChild.children[1];
-const fruitRedWrap = document.createElement('span');
-const fruitYellowWrap = document.createElement('span');
-const fish = document.querySelector('.tree').lastElementChild;
-const fishWrap = document.createElement('span');
-const fishOceanFishes = fish.firstElementChild.children[0];
-const fishOceanFishesWrap = document.createElement('span');
-const fishFreshWaterFish = fish.firstElementChild.children[1];
-const fishFreshWaterFishWrap = document.createElement('span');
+const items = list.querySelectorAll('li');
 
-fruitWrap.append(fruit.firstChild);
-list.firstElementChild.prepend(fruitWrap);
+for (const item of items) {
+  if (item.firstElementChild) {
+    const wrap = document.createElement('span');
 
-fruitRedWrap.append(fruitRed.firstChild);
-fruitRed.prepend(fruitRedWrap);
-
-fruitYellowWrap.append(fruitYellow.firstChild);
-fruitYellow.prepend(fruitYellowWrap);
-
-fishWrap.append(fish.firstChild);
-list.lastElementChild.prepend(fishWrap);
-
-fishOceanFishesWrap.append(fishOceanFishes.firstChild);
-fishOceanFishes.prepend(fishOceanFishesWrap);
-
-fishFreshWaterFishWrap.append(fishFreshWaterFish.firstChild);
-fishFreshWaterFish.prepend(fishFreshWaterFishWrap);
+    wrap.append(item.firstChild);
+    item.prepend(wrap);
+  }
+}
 
 list.addEventListener('click', (occurrent) => {
   const item = occurrent.target;
