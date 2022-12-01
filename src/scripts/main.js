db7b53405704dc08a726d57a5190f52297f86f66
Yurii Teslia <teslya.yuriy@gmail.com>
2022-12-01 22:18:44 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+function listItems(collection) {
+  for (const item of collection) {
+    if (item.nodeName === 'LI') {
+      if (item.children.length > 0) {
+        item.innerHTML = editInnerHtml(item.innerHTML);
+      }
+
+      if (Object.values(item.children).length === 2) {
+        item.addEventListener('click', (e) => {
+          e.stopPropagation();
+
+          const currentElement = e.target.parentElement.lastElementChild;
+          const getElStyles = getComputedStyle(currentElement);
+          const currentDisplayStyel = getElStyles.getPropertyValue('display');
+
+          if (e.currentTarget === e.target.parentElement) {
+            if (currentDisplayStyel === 'none') {
+              e.target.parentElement.lastElementChild.style.display = 'block';
+            } else {
+              e.target.parentElement.lastElementChild.style.display = 'none';
+            }
+          }
+        });
+      }
+    }
+    listItems(item.children);
+  }
+}
+
+listItems(tree.children);
+
+function editInnerHtml(code) {
+  const index = code.indexOf('<') > 0 ? code.indexOf('<') : code.length;
+  const title = code.slice(0, index).trim();
+  const restOfHtml = code.slice(index);
+
+  return `<span>${title}</span>${restOfHtml}`;
+}
