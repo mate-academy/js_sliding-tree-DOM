72c568088112744694616b086d2b0cc60fdc2743
Panianchuk M <panianchuk@gmail.com>
2022-12-02 22:36:02 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const listItem = document.querySelectorAll('li');
+
+for (const li of listItem) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+list.addEventListener('click', (e) => {
+  const item = e.target.closest('span').nextSibling;
+
+  item.hidden = !item.hidden;
+});
