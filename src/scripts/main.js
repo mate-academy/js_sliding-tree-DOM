e0c8ebc21da0c0a5771b52eb88b400e0af50f492
Sergey Mironenko <serg1prime@gmail.com>
2022-12-04 20:09:56 +0200

    Task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listElements = document.querySelectorAll('li');
+let clicked = false;
+ 
+for (let element of listElements) {
+  const newElement = document.createElement('span');
+
+  newElement.textContent = element.childNodes[0].nodeValue;
+  element.prepend(newElement);
+  element.childNodes[1].remove();
+}
+
+list.addEventListener('click', action => {
+   const targetElement = action.target;
+
+   if (!targetElement.matches('span')) {
+     return;
+   }
+
+   if (clicked === false) {
+     targetElement.nextElementSibling.style.display = 'none';
+     clicked = true;
+   } else {
+     targetElement.nextElementSibling.style.display = 'block';
+     clicked = false;
+   }
+});
