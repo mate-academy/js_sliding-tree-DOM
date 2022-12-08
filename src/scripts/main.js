b04f0e03bffe3b83357940195a94a9bff4079dda
Olena Shevchenko <shevochkas@gmail.com>
2022-12-08 12:15:44 +0200

    sliding list was added with js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const li = document.querySelectorAll('li');
+
+for (const item of li) {
+  if (item.children.length > 0) {
+    const span = document.createElement('span');
+    span.textContent = item.childNodes[0].nodeValue.trim();
+    item.childNodes[0].nodeValue = '';
+    item.prepend(span);
+  }
+}
+
+tree.addEventListener('click', (e) => {
+  if (e.target.closest('span')) {
+    const ul = e.target.nextElementSibling;
+    const hiddenStatus = ul.hidden;
+
+    ul.hidden = !hiddenStatus;
+  }
+});
