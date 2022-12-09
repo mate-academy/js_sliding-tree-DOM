c0232fc0048f9aa7fcd0ca132281eb097973b94e
Artem Hirzhev <a.hirzhev@gmail.com>
2022-12-09 10:23:38 +0200

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+for (let i = 0; i < li.length; i++) {
+  const span = document.createElement('span');
+
+  li[i].prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const children = e.target.parentNode.querySelector('ul');
+
+  if (!children) {
+    return;
+  }
+
+  children.hidden = !children.hidden;
+});
