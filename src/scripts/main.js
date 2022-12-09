eb13dd0db748f4fae26fa54c11df1f1d8713bb4d
Yevhen Petrovskyi <petrovskyi.yevhen@gmail.com>
2022-12-09 23:13:14 +0200

    Solution for js_sliding-tree-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+for (const li of document.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+list.addEventListener('click', (e) => {
+  const item = e.target.closest('span').nextSibling;
+
+  item.hidden = !item.hidden;
+});
