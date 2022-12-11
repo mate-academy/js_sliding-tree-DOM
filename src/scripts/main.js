1a5d2d41ee1a1c5e74e793fcc20e8604d79457ad
vadiimvooo <ASD4S.Profile@gmail.com>
2022-12-11 03:38:25 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const allLi = document.querySelectorAll('li');
+
+allLi.forEach(item => {
+  const span = document.createElement('span');
+
+  span.innerText = item.firstChild.textContent.trim();
+  item.replaceChild(span, item.firstChild);
+});
+
+tree.addEventListener('click', e => {
+  if (e.target.nextElementSibling === null) {
+    return;
+  }
+
+  e.target.closest('span').nextElementSibling.hidden
+    = !e.target.closest('span').nextElementSibling.hidden;
+});
