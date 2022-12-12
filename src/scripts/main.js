4023d4a423c8c6f455cb779b7ed15cafc82e06b3
Yevhen Petrovskyi <petrovskyi.yevhen@gmail.com>
2022-12-12 22:55:20 +0200

    requested changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const list = document.querySelector('.tree');
+const listItem = document.querySelectorAll('li');
 
-for (const li of document.querySelectorAll('li')) {
+for (const li of listItem) {
   const span = document.createElement('span');
 
   li.prepend(span);
   span.append(span.nextSibling);
 }
 
-list.addEventListener('click', (e) => {
-  const item = e.target.closest('span').nextSibling;
+list.addEventListener('click', (event) => {
+  const item = event.target.closest('span').nextSibling;
 
   item.hidden = !item.hidden;
 });
