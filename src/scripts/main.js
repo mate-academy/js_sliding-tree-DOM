9b0bbf5ab5f091bba495d9728034cd45e35c5b56
Nick_Radchuk <mukola.radchuk@gmail.com>
2022-12-13 11:31:20 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const list = document.querySelectorAll('li');
+
+for (const li of list) {
+  if (li.children.length !== 0) {
+    const span = document.createElement('span');
+
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+tree.addEventListener('click', (e) => {
+  const item = e.target.closest('span');
+
+  if (!item) {
+    return;
+  }
+
+  const itemList = item.parentNode.querySelector('ul');
+
+  itemList.hidden = !itemList.hidden;
+});
