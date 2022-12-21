99c595d4e207d046bc2430b687d96000f29ed295
Deniskakaka <krollik1000000@gmail.com>
2022-12-21 16:21:28 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+for (const li of document.querySelectorAll('li')) {
+  const span = document.createElement('span');
+  const listUlInLi = li.querySelectorAll('ul');
+
+  if (listUlInLi.length) {
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+const spanList = document.querySelectorAll('span');
+
+spanList.forEach(el => {
+  el.addEventListener('click', (e) => {
+    const displayStatus = e.target.parentNode.querySelector('ul').style.display;
+
+    e.target.parentNode
+      .querySelector('ul').style.display = displayStatus !== 'none'
+        ? 'none'
+        : 'block';
+  });
+});
