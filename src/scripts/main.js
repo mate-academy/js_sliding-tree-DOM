6eee8c0e4e51942185565500ed5d8126bdd7fa9b
Serhii Vovchenko <seriivigas@gmail.com>
2022-12-22 12:53:44 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const listHeadlines = list.querySelectorAll('li');
+
+listHeadlines.forEach(element => {
+  const createSpan = document.createElement('span');
+
+  element.prepend(createSpan);
+  createSpan.append(createSpan.nextSibling);
+});
+
+list.addEventListener('click', dropdown);
+
+function dropdown(events) {
+  const item = events.target.closest('span').nextSibling;
+
+  item.hidden = !item.hidden;
+};
