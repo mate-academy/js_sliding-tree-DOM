34050f127ffc7e3d719bbb65d08277805d9fd76d
OksanaBaloh <obaloh1108@gmail.com>
2022-12-23 18:35:13 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const headers = [...document.querySelectorAll('li')];
+
+for (const header of headers) {
+  if (header.children.length !== 0) {
+    const span = document.createElement('span');
+
+    header.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+tree.addEventListener('click', e => {
+  if (!e.target.closest('span')) {
+    return;
+  };
+
+  const header = e.target.parentElement.querySelector('ul');
+
+  if (!header) {
+    return;
+  }
+
+  header.hidden = !header.hidden;
+});
