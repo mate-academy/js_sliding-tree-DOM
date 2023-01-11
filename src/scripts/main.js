f47398eaba9b5ff7e6ae885a10a2ceec3d6b603d
Oleksii Pishchuhin <aliliolek@gmail.com>
2023-01-11 01:56:42 +0100

    solution 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write code here
+const header = document.querySelectorAll('li');
+let headerTitle = '';
+const arrOftitles = [];
+
+header.forEach(el => {
+  if (el.children.length > 0) {
+    headerTitle = el.textContent.split(`\n`)[0];
+    arrOftitles.push(headerTitle);
+  }
+});
+
+for (const key of arrOftitles) {
+  document.querySelector('body').innerHTML
+    = document.querySelector('body').innerHTML.replaceAll(key, `
+      <span class="listTitle ${key}">${key}</span>`);
+}
+
+document.querySelector('body').innerHTML
+  = document.querySelector('body').innerHTML.replaceAll('<ul>', `
+    <ul class="toRoll">`);
+
+document.addEventListener('click', e => {
+  if (e.target.className.includes('listTitle')) {
+    const list = e.target.parentElement.querySelector('.toRoll');
+
+    list.style.display = (list.style.display === 'none') ? 'inherit' : 'none';
+
+  }
+});
