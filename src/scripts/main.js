f9f46b37c786ed8c4d59b5e45024ccaf440ff80b
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-01-12 20:49:59 +0000

    sliding tree DOM task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const liArray = [...document.querySelectorAll('li')];
+const liWithChildren = liArray.filter(li => li.children.length);
+const list = document.querySelector('.tree');
+
+for (const li of liWithChildren) {
+  const span = document.createElement('span');
+  const text = li.firstChild.textContent;
+
+  li.firstChild.remove();
+  li.insertBefore(span, li.firstChild);
+  span.innerText = text;
+}
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  if (e.target.nextSibling.style.display === 'none') {
+    e.target.nextSibling.style.display = 'inherit';
+  } else {
+    e.target.nextSibling.style.display = 'none';
+  }
+});
