da7752a47a301df08fde29444b1e75a622ae1cb5
Nick Busarhin <busarginkolia@gmail.com>
2023-01-13 09:07:22 +0200

    tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+const listItems = [...document.querySelectorAll('li')];
+
+for (const item of listItems) {
+  if (item.children.length !== 0) {
+    const wrapper = document.createElement('span');
+
+    item.prepend(wrapper);
+    wrapper.append(wrapper.nextSibling);
+  }
+}
+
+list.addEventListener('click', function changeList(e) {
+  if (e.target.closest('span').nextSibling.hasAttribute('hidden')) {
+    e.target.closest('span').nextSibling.removeAttribute('hidden');
+  } else if (e.target.closest('span').nextSibling) {
+    e.target.closest('span').nextSibling.setAttribute('hidden', '');
+  }
+});
