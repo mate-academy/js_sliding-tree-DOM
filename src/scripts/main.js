03e827738f0be1ab98e9065fb575a0fb2e6e4fd8
Anna Akhalmosulishvili <akhala33@gmail.com>
2023-01-17 11:13:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const options = [...document.querySelectorAll('li')];
+
+for (const option of options) {
+  if (option.children.length > 0) {
+    const innerList = option.children[0];
+    const spanText = option.innerText.slice(0, option.innerText.indexOf('\n'));
+    const wrapper = document.createElement('span');
+
+    innerList.insertAdjacentElement('beforebegin', wrapper);
+    wrapper.textContent = spanText;
+
+    wrapper.previousSibling.remove();
+  }
+}
+
+const titles = [...document.querySelectorAll('span')];
+
+for (const title of titles) {
+  title.addEventListener('click', e => {
+    if (title.nextSibling.style.display === 'none') {
+      title.nextSibling.style.display = 'block';
+    } else {
+      title.nextSibling.style.display = 'none';
+    }
+  });
+}
