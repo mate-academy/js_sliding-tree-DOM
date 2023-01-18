9637a22db5e424f0f7f3d7c3b1f5f3697e146190
Ihor Chaikovskyi <ihorchaikovskyiweb@gmail.com>
2023-01-18 17:09:34 +0200

    first attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const allLi = [...tree.querySelectorAll('li')];
+
+allLi.map(x => {
+  const span = document.createElement('span');
+
+  x.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.addEventListener('click', x => {
+  if (x.target.tagName === 'SPAN') {
+    if (x.target.nextSibling.hidden === false) {
+      x.target.nextSibling.hidden = true;
+    } else {
+      x.target.nextSibling.hidden = false;
+    }
+    ;
+  };
+});
