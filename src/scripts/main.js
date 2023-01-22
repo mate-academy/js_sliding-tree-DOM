27ba75dded7dba684f970bc2d0c49d100eecc219
Taras Baran <baranprivat22@gmail.com>
2023-01-22 18:46:01 +0200

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+window.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  [...tree.querySelectorAll('li')].forEach(el => {
+    if (el.children.length !== 0) {
+      const span = document.createElement('span');
+
+      span.innerHTML = el.firstChild.textContent.trim();
+
+      el.firstChild.replaceWith(span);
+    }
+  });
+
+  tree.addEventListener('click', e => {
+    const { target } = e;
+
+    if (target.tagName === 'SPAN') {
+      const nextTag = target.nextSibling;
+
+      nextTag.hidden = !nextTag.hidden;
+    }
+  });
+});
