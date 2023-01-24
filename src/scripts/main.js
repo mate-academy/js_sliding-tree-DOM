3c0a84dfe61cf2bbd678a2fea6e8725121786679
abobyr <artem.bobyr@dataart.com>
2023-01-24 23:19:01 +0200

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,31 @@
 'use strict';
 
-const listItems = document.querySelectorAll('li')
-const list = document.querySelector('.tree')
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('.tree');
 
 listItems.forEach(item => {
   if (item.children.length > 0) {
-    item.insertAdjacentHTML("afterbegin", '<span></span>')
-    item.children[0].appendChild(item.childNodes[1])
+    item.insertAdjacentHTML('afterbegin', '<span></span>');
+    item.children[0].appendChild(item.childNodes[1]);
   }
-})
+});
 
 list.addEventListener('click', (e) => {
-  const headerEl = e.target
+  const headerEl = e.target;
+
   if (headerEl.tagName !== 'SPAN') {
-      return
-    }
+    return;
+  }
 
-    [...headerEl.nextElementSibling.children].forEach(li => {
-      li.hidden = !li.hidden;
+  [...headerEl.nextElementSibling.children].forEach(li => {
+    li.hidden = !li.hidden;
 
-      // const isHidden = li.style.display === 'none';
-      //
-      // if (isHidden) {
-      //   li.style.display = 'block';
-      // } else {
-      //   li.style.display = 'none';
-      // }
-    })
-  })
+    // const isHidden = li.style.display === 'none';
+    //
+    // if (isHidden) {
+    //   li.style.display = 'block';
+    // } else {
+    //   li.style.display = 'none';
+    // }
+  });
+});
