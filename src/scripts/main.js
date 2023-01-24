930c4b11e802bd01d94203ef835ec579952514aa
abobyr <artem.bobyr@dataart.com>
2023-01-24 23:18:06 +0200

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li')
+const list = document.querySelector('.tree')
+
+listItems.forEach(item => {
+  if (item.children.length > 0) {
+    item.insertAdjacentHTML("afterbegin", '<span></span>')
+    item.children[0].appendChild(item.childNodes[1])
+  }
+})
+
+list.addEventListener('click', (e) => {
+  const headerEl = e.target
+  if (headerEl.tagName !== 'SPAN') {
+      return
+    }
+
+    [...headerEl.nextElementSibling.children].forEach(li => {
+      li.hidden = !li.hidden;
+
+      // const isHidden = li.style.display === 'none';
+      //
+      // if (isHidden) {
+      //   li.style.display = 'block';
+      // } else {
+      //   li.style.display = 'none';
+      // }
+    })
+  })
