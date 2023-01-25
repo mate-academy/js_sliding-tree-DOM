cb27791424dfb372a2debafd09db5295d02c75c2
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-25 08:45:13 +0200

    Use ternary operator in toggleBlockElementVisibility(), move toggleBlockElementVisibility() inside if block in list.addEventListener()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,29 @@
-'use strict';
+"use strict";
 
-const list = document.querySelector('.tree');
-const listItems = document.querySelectorAll('li');
+const list = document.querySelector(".tree");
+const listItems = document.querySelectorAll("li");
 
 listItems.forEach((li) => {
   const children = li.children;
 
-  if (children.length > 0 && children[0].tagName === 'UL') {
-    const wrap = document.createElement('span');
+  if (children.length > 0 && children[0].tagName === "UL") {
+    const wrap = document.createElement("span");
 
     li.insertBefore(wrap, li.firstChild);
     wrap.appendChild(li.childNodes[1]);
   }
 });
 
-list.addEventListener('click', (e) => {
+list.addEventListener("click", (e) => {
   const item = e.target;
 
-  if (item.tagName !== 'SPAN') {
-    return;
+  if (item.tagName === "SPAN") {
+    toggleBlockElementVisibility(item.nextElementSibling);
   }
-
-  toggleBlockElementVisibility(item.nextElementSibling);
 });
 
 const toggleBlockElementVisibility = (el) => {
-  const isHidden = el.style.display === 'none';
+  const isHidden = el.style.display === "none";
 
-  if (isHidden) {
-    el.style.display = 'block';
-  } else {
-    el.style.display = 'none';
-  }
+  isHidden ? (el.style.display = "block") : (el.style.display = "none");
 };
