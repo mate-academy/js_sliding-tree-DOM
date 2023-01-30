2e1ba579e883c63cd59f46b20ce895ac0760c3dd
Anzhelika Shyrokova <shirokovaanzhela@gmail.com>
2023-01-30 17:00:41 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const items = [ ...document.querySelectorAll('li') ];
+const tree = document.querySelector('.tree');
+
+items.forEach(item => {
+  const index = item.innerText.indexOf('\n');
+  let itemName = '';
+
+  if (index !== -1) {
+    itemName = item.innerText.slice(0, index + 1);
+  } else {
+    itemName = item.innerText;
+  }
+
+  const wrap = document.createElement('span');
+
+  wrap.className = 'item-title';
+  wrap.innerText = itemName;
+  item.firstChild.remove();
+  item.prepend(wrap);
+});
+
+tree.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('item-title')) {
+    return;
+  }
+
+  if (e.target.nextSibling.hidden !== true) {
+    e.target.nextSibling.setAttribute('hidden', true);
+  } else {
+    e.target.nextSibling.removeAttribute('hidden');
+  };
+});
