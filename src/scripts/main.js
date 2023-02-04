aede4eb8ccdd1845741d30faeafea1ca0e0c3de0
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-02-04 15:28:27 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
+const branches = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+for (const branch of branches) {
+  const span = document.createElement('span');
+
+  branch.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', e => {
+  e.target.nextSibling.hidden = !e.target.nextSibling.hidden;
+});
 // write code here
