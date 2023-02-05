1d59a4af3f41438253f85bb0776b3f4311d1ac3e
Aleksey-Aleksey <l0734062646@gmail.com>
2023-02-05 23:31:57 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const list = document.querySelectorAll('li');
+
+for (const el of list) {
+  const span = document.createElement('span');
+
+  el.prepend(span);
+  span.prepend(span.nextSibling);
+}
+
+tree.addEventListener('click', (e) => {
+  const item = e.target.closest('li').children[1];
+
+  item.hidden = !item.hidden;
+});
