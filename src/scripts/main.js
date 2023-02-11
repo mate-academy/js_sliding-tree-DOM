dfbec3f4f407ae925b8aea7674d470fef307228b
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-11 20:33:59 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,14 +4,16 @@ const tree = document.querySelector('.tree');
 
 tree.addEventListener('click', e => {
   const child = e.target.children[0];
-  const height = +getComputedStyle(child).height.slice(0, -2);
+  const height = child ? +getComputedStyle(child).height.slice(0, -2) : null;
 
-  if (child && height > 0) {
-    child.style.cssText = 'height: 0; width: 0; opacity: 0;';
-  }
+  if (child) {
+    if (child.tagName === 'UL' && height > 0) {
+      child.style.cssText = 'height: 0; width: 0; opacity: 0;';
+    }
 
-  if (child && height === 0) {
-    child.style.cssText
-      = 'height: fit-content; width: fit-content; opacity: 1;';
+    if (child.tagName === 'UL' && height === 0) {
+      child.style.cssText
+        = 'height: fit-content; width: fit-content; opacity: 1;';
+    }
   }
 });
