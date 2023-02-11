09ef29746834f53cd2267a997fefd0270f5b2a55
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-11 20:13:25 +0200

    task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', e => {
+  const child = e.target.children[0];
+  const height = +getComputedStyle(child).height.slice(0, -2);
+
+  if (child && height > 0) {
+    child.style.cssText = 'height: 0; width: 0; opacity: 0;';
+  }
+
+  if (child && height === 0) {
+    child.style.cssText
+      = 'height: fit-content; width: fit-content; opacity: 1;';
+  }
+});
