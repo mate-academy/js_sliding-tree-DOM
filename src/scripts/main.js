9f796dd6d1e8d8fe61849dd4cdb82f293c085f36
Jaxxxon78 <jaxxxon78@ukr.net>
2023-02-13 18:04:06 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+document.querySelectorAll('li').forEach(elem => {
+  const span = document.createElement('span');
+
+  elem.prepend(span);
+  span.prepend(span.nextSibling);
+});
+
+tree.addEventListener('click', el => {
+  const item = el.target.closest('li').children[1];
+
+  if (!item) {
+    return;
+  }
+  item.hidden = !item.hidden;
+});
