79246cacabb4917202ab91fb503574dacc5417df
haduigon <haduigon@gmail.com>
2023-02-15 01:15:35 +0200

    seconde commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,60 +2,41 @@
 
 // write code here
 
-const tree = document.querySelector('.tree')
+const tree = document.querySelector('.tree');
 
-treeRecursion([tree][0])
+treeRecursion([tree][0]);
 
 function treeRecursion(elem) {
-
-
   if (elem.children.length !== 0) {
+    const index = elem.innerHTML.indexOf('\n');
 
-    let index = elem.innerHTML.indexOf('\n')
     if (index > 0) {
-      let oldString = elem.innerHTML.slice(0, elem.innerHTML.indexOf('\n'))
+      const oldString = elem.innerHTML.slice(0, elem.innerHTML.indexOf('\n'));
 
-      let temp = `<span>${oldString}</span>`
+      const temp = `<span>${oldString}</span>`;
 
-      let oldString2 = elem.innerHTML.replace(oldString, temp)
+      const oldString2 = elem.innerHTML.replace(oldString, temp);
 
-      elem.innerHTML = oldString2
+      elem.innerHTML = oldString2;
 
-      console.log(elem.children[0].setAttribute('data-toggle-id', oldString))
-      console.log(elem.children[1].setAttribute('id', oldString))
-
+      elem.children[0].setAttribute('data-toggle-id', oldString);
+      elem.children[1].setAttribute('id', oldString);
     }
-            if (typeof elem.firstChild.tagName === 'LI') {
-
-            }
 
     for (let i = 0; i < elem.children.length; i++) {
-
-
-      treeRecursion(elem.children[i])
+      treeRecursion(elem.children[i]);
     }
-
   }
-
 }
 
-let li = document.getElementsByTagName('li')
-
-let fruit = document.querySelectorAll('span')
-
-const body = document.querySelector('body')
-
 tree.addEventListener('click', () => {
+  const id = event.target.dataset.toggleId;
 
- let id = event.target.dataset.toggleId;
+  if (!id) {
+    return;
+  }
 
- if (!id) return;
+  const elem = document.getElementById(id);
 
- let elem = document.getElementById(id);
-
-elem.hidden = !elem.hidden
-
-})
-
-
-
+  elem.hidden = !elem.hidden;
+});
