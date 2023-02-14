fb46c33346a95266a0b6580748deee5a42b5cac1
haduigon <haduigon@gmail.com>
2023-02-15 01:02:07 +0200

    first sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,61 @@
 'use strict';
 
 // write code here
+
+const tree = document.querySelector('.tree')
+
+treeRecursion([tree][0])
+
+function treeRecursion(elem) {
+
+
+  if (elem.children.length !== 0) {
+
+    let index = elem.innerHTML.indexOf('\n')
+    if (index > 0) {
+      let oldString = elem.innerHTML.slice(0, elem.innerHTML.indexOf('\n'))
+
+      let temp = `<span>${oldString}</span>`
+
+      let oldString2 = elem.innerHTML.replace(oldString, temp)
+
+      elem.innerHTML = oldString2
+
+      console.log(elem.children[0].setAttribute('data-toggle-id', oldString))
+      console.log(elem.children[1].setAttribute('id', oldString))
+
+    }
+            if (typeof elem.firstChild.tagName === 'LI') {
+
+            }
+
+    for (let i = 0; i < elem.children.length; i++) {
+
+
+      treeRecursion(elem.children[i])
+    }
+
+  }
+
+}
+
+let li = document.getElementsByTagName('li')
+
+let fruit = document.querySelectorAll('span')
+
+const body = document.querySelector('body')
+
+tree.addEventListener('click', () => {
+
+ let id = event.target.dataset.toggleId;
+
+ if (!id) return;
+
+ let elem = document.getElementById(id);
+
+elem.hidden = !elem.hidden
+
+})
+
+
+
