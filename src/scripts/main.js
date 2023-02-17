40b32731a900d1becf308bea24a8d8d0ae0caeb1
ivanm2706 <ivanmorhun664@gmail.com>
2023-02-17 12:40:19 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,21 +5,26 @@ const items = document.querySelectorAll('li');
 
 items.forEach(e => {
   const span = document.createElement('span');
+
   span.innerText = e.childNodes[0].textContent;
   e.childNodes[0].textContent = '';
-  e.prepend(span)
-})
+  e.prepend(span);
+});
 
 list.addEventListener('click', showList);
 
 function showList(e) {
   const ul = e.target.closest('li').querySelector('ul');
-  if (!ul) return;
-  
-  if (ul.hidden) {
-    ul.hidden = false;
+
+  if (!ul) {
     return;
   }
-  
+
+  if (ul.hidden) {
+    ul.hidden = false;
+
+    return;
+  }
+
   ul.hidden = true;
 }
