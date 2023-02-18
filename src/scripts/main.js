4eee4ae6e00331e6df09f4dd5a41d7a5f307c0c6
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-02-18 19:26:27 +0200

    Solution sliding-tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+[...document.querySelectorAll('li')].forEach(item => {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+});
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', e => {
+  const item = e.target.closest('li').children[1];
+
+  if (!item) {
+    return;
+  }
+
+  item.hidden = !item.hidden;
+});
