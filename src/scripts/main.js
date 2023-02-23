17e18301c7268ca596a59b5ff68f7a0225645bb5
Margarita Chorniak <chorniak.rita@gmail.com>
2023-02-23 14:33:57 +0200

    js_sliding-tree-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+for (const li of document.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const container = e.target.parentNode.querySelector('ul');
+
+  if (!container) {
+    return;
+  }
+
+  container.hidden = !container.hidden;
+});
