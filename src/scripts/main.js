2d7a1ded66979cd24efe1f2f21aeeaf37a052210
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-02-24 01:26:43 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+const ulListRef = document.querySelector('.tree');
+
+ulListRef.addEventListener('click', (e) => {
+  const item = e.target;
+  const listChildrens = [...item.children];
+  const isLastLi = listChildrens.length === 0;
+  const ul = listChildrens[0];
+
+  if (isLastLi) {
+    return false;
+  }
+
+  if (ul.hidden === true) {
+    ul.hidden = false;
+  } else {
+    ul.hidden = true;
+  }
+});
