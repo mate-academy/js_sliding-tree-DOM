8036a5dd25d4e02f9f81caa284cc84205751a464
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-02-24 14:20:59 +0200

    Fixed problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,16 +6,12 @@ const ulListRef = document.querySelector('.tree');
 ulListRef.addEventListener('click', (e) => {
   const item = e.target;
   const listChildrens = [...item.children];
-  const isLastLi = listChildrens.length === 0;
+  const isLastLi = !listChildrens.length;
   const ul = listChildrens[0];
 
   if (isLastLi) {
     return false;
   }
 
-  if (ul.hidden === true) {
-    ul.hidden = false;
-  } else {
-    ul.hidden = true;
-  }
+  ul.hidden = !ul.hidden
 });
