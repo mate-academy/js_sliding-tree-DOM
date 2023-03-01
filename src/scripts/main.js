7092b24ef940a5951a760f4b86dc103bf931927f
Iryna Konovalova <iruna191020@gmail.com>
2023-03-01 11:59:10 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const items = document.body.querySelectorAll('li');
+const list = document.body.querySelector('.tree');
+
+items.forEach(item => {
+  const itemInnerText = item.firstChild.nodeValue;
+  const span = document.createElement('span');
+
+  span.innerHTML = itemInnerText;
+  span.dataset.clickCount = 0;
+  item.firstChild.replaceWith(span);
+});
+
+list.addEventListener('click', toHide);
+
+function toHide(e) {
+  const parentChildren = [...e.target.parentElement.children];
+  const target = e.target;
+
+  if (target !== 'span' && !target.children) {
+    return;
+  }
+
+  if (!list.contains(target)) {
+    return;
+  }
+
+  target.dataset.clickCount++;
+
+  parentChildren.forEach((child) => {
+    if (
+      parentChildren.indexOf(child) !== 0
+      && target.dataset.clickCount % 2 !== 0
+    ) {
+      child.hidden = true;
+    } else {
+      child.hidden = '';
+    }
+  });
+}
