06970f7509d3f4788e47aa2566fc587dd75ed6e1
tarasnakonechnyi <nakonechnyi.taras@gmail.com>
2023-03-03 19:53:52 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const items = Array.from(document.querySelectorAll('li'));
+
+items.forEach(item => {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(item.childNodes[1]);
+
+  item.addEventListener('click', (ev) => {
+    if (ev.isClick) {
+      return;
+    }
+    ev.isClick = true;
+    item.childNodes[1].hidden = !item.childNodes[1].hidden;
+  });
+});
