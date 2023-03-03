d4593eecb1d880add1c63f89037935d2d9490aca
Oleksandr <98667890az@gmail.com>
2023-03-03 14:48:36 +0200

    Sliding tree DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const links = document.querySelectorAll('li');
+
+for (const li of links) {
+  const span = document.createElement('span');
+  const text = li.firstChild.textContent;
+  li.firstChild.textContent = '';
+
+  span.innerHTML = `${text}`;
+  li.prepend(span);
+}
+
+tree.addEventListener('click', (e) => {
+  const element = e.target.closest('li');
+
+  for (const item of element.children) {
+    if (!item.hasAttribute('hidden') && item.querySelector('span')) {
+      item.setAttribute('hidden', true);
+    } else {
+      item.removeAttribute('hidden');
+    }
+  }
+});
