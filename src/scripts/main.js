c00f83f1660a7df61d8f3d9677cc5fa4856a40bf
tarasnakonechnyi <nakonechnyi.taras@gmail.com>
2023-03-04 14:32:44 +0200

    Solution 3.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,15 @@
 'use strict';
 
 const items = Array.from(document.querySelectorAll('li'));
+const tree = document.querySelector('.tree');
 
 items.forEach(item => {
   const span = document.createElement('span');
 
   item.prepend(span);
   span.append(item.childNodes[1]);
+});
 
-  span.addEventListener('click', (ev) => {
-    if (ev.isClick) {
-      return;
-    }
-
-    ev.isClick = true;
-    item.childNodes[1].hidden = !item.childNodes[1].hidden;
-  });
+tree.addEventListener('click', (ev) => {
+  ev.target.nextSibling.hidden = !ev.target.nextSibling.hidden;
 });
