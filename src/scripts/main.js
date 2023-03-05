59bfedf1cc413470aeb4d4f73b90b7d96916da80
Vadim Drobyazko <volandemortuwka@gmail.com>
2023-03-05 15:45:55 +0200

    Sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+for (const item of li) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', e => {
+  const item = e.target.closest('li').children[1];
+
+  if (!item) {
+    return;
+  }
+
+  item.hidden = !item.hidden;
+});
