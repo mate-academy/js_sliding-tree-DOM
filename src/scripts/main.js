784c297307af60d27e2f9471400557f4288f535e
Serhii Nikolin <sergeynikolin@gmail.com>
2023-03-05 20:31:58 +0200

    Ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+const unList = tree.querySelectorAll('ul');
+
+for (const li of list) {
+  if (li.children.length > 0) {
+    const liTitle = li.firstChild.data.trim();
+    const span = document.createElement('span');
+
+    span.innerText = liTitle;
+    li.firstChild.remove();
+    li.prepend(span);
+  }
+};
+
+for (const ul of unList) {
+  ul.style.maxHeight = ul.offsetHeight + 'px';
+  ul.style.overflow = 'hidden';
+  ul.style.transition = 'max-height 0.5s';
+}
+
+function slideList(e) {
+  const targetUnList = e.target.nextElementSibling;
+  const currentHeight = targetUnList.offsetHeight;
+
+  if (currentHeight === 0) {
+    targetUnList.style.maxHeight = `270px`;
+  } else {
+    targetUnList.style.maxHeight = 0;
+
+    for (const item of targetUnList.querySelectorAll('ul')) {
+      item.style.maxHeight = 0;
+    }
+  }
+};
+
+tree.addEventListener('click', slideList);
