a57a1d5117c873eb177a4e2e2d285fd6326f3241
Mykola Huryn <mukola197022@gmail.com>
2023-03-16 12:46:16 +0100

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const items = document.querySelectorAll('li');
+
+for (const item of items) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+}
+
+list.addEventListener('click', e => {
+  const sibling = e.target.closest('span');
+
+  if (sibling.nextSibling.hidden === false) {
+    sibling.nextSibling.hidden = true;
+  } else {
+    sibling.nextSibling.hidden = false;
+  }
+});
