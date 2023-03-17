ee6c1bcb776581f4b4715328173ff521af94f745
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-17 18:52:05 +0100

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liAll = document.querySelectorAll('li');
+
+for (const item of liAll) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', (e) => {
+  const item = e.target;
+  const child = item.closest('li').children[1];
+
+  if (!child) {
+    return;
+  }
+
+  child.hidden = !child.hidden;
+});
