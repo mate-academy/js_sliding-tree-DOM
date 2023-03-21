e42aec2c37b4fccea87bf2dfb2a41f690380aea4
Serhii Melnychenko <8melya8@gmail.com>
2023-03-21 15:25:29 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+for (const item of li) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+
+  document.addEventListener('click', e => {
+    if (e.target.closest('span')) {
+      e.target.nextSibling.hidden = !e.target.nextSibling.hidden;
+    }
+  });
+}
