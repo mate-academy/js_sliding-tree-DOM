27ae6dd6ec90216a57cfdc422d62bff0221f9b82
IvanVaverchak <ivanvaverchak@2000gmail.com>
2023-03-24 11:36:47 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const ul = document.querySelectorAll('ul');
+
+list.forEach(element => {
+  if ([...ul].includes(element.firstElementChild)) {
+    const newHtml = `<span>${element.childNodes[0].textContent}</span>`;
+
+    element.childNodes[0].remove();
+    element.insertAdjacentHTML('afterbegin', newHtml);
+  }
+});
+
+addEventListener('click', e => {
+  const span = document.querySelectorAll('span');
+
+  span.forEach(element => {
+    if (element === e.target) {
+      const nextElement = element.nextElementSibling;
+
+      if (nextElement.hidden === true) {
+        nextElement.hidden = false;
+      } else {
+        nextElement.hidden = true;
+      }
+    }
+  });
+});
