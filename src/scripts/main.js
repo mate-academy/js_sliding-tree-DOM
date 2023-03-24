b952f7506ed185eab8bdd2ae07cba4706e3ab680
Sergey Lipikhin <football3@ukr.net>
2023-03-24 21:40:40 +0200

    Complete solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const treeListElement = document.querySelector('.tree');
+
+treeListElement.addEventListener('click', (eventClick) => {
+  if (!eventClick.target.matches('li span')) {
+    return;
+  }
+
+  const innerList = eventClick.target.parentElement.lastElementChild;
+
+  if (!innerList) {
+    return;
+  }
+
+  innerList.style.display = innerList.style.display === 'none'
+    ? innerList.style.display = 'revert'
+    : innerList.style.display = 'none';
+});
+
+treeListElement.querySelectorAll('.tree li').forEach(liElement => {
+  const spanElement = document.createElement('span');
+
+  spanElement.textContent = liElement.firstChild.textContent.trim();
+
+  if (liElement.children.length) {
+    liElement.firstChild.replaceWith(spanElement);
+  }
+});
