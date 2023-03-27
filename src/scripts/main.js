1b2d459f9d757b70355b9203d8cce2d3cc5fcad4
Anastasiia Em <ema199625@gmail.com>
2023-03-27 18:34:31 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const headers = document.querySelectorAll('li');
+
+for (const header of headers) {
+  const span = document.createElement('span');
+
+  header.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', (e) => {
+  if (e.target.closest('span').nextSibling.style.display === 'none') {
+    e.target.closest('span').nextSibling.style.display = 'block';
+  } else {
+    e.target.closest('span').nextSibling.style.display = 'none';
+  }
+});
