56b43a1d37f916e3b98663c9a2079221cf3d3159
Diana Snihur <dianasnigur2@gmail.com>
2023-04-05 18:24:56 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const headers = tree.querySelectorAll('li');
+
+for (let i = 0; i < headers.length; i++) {
+  const header = headers[i];
+  const text = header.firstChild.textContent;
+  const span = document.createElement('span');
+
+  span.textContent = text;
+  header.replaceChild(span, header.firstChild);
+}
+
+const spanElement = document.querySelectorAll('span');
+
+spanElement.forEach(item => {
+  const list = item.nextElementSibling;
+
+  item.addEventListener('click', () => {
+    if (list.style.display === 'none') {
+      list.style.display = '';
+    } else {
+      list.style.display = 'none';
+    }
+  });
+});
