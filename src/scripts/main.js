04ac18757e151bb9ad5816b1376f4f9447d29323
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-04-09 13:46:37 +0300

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const li = document.querySelectorAll('li');
+
+for (const item of li) {
+  if (item.children.length) {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+    span.append(span.nextSibling);
+  };
+}
+
+list.addEventListener('click', e => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const element = e.target.parentNode.children[1];
+
+  element.hidden = !element.hidden;
+});
