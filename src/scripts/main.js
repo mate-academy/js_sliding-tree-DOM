ac326e2d481143ee05f424bee0c75ed4d40d884d
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-11 23:51:35 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+function addSpan([...elements]) {
+  elements.forEach(item => {
+    const listItem = item.querySelector('ul')
+    if(item.children.length > 0) {
+      const textHead = item.firstChild.textContent.trim();
+      const span = document.createElement('span');
+
+      span.innerText = `${textHead}`;
+      item.firstChild.textContent = '';
+      item.prepend(span);
+
+      span.addEventListener('click', (e) =>{
+        if(!listItem.hidden) {
+          listItem.hidden = true;
+        } else {
+          listItem.hidden = false;
+        }
+      })
+    }
+  })
+}
+
+addSpan(li);
