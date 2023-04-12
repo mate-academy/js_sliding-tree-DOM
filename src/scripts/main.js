3d003fd5490403ba699fe59e23cc403a96fafd40
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-04-12 15:27:55 +0300

    the task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+list.forEach(el => {
+  if (el.children.length) {
+    const span = document.createElement('span');
+
+    el.prepend(span);
+    span.append(span.nextSibling);
+  }
+});
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', e => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  e.target.nextSibling.hidden = !e.target.nextSibling.hidden;
+});
