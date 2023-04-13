672243993a3a038e198bcb81022af9fe9a394533
Anna Grechana <grechanaanna@gmial.com>
2023-04-13 16:16:37 +0300

    1st version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const listOfli = document.querySelectorAll('li');
+
+for (const li of listOfli) {
+  const span = document.createElement('span');
+
+  if (li.children.length !== 0) {
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+list.addEventListener('click', (events) => {
+  const item = events.target.matches('span');
+
+  if (!item) {
+    return;
+  }
+
+  const itemCliked = events.target;
+
+  if (itemCliked.nextElementSibling.hidden === true) {
+    itemCliked.nextElementSibling.hidden = false;
+  } else {
+    itemCliked.nextElementSibling.hidden = true;
+  }
+}
+);
