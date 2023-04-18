0462768afafaf9950d482646900154046f52e0b5
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-04-18 16:18:35 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const ulElements = document.querySelectorAll('ul');
+const tree = document.querySelector('.tree');
+
+ulElements.forEach((ul) => {
+  if (ul.parentNode.tagName === 'LI') {
+    const span = document.createElement('span');
+
+    span.classList.add('header');
+
+    span.innerHTML = ul.parentNode.firstChild.textContent.trim();
+    ul.parentNode.firstChild.replaceWith(span);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.classList.contains('header')) {
+    const ul = e.target.nextElementSibling;
+
+    if (ul.style.display === 'none') {
+      ul.style.display = 'block';
+    } else {
+      ul.style.display = 'none';
+    }
+  }
+});
