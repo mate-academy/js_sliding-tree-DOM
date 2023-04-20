c79b60d77ef27073dca1e182d0e426129bed1359
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-04-20 11:50:17 +0200

    hidden

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,25 +3,21 @@
 const ulElements = document.querySelectorAll('ul');
 const tree = document.querySelector('.tree');
 
-ulElements.forEach((ul) => {
-  if (ul.parentNode.tagName === 'LI') {
+ulElements.forEach((element) => {
+  if (element.parentNode.tagName === 'LI') {
     const span = document.createElement('span');
 
     span.classList.add('header');
 
-    span.innerHTML = ul.parentNode.firstChild.textContent.trim();
-    ul.parentNode.firstChild.replaceWith(span);
+    span.innerHTML = element.parentNode.firstChild.textContent.trim();
+    element.parentNode.firstChild.replaceWith(span);
   }
 });
 
-tree.addEventListener('click', (e) => {
-  if (e.target.classList.contains('header')) {
-    const ul = e.target.nextElementSibling;
+tree.addEventListener('click', (clickEvent) => {
+  if (clickEvent.target.classList.contains('header')) {
+    const listElements = clickEvent.target.nextElementSibling;
 
-    if (ul.style.display === 'none') {
-      ul.style.display = 'block';
-    } else {
-      ul.style.display = 'none';
-    }
+    listElements.hidden = !listElements.hidden;
   }
 });
