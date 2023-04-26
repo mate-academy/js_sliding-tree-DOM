a78cbaaaae9111675720bdfe8b5d19579851bc3a
Victor Buldenko <gookidoo@gmail.com>
2023-04-26 17:56:28 +0300

    show\hide list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', e => {
+  const list = e.target.firstElementChild;
+
+  if (list.tagName === 'UL') {
+    if (list.style.display === 'none') {
+      list.style.display = 'block';
+    } else {
+      list.style.display = 'none';
+    }
+  }
+});
