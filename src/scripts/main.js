d3d7574da91f24006ece3ed30393ec079af5a186
Victor Buldenko <gookidoo@gmail.com>
2023-04-27 00:06:48 +0300

    + check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ const tree = document.querySelector('.tree');
 tree.addEventListener('click', e => {
   const list = e.target.firstElementChild;
 
-  if (list.tagName === 'UL') {
+  if (list && list.tagName === 'UL') {
     if (list.style.display === 'none') {
       list.style.display = 'block';
     } else {
