9786e2243a7a4573d07f213c604009b12a653ccb
Oksana Tytanych <oksanatytanych@gmail.com>
2023-04-27 18:11:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', (e) => {
+  const item = e.target.children[0];
+
+  if (item) {
+    const isHidden = item.style.display === 'none';
+
+    isHidden ? item.style.display = 'block' : item.style.display = 'none';
+  }
+});
