a48092fe9e11a3eb801dcba3c7e7d247d3be7761
Vladyslav Fedenko <vadondrummer@gmail.com>
2023-04-30 14:36:33 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const liElems = document.querySelectorAll('li');
+const list = document.querySelector('.tree');
+
+for (const item of liElems) {
+  const spanEl = document.createElement('span');
+
+  item.prepend(spanEl);
+  spanEl.append(spanEl.nextSibling);
+}
+
+list.addEventListener('click', (evnt) => {
+  const item = evnt.target;
+
+  if (item.nextSibling === null) {
+    return;
+  }
+
+  if (item.nextSibling.style.display === 'none') {
+    item.nextSibling.style.display = 'block';
+
+    return;
+  }
+
+  item.nextSibling.style.display = 'none';
+});
