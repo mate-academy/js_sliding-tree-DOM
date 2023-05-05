e2a489a3f27409faf5a2a008e9103c68000aa166
Oksana Skuba <skuba.o.ua@gmail.com>
2023-05-05 21:10:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liAll = document.querySelectorAll('li');
+
+liAll.forEach(li => {
+  const span = document.createElement('span');
+
+  span.innerText = li.firstChild.textContent;
+  li.replaceChild(span, li.firstChild);
+}
+);
+
+tree.addEventListener('click', e => {
+  const targetList = e.target.parentNode.querySelector('ul');
+
+  if (e.target.tagName !== 'SPAN' || !targetList) {
+    return;
+  }
+
+  targetList.style.display = targetList.style.display === 'none'
+    ? 'block' : 'none';
+});
