ca5ede8339dd7ffcc7138eb323dd0c4c51f3fbd6
Roman Ostrous <ostrous.roman@gmail.com>
2023-05-06 17:20:19 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const li = document.querySelectorAll('li');
+
+for (const item of li) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', e => {
+  const element = e.target.closest('li').children[1];
+
+  if (!element) {
+    return;
+  }
+
+  element.hidden = !element.hidden;
+});
