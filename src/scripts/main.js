b4782f8c4d0ec82090928860c9df8ba430c8dec7
Maria Shmakova <masha.shmakova@gmail.com>
2023-05-11 17:01:11 +0300

    tree were created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+for (const item of list) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', e => {
+  const item = e.target.nextSibling;
+
+  if (!item) {
+    return;
+  }
+
+  item.hidden = !item.hidden;
+});
