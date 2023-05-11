cf8230810c70e36c5941777035083965b3289ad1
OlegRak86 <rakolegit@gmail.com>
2023-05-11 13:43:02 +0300

    js_sliding-tree-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const myList = document.querySelector('.tree');
+
+const listItems = myList.getElementsByTagName('li');
+
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].hidden = false;
+
+  const newSpan = document.createElement('span');
+
+  listItems[i].insertAdjacentElement('beforebegin', newSpan);
+  newSpan.append(listItems[i]);
+}
+
+const add = (e) => {
+  if (!e.target.children.length || e.target.tagName === 'UL') {
+    return;
+  }
+
+  e.target.children[0].hidden = !e.target.children[0].hidden;
+};
+
+myList.addEventListener('click', add);
