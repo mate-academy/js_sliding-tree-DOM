ea8fbbd3d5f51b590bf9c32ddf00ca21d0c4a430
Yura Enttsel <equikmla@gmail.com>
2023-05-15 23:30:33 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const liAll = document.querySelectorAll('li');
+const ulAll = document.querySelectorAll('ul');
+
+for (const li of liAll) {
+  for (const ul of ulAll) {
+    if (li.contains(ul)) {
+      li.classList.add('tree_header');
+    }
+  }
+}
+
+document.querySelector('.tree').addEventListener('click', e => {
+  const item = e.target;
+
+  if (!item.classList.contains('tree_header')) {
+    return;
+  }
+
+  const itemToRoll = item.firstElementChild;
+
+  (itemToRoll.hidden) ? itemToRoll.hidden = false : itemToRoll.hidden = true;
+});
