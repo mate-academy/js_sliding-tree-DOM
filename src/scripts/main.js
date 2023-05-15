6119d665d86ce7f9bab9fa74906d20f8a1384029
Yura Enttsel <equikmla@gmail.com>
2023-05-16 00:47:34 +0300

    Solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,11 @@ const liAll = document.querySelectorAll('li');
 const ulAll = document.querySelectorAll('ul');
 
 for (const li of liAll) {
+  const span = document.createElement('span');
+
+  span.append(li.firstChild);
+  li.prepend(span);
+
   for (const ul of ulAll) {
     if (li.contains(ul)) {
       li.classList.add('tree_header');
@@ -14,11 +19,11 @@ for (const li of liAll) {
 document.querySelector('.tree').addEventListener('click', e => {
   const item = e.target;
 
-  if (!item.classList.contains('tree_header')) {
+  if (!item.parentElement.classList.contains('tree_header')) {
     return;
   }
 
-  const itemToRoll = item.firstElementChild;
+  const itemToRoll = item.nextSibling;
 
   (itemToRoll.hidden) ? itemToRoll.hidden = false : itemToRoll.hidden = true;
 });
