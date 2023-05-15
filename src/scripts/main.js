3d48b63c37920ca215511e503cfd4f67fe84e0e8
Valeria Oleynik <oleynik.valeria27@gmail.com>
2023-05-15 17:35:21 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+li.forEach(elem => {
+  const span = document.createElement('span');
+
+  elem.prepend(span);
+  span.append(span.nextSibling);
+});
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', function(e) {
+  const item = e.target.closest('li');
+
+  if (!item || !tree.contains(item)) {
+    return;
+  }
+
+  const childrens = e.target.parentElement.querySelector('ul');
+
+  if (!childrens) {
+    return;
+  }
+
+  childrens.hidden = !childrens.hidden;
+});
