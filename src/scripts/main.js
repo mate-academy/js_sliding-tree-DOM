6ae5b14af755723177218c0a2410cda072d28f84
Yura Enttsel <equikmla@gmail.com>
2023-05-16 11:22:11 +0300

    Fixes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const liAll = document.querySelectorAll('li');
-const ulAll = document.querySelectorAll('ul');
+const allItemsOfTree = document.querySelectorAll('li');
+const allListsOfTree = document.querySelectorAll('ul');
 
-for (const li of liAll) {
+for (const li of allItemsOfTree) {
   const span = document.createElement('span');
 
   span.append(li.firstChild);
   li.prepend(span);
 
-  for (const ul of ulAll) {
+  for (const ul of allListsOfTree) {
     if (li.contains(ul)) {
       li.classList.add('tree_header');
     }
@@ -22,8 +22,5 @@ document.querySelector('.tree').addEventListener('click', e => {
   if (!item.parentElement.classList.contains('tree_header')) {
     return;
   }
-
-  const itemToRoll = item.nextSibling;
-
-  (itemToRoll.hidden) ? itemToRoll.hidden = false : itemToRoll.hidden = true;
+  item.nextSibling.toggleAttribute('hidden');
 });
