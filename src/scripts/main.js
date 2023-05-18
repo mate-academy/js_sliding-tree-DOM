ba238ac192f46746d00c3df515b73a38a8e00f73
DefaultGitUser <usingforgit@gmail.com>
2023-05-18 17:23:51 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const liElements = [...document.querySelectorAll('ul > li')];
+
+for (const element of liElements) {
+  const span = document.createElement('span');
+
+  span.innerText = element.firstChild.data.trim();
+
+  if (element.firstElementChild) {
+    element.firstChild.replaceWith(span);
+  }
+}
+
+document.querySelector('.tree').addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+  }
+});
