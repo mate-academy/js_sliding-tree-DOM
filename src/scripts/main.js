f6798c1bf1a9a3918a3f2ae023d2669534d577db
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-05-18 22:13:59 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
+/* eslint-disable */
 
-// write code here
+const tree = document.querySelector('.tree');
+tree.addEventListener('click', (event) => {
+  const targetElement = event.target;
+
+  if (!targetElement.contains(targetElement.querySelector('ul'))) {
+    return;
+  }
+
+  targetElement.querySelector('ul').hidden = !targetElement.querySelector('ul').hidden;
+  targetElement.style.fontWeight = targetElement.style.fontWeight === 'bold' ? 'normal' : 'bold';
+
+});
