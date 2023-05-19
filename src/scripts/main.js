f7fb81258119966215e61a6f63ee709456433b01
Yelyzaveta <molochiza@gmail.com>
2023-05-19 15:04:35 +0300

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,18 +4,20 @@ const list = document.querySelector('ul');
 const listItems = list.querySelectorAll('li');
 
 listItems.forEach(li => {
-  const span = document.createElement('span');
+  if (li) {
+    const span = document.createElement('span');
 
-  li.prepend(span);
-
-  span.append(span.nextSibling);
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
 });
 
 list.addEventListener('click', (e) => {
-  const item = e.target.closest('li').children[1];
-
-  if (!item) {
+  if (!e.target.closest('li')) {
     return;
   }
-  item.hidden ? item.hidden = false : item.hidden = true;
+
+  const item = e.target.closest('li').children[1];
+
+  item.hidden = !item.hidden;
 });
