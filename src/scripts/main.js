e78de2ddfdd67497b25dc41316fd3d8d22a98945
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-05-19 16:21:39 +0300

    js_sliding-tree-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const list = document.querySelector('.tree');
+
+  chooseItemWithSublist(list);
+
+  function chooseItemWithSublist(table) {
+    table.querySelectorAll('li').forEach(item => {
+      if (item.children.length > 0) {
+        const innerText = item.firstChild.textContent;
+
+        item.firstChild.textContent = '';
+
+        item.insertAdjacentHTML('afterbegin', `<span>${innerText}</span>`);
+      }
+    });
+  }
+
+  list.addEventListener('click', e => {
+    if (e.target.closest('span')) {
+      const subList = e.target.closest('span').nextElementSibling;
+
+      // subList.style.display = 'none';
+      if (getComputedStyle(subList).display !== 'none') {
+        subList.style.display = 'none';
+      } else {
+        subList.style.display = 'block';
+      }
+
+      // if (!subList.style.display) {
+
+      // }
+    };
+  });
+});
