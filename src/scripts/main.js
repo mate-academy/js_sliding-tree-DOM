b5a28246bcc9a5e21253990a9bac3d4953787120
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-05-23 16:55:20 +0200

    js sliding tree was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const listArr = document.querySelectorAll('li');
+
+listArr.forEach(list => {
+  const span = document.createElement('span');
+
+  span.append(list.firstChild);
+
+  list.insertAdjacentElement('afterbegin', span);
+});
+
+document.addEventListener('click', evetio => {
+  if (evetio.target.tagName === 'SPAN') {
+    const eventParent = evetio.target.parentElement;
+
+    eventParent.innerHTML = ``;
+
+    eventParent.append(evetio.target);
+  }
+});
