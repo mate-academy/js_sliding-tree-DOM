6a19be6f6a0a8d37579c3e35247f9bc7f2367d71
Vitalii Drabynko <vpdrabynko@gmail.com>
2023-05-23 17:51:00 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const itemsList = list.querySelectorAll('li');
+
+itemsList.forEach(li => {
+  if (li) {
+    const span = document.createElement('span');
+
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+});
+
+list.addEventListener('click', (e) => {
+  if (!e.target.closest('li')) {
+    return;
+  }
+
+  const item = e.target.closest('li').children[1];
+
+  item.hidden = !item.hidden;
+});
