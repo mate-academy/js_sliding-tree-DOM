89ee940bf6ac6b5cffc06b87a8b6b2131244fd87
Vitalii Drabynko <vpdrabynko@gmail.com>
2023-05-24 16:22:36 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,10 @@ const list = document.querySelector('ul');
 const itemsList = list.querySelectorAll('li');
 
 itemsList.forEach(li => {
-  if (li) {
-    const span = document.createElement('span');
+  const span = document.createElement('span');
 
-    li.prepend(span);
-    span.append(span.nextSibling);
-  }
+  li.prepend(span);
+  span.append(span.nextSibling);
 });
 
 list.addEventListener('click', (e) => {
