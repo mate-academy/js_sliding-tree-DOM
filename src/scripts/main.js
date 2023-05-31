a7dc8d94dbcd910690b2f353a0cfc0cb94f8c3a5
Roman <rdyakovich@gmail.com>
2023-05-31 20:19:08 +0300

    sliding

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const list = tree.querySelectorAll('li');
+
+list.forEach((item) => {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.addEventListener('click', (e) => {
+  const item = e.target.nextSibling;
+
+  if (item.style.display === 'none') {
+    item.style.display = 'block';
+  } else {
+    item.style.display = 'none';
+  }
+});
