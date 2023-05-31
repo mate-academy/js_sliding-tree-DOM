7b684061ab066f97e7ea28c32e28401c3776648e
oleg-gord <gord_oll@ukr.net>
2023-05-31 21:59:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const list = document.querySelectorAll('li');
+
+list.forEach(item => {
+  if (item.firstElementChild) {
+    const span = document.createElement('span');
+
+    span.appendChild(document.createTextNode(item.firstChild.nodeValue));
+    item.replaceChild(span, item.firstChild);
+  }
+});
+
+tree.addEventListener('click', hideShow);
+
+function hideShow(ev) {
+  const el = ev.target.nextElementSibling;
+
+  if (el.nodeName === 'UL') {
+    el.hidden = !el.hidden;
+  }
+}
