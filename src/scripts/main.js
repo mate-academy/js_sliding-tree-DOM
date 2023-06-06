f256d76358ada1b54f98ad21f6c1631ae5f1f485
Dmytro Vovk <bellboy.dv@gmail.com>
2023-06-06 16:39:31 +0300

    message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = tree.querySelectorAll('li');
+
+listItems.forEach(item => {
+  const sublist = item.querySelector('ul');
+
+  if (sublist) {
+    const span = document.createElement('span');
+    const text = item.firstChild.textContent;
+
+    span.textContent = text;
+    item.replaceChild(span, item.firstChild);
+
+    span.addEventListener('click', (e) => {
+      if (e.target === span) {
+        sublist.style.display = sublist.style.display === 'none'
+          ? 'block' : 'none';
+      }
+    });
+  }
+});
