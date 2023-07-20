3aa6067efdf7391555172fb5f3ef1320f146d7c5
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-07-20 18:47:50 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+for (let i = 0; i < li.length; i++) {
+  const span = document.createElement('span');
+  const firstChild = li[i].firstChild;
+
+  if (firstChild.textContent !== undefined) {
+    span.textContent = firstChild.textContent;
+    li[i].insertBefore(span, firstChild);
+    firstChild.textContent = '';
+  }
+}
+
+const spans = document.querySelectorAll('span');
+
+for (let i = 0; i < li.length; i++) {
+
+  spans[i].addEventListener('click', e => {
+    e.stopPropagation();
+
+    const liChilds = li[i].querySelectorAll('li');
+
+    for (let j = 0; j < liChilds.length; j++) {
+      liChilds[j].style.display !== 'none'
+        ? liChilds[j].style.display = 'none'
+        : liChilds[j].style.display = 'list-item';
+    }
+  });
+}
