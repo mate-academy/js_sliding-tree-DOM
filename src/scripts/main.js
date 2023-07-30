64e3e2543ccaafbd5e4e0300e7f3ceb723a4d764
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-07-30 22:10:19 +0300

    sliding tree _ DOM - first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', e => {
+  const children = e.target.firstElementChild.children;
+
+  for (const child of children) {
+    if (!child.style.display) {
+      child.style.display = 'none';
+    } else {
+      child.style.display = '';
+    }
+  }
+});
