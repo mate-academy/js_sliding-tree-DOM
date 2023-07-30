33e027d79e1779e0498675d4326e2069c8cd2789
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-07-31 00:24:43 +0300

    code was changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,28 @@
 'use strict';
 
 const tree = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach(item => {
+  if (item.firstElementChild) {
+    const span = document.createElement('span');
+
+    span.textContent = item.childNodes[0].textContent.trim();
+    item.childNodes[0].textContent = '';
+    item.prepend(span);
+  }
+});
 
 tree.addEventListener('click', e => {
-  const children = e.target.firstElementChild.children;
+  if (e.target.tagName === 'SPAN') {
+    const children = e.target.parentNode.lastElementChild.children;
 
-  for (const child of children) {
-    if (!child.style.display) {
-      child.style.display = 'none';
-    } else {
-      child.style.display = '';
+    for (const child of children) {
+      if (!child.style.display) {
+        child.style.display = 'none';
+      } else {
+        child.style.display = '';
+      }
     }
   }
 });
