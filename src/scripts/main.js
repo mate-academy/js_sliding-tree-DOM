fcfec27b4127e082c7119543592f9c1c7ee5b574
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-02 16:48:26 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('title')) {
+    return;
+  }
+
+  const neighbor = e.target.nextElementSibling;
+
+  if (neighbor.style.display === 'none') {
+    neighbor.style.display = 'block';
+  } else {
+    neighbor.style.display = 'none';
+  }
+});
