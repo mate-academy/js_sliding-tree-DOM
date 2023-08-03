986690d90bbbfebf70a01d813d8634e792d2d386
Oleksii Lavka <lavka.alexey37gmail.com>
2023-08-03 15:45:35 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+/* eslint-disable-next-line no-shadow */
+const toggleList = (event) => {
+  const target = event.target;
+  const listTitle = target.closest('.list-title');
+
+  if (!listTitle || !tree.contains(listTitle)) {
+    return;
+  }
+
+  const targetList = target.nextElementSibling;
+
+  targetList.hidden = !targetList.hidden;
+};
+
+tree.addEventListener('click', toggleList);
