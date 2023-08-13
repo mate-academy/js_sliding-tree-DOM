32dff6bb5f144892556da05c62596f82d2d1ad9b
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-13 21:26:51 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,25 @@
 
 const tree = document.querySelector('.tree');
 
+for (const li of tree.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  span.classList.add('title');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
 tree.addEventListener('click', (e) => {
   if (!e.target.classList.contains('title')) {
     return;
   }
 
   const neighbor = e.target.nextElementSibling;
+
+  if (!neighbor) {
+    return;
+  }
 
   if (neighbor.style.display === 'none') {
     neighbor.style.display = 'block';
