9b14e6c5d9d01b04bf1c96c334b137e11f03410c
Edgar Kocuba <edgarkocuba@gmail.com>
2023-08-23 18:51:31 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write code here
+
+const mainList = document.querySelector('.tree');
+const subList = [...mainList.getElementsByTagName('li')].filter(
+  el => el.firstElementChild
+);
+
+for (const element of subList) {
+  const span = document.createElement('span');
+
+  const firstChild = element.firstChild;
+
+  firstChild.after(span);
+  span.appendChild(firstChild);
+
+  span.addEventListener('click', e => {
+    if (element.children[1].style.display === 'none') {
+      element.children[1].style.display = 'block';
+    } else {
+      element.children[1].style.display = 'none';
+    }
+  }
+  );
+}
