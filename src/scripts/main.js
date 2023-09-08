2ea3766afd2855ac96c16e03420378a5afa70f15
Andrii Khomiak <andr.hamster@gmail.com>
2023-09-09 00:00:06 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const list = e.target.querySelector('ul');
+
+  if (e.target.tagName === 'LI' && list) {
+    list.style.display = getComputedStyle(list).display === 'block'
+      ? 'none'
+      : 'block';
+  }
+});
