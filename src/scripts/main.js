7cbf1e41f4fb884c91e76850d83fbe26dbe4a4f9
spencerspe <spencerspe@gmail.com>
2023-10-04 13:21:33 +0100

    first solution works but fails test and I am stuck

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write code here
+const list = document.querySelector('.tree');
+
+const listitems = document.querySelectorAll('li');
+
+listitems.forEach(listHeader => {
+  const listitemssWrap = document.createElement('span');
+
+  listitemssWrap.hidden = false;
+
+  listHeader.insertAdjacentElement('beforebegin', listitemssWrap);
+
+  listitemssWrap.insertAdjacentElement('afterbegin', listHeader);
+});
+
+list.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
+  if (evt.target.parentNode.tagName === 'SPAN') {
+    const clicked = evt.target;
+
+    if (clicked.firstElementChild) {
+      const child = clicked.firstElementChild;
+
+      child.hidden === false
+        ? child.hidden = true
+        : child.hidden = false;
+    };
+  }
+});
