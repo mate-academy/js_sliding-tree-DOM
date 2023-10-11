28135f97e9edba616b4c859af72968ebca5cbb6d
david.shamiiev <david.shamiiev@rad4m.com>
2023-10-11 16:52:21 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('.tree li').forEach(item => {
+  const title = item.firstChild.textContent.trim();
+
+  const titleSpan = document.createElement('span');
+
+  titleSpan.textContent = title;
+
+  item.replaceChild(titleSpan, item.firstChild);
+
+  titleSpan.addEventListener('click', () => {
+    const sublist = item.querySelector('ul');
+
+    if (sublist) {
+      const ulVisibility = sublist.style.display === 'none';
+
+      sublist.style.display = ulVisibility ? 'block' : 'none';
+    }
+  });
+});
