9ccf5eb60d888929111763d6554766a4d7582aa9
Andrii Rykal <andreuka79@gmail.com>
2023-10-23 13:35:50 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const elementsLi = [...document.querySelectorAll('li')];
+
+for (const element of elementsLi) {
+  if (element.children.length === 0) {
+    continue;
+  }
+
+  const span = document.createElement('span');
+
+  span.textContent = element.firstChild.textContent.trim();
+  element.firstChild.remove();
+  element.prepend(span);
+};
+
+// eslint-disable-next-line no-shadow
+document.addEventListener('click', (event) => {
+  if (event.target.matches('span')) {
+    const list = event.target.nextElementSibling;
+
+    list.hidden = !list.hidden;
+  }
+});
