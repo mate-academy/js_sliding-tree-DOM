80371145797c0037dc41460a49d8957194fa7394
alex <cebanoleksandr@gmail.com>
2023-11-02 17:36:53 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+list.forEach(item => {
+  item.childNodes.forEach(node => {
+    if (node.nodeType === Node.TEXT_NODE) {
+      const span = document.createElement('span');
+      span.textContent = node.textContent;
+      item.replaceChild(span, node);
+    }
+  });
+
+  const title = item.querySelector('span');
+
+  if (title) {
+    title.addEventListener('click', () => {
+      const ul = item.querySelector('ul');
+
+      if (ul) {
+        if (item.querySelector('ul').style.display) {
+          item.querySelector('ul').style.display = ''
+        } else {
+          item.querySelector('ul').style.display = 'none'
+        }
+      }
+    });
+  }
+});
+
+document.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('tree-title')) {
+    event.stopPropagation();
+  }
+});
