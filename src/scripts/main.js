a7a2a74102e8bdd8813679ccc6dee291db15587a
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-25 23:54:19 +0100

    add task soulution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write code here
+const trre = document.querySelector('ul');
+
+trre.addEventListener('click', (el) => {
+  if (el.target.children[0].style.display === 'none') {
+    el.target.children[0].style.display = 'block';
+  } else{
+    el.target.children[0].style.display = 'none';
+  }
+})
