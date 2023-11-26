65cf88571ec98c9f9e7c342e719648b670f5d06d
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-26 01:09:35 +0100

    add task soulution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,18 @@
 'use strict';
 
-// write code here
-const trre = document.querySelector('ul');
+const trArr = document.querySelectorAll('li');
 
-trre.addEventListener('click', (el) => {
-  if (el.target.children[0].style.display === 'none') {
-    el.target.children[0].style.display = 'block';
-  } else{
-    el.target.children[0].style.display = 'none';
-  }
-})
+for (const li of trArr) {
+  const span = document.createElement('span');
+
+  span.appendChild(li.firstChild);
+  li.insertBefore(span, li.firstChild);
+
+  span.addEventListener('click', (el) => {
+    if (el.target.parentElement.children[1].style.display === 'none') {
+      el.target.parentElement.children[1].style.display = 'block';
+    } else {
+      el.target.parentElement.children[1].style.display = 'none';
+    }
+  });
+}
