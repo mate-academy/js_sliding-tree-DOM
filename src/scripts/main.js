ae0b9f4e8c70e30b4678eceeddd8cbf974240984
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-26 12:48:27 +0100

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.onclick = function(e) {
+  const ulElem = e.target.querySelector('ul');
+
+  if (ulElem && e.target.tagName === 'LI') {
+    ulElem.hidden = !ulElem.hidden;
+  }
+};
+
+const li = tree.querySelectorAll('li');
+
+li.forEach(elem => {
+  const span = document.createElement('span');
+
+  span.className = 'list-item';
+  elem.prepend(span);
+  span.append(span.nextSibling);
+});
