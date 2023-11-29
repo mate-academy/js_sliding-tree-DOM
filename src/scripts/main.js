1e942f50b10d74d111ecdef810ca942f089ae04e
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-11-29 12:16:55 +0200

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.querySelectorAll('li').forEach(li => {
+  const span = document.createElement('span');
+
+  span.className += 'list-item';
+  li.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.onclick = function(action) {
+  if (action.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const children = action.target.parentNode.querySelector('ul');
+
+  if (children) {
+    children.hidden = !children.hidden;
+  }
+};
