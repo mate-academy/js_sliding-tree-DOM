b8c4700446c1a9367e68c079d60149d9f152b724
Nikita Orliak <nikitonorl@gmail.com>
2023-11-30 21:08:55 +0100

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const elements = document.querySelectorAll('li');
+
+for (const li of elements) {
+  if (li.children[0]) {
+    const span = document.createElement('span');
+
+    span.className = 'item';
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+tree.addEventListener('click', e => {
+  if (e.target.className === 'item') {
+    const children = e.target.parentNode.children;
+
+    if (children.length === 0) {
+      return;
+    }
+
+    const ul = children[1];
+
+    ul.hidden = !ul.hidden;
+  }
+});
