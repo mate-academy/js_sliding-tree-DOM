4c8ae19f97296db4f8deeecb19f03c1c99e869f3
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-13 18:14:08 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  tree.addEventListener('click', (e) => {
+    const clickedElement = e.target;
+
+    // Check if the clicked element is a span inside an li element
+    if (clickedElement.tagName === 'SPAN'
+    && clickedElement.parentElement.tagName === 'LI') {
+      // Toggle the visibility of the nested ul element
+      const sublist = clickedElement.parentElement.querySelector('ul');
+
+      if (sublist) {
+        sublist.style.display = sublist.style.display
+        === 'none' ? 'block' : 'none';
+      }
+    }
+  });
+});
