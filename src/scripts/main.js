26b22c927e8ce9a62ab2ed0180099c54fbb3da73
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-15 18:42:52 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,24 @@
-"use strict";
+'use strict';
 
-document.querySelectorAll(".tree li").forEach((li) => {
-  const span = document.createElement("span");
+document.querySelectorAll('.tree').forEach((tree) => {
+  tree.addEventListener('click', function(e) {
+    if (e.target.tagName !== 'SPAN') {
+      return;
+    }
+
+    const li = e.target.closest('li');
+
+    const childrenContainer = li.querySelector('ul');
+
+    if (childrenContainer) {
+      childrenContainer.hidden = !childrenContainer.hidden;
+    }
+  });
+});
+
+document.querySelectorAll('.tree li').forEach((li) => {
+  const span = document.createElement('span');
 
   li.prepend(span);
   span.appendChild(span.nextSibling);
-});
-
-document.querySelector(".tree").addEventListener("click", function (e) {
-  if (e.target.tagName !== "SPAN") {
-    return;
-  }
-
-  const childrenContainer = e.target.parentNode.querySelector("ul");
-
-  if (childrenContainer) {
-    childrenContainer.hidden = !childrenContainer.hidden;
-  }
 });
