b77ed7c9796fc2b901d2bc3b0d27f91d01752eff
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-20 23:51:54 +0200

    Revert "Sliding tree"
    This reverts commit cd9b030de767b9e7aaa06972410123d2366631ad.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,3 @@
 'use strict';
 
-const ul = document.querySelector('.tree');
-const li = document.querySelectorAll('li');
-
-[...li].forEach(e => {
-  const span = document.createElement('span');
-
-  e.prepend(span);
-  span.append(span.nextSibling);
-});
-
-ul.addEventListener('click', e => {
-  const item = e.target.closest('span').nextSibling;
-
-  item.hidden = !item.hidden;
-});
+// write code here
