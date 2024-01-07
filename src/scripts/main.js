50305b5dd12874a0b5abd9f5b8658901381eec33
Alla Gutsul <allagutsul1990@gmail.com>
2024-01-07 17:58:21 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const headlines = document.querySelectorAll('li');
+
+for (const elem of headlines) {
+  if (elem.children.length > 0) {
+    const span = document.createElement('span');
+    const firstChildContent = elem.firstChild.textContent;
+
+    span.textContent = firstChildContent;
+    span.classList.add('active');
+    elem.firstChild.remove();
+    elem.prepend(span);
+  }
+}
+
+const spans = document.querySelectorAll('span');
+
+spans.forEach(span => {
+  span.addEventListener('click', () => {
+    span.classList.toggle('active');
+
+    const spanChildren = [...span.nextElementSibling.children];
+
+    spanChildren.forEach(child => {
+      child.style.display = span.classList.contains('active')
+        ? 'list-item' : 'none';
+    });
+  });
+});
