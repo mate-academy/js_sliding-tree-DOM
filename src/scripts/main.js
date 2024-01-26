9897934d0d21fab8a90bcf796f8bc4192b6fbe43
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-01-26 19:11:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,7 @@
 'use strict';
 
-const tree = document.querySelector('.tree');
-
-const fruit = tree.firstElementChild;
-
-const redFruits = fruit.firstElementChild.firstElementChild;
-
-const yellowFruits = fruit.firstElementChild.lastElementChild;
-
-const fish = tree.lastElementChild;
-
-const oceanFish = fish.firstElementChild.firstElementChild;
-
-const freshWaterFish = fish.firstElementChild.lastElementChild;
+const headers = document.querySelectorAll('li>ul>li');
+const topHeaders = document.querySelectorAll('ul>li');
 
 function spanWrap(input) {
   const text = input.innerHTML;
@@ -34,47 +23,17 @@ function spanWrap(input) {
   input.innerHTML = newText;
 }
 
-spanWrap(redFruits);
-spanWrap(yellowFruits);
-spanWrap(oceanFish);
-spanWrap(freshWaterFish);
+headers.forEach(element => {
+  spanWrap(element);
+});
 
-const spanList = document.querySelectorAll('span');
-
-spanList.forEach(element => {
-  element.dataset.dropDown = false;
+topHeaders.forEach(element => {
+  spanWrap(element);
 });
 
 document.addEventListener('click', (e) => {
   if (e.target.tagName === 'SPAN') {
-    switch (e.target.dataset.dropDown) {
-      case 'false':
-        e.target.parentElement.lastElementChild.hidden = true;
-        e.target.dataset.dropDown = true;
-
-        break;
-
-      case 'true':
-        e.target.parentElement.lastElementChild.hidden = false;
-        e.target.dataset.dropDown = false;
-
-        break;
-    }
+    e.target.parentElement.lastElementChild.hidden
+      = !e.target.parentElement.lastElementChild.hidden;
   }
 });
-
-// document.addEventListener('click', (e) => {
-//   if (e.target.tagName === 'SPAN') {
-//     if (e.target.dataset.dropDown === 'false') {
-//       e.target.parentElement.lastElementChild.hidden = true;
-//       e.target.dataset.dropDown = true;
-
-//       return;
-//     }
-
-//     if (e.target.dataset.dropDown === 'true') {
-//       e.target.parentElement.lastElementChild.hidden = false;
-//       e.target.dataset.dropDown = false;
-//     }
-//   }
-// });
