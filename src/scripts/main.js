2eea91d34a014eebcd468e4809a6ff04ab9f2f1c
Lubomur Chuchvara <lubomircucvara@gmail.com>
2024-02-05 16:12:26 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const menu = document.querySelector('.tree');
+const items = document.querySelector('li');
+
+[...items].map(item => {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+});
+
+menu.addEventListener('click', (e) => {
+  const item = e.target;
+
+  if (item.parentNode.children[1]) {
+    item.parentNode.children[1].hidden = !item.parentNode.children[1].hidden;
+  }
+});
