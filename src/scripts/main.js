a61ef62976ff0ca5b0a951d692cc1312f7b50598
unknown <fanton81@gmail.com>
2024-02-10 16:02:01 +0200

    mySolution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+document.querySelectorAll('.tree li').forEach((li) => {
+  const span = document.createElement('span');
+  const item = li.firstChild;
+
+  li.prepend(span);
+  span.append(item);
+});
+
+tree.addEventListener('click', (e) => {
+  const targetElem = e.target;
+
+  if (targetElem.tagName === 'SPAN') {
+    const list = targetElem.closest('li').querySelector('ul');
+
+    if (list) {
+      list.hidden = !list.hidden;
+    }
+  }
+});
