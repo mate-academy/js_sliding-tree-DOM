b42b85fe52510764b74a802e16844d3800a09f5e
oleksandrshtonda <oleksandrshtonda@gmail.com>
2024-02-17 14:27:11 +0100

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const categories = document.querySelector('.tree');
+
+// { parent-owner: his html }
+const hidedElements = new Map();
+
+categories.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target;
+  const itemHTMLBeforeHiding
+    = item.innerHTML.replaceAll(' ', '').split('\n')[0];
+
+  if (e.target === categories || e.target.tagName === 'UL') {
+    return;
+  }
+
+  if (hidedElements.get(itemHTMLBeforeHiding)) {
+    item.innerHTML = hidedElements.get(itemHTMLBeforeHiding);
+    hidedElements.delete(itemHTMLBeforeHiding);
+
+    return;
+  }
+
+  const itemHTML = item.innerHTML.replaceAll(' ', '');
+  const itemKey = itemHTML.split('\n')[0];
+
+  hidedElements.set(itemKey, itemHTML || '');
+  item.innerHTML = itemHTML.split('\n')[0];
+});
