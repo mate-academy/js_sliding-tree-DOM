d7eba79b71a0117a2671a8209f483d204aea0cdc
Davwidos <d.jurkowski015@gmail.com>
2024-02-29 15:10:21 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write code here
+function toggleListener(ev) {
+  const target = ev.target;
+
+  if (target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const ul = target.nextSibling;
+  const ulDisplay = ul.style.display;
+
+  ul.style.display = ulDisplay === 'none' ? null : 'none';
+}
+
+function wrapUlTitleInSpan(rootElement) {
+  rootElement.querySelectorAll('ul').forEach((el) => {
+    const previous = el.previousSibling;
+    const span = document.createElement('SPAN');
+
+    span.append(previous);
+    el.before(span);
+  });
+}
+
+const tree = document.querySelector('.tree');
+
+wrapUlTitleInSpan(tree);
+tree.addEventListener('click', toggleListener);
