afd9b7c9c589b9a1fec18dc411f751d1b10feef5
Oleksandr Kosenko <d1izel@ukr.net>
2024-03-25 23:42:18 +0200

    add sliding-tree-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const items = document.querySelectorAll('li');
+
+items.forEach((item) => {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.addEventListener('click', (e) => {
+  const item = e.target.closest('li');
+
+  if (!item) {
+    return;
+  }
+
+  const child = item.children[1];
+
+  if (child) {
+    child.hidden = !child.hidden;
+  }
+});
