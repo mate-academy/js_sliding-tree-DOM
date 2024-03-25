93e3bb7b7637f1ff4f12388f83d8cf4b30d5ecb0
Vladyslav Filonenko <oladik05@gmail.com>
2024-03-25 22:42:04 +0200

    tree dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+// eslint-disable-next-line no-shadow
+list.addEventListener('click', (event) => {
+  if (event.target.className === 'tree') {
+    return;
+  }
+
+  const item = event.target.children[0];
+
+  if (item.style.display === 'none') {
+    item.style.display = 'block';
+
+    return;
+  }
+
+  item.style.display = 'none';
+});
