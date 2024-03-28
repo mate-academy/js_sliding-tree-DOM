b173f91893dd904297781397f49cb7445a43be34
Semen Sokolovskyi <anori.fjord@gmail.com>
2024-03-28 12:01:01 +0200

    sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+document.querySelectorAll('.tree li').forEach((li) => {
+  const span = document.createElement('span');
+  const element = li.firstChild;
+
+  li.prepend(span);
+  span.append(element);
+});
+
+tree.addEventListener('click', (el) => {
+  const target = el.target;
+
+  if (target.tagName === 'SPAN') {
+    const showList = target.closest('li').querySelector('ul');
+
+    if (showList) {
+      showList.hidden = !showList.hidden;
+    }
+  }
+});
