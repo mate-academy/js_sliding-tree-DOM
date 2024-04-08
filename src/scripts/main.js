54b8de3a9191baba39e4c185a1362108a32c4465
ArsenSkandalenko <arsenskandal@gmail.com>
2024-04-08 16:57:16 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+function toggleDisplay(listEvent) {
+  const list = listEvent.target.nextSibling;
+
+  if (list) {
+    if (list.style.display === 'none') {
+      list.style.display = 'block';
+    } else {
+      list.style.display = 'none';
+    }
+  }
+}
+
+const items = document.querySelectorAll('li');
+
+items.forEach((item) => {
+  const span = document.createElement('span');
+
+  span.addEventListener('click', toggleDisplay);
+  span.innerText = item.firstChild.nodeValue.trim();
+  item.firstChild.replaceWith(span);
+});
