02d6b6fe2ca8ab41c0e135e19bea84234a8ee2a9
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2024-04-14 12:19:41 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const lis = document.querySelectorAll('li');
+
+lis.forEach((li) => {
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent.trim();
+  li.firstChild.replaceWith(span);
+});
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const element = e.target.closest('li');
+
+  if (!element) {
+    return;
+  }
+
+  const ul = element.children[1];
+
+  if (!ul) {
+    return;
+  }
+
+  ul.hidden = !ul.hidden;
+});
