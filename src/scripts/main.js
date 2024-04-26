615f9956ea7515588cc64a2ac26eccbe7b3394ff
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2024-04-26 15:17:28 +0300

    added sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
+/* eslint-disable no-shadow */
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const list = [...document.querySelectorAll('li')];
+
+list.map((li) => {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.addEventListener('click', (event) => {
+  const ul = event.target.nextElementSibling;
+
+  if (ul.tagName === 'UL') {
+    ul.hidden = !ul.hidden;
+  }
+});
