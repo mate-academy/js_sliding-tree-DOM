8ca912e6ce5c4ce3885bf06e617a6da35b49e4c2
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-05-08 12:35:53 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const lists = tree.querySelectorAll('li');
+
+lists.forEach((list) => {
+  if (list.childNodes.length !== 1) {
+    const textNode = list.childNodes[0];
+    const text = textNode.textContent.trim();
+    const span = document.createElement('span');
+
+    span.textContent = text;
+    list.insertBefore(span, textNode);
+    textNode.remove();
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const hiddenUl = e.target.nextSibling;
+
+    if (hiddenUl) {
+      hiddenUl.hidden = !hiddenUl.hidden;
+    }
+  }
+});
