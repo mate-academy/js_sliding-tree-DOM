a3809d88b3726a9482fea7f80d37525f3d9ba37d
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-05-09 09:24:18 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liItems = tree.querySelectorAll('li');
+
+liItems.forEach((li) => {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'span') {
+    return;
+  }
+
+  const childrenContainer = e.target.parentNode.querySelector('ul');
+
+  if (!childrenContainer) {
+    return;
+  }
+
+  childrenContainer.hidden = !childrenContainer.hidden;
+});
