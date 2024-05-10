036148900cec54bb063a69d3c6618dbd7f953b90
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2024-05-10 21:59:12 +0300

    sliding tree DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+document.querySelectorAll('li').forEach((item) => {
+  if (item.childNodes.length > 1) {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+    span.appendChild(span.nextSibling);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (target.tagName === 'SPAN') {
+    target.nextSibling.hidden = !target.nextSibling.hidden;
+  }
+});
