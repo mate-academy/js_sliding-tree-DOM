f45bb2eaa85e6b41e921b0800ddf7df17d95a985
katya-sn <kattya.sagun@gmail.com>
2024-05-15 19:07:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const headers = document.querySelectorAll('li');
+
+headers.forEach((header) => {
+  if (header.childNodes.length !== 1) {
+    const span = document.createElement('span');
+
+    header.prepend(span);
+    span.append(span.nextSibling);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const hiddenUl = e.target.nextSibling;
+
+    if (hiddenUl.style.display !== 'none') {
+      hiddenUl.style.display = 'none';
+    } else {
+      hiddenUl.style.display = '';
+    }
+  }
+});
