ef545b579e718ccaa119d42aad88aa0d5be60fb4
ArtyWhit <julwer1k@gmail.com>
2024-05-17 17:04:16 +0300

    finished task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const treeItems = document.querySelectorAll('.tree li');
+
+treeItems.forEach((treeItem) => {
+  const span = document.createElement('span');
+
+  treeItem.prepend(span);
+  span.append(span.nextSibling);
+});
+
+function slideItem(e) {
+  e.preventDefault();
+
+  if (e.target.tagName === 'SPAN') {
+    const list = e.target.nextSibling;
+
+    if (list) {
+      list.hidden = !list.hidden;
+    }
+  }
+}
+
+tree.addEventListener('click', slideItem);
