7219df3b1daa4c3637173911d6b6850722f3502c
OleksiiNesteruk <nesteruk.oleksiy@gmail.com>
2024-05-28 13:34:53 +0100

    Added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const listItems = tree.querySelectorAll('li');
+
+listItems.forEach((li) => {
+  const wrapper = document.createElement('span');
+
+  wrapper.textContent = li.firstChild.textContent;
+  li.replaceChild(wrapper, li.firstChild);
+
+  wrapper.addEventListener('click', (e) => {
+    e.target.nextSibling.hidden = !e.target.nextSibling.hidden;
+  });
+});
