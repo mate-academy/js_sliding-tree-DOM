476f22e54d34e581a51f8bba5755633c4996f1d4
Roma <roma150269@gmail.com>
2024-06-07 00:14:15 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+li.forEach((item) => {
+  const text = item.firstChild.textContent.trim();
+  const span = document.createElement('span');
+
+  span.textContent = text;
+
+  item.firstChild.textContent = '';
+
+  item.prepend(span);
+});
+
+document.addEventListener('click', (e) => {
+  const title = e.target.closest('li');
+
+  if (title.lastElementChild.style.display === 'none') {
+    title.lastElementChild.style.display = 'block';
+  } else {
+    title.lastElementChild.style.display = 'none';
+  }
+});
