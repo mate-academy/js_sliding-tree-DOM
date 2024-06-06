471b805318cae2073a2bde00acbf60559541ef08
Nastya <anastasiia.y.sitnikova@gmail.com>
2024-06-06 18:21:40 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach((item) => {
+  const span = document.createElement('span');
+
+  span.textContent = item.firstChild.textContent.trim();
+
+  item.firstChild.textContent = '';
+
+  item.prepend(span);
+});
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const item = e.target.closest('li');
+
+  if (item.lastElementChild.tagName === 'UL') {
+    const ulElem = item.querySelector('ul');
+
+    ulElem.hidden = !ulElem.hidden;
+  }
+});
