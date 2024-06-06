32a63c00d477680d82f42a8497567296616d1b86
Yevhenii <polotnyak2004@gmail.com>
2024-06-06 17:18:57 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,7 +11,7 @@
 document.querySelector('.tree').addEventListener('click', (e) => {
   const li = e.target.closest('li');
 
-  if (li.children.length > 1) {
+  if (li.children.length > 1 && e.target === li.firstChild) {
     if (li.lastElementChild.style.display === 'none') {
       li.lastElementChild.style.display = 'block';
     } else {
