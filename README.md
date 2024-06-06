0a96a4691efc9f4526fd9988cfdf7e764678cbd1
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-06-06 18:48:12 +0300

    add solution

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 1. Replace `<your_account>` with your Github username in the link
-    - [DEMO LINK](https://<your_account>.github.io/js_sliding-tree-DOM/)
+    - [DEMO LINK](https://Nazarin565.github.io/js_sliding-tree-DOM/)
 2. Follow [this instructions](https://mate-academy.github.io/layout_task-guideline/)
     - Run `npm run test` command to test your code;
     - Run `npm run test:only -- -n` to run fast test ignoring linter;
@@ -9,7 +9,7 @@
 
 Create an expandable tree.
 
-Requirements: 
+Requirements:
 - Clicking on the headline should hide or show elements.
 - Clicking outside the title text (in an empty place) should do nothing.
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const li = tree.querySelectorAll('li');
+
+li.forEach((item) => {
+  if (item.querySelector('li')) {
+    const spanElem = document.createElement('span');
+    const changed = item.childNodes[0].textContent.trim();
+
+    spanElem.textContent = changed;
+
+    item.removeChild(item.childNodes[0]);
+    item.insertBefore(spanElem, item.childNodes[0]);
+  }
+});
+
+const span = document.querySelectorAll('span');
+
+span.forEach((item) => {
+  const list = item.nextElementSibling;
+
+  list.style.display = 'block';
+
+  item.addEventListener('click', () => {
+    if (list.style.display === 'block') {
+      list.style.display = 'none';
+    } else {
+      list.style.display = 'block';
+    }
+  });
+});
