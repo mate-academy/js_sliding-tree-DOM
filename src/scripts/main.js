ede4d82ed9bb2eecbbf91dee6ffa3839582af470
Krzysztof <krzysiek.gugala@gmail.com>
2024-06-07 10:10:27 +0200

    solutionV1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const lis = document.querySelectorAll('li');
+const titles = [];
+
+lis.forEach((li) => {
+  if (li.hasChildNodes()) {
+    const span = document.createElement('span');
+
+    span.textContent = li.firstChild.textContent;
+    li.childNodes[0].remove();
+    li.prepend(span);
+
+    titles.push(span);
+  }
+});
+
+titles.forEach((title) => {
+  title.addEventListener('click', () => {
+    const li = title.parentElement;
+    const children = Array.from(li.children).slice(1);
+
+    children.forEach((child) => {
+      if (child.style.display === 'none') {
+        child.style.display = '';
+      } else {
+        child.style.display = 'none';
+      }
+    });
+  });
+});
