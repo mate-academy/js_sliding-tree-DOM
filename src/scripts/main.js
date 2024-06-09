4234ace7ea8eb135d6588e99bf1edd9ba314584b
Vladyslav <vladbuldenko@gmail.com>
2024-06-09 23:56:41 +0300

    VB solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const list = document.querySelectorAll('li');
+
+list.forEach((li) => {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+});
+
+tree.addEventListener('click', (e) => {
+  const ul = e.target.nextElementSibling;
+
+  ul.hidden = !ul.hidden;
+});
