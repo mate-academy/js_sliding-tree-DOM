22435f44e50d6dd4dc49bf3644a2306a0167b06e
Viktoriya Riabets <vikamalik30@gmail.com>
2024-06-13 17:15:28 -0400

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.tree li').forEach((item) => {
+    const span = document.createElement('span');
+
+    span.innerHTML = item.firstChild.nodeValue.trim();
+    item.insertBefore(span, item.firstChild);
+    item.firstChild.nextSibling.remove();
+
+    span.addEventListener('click', (e) => {
+      e.stopPropagation();
+
+      const children = item.querySelectorAll('ul > li');
+
+      if (children.length > 0) {
+        children.forEach((child) => {
+          child.style.display =
+            child.style.display === 'none' ? 'block' : 'none';
+        });
+      }
+    });
+  });
+});
