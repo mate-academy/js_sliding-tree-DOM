8d16dc1abf96343f4eafb23fb25012d6fa2a20e7
getchards <kazansasha02@gmail.com>
2024-06-18 20:02:45 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const slidingLists = document.querySelectorAll('span');
+
+slidingLists.forEach((span) => {
+  span.addEventListener('click', () => {
+    const parentLi = span.parentElement;
+    const childUl = parentLi.querySelector('ul');
+
+    if (childUl) {
+      toggleVisibility(childUl);
+    }
+  });
+});
+
+const toggleVisibility = (element) => {
+  if (element.style.display !== 'none') {
+    element.style.display = 'none';
+  } else {
+    element.style.display = '';
+  }
+};
