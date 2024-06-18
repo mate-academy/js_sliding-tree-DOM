7b33f8b6da22c05ea177d44d29328d7f1e5bb173
getchards <kazansasha02@gmail.com>
2024-06-18 20:45:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,3 +20,30 @@ const toggleVisibility = (element) => {
     element.style.display = '';
   }
 };
+
+function createSpansForCategories() {
+  const tree = document.querySelector('.tree');
+
+  const listItemsWithUl = tree.querySelectorAll('li > ul');
+
+  listItemsWithUl.forEach((ul) => {
+    const parentText = ul.parentElement.firstChild.textContent;
+
+    ul.parentElement.firstChild.textContent = '';
+
+    if (!ul.parentElement.querySelector('span.sliding-span')) {
+      const span = document.createElement('span');
+
+      span.textContent = parentText;
+      span.classList.add('sliding-span');
+
+      span.addEventListener('click', () => {
+        toggleVisibility(ul);
+      });
+
+      ul.parentElement.insertBefore(span, ul);
+    }
+  });
+}
+
+createSpansForCategories();
