f44452e313db958bc287018f99efab835f42b81c
Valentyn Sobol <valiksobol14@gmail.com>
2024-06-20 19:33:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const list = document.querySelectorAll('li');
+
+for (const item of list) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', (action) => {
+  if (action.target.tagName === 'SPAN') {
+    const children = action.target.nextSibling;
+
+    if (children) {
+      children.hidden = !children.hidden;
+    }
+  }
+});
