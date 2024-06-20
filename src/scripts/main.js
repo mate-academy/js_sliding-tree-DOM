f5b37d5c773cd832c48703adefd7ad908cfc162b
ZakZh <38009936+ZakZh@users.noreply.github.com>
2024-06-20 21:51:09 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const dataTree = document.querySelector('.tree');
+
+function modList(node) {
+  for (const el of node.children) {
+    const span = document.createElement('span');
+    const ul = el.querySelector('ul');
+
+    span.innerHTML = el.firstChild.textContent;
+    el.firstChild.replaceWith(span);
+
+    if (ul) {
+      el.firstChild.addEventListener('click', () => {
+        ul.hidden = !ul.hidden;
+      });
+
+      modList(ul);
+    }
+  }
+}
+
+modList(dataTree);
