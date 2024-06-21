ffee952bc00a79aac12c7551af9e0d034c31fd40
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-21 18:39:01 +0200

    First try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const closestList = e.target.closest('li');
+
+  if (closestList.children.length !== 0) {
+    const listStyle = closestList.children[0].style;
+
+    if (listStyle.display !== 'none') {
+      listStyle.display = 'none';
+    } else {
+      listStyle.display = 'block';
+    }
+  }
+});
