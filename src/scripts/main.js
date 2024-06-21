fe7766c856c1076e90aad408825ed75142b4e28a
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-21 19:19:26 +0200

    Fixes after APR

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,31 @@
 'use strict';
 
 const tree = document.querySelector('.tree');
+let headers = document.querySelectorAll('li');
+
+headers = [...headers].filter((header) => header.querySelector('ul'));
+
+headers.forEach((header) => {
+  if (header.hasChildNodes()) {
+    const headerText = header.firstChild;
+    const span = document.createElement('span');
+
+    span.innerHTML = headerText.textContent;
+    header.replaceChild(span, headerText);
+  }
+});
 
 tree.addEventListener('click', (e) => {
-  const closestList = e.target.closest('li');
+  const closestList = e.target.closest('span');
 
-  if (closestList.children.length !== 0) {
-    const listStyle = closestList.children[0].style;
+  if (closestList) {
+    const firstChild = closestList.nextSibling;
+    const listStyle = firstChild.style;
 
     if (listStyle.display !== 'none') {
       listStyle.display = 'none';
     } else {
-      listStyle.display = 'block';
+      listStyle.display = '';
     }
   }
 });
