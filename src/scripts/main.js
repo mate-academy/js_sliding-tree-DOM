6eda01a51adc1dc0c58015dbcd0c2e90f1909ca0
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-06-21 13:06:10 +0200

    add sliding for listItems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const titles = document.querySelectorAll('.tree li');
+
+  titles.forEach((title) => {
+    const span = document.createElement('span');
+
+    span.textContent = title.firstChild.textContent.trim(); // copy w/out spaces
+
+    // replace text with span
+    title.firstChild.replaceWith(span);
+
+    span.addEventListener('click', (e) => {
+      e.stopPropagation(); // stop expanding event on other elements
+
+      const childrenContainer = title.querySelector('ul');
+
+      if (childrenContainer) {
+        if (childrenContainer.style.display === 'block') {
+          childrenContainer.style.display = 'none';
+        } else {
+          childrenContainer.style.display = 'block';
+        }
+      }
+    });
+  });
+});
