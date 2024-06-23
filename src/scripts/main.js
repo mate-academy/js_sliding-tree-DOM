734f8f96692c835e8cc0b946ad12d03129ea8e4c
Victor Skits <vityok.nice@gmail.com>
2024-06-23 12:14:52 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.tree');
+
+container.addEventListener('click', (e) => {
+  const list = e.target.nextSibling;
+
+  if (list) {
+    list.hidden = !list.hidden;
+  }
+});
+
+function wrapWithSpan() {
+  const listItems = container.querySelectorAll('li');
+
+  listItems.forEach((item) => {
+    const wrapper = document.createElement('span');
+
+    item.prepend(wrapper);
+    wrapper.append(wrapper.nextSibling);
+  });
+}
+
+wrapWithSpan();
