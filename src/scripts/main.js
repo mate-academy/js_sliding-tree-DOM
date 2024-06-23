aeac8f07228da1bd0669b9cbc5e5191e8cf1c41f
Dmytro Bashlai <imaxs2016@gmail.com>
2024-06-23 20:58:05 +0300

    The sliding tree has implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = document.querySelectorAll('.tree li');
+
+listItems.forEach((el) => {
+  const span = document.createElement('span');
+
+  if (el.children.length !== 0) {
+    span.textContent = el.firstChild.textContent.trim();
+    el.firstChild.replaceWith(span);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  const headerItem = e.target.tagName === 'SPAN';
+  const childItem = e.target.nextElementSibling;
+
+  if (headerItem && !childItem.hasAttribute('hidden')) {
+    childItem.setAttribute('hidden', '');
+  } else {
+    childItem.removeAttribute('hidden');
+  }
+});
