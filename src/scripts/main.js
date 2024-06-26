2943565b7f07935115acb60b20f98eb42bbacf59
Voloshynd <Voloshynd13@gmail.com>
2024-06-26 22:01:08 +0200

    add solution of task js_sliding-tree-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const fullList = document.querySelector('.tree');
+const arrLi = [...fullList.children]
+  .map((li) => [...li.children[0].children])
+  .flat();
+const arrHeadlines = arrLi.map((li) => li.childNodes[0].wholeText.trim());
+
+arrLi.forEach((li, idx) => {
+  li.removeChild(li.firstChild);
+  li.prepend(wrapHeadline(idx));
+});
+
+function wrapHeadline(i) {
+  const span = document.createElement('span');
+
+  span.innerHTML = arrHeadlines[i];
+
+  return span;
+}
+
+fullList.addEventListener('click', (e) => {
+  const value =
+    e.target.tagName === 'SPAN' ? e.target.nextSibling.style.display : null;
+
+  if (
+    (e.target.tagName === 'SPAN' && !value) ||
+    (e.target.tagName === 'SPAN' && value === 'block')
+  ) {
+    e.target.nextSibling.style.display = 'none';
+  }
+
+  if (e.target.tagName === 'SPAN' && value === 'none') {
+    e.target.nextSibling.style.display = 'block';
+  }
+});
