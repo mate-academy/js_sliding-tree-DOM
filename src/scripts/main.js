89db4145b28a8baa393f5c53f83578c39a089407
Marian <getmoodyup@gmail.com>
2024-06-26 10:36:24 +0300

    clickable toggle lis

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+function toggleDisplay(headingLi) {
+  const ul = headingLi.querySelector('ul');
+
+  if (ul) {
+    ul.style.display = (ul.style.display === 'none' || ul.style.display === '') ? 'block' : 'none';
+  }
+}
+
+tree.addEventListener('click', function(e) {
+  const li = e.target.closest('li');
+
+  if (!li) {
+    return;
+  }
+
+  toggleDisplay(li);
+});
