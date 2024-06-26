ac3208211bc7bd13072d770dce84afc6cad3a686
Marian <getmoodyup@gmail.com>
2024-06-26 10:37:09 +0300

    linter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,12 @@ function toggleDisplay(headingLi) {
   const ul = headingLi.querySelector('ul');
 
   if (ul) {
-    ul.style.display = (ul.style.display === 'none' || ul.style.display === '') ? 'block' : 'none';
+    ul.style.display =
+      ul.style.display === 'none' || ul.style.display === '' ? 'block' : 'none';
   }
 }
 
-tree.addEventListener('click', function(e) {
+tree.addEventListener('click', function (e) {
   const li = e.target.closest('li');
 
   if (!li) {
