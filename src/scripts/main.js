c2f248096d913e39b825e7cc3d8cb7c7e170dc1b
Oleksii Kozlov <oleksiikozlov@gmail.com>
2024-07-01 18:54:57 +0300

    created js sliding tree DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('ul');
+const list = document.querySelectorAll('li');
+
+for (const li of list) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const span = e.target;
+    const nextElement = span.nextElementSibling;
+
+    if (nextElement.style.display === 'none') {
+      nextElement.style.display = 'block';
+    } else {
+      nextElement.style.display = 'none';
+    }
+  }
+});
