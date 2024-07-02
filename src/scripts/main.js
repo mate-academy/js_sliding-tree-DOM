3e917d7095af80575d7c5f186fdb06fdf170acef
Vadym Bakuma <bakuma.2001@gmail.com>
2024-07-02 21:29:03 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const spans = document.querySelectorAll('.tree span');
+
+spans.forEach((span) => {
+  span.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    const nextUl = span.nextElementSibling;
+
+    if (nextUl && nextUl.tagName === 'UL') {
+      nextUl.style.display = nextUl.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
