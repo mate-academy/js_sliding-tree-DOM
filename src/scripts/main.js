eb122e6a7c4d126fdd86a17618658b6248f7e389
under-a <mymail15@i.ua>
2024-07-05 20:49:01 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const lists = document.querySelectorAll('li');
+
+lists.forEach((list) => {
+  const title = list.firstChild;
+
+  if (title.nextSibling) {
+    const span = document.createElement('span');
+
+    span.textContent = title.nodeValue;
+
+    title.textContent = '';
+    list.prepend(span);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.nodeName === 'SPAN') {
+    const ul = e.target.nextElementSibling;
+
+    if (ul.classList.contains('hidden')) {
+      ul.classList.toggle('hidden');
+      ul.style.display = 'block';
+    } else {
+      ul.classList.toggle('hidden');
+      ul.style.display = 'none';
+    }
+  }
+});
