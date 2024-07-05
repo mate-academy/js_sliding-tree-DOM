3d5fe038033b359c4ecb85a6b196aadb4eea135f
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-05 08:55:46 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listOfLi = document.querySelectorAll('li');
+
+listOfLi.forEach((li) => {
+  const text = li.firstChild.data.replace(/\W/gi, '');
+
+  const span = document.createElement('span');
+
+  span.textContent = text;
+
+  li.firstChild.data = '';
+  li.insertAdjacentElement('afterbegin', span);
+});
+
+tree.addEventListener('click', (e) => {
+  const children = [...e.target.parentElement.children];
+
+  children.forEach((element) => {
+    if (element.tagName === 'UL' && element.style.display === 'none') {
+      element.style.display = 'block';
+
+      return;
+    }
+
+    if (
+      element.tagName === 'UL' &&
+      getComputedStyle(element).display === 'block'
+    ) {
+      element.style.display = 'none';
+    }
+  });
+});
