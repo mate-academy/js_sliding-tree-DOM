6943aa81d43c4e2a8d0d51532356d303c2311118
Roman Perepelkin <perepelkinrs@gmail.com>
2024-07-08 10:35:44 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const li = document.querySelectorAll('li');
+
+li.forEach((item) => {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.prepend(span.nextSibling);
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const nextElement = e.target.nextSibling;
+
+    if (nextElement.tagName === 'UL') {
+      nextElement.hidden = !nextElement.hidden;
+    }
+  }
+});
