1b1ca882388ce385d02e0bb8476b70f74f62cc81
Kirill <ovoo1339@gmail.com>
2024-07-09 18:35:19 +0300

    work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
-'use strict';
+const tree = document.querySelector('.tree');
+const liElements = document.querySelectorAll('li');
 
-// write code here
+liElements.forEach(li => {
+  const ul = li.querySelector('ul');
+
+  if (ul) {
+    const span = document.createElement('span');
+    const first = li.firstChild;
+
+    span.textContent = first.textContent;
+    li.insertBefore(span, first);
+    first.remove();
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const nextItem = e.target.nextElementSibling;
+
+    if (nextItem && nextItem.tagName === 'UL') {
+      nextItem.hidden = !nextItem.hidden;
+    }
+  }
+});
