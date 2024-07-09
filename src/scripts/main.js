40c038453bdeb7298ceaeadc121d0d193bcddf2c
valentyn19939 <valik.hai1993@gmail.com>
2024-07-09 17:02:19 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+li.forEach((item) => {
+  const span = document.createElement('span');
+  const text = item.firstChild.textContent;
+  const listUl = item.querySelector('ul');
+
+  if (listUl) {
+    span.textContent = text;
+    item.firstChild.textContent = '';
+
+    item.prepend(span);
+  }
+
+  span.addEventListener('click', (e) => {
+    listUl.style.display = listUl.style.display === 'none' ? 'block' : 'none';
+  });
+});
