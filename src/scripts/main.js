c5f53fef680678ffb7ea60db7f070a9fb2be0757
Seva Podolskiy <s_podolskiy@live.com>
2024-07-09 12:58:28 -0400

    solutioin

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+document.querySelector('ul').addEventListener('click', (e) => {
+  const children = e.target.children;
+
+  for (let i = 0; i < children.length; i++) {
+    const child = children[i];
+    if (child.style.display === '') {
+      child.style.setProperty('display', 'none');
+    } else {
+      child.style.setProperty('display', '');
+    }
+  }
+});
