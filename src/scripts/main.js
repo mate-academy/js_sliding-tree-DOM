7509913b673438060e926e415040e4f8a77d9469
Mykhal <misha.soroka.49@gmail.com>
2024-07-12 13:32:21 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const headers = document.querySelectorAll('li > ul > li');
+
+  headers.forEach((header) => {
+    const span = document.createElement('span');
+
+    span.innerHTML = header.innerHTML;
+    header.innerHTML = '';
+    header.appendChild(span);
+
+    // eslint-disable-next-line no-shadow
+    span.addEventListener('click', (event) => {
+      event.stopPropagation();
+
+      const nestedList = header.querySelector('ul');
+
+      if (nestedList) {
+        nestedList.style.display =
+          nestedList.style.display === 'none' ? 'block' : 'none';
+      }
+    });
+  });
+});
