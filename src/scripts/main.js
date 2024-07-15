49a7cc43467152fdb61f9e77321fd446c85e513a
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-07-15 10:21:14 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const subList = document.querySelectorAll('ul');
+
+[...document.querySelectorAll('li')]
+  .filter((item) => {
+    let containsList = false;
+
+    subList.forEach((list) => {
+      if (item.contains(list)) {
+        containsList = true;
+      }
+    });
+
+    return containsList;
+  })
+  .forEach((item) => {
+    const text = item.firstChild.textContent.trim();
+
+    item.lastElementChild.style.maxHeight = '300px';
+
+    const newChild = document.createElement('span');
+
+    newChild.style.cursor = 'pointer';
+
+    newChild.innerText = text;
+    item.firstChild.replaceWith(newChild);
+  });
+
+document.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const list = e.target.parentElement.lastElementChild;
+    const isDisplayed = list.style.opacity !== '0';
+
+    list.style.transition = 'all 1s linear';
+    list.style.maxHeight = isDisplayed ? '0' : '200px';
+    list.style.opacity = isDisplayed ? '0' : '1';
+  }
+});
