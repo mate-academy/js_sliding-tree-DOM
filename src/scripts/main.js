efb390eeeac628078e7a24969a384fcd465e87f0
Artem Pinchuk <103887088+Artem193@users.noreply.github.com>
2024-07-16 13:33:54 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const wrapTextWithSpan = (elements) => {
+  elements.forEach((element) => {
+    if (element.querySelector('ul') !== null) {
+      element.childNodes.forEach((node) => {
+        if (node.nodeType === 3 && node.nodeValue.trim() !== '') {
+          const span = document.createElement('span');
+
+          span.textContent = node.nodeValue.trim();
+          element.insertBefore(span, node);
+          node.remove();
+        }
+      });
+    }
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll('li');
+
+  wrapTextWithSpan(elements);
+
+  const span = document.querySelectorAll('span');
+
+  span.forEach((title) => {
+    title.addEventListener('click', () => {
+      const nextElement = title.nextElementSibling;
+
+      if (nextElement.tagName === 'UL') {
+        if (nextElement.hidden) {
+          nextElement.hidden = false;
+        } else {
+          nextElement.hidden = true;
+        }
+      }
+    });
+  });
+});
