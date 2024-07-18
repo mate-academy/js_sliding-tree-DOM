7091c0b220f1cf41605713ef42c9a9fee459860e
Mari Andrushko <kap.mebel.a@gmail.com>
2024-07-18 15:45:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('.tree li')];
+
+function onClick(e) {
+  const style = e.target.nextElementSibling.style;
+
+  style.display = style.display === 'none' ? '' : 'none';
+}
+
+list.forEach((node) => {
+  const txt = node.childNodes[0].textContent.trim();
+
+  const childLi = node.querySelectorAll('li');
+
+  if (childLi.length > 0) {
+    node.childNodes[0].textContent = '';
+
+    const span = document.createElement('span');
+
+    span.textContent = txt;
+    span.addEventListener('click', onClick);
+
+    node.insertBefore(span, node.firstChild);
+  }
+});
