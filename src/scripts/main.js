1455c685542b8e17478b53b924448b86f690b3d1
Jane Shavrukova <eva.shavrukova@gmail.com>
2024-07-17 21:34:51 -0700

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach((item) => {
+  item.childNodes.forEach((node) => {
+    if (node.nodeType === Node.TEXT_NODE) {
+      const text = node.textContent.trim();
+
+      const span = document.createElement('span');
+
+      span.className = 'listHead';
+      span.textContent = text;
+
+      node.parentNode.insertBefore(span, node);
+      node.remove();
+    }
+  });
+});
+
+document.addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (target.classList.contains('listHead')) {
+    const next = target.nextElementSibling;
+
+    if (next.style.display === 'none') {
+      next.style.display = 'block';
+    } else {
+      next.style.display = 'none';
+    }
+  }
+});
