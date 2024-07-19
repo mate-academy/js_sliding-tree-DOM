bf63ec881fd92dfe295bf6fb31ae7e10f18749d0
Olha Rypich <olha.rypich@gmail.com>
2024-07-19 23:22:51 +0300

    js_sliding-tree-DOM_solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('.tree');
+
+listItems.forEach((li) => {
+  if (li.children.length > 0) {
+    const span = document.createElement('span');
+
+    li.prepend(span);
+    span.append(span.nextSibling);
+    span.innerText = span.innerText.trim();
+  }
+});
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    e.target.nextSibling.hidden = !e.target.nextSibling.hidden;
+  }
+});
