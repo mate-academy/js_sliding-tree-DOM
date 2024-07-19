e2ac12ae84c3c7a2ce1c50f51442890fb738c69d
Slava Filimonenko <=>
2024-07-19 14:58:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const treeElement = document.querySelector('.tree');
+
+function toggleHeaders(ulElement) {
+  const liElements = [...ulElement.children];
+
+  liElements.forEach((li) => {
+    const childUl = li.querySelector('ul');
+
+    if (childUl) {
+      const headerText = li.firstChild.textContent;
+      const span = document.createElement('span');
+
+      span.textContent = headerText;
+
+      li.firstChild.replaceWith(span);
+
+      span.addEventListener('click', () => {
+        childUl.style.display =
+          childUl.style.display === 'none' ? 'block' : 'none';
+      });
+
+      toggleHeaders(childUl);
+    }
+  });
+}
+
+toggleHeaders(treeElement);
