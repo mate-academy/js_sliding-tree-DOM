9571361d94df9076a1ca7db0e8f7d0a347989b81
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-20 10:01:54 +0300

    add draft

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const lis = document.querySelectorAll('li:has(ul)');
+
+lis.forEach((el) => {
+  const span = document.createElement('span');
+
+  span.className = 'tree__headline';
+  span.append(el.firstChild);
+  span.after(el.firstElementChild);
+  el.prepend(span);
+});
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const headLine = e.target.closest('.tree__headline');
+
+  if (!headLine) {
+    return;
+  }
+
+  if (headLine.nextSibling.style.display === 'none') {
+    headLine.nextSibling.style.display = '';
+  } else {
+    headLine.nextSibling.style.display = 'none';
+  }
+});
