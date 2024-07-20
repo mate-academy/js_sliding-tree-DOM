4c091914b55f0fbd0682ce071498acaae477401f
Andrii <andriykostiuk1@gmail.com>
2024-07-20 19:29:54 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach((item) => {
+  if (item.children.length > 0) {
+    const span = document.createElement('span');
+
+    item.prepend(span);
+    span.append(span.nextSibling);
+    span.textContent = span.textContent.trim();
+  }
+});
+
+document.addEventListener('click', (evt) => {
+  if (!tree || !tree.contains(evt.target)) {
+    return;
+  }
+
+  if (evt.target.tagName === 'SPAN') {
+    evt.target.nextSibling.hidden = !evt.target.nextSibling.hidden;
+  }
+});
