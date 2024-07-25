ae58daa3f1162b4f752bf02f12448a4a2c8cf3c7
Anna <ania.somko@gmail.com>
2024-07-25 15:36:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+const li = document.querySelectorAll('li');
+const ul = document.querySelector('ul');
+
+li.forEach((item) => {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.prepend(span.nextSibling);
+});
+
+ul.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  e.target.nextSibling.hidden = !e.target.nextSibling.hidden;
+});
