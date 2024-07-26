3fb71024e92317c9a4a427a2c875c6a5137695dc
svyatoslav <svyatoslavik9@gmail.com>
2024-07-26 13:18:47 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const headlines = [...tree.querySelectorAll('li')];
+const indexTitle = 1;
+
+const SpanElement = (text) => {
+  const span = document.createElement('span');
+
+  span.textContent = text;
+
+  return span;
+};
+
+const App = () => {
+  for (const headline of headlines) {
+    const span = SpanElement();
+
+    if (headline.firstChild.textContent.includes('  ')) {
+      span.textContent = headline.firstChild.textContent;
+      headline.firstChild.replaceWith(span);
+    }
+
+    span.addEventListener('click', () => {
+      const list = headline.children[indexTitle];
+
+      if (list.style.display === '') {
+        list.style.display = 'none';
+      } else {
+        list.style.display = '';
+      }
+    });
+  }
+};
+
+document.addEventListener('DOMContentLoaded', App);
