ec3e1deac394af5610a91d0d95f6bc756b8471ad
Mykola Bilenkiy <nikolosknv@gmail.com>
2024-07-31 15:32:42 +0300

    make task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+for (const li of document.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  span.textContent = li.childNodes[0].textContent.trim();
+  li.childNodes[0].textContent = '';
+  li.prepend(span);
+}
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', function (e) {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const childContainer = e.target.parentNode.querySelector('ul');
+
+  if (!childContainer) {
+    return;
+  }
+
+  childContainer.hidden = !childContainer.hidden;
+});
