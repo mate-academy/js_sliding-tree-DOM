e3ca62ca56d255f35ade05ed4f51ed84812ae97f
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2024-08-05 21:02:21 +0200

    created functionality but need some support in providing list styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const spans = document.querySelectorAll('.tree span');
+
+  spans.forEach((span) => {
+    const nested = span.nextElementSibling;
+
+    if (nested && nested.tagName === 'UL') {
+      nested.style.display = 'none';
+    }
+  });
+
+  spans.forEach((span) => {
+    span.addEventListener('click', (e) => {
+      const nested = span.nextElementSibling;
+
+      if (nested && nested.tagName === 'UL') {
+        nested.style.display =
+          nested.style.display === 'none' ? 'block' : 'none';
+      }
+
+      e.stopPropagation();
+    });
+  });
+});
