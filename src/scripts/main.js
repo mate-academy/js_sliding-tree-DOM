01aa2d52acdcd478f39bb8eab36a5ea988f3a0d6
Sergiy Dolgov <info@devmax.pro>
2024-08-05 13:44:58 -0400

    v 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', DOMready);
+
+function DOMready() {
+  const tree = document.querySelector('.tree');
+
+  markHeaders(tree);
+
+  tree.addEventListener('click', (e) => {
+    if (e.target.tagName === 'SPAN') {
+      const childUl = e.target.parentNode.querySelector('ul');
+
+      childUl.hidden = !childUl.hidden;
+    }
+  });
+}
+
+function markHeaders(list) {
+  const allLi = list.querySelectorAll('li');
+
+  for (const li of allLi) {
+    if (li.children.length > 0) {
+      const span = document.createElement('span');
+
+      li.prepend(span);
+      span.append(span.nextSibling);
+    }
+  }
+}
