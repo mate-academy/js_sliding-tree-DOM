330a5ee50a28157920cfd3c44d064cf0a6f42b34
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-08-06 09:43:15 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const li = tree.querySelectorAll('li');
+
+for (const item of li) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', function (e) {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const parentContainer = e.target.parentNode.querySelector('ul');
+
+  if (!parentContainer) {
+    return;
+  }
+
+  if (!parentContainer.hidden) {
+    parentContainer.hidden = true;
+  } else {
+    parentContainer.hidden = false;
+  }
+});
