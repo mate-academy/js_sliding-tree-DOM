7e50771596aa80720bfb8d0cecd0d7a516b239fc
Ostap Hedz <lordanvil18@gmail.com>
2024-08-07 12:18:47 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const items = tree.querySelectorAll('li');
+
+items.forEach((item) => {
+  const spanItem = document.createElement('span');
+
+  item.prepend(spanItem);
+  spanItem.append(spanItem.nextSibling);
+});
+
+tree.addEventListener('click', function (e) {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const eventUl = e.target.parentNode.querySelector('ul');
+
+  if (!eventUl) {
+    return;
+  }
+
+  eventUl.hidden = !eventUl.hidden;
+});
