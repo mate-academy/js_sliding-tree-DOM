212ce662fb578825da6ef9ea9b834b567a466048
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2024-08-08 10:30:49 +0200

    wrapped with spans with DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,27 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  const wrapTextNodes = (element) => {
+    element.childNodes.forEach((child) => {
+      if (
+        child.nodeType === Node.TEXT_NODE &&
+        child.textContent.trim() !== ''
+      ) {
+        const span = document.createElement('span');
+
+        span.textContent = child.textContent.trim();
+        element.insertBefore(span, child);
+        element.removeChild(child);
+      } else if (child.nodeType === Node.ELEMENT_NODE) {
+        wrapTextNodes(child);
+      }
+    });
+  };
+
+  wrapTextNodes(tree);
+
   const spans = document.querySelectorAll('.tree span');
 
   spans.forEach((span) => {
