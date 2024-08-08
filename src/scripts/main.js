49dd4064408ab49d64ecf46e0c172d3718229989
LadyLiza <elizabet132010@gmail.com>
2024-08-08 14:28:31 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach((listItem) => {
+  const span = document.createElement('span');
+
+  span.textContent = listItem.firstChild.textContent;
+
+  listItem.replaceChild(span, listItem.firstChild);
+
+  span.addEventListener('click', () => {
+    const nestedList = listItem.querySelector('ul');
+
+    if (nestedList) {
+      nestedList.hidden = !nestedList.hidden;
+    }
+  });
+});
