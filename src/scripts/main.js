0a4186382db6502b04a30e39aba1996cf6720585
Olexander <v.h.olexander@gmail.com>
2024-08-09 13:00:56 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.style.cursor = 'pointer';
+
+tree.addEventListener('click', (e) => {
+  const li = e.target.closest('li');
+
+  if (li) {
+    if (li.firstElementChild.style.display === '') {
+      li.firstElementChild.style.display = 'none';
+    } else {
+      li.firstElementChild.style.display = '';
+      li.firstElementChild.removeAttribute('style');
+    }
+  }
+});
