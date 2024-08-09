df11ac0d990953319123d0b02280c1fae2cff624
Maksim Atanasov <atanasovmaksim56@gmail.com>
2024-08-09 13:40:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const titles = document.querySelectorAll('li');
+
+titles.forEach((element) => {
+  if (element.children.length === 0) {
+    return;
+  }
+
+  const span = document.createElement('span');
+
+  span.textContent = element.firstChild.nodeValue.trim();
+  span.style.cursor = 'pointer';
+
+  element.prepend(span);
+
+  if (span.nextSibling) {
+    span.nextSibling.remove();
+  }
+});
+
+tree.addEventListener('click', (clickEvent) => {
+  if (clickEvent.target.tagName === 'SPAN') {
+    const childList = clickEvent.target.nextSibling;
+
+    if (childList && childList.tagName === 'UL') {
+      childList.hidden = !childList.hidden;
+    }
+  }
+});
