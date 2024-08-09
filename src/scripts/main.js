e6927ea2dbaa7cb80f0ad5305ea76d7625e90cd6
Maria <maria.pozniak2013@gmail.com>
2024-08-09 13:24:02 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('li').forEach(item => {
+    if (item.childNodes.length > 1) {
+        let [text, content] = [item.childNodes[0].textContent, item.childNodes[1]];
+        item.innerHTML = `<span class="slider">${text}</span>`;
+        item.appendChild(content);
+    }
+});
+
+document.querySelectorAll('.slider').forEach(slider => {
+    slider.addEventListener('click', () => {
+        let sibling = slider.nextElementSibling;
+        sibling.style.display = sibling.style.display === 'none' ? '' : 'none';
+    });
+});
