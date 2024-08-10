b160d22142551fa0fdcf9b05425df258a442bab2
Olexander <v.h.olexander@gmail.com>
2024-08-10 19:09:52 +0300

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,30 @@ const tree = document.querySelector('.tree');
 
 tree.style.cursor = 'pointer';
 
+const list = document.querySelectorAll('li');
+
+list.forEach((li) => {
+  if (li.firstChild) {
+    const textNode = li.childNodes[0];
+    const span = document.createElement('span');
+
+    span.textContent = textNode.textContent;
+    li.replaceChild(span, textNode);
+  }
+});
+
 tree.addEventListener('click', (e) => {
   const li = e.target.closest('li');
 
   if (li) {
-    if (li.firstElementChild.style.display === '') {
-      li.firstElementChild.style.display = 'none';
-    } else {
-      li.firstElementChild.style.display = '';
-      li.firstElementChild.removeAttribute('style');
+    const ul = li.querySelector('ul');
+
+    if (ul) {
+      if (ul.style.visibility === 'hidden') {
+        ul.style.visibility = 'visible';
+      } else {
+        ul.style.visibility = 'hidden';
+      }
     }
   }
 });
