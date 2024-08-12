9fa745ff03ba66f7661b285ce24eae18af74193b
Mainaiev <farid.mainaiev@proton.me>
2024-08-12 03:45:03 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const list = tree.querySelectorAll('li');
+
+document.addEventListener('DOMContentLoaded', createSpan);
+tree.addEventListener('click', hideElement);
+
+function createSpan() {
+  for (const li of list) {
+    const span = document.createElement('span');
+
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+function hideElement(e) {
+  e.preventDefault();
+
+  const hiddenElement = e.target.parentNode.querySelector('ul');
+
+  if (e.target.tagName === 'SPAN' && hiddenElement) {
+    hiddenElement.hidden = !hiddenElement.hidden;
+  }
+}
