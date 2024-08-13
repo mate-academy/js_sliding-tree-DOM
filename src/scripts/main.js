4ca85741f68dcfec125a723dc585e077f4b43682
Victor Komara <vitioklss@gmail.com>
2024-08-13 16:47:32 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const arrLi = document.querySelectorAll('li');
+
+function getTextNodes(element) {
+  return Array.from(element.childNodes).filter(
+    (node) => node.nodeType === 3 && node.textContent.trim().length > 1,
+  );
+}
+
+function createSpan(node) {
+  const span = document.createElement('span');
+
+  node.after(span);
+  span.appendChild(node);
+}
+
+for (let i = 0; i < arrLi.length; i++) {
+  arrLi[i].setAttribute('id', `${i}`);
+
+  if (arrLi[i].firstElementChild) {
+    const textNodes = getTextNodes(arrLi[i]);
+
+    createSpan(textNodes[0]);
+  }
+}
+
+document.addEventListener('click', function (e) {
+  const id = e.target.closest('li').id;
+  const elem = document.getElementById(id).lastElementChild;
+
+  if (!id && elem) {
+    return;
+  }
+
+  elem.hidden = !elem.hidden;
+});
