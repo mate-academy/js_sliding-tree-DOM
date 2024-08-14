8ec66d06ef791e2c680e00b7a5d75ec5ac06a8d2
Fetrw <21vladmel11@gmail.com>
2024-08-14 17:58:14 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('.tree li').forEach((listItem) => {
+  const headerSpan = document.createElement('span');
+
+  headerSpan.textContent = listItem.firstChild.textContent;
+  headerSpan.style.cursor = 'pointer';
+
+  listItem.insertBefore(headerSpan, listItem.firstChild);
+  listItem.firstChild.nextSibling.remove();
+
+  headerSpan.addEventListener('click', () => {
+    const subList = listItem.querySelector('ul');
+
+    if (subList) {
+      subList.hidden = !subList.hidden;
+    }
+  });
+});
