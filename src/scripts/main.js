b1ee58960db6c1cb6672f71fc3ca43aecd1e9c03
Tatiana <tanya_potapenko@bigmir.net>
2024-08-15 15:38:51 +0200

    js_sliding-tree_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const headlines = document.querySelectorAll('.headline');
+
+headlines.forEach((headline) => {
+  headline.addEventListener('click', (e) => {
+    const list = headline.nextElementSibling;
+
+    if (list && list.tagName === 'UL') {
+      if (e.target.tagName === 'SPAN') {
+        const isListHidden = getComputedStyle(list).display === 'none';
+        // getComputedStyle()-
+        // returns an object containing all the styles applied to the element;
+
+        if (isListHidden) {
+          list.style.display = 'block';
+        } else {
+          list.style.display = 'none';
+        }
+      }
+    }
+  });
+});
