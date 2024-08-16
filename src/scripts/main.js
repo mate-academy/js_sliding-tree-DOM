64bcb5f7c3e3c554095af9bd6a9e54a6a1303300
Daria Sydorenko <dashasidorenko123@gmail.com>
2024-08-16 14:30:39 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+function wrapIntoSpan(node) {
+  const textNodes = Array.from(node.childNodes).filter(
+    (child) => child.nodeType === node.TEXT_NODE,
+  );
+
+  textNodes.forEach((textNode) => {
+    const span = document.createElement('span');
+
+    span.textContent = textNode.textContent.trim();
+    node.insertBefore(span, textNode);
+    node.removeChild(textNode);
+  });
+}
+
+tree.querySelectorAll('li').forEach((li) => {
+  wrapIntoSpan(li);
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const li = e.target.parentElement;
+    const subUl = li.querySelector('ul');
+
+    if (subUl) {
+      subUl.hidden = !subUl.hidden;
+    }
+  }
+});
