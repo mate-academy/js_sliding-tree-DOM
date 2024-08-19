77d00ea4e5e4843d6263d4f9b97bfbdef9381b65
Mikhailo Koshilka <mkosilka@gmail.com>
2024-08-19 23:01:09 +0100

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const allLi = document.querySelectorAll('li');
+const filteredLi = Array.from(allLi).filter(
+  (li) => li.querySelector('ul') !== null,
+);
+
+filteredLi.forEach((li) => {
+  const targ = li.firstChild;
+
+  const span = document.createElement('span');
+
+  span.textContent = targ.textContent.trim();
+  li.replaceChild(span, targ);
+});
+
+document.addEventListener('click', (eve) => {
+  const clickedLi = eve.target.closest('li');
+
+  if (clickedLi) {
+    const nestedUl = clickedLi.querySelector('ul');
+
+    if (nestedUl) {
+      if (getComputedStyle(nestedUl).display === 'none') {
+        nestedUl.style.display = 'block';
+      } else {
+        nestedUl.style.display = 'none';
+      }
+    }
+  }
+});
