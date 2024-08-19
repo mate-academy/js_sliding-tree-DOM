002e0e9673f61d79cf9479b0877dce67e711f5c4
Denys209818 <deniskravcuk63@gmail.com>
2024-08-20 01:22:33 +0300

    fixed bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,8 @@ list.addEventListener('click', (e) => {
   const span = e.target.closest('span');
 
   if (span) {
-    const current = span.closest('ul');
+    const current = span.closest('li');
+
     const subList = current.querySelector('ul');
 
     if (subList) {
