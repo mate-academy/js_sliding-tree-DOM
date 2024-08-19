62a2b470e4df94eecb93d489a10705ebb4e56fc2
Denys209818 <deniskravcuk63@gmail.com>
2024-08-20 01:17:42 +0300

    Load full realization

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const list = document.getElementsByClassName('tree')[0];
+
+list.addEventListener('click', (e) => {
+  const span = e.target.closest('span');
+
+  if (span) {
+    const current = span.closest('ul');
+    const subList = current.querySelector('ul');
+
+    if (subList) {
+      subList.hidden = !subList.hidden;
+    }
+  }
+});
