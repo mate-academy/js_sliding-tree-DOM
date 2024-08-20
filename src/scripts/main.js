b3ffe7062f49dbd6b4cd6e3875bff75c113f9d7e
Denys209818 <deniskravcuk63@gmail.com>
2024-08-20 13:17:01 +0300

    fix failures

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,26 @@
 // write code here
 const list = document.getElementsByClassName('tree')[0];
 
+const lies = document.getElementsByTagName('li');
+
+[...lies].reverse().forEach((el) => {
+  const newLiText = el.innerHTML;
+
+  el.innerHTML = '';
+
+  const spanItem = document.createElement('span');
+
+  const allContent = newLiText
+    .split('\n')
+    .filter((x) => x)
+    .map((l) => l.trim());
+
+  spanItem.innerText = allContent[0];
+
+  el.append(spanItem);
+  el.innerHTML += allContent.slice(1).reduce((prev, curr) => prev + curr, '');
+});
+
 list.addEventListener('click', (e) => {
   const span = e.target.closest('span');
 
