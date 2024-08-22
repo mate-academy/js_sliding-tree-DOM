338660e0fb875715d5b6fd64f0fec097139d6281
Burchak Dmitry <burchak.dmitry@icloud.com>
2024-08-22 15:21:41 +0300

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('.tree li');
+
+listItems.forEach((item) => {
+  const span = document.createElement('span');
+
+  span.textContent = item.firstChild.textContent.trim();
+  item.firstChild.replaceWith(span);
+
+  span.addEventListener('click', (e) => {
+    const subList = item.querySelector('ul');
+
+    if (subList) {
+      subList.style.display =
+        subList.style.display === 'none' ? 'block' : 'none';
+    }
+
+    e.stopPropagation();
+  });
+});
