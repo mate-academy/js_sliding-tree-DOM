ea5b5ae2f6770b9c91d0f20b9f43064742055c12
Daria Nastas <bycinska@gmail.com>
2024-08-27 11:04:37 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', function () {
+  // Додаємо тег `span` для кожного заголовка
+  document.querySelectorAll('.tree li').forEach((li) => {
+    // Якщо в li є вкладений ul, то обгортаємо текст у span
+    if (li.querySelector('ul')) {
+      const span = document.createElement('span');
+
+      span.textContent = li.childNodes[0].textContent.trim();
+      li.childNodes[0].replaceWith(span);
+    }
+  });
+
+  // Додаємо обробник подій на кожен span
+  document.querySelectorAll('.tree span').forEach((span) => {
+    span.addEventListener('click', function () {
+      const childUl = this.nextElementSibling;
+
+      if (childUl) {
+        childUl.style.display =
+          childUl.style.display === 'none' ? 'block' : 'none';
+      }
+    });
+  });
+});
