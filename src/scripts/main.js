267aeb166c2621730eaa4dc9c64cc04d004c4c57
yursprava <yursprava@gmail.com>
2024-08-28 23:26:09 -0400

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const liElements = document.querySelectorAll('.tree li');
+
+liElements.forEach((li) => {
+  if (li.childNodes.length !== 1) {
+    const newSpan = document.createElement('span');
+
+    newSpan.textContent = li.childNodes[0].textContent;
+    li.childNodes[0].textContent = '';
+    li.prepend(newSpan);
+  }
+});
+
+const headTag = document.querySelector('head');
+const styleTag = document.createElement('style');
+
+styleTag.textContent = '.close { display: none; }';
+headTag.prepend(styleTag);
+
+const spanCollection = document.querySelectorAll('span');
+
+spanCollection.forEach((span) => {
+  span.onclick = function () {
+    span.nextElementSibling.classList.toggle('close');
+  };
+});
