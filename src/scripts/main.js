f9e53321bccc4a7ec93047aca144a2bf4101dca6
denys-khvashchenko <denys.khvashchenko@gmail.com>
2024-08-30 13:09:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+li.forEach((el) => {
+  if (el.querySelector('ul')) {
+    el.classList.add('has-child');
+
+    const childUl = el.querySelector('ul');
+
+    childUl.style.display = 'block';
+
+    const textNode = el.firstChild;
+    const span = document.createElement('span');
+
+    span.style.cursor = 'pointer';
+    span.textContent = textNode.textContent.trim();
+
+    el.insertBefore(span, textNode);
+    el.removeChild(textNode);
+
+    span.addEventListener('click', (e) => {
+      e.stopPropagation();
+
+      if (childUl.style.display === 'block') {
+        childUl.style.display = 'none';
+      } else {
+        childUl.style.display = 'block';
+      }
+    });
+  }
+});
