ac3e7702a0424e9fbb2ef88b81e7726ce84fcc22
sinner1993 <sergio1993goga@gmail.com>
2024-08-31 20:36:25 +0200

    adasdsa

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const ul = document.querySelectorAll('ul');
+
+ul.forEach((el) => {
+  if (!el.className) {
+    el.style.visibility = 'hidden';
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'LI') {
+    const childUl = e.target.querySelector('ul');
+
+    if (childUl) {
+      childUl.style.visibility =
+        childUl.style.visibility === 'hidden' ? 'visible' : 'hidden';
+    }
+  }
+});
