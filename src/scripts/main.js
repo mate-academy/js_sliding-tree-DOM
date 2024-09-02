a734533d374482132ef1d0fac2e58d4c8120c4bc
Ostap Kushnir <kushnirostap04@gmail.com>
2024-09-02 16:04:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('ul');
+
+list.forEach((element) => {
+  const lis = element.querySelectorAll('li');
+
+  lis.forEach((li) => {
+    const header = li.firstChild.textContent;
+    const span = document.createElement('span');
+    const ulInLi = li.querySelector('ul');
+
+    if (header && ulInLi) {
+      li.firstChild.textContent = '';
+
+      span.textContent = header;
+
+      li.insertBefore(span, ulInLi);
+    }
+
+    let isHidden = false;
+
+    span.addEventListener('click', () => {
+      if (isHidden) {
+        ulInLi.style.display = 'block';
+      } else {
+        ulInLi.style.display = 'none';
+      }
+
+      isHidden = !isHidden;
+    });
+  });
+});
