4671e168d9f70891e68a737207f68cc0809d8c9f
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-09-05 08:27:21 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liItems = document.querySelectorAll('li');
+
+for (const item of liItems) {
+  const firstChild = item.firstChild;
+  const span = document.createElement('span');
+
+  span.append(item.firstChild.textContent.trim());
+  firstChild.replaceWith(span);
+}
+
+tree.addEventListener('click', (e) => {
+  const spans = [...document.querySelectorAll('span')];
+
+  if (spans.includes(e.target)) {
+    const nextSibling = e.target.nextElementSibling;
+
+    if (nextSibling) {
+      nextSibling.style.display =
+        nextSibling.style.display === 'none' ? 'block' : 'none';
+    }
+  }
+});
