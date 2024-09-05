0749d392f5cee30193728d58f342421bd0f789e5
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-09-06 06:51:46 +0700

    add js sliding tree solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const li = document.querySelectorAll('li');
+
+const liWithUL = [...li].filter((item) => item.children.length > 0);
+
+liWithUL.forEach((element) => {
+  const span = document.createElement('span');
+
+  span.append(element.firstChild);
+  element.prepend(span);
+});
+
+tree.addEventListener('click', (e) => {
+  const { target } = e;
+
+  if (target.nextElementSibling.style.display === 'none') {
+    target.nextElementSibling.style.display = 'block';
+
+    return;
+  }
+
+  if (target.tagName === 'SPAN') {
+    target.nextElementSibling.style.display = 'none';
+  }
+});
