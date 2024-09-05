084ebc2139a53041b1d80a4afa94618ec167ee85
Rostyslav <rostislavkpi@gmail.com>
2024-09-05 20:26:19 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const listItems = document.querySelectorAll('li');
+const fragment = document.createDocumentFragment();
+
+listItems.forEach((listItem) => {
+  const span = document.createElement('span');
+
+  span.className = 'list-item';
+  span.append(listItem.firstChild);
+  fragment.appendChild(span);
+  listItem.prepend(fragment);
+});
+
+list.addEventListener('click', (e) => {
+  if (e.target.className === 'list-item') {
+    const currentHeader = e.target.parentNode.querySelector('ul');
+
+    if (currentHeader) {
+      currentHeader.hidden = !currentHeader.hidden;
+    }
+  }
+});
