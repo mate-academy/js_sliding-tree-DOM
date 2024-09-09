7e90efa7dfb1a776545095619471d19208b96bfb
Nataliia <koreniuknataliia@gmail.com>
2024-09-09 18:46:40 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('.tree li');
+
+for (const li of list) {
+  if (li.children.length) {
+    const textNode = li.firstChild;
+
+    const span = document.createElement('span');
+
+    span.textContent = textNode.textContent.trim();
+
+    textNode.replaceWith(span);
+
+    span.addEventListener('click', () => {
+      const sibling = span.nextElementSibling;
+
+      if (sibling) {
+        sibling.style.display = sibling.style.display === 'none' ? '' : 'none';
+      }
+    });
+  }
+}
