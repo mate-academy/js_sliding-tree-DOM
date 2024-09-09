9b1fd914caab0c7f7e40e0557dd8fae9f71023ad
Artem-Brui <artembryj@gmail.com>
2024-09-09 17:35:39 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const headlines = document.querySelectorAll('li');
+
+function wrapTextToSpan(element) {
+  const children = [...element.children];
+
+  for (const child of element.children) {
+    child.remove();
+  }
+
+  const copyText = element.innerText.trim();
+  const newSpan = document.createElement('span');
+
+  element.textContent = '';
+  newSpan.innerText = copyText;
+  element.appendChild(newSpan);
+
+  children.forEach((item) => element.appendChild(item));
+}
+
+for (const el of headlines) {
+  if ([...el.children].length > 0) {
+    wrapTextToSpan(el);
+  }
+}
+
+tree.addEventListener(
+  'click',
+  (ev) => {
+    if (ev.target.tagName === 'SPAN') {
+      for (const child of ev.target.closest('li').children) {
+        child.style.display =
+          child.style.display !== 'none' && child.tagName !== 'SPAN'
+            ? 'none'
+            : 'block';
+      }
+    }
+  },
+  true,
+);
