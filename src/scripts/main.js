28c4eb47e16caf8888be70e9a274fd741ec38324
Pavlo Pukalo <misterkatlet@gmail.com>
2024-09-12 11:44:41 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const treeItems = document.querySelectorAll('.tree li');
+
+treeItems.forEach((treeItem) => {
+  const span = document.createElement('span');
+
+  span.textContent = treeItem.firstChild.textContent.trim();
+  treeItem.firstChild.textContent = '';
+  treeItem.prepend(span);
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const childUl = e.target.parentNode.querySelector('ul');
+
+    if (childUl) {
+      childUl.style.display =
+        childUl.style.display === 'none' ? 'block' : 'none';
+    }
+  }
+});
