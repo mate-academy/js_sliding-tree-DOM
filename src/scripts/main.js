34be7b6e664f17c7ac6c0195b1d40a3aa6b3cdf1
Ren <buher1994@gmail.com>
2024-09-14 23:45:34 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write code here
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach((item) => {
+  const span = document.createElement('span');
+
+  span.textContent = item.firstChild.textContent;
+  item.firstChild.replaceWith(span);
+
+  if (span.nextElementSibling) {
+    span.addEventListener('click', () => {
+      span.nextElementSibling.style.display =
+        span.nextElementSibling.style.display !== 'none' ? 'none' : '';
+    });
+  }
+});
