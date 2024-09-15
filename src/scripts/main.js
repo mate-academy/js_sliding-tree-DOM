196294e91d846b2287771da80c7b81c89676e486
Oleksii Kudriavtsev <Lesha61945@gmail.com>
2024-09-15 19:12:29 +0300

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('li').forEach((li) => {
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent.trim();
+
+  li.firstChild.replaceWith(span);
+
+  span.addEventListener('click', (e) => {
+    const subList = li.querySelector('ul');
+
+    if (subList) {
+      subList.hidden = !subList.hidden;
+    }
+  });
+});
