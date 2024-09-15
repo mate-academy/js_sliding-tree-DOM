34a7f00ba55983d17b2ad8615bcc7db6b368ab52
Hryniuk_Taras <gritaras484@gmail.com>
2024-09-15 15:40:46 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+function wrapInSpan(element) {
+  const span = document.createElement('span');
+
+  span.textContent = element.textContent;
+  element.replaceWith(span);
+}
+
+const listItems = tree.querySelectorAll('li > ul');
+
+listItems.forEach((ul) => {
+  const li = ul.parentElement;
+
+  wrapInSpan(li.firstChild);
+});
+
+document.addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (target.tagName === 'SPAN') {
+    const parentLi = target.parentElement;
+    const sublist = parentLi.querySelector('ul');
+
+    if (sublist.style.display === 'none') {
+      sublist.style.display = 'block';
+    } else {
+      sublist.style.display = 'none';
+    }
+  }
+});
