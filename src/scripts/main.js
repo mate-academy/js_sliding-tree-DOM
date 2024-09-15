4bee74ebfe245892dc3bf2d8e694e3404c2e1aac
sinner1993 <sergio1993goga@gmail.com>
2024-09-15 16:21:26 +0200

    asdasd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,6 @@
 const tree = document.querySelector('.tree');
 
 const listItems = document.querySelectorAll('.tree li');
-const ul = document.querySelectorAll('ul');
-
-ul.forEach((el) => {
-  el.style.maxHeight = '100vh';
-  el.style.transition = 'max-height 300ms ease-in-out';
-});
 
 listItems.forEach((item) => {
   const span = document.createElement('span');
@@ -23,25 +17,11 @@ tree.addEventListener('click', (e) => {
     const childUl = e.target.parentNode.querySelector('ul');
 
     if (childUl) {
-      if (window.getComputedStyle(childUl).visibility === 'visible') {
-        Array.from(childUl.children).forEach(function (element) {
-          element.style.opacity = '0';
-          element.style.transition = 'opacity 300ms ease-in-out';
-          element.style.transition = 'max-height 300ms ease-in-out';
-        });
-      } else {
-        Array.from(childUl.children).forEach(function (element) {
-          element.style.opacity = '1';
-          element.style.transition = 'opacity 300ms ease-in-out';
-          element.style.transition = 'max-height 300ms ease-in-out';
-        });
-      }
-
       childUl.style.maxHeight =
         childUl.style.maxHeight === '0vh' ? '100vh' : '0vh';
 
-      childUl.style.visibility =
-        childUl.style.visibility === 'hidden' ? 'visible' : 'hidden';
+      childUl.style.display =
+        childUl.style.display === 'none' ? 'block' : 'none';
     }
   }
 });
