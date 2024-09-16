864e8ecfcd4d17aa84e55235e176c95e6c53c39b
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2024-09-16 14:22:55 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const treeItems = document.querySelectorAll('.tree > li, .tree > li > ul > li');
+const li = document.querySelectorAll('li');
+
+treeItems.forEach((item) => {
+  const textNode = item.firstChild;
+  const span = document.createElement('span');
+
+  span.textContent = textNode.textContent;
+  item.replaceChild(span, textNode);
+});
+
+li.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    if (element.children.length > 0) {
+      if (element.lastElementChild.style.display === 'none') {
+        element.lastElementChild.style.display = 'block';
+      } else {
+        element.lastElementChild.style.display = 'none';
+      }
+    }
+  });
+});
