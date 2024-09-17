0cf33d92034def8e573bfbc14a08a3ae60fad3bf
Mariia <maria.gula0307@gmail.com>
2024-09-17 18:14:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+function wrapHeaderIntoSpan(parentElement) {
+  Array.from(parentElement).forEach((child) => {
+    if (child.querySelector('ul')) {
+      const span = document.createElement('span');
+      const childText = child.firstChild;
+
+      span.textContent = childText.textContent.trim();
+      child.replaceChild(span, childText);
+
+      wrapHeaderIntoSpan(child.querySelectorAll('ul > li'));
+    }
+  });
+}
+
+const treeChildren = document.querySelectorAll('.tree > li');
+
+wrapHeaderIntoSpan(treeChildren);
+
+const spans = document.querySelectorAll('span');
+
+spans.forEach((span) => {
+  span.addEventListener('click', () => {
+    const list = span.nextSibling;
+
+    if (list) {
+      list.style.display = list.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
