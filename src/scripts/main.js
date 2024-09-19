74a9c0b573b1724dd149e3a8b14f8e431a901b1a
vshvanska <valeriiashvanska@gmail.com>
2024-09-19 14:53:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('.tree li');
+
+headers.forEach((header) => {
+  const textNode = header.firstChild;
+  const span = document.createElement('span');
+
+  span.textContent = textNode.textContent.trim();
+  header.replaceChild(span, textNode);
+
+  span.addEventListener('click', () => {
+    const sublist = header.querySelector('ul');
+
+    if (sublist) {
+      if (sublist.style.display === 'none') {
+        sublist.style.display = 'block';
+      } else {
+        sublist.style.display = 'none';
+      }
+    }
+  });
+});
