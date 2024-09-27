1dcb877d1870e04f93bd54ea6185e551424121be
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2024-09-28 00:09:15 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const treeItems = document.querySelectorAll('li');
+
+treeItems.forEach((item) => {
+  const span = document.createElement('span');
+
+  span.textContent = item.firstChild.textContent;
+  item.firstChild.replaceWith(span);
+
+  span.addEventListener('click', () => {
+    const ul = item.querySelector('ul');
+
+    if (ul) {
+      ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
