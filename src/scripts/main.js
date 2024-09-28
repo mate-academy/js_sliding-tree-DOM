507d2c57eff2d06d9be7674542560776357502d9
Alex Popov <alexpopovdm@outlook.com>
2024-09-28 23:42:37 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('.tree li');
+
+items.forEach((item) => {
+  const span = document.createElement('span');
+  const sublist = item.querySelector('ul');
+
+  span.textContent = item.firstChild.textContent;
+  item.firstChild.replaceWith(span);
+
+  if (sublist) {
+    span.addEventListener('click', () => {
+      sublist.style.display =
+        sublist.style.display === 'none' ? 'block' : 'none';
+    });
+  }
+});
