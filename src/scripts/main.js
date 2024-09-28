b6f55d8b9d8565ffd0af2ef303c68c19fce665cc
Viktoriia Dereza <viktoriiadereza7@gmail.com>
2024-09-28 23:18:14 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  tree.querySelectorAll('li').forEach((li) => {
+    const span = document.createElement('span');
+
+    span.textContent = li.firstChild.textContent.trim();
+    li.firstChild.replaceWith(span);
+
+    span.addEventListener('click', () => {
+      const childrenContainer = li.querySelector('ul');
+
+      if (childrenContainer) {
+        childrenContainer.hidden = !childrenContainer.hidden;
+      }
+    });
+  });
+});
