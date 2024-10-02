ec0e6d0a830f9f95755148b80222914234978de4
Ivan Dovzhanyn <ivan.dovzhanyn@gmail.com>
2024-10-02 22:05:07 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+list.forEach((element) => {
+  if (element.querySelector('ul')) {
+    const span = document.createElement('span');
+
+    const text = element.firstChild.textContent.trim();
+
+    span.textContent = text;
+
+    element.prepend(span);
+
+    element.firstChild.nextSibling.nodeValue = '';
+
+    span.addEventListener('click', (e) => {
+      const field = e.target;
+      const ul = field.parentElement.querySelector('ul');
+
+      if (ul) {
+        if (ul.style.display === 'none') {
+          ul.style.display = 'block';
+        } else {
+          ul.style.display = 'none';
+        }
+      }
+    });
+  }
+});
