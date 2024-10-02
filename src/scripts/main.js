3b9b647f0d5220b491660da52cdbb315bd085cd5
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-10-02 15:33:00 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+headers.forEach((node) => {
+  const spanElem = document.createElement('span');
+
+  spanElem.textContent = node.firstChild.textContent.trim();
+
+  node.replaceChild(spanElem, node.firstChild);
+});
+
+tree.addEventListener('click', (e) => {
+  const list = e.target.closest('li').querySelector('ul');
+
+  if (list) {
+    list.classList.toggle('hidden');
+  }
+});
