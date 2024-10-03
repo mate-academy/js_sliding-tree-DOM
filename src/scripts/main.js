a966330a3fd2abc0c0f3102fe47cd1bab9e7bd51
kovaltar <kovaltar@gmail.com>
2024-10-03 18:14:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const headlines = document.querySelectorAll('li');
+
+headlines.forEach((item, ind, arr) => {
+  const text = item.firstChild.textContent.trim();
+  const span = document.createElement('span');
+
+  item.firstChild.remove();
+  item.prepend(span);
+  span.innerText = text;
+});
+
+tree.addEventListener('click', toogleList);
+
+function toogleList(e) {
+  const targetTag = e.target.tagName;
+  const targetSib = e.target.nextSibling;
+
+  if (targetTag === 'SPAN' && targetSib) {
+    if (targetSib.style.display !== 'none') {
+      targetSib.style.display = 'none';
+    } else {
+      targetSib.style.display = 'block';
+    }
+  }
+}
