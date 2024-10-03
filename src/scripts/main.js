306df1d7dd75ab7b1d9fbd445d257e5adcc6555d
missk <rostock.vasiljev@gmail.com>
2024-10-03 16:30:18 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const hover = document.querySelectorAll('span');
+
+hover.forEach((span) => {
+  span.addEventListener('click', (e) => {
+    const content = e.target.parentElement.querySelector('ul');
+
+    if (content.style.display === 'none') {
+      content.style.display = 'block';
+    } else {
+      content.style.display = 'none';
+    }
+  });
+});
