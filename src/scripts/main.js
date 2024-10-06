ea36d1b3a16c2762367cc1f04f1ec822239499ba
ab3MN <ab3mn3@gmail.com>
2024-10-06 22:54:14 +0300

    feat:implemented solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const treeItems = document.querySelectorAll('.tree li');
+
+treeItems.forEach((item) => {
+  const textNode = item.firstChild;
+
+  const category = document.createElement('span');
+
+  category.textContent = textNode.textContent.trim();
+
+  item.replaceChild(category, textNode);
+
+  category.onclick = () => {
+    const categoryList = item.querySelector('ul');
+
+    if (categoryList) {
+      if (categoryList.style.display === 'none') {
+        categoryList.style.display = '';
+      } else {
+        categoryList.style.display = 'none';
+      }
+    }
+  };
+});
