254b38dda65ff0b34405f219ba0d1ab0e7dadc1d
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2024-10-06 22:29:48 +0300

    Task was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const itemsOfList = [...document.querySelectorAll('li')];
+
+itemsOfList.forEach((item) => {
+  if (item.children.length) {
+    const span = document.createElement('span');
+
+    span.textContent = item.childNodes[0].textContent.trim();
+    item.prepend(span);
+    item.childNodes[1].textContent = '';
+
+    let hidden = false;
+
+    span.onclick = (e) => {
+      e.preventDefault();
+      span.nextElementSibling.style.display = hidden ? 'block' : 'none';
+      hidden = !hidden;
+    };
+  }
+});
