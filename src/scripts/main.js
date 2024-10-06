8050e1527a57966687737f8e52bfbf8a961b7aa0
Gennadiy Boreyko <gennadiyboreyko0@gmail.com>
2024-10-06 10:59:12 +0300

    creation the expandable tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const slideLists = tree.querySelectorAll('ul');
+
+for (const slideList of slideLists) {
+  const title = slideList.previousSibling.data;
+
+  slideList.previousSibling.remove();
+
+  const span = document.createElement('span');
+
+  span.textContent = title;
+  slideList.before(span);
+}
+
+tree.addEventListener('click', (e) => {
+  if (e.target.matches('span')) {
+    const ul = e.target.nextElementSibling;
+
+    ul.hidden = !ul.hidden;
+  }
+});
