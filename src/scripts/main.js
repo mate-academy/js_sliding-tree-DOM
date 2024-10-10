d5ac0d4c7e15999aba09de248a44e1e3bc431737
Ivan <ivan.kartavy@icloud.com>
2024-10-10 18:39:13 +0300

    realized sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+
+tree.querySelectorAll('li').forEach((item) => {
+  const span = document.createElement('span');
+
+  span.textContent = item.firstChild.textContent.trim();
+
+  item.firstChild.replaceWith(span);
+});
+
+tree.addEventListener('click', (e) => {
+  const clickedElement = e.target;
+  const childList = clickedElement.parentElement.querySelector('ul');
+
+  if (clickedElement.tagName !== 'SPAN') {
+    return;
+  }
+
+  if (getComputedStyle(childList).display === 'none') {
+    childList.style.display = '';
+    clickedElement.style.fontWeight = 400;
+  } else {
+    childList.style.display = 'none';
+    clickedElement.style.fontWeight = 700;
+  }
+});
