ed05055eac23b17c7820d4642db833936623a70e
SofiiaPelenska <soniapelenskaoe2002@gmail.com>
2025-03-02 14:35:18 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+document.querySelectorAll('li').forEach((li) => {
+  const firstTextNode = Array.from(li.childNodes).find(
+    (node) => node.nodeType === 3,
+  );
+
+  if (firstTextNode) {
+    const span = document.createElement('span');
+
+    span.textContent = firstTextNode.textContent.trim();
+
+    li.insertBefore(span, firstTextNode);
+
+    firstTextNode.remove();
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const target = e.target.parentElement.querySelector('ul');
+
+  if (target === null) {
+    return;
+  }
+
+  if (target.style.display === 'none') {
+    target.style.display = '';
+  } else {
+    target.style.display = 'none';
+  }
+});
