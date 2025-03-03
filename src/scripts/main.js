94c5d2272ee326db9a4076c2659ad41e3e275758
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-03-03 20:13:35 +0100

    sliding

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+  const headers = document.querySelectorAll('.tree-header');
 
-// write code here
+  headers.forEach((header) => {
+    const span = document.createElement('span');
+
+    span.textContent = header.textContent;
+    header.textContent = '';
+    header.appendChild(span);
+
+    const treeChildren = header.nextElementSibling;
+
+    treeChildren.style.display = 'none';
+
+    span.addEventListener('click', function (customEvent) {
+      if (treeChildren.style.display === 'none') {
+        treeChildren.style.display = 'block';
+      } else {
+        treeChildren.style.display = 'none';
+      }
+
+      customEvent.stopPropagation();
+    });
+  });
+
+  document.addEventListener('click', function () {
+    const allChildren = document.querySelectorAll('.tree-children');
+
+    allChildren.forEach((child) => {
+      child.style.display = 'none';
+    });
+  });
+});
