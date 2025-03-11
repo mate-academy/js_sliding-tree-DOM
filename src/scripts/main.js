3b054f918f216def3a77aae6c8f0d7cf3869297c
annaabramovaa <abramovaana193@gmail.com>
2025-03-11 18:53:45 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.tree li').forEach((li) => {
+    const span = document.createElement('span');
+
+    span.textContent = li.firstChild.textContent.trim();
+    li.firstChild.replaceWith(span);
+
+    span.addEventListener('click', () => {
+      const sublist = li.querySelector('ul');
+
+      if (sublist) {
+        sublist.hidden = !sublist.hidden;
+      }
+    });
+  });
+});
