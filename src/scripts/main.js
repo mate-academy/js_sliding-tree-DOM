d6ad95ecdaeb7c99014767855bcb4d475087592b
Owk <kkoctuk2005@gmail.com>
2025-03-11 22:06:14 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const liList = document.querySelectorAll('.tree li');
+
+function createSpan(list) {
+  list.forEach((el) => {
+    const header = el.childNodes[0];
+
+    const span = document.createElement('span');
+
+    span.textContent = header.textContent;
+    header.replaceWith(span);
+  });
+}
+
+createSpan(liList);
+
+document.addEventListener('click', (e) => {
+  const header = e.target.closest('span');
+
+  if (header) {
+    const li = header.closest('li');
+    const ul = li.querySelector('ul');
+
+    if (ul) {
+      if (ul.style.display === 'none') {
+        ul.style.display = 'block';
+      } else {
+        ul.style.display = 'none';
+      }
+    }
+  }
+});
