f5132512dcdd0e8bf06bc29bb76fdba4129e945e
Lina Do <dykaya0916@gmail.com>
2025-03-12 18:13:29 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const titles = Array.from(document.querySelectorAll('.title'));
+const subTitles = Array.from(document.querySelectorAll('.subTitle'));
+
+titles.forEach((title) => {
+  const subling = title.nextElementSibling;
+
+  title.addEventListener('click', 
+    subling.hasAttribute('hidden')
+      ? subling.removeAttribute('hidden')
+      : subling.setAttribute('hidden', 'true')
+  );
+});
+
+subTitles.forEach((subTitle) => {
+  const subling = subTitle.nextElementSibling;
+
+  subTitle.addEventListener('click', 
+    subling.hasAttribute('hidden')
+      ? subling.removeAttribute('hidden')
+      : subling.setAttribute('hidden', 'true')
+  );
+});
