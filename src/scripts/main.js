c5e2fe33fde599f69fb33cf34b20ee14e64d3558
inikiforov <ievgen.nikiforov@tedata.de>
2025-03-13 11:28:18 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+const headers = document.querySelectorAll('li');
+
+for (const header of headers) {
+  header.addEventListener('click', function () {
+    event.stopPropagation();
+
+    for (const child of header.children) {
+      child.style.display = child.style.display === 'none' ? '' : 'none';
+    }
+  });
+}
