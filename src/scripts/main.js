e6876cdb7344c1eac4e9650a71c992de5d52c996
Ostap Danyliv <danyliv.ostap@gmail.com>
2025-03-18 09:58:27 -0500

    Sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('li');
+
+headers.forEach((header) => {
+  const subList = header.querySelector('ul');
+
+  if (subList) {
+    const span = document.createElement('span');
+
+    span.textContent = header.firstChild.textContent.trim();
+    header.firstChild.textContent = '';
+
+    header.insertBefore(span, header.firstChild);
+
+    span.addEventListener('click', () => {
+      if (subList.style.display === 'none') {
+        subList.style.display = 'block';
+      } else {
+        subList.style.display = 'none';
+      }
+    });
+  }
+});
