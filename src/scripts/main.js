a1f4fd35a9ceb0d1fc6ca54db1723f2e9a9b490e
Illia <hetsko.ilya@gmail.com>
2025-03-18 19:25:42 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write code here
+const treeItems = document.querySelectorAll('.tree li');
+
+treeItems.forEach((li) => {
+  const firstChild = li.firstChild;
+
+  if (firstChild && firstChild.nodeType === 3) {
+    const span = document.createElement('span');
+
+    span.textContent = firstChild.textContent;
+    li.replaceChild(span, firstChild);
+
+    span.addEventListener('click', () => {
+      const ul = li.querySelector('ul');
+
+      if (ul) {
+        ul.style.display =
+          ul.style.display === 'none' || ul.style.display === ''
+            ? 'block'
+            : 'none';
+      }
+    });
+  }
+});
