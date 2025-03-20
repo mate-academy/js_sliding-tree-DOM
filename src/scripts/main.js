b19a4ff7be89487ddcd1136b1eb6d49c8d0e6242
Lera <valeriamelesenko4@gmail.com>
2025-03-20 13:03:17 +0100

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+const list = document.querySelectorAll('li');
+
+list.forEach((header) => {
+  const subList = header.querySelector('ul');
+
+  if (subList) {
+    const span = document.createElement('span');
+
+    span.textContent = header.firstChild.textContent.trim();
+    header.firstChild.textContent = '';
+
+    header.insertBefore(span, header.firstChild);
+
+    span.addEventListener('click', () => {
+      if (subList.style.display === 'none') {
+        subList.style.display = 'block';
+      } else {
+        subList.style.display = 'none';
+      }
+    });
+  }
+});
