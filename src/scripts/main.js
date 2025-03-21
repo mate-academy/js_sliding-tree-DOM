0de9fbaee3394682912397d9e9003c10bb11aa27
Ivan Huzhva <iwan_guzhwa@gmail.com>
2025-03-21 09:50:23 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
-
 // write code here
+
+const liElements = document.querySelectorAll('li');
+
+liElements.forEach((elLi) => {
+  if (elLi.querySelector('ul')) {
+    const newSpan = document.createElement('span');
+    const headerText = elLi.firstChild.textContent.trim();
+
+    newSpan.textContent = headerText;
+    elLi.firstChild.replaceWith(newSpan);
+  }
+});
+
+document.addEventListener('click', (ev) => {
+  const header = ev.target.closest('span');
+
+  if (!header) {
+    return;
+  }
+
+  const list = header.nextElementSibling;
+
+  if (list.style.display === 'none') {
+    list.style.display = '';
+  } else {
+    list.style.display = 'none';
+  }
+});
