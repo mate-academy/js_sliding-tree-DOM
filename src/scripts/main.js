e30c3bd067c8030746048a8e72cd6360a0d2b956
Ivan Sierov <iserov1989@gmail.com>
2025-03-23 12:02:06 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.tree li').forEach((li) => {
+    const text = li.firstChild.textContent.trim();
+
+    if (text) {
+      const span = document.createElement('span');
+
+      span.textContent = text;
+      li.firstChild.replaceWith(span);
+    }
+  });
+
+  document.querySelector('.tree').addEventListener('click', (e) => {
+    if (e.target.tagName === 'SPAN') {
+      const parentLi = e.target.parentElement;
+      const nestedUl = parentLi.querySelector('ul');
+
+      if (nestedUl) {
+        nestedUl.hidden = !nestedUl.hidden;
+      }
+    }
+  });
+});
