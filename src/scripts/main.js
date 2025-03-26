c1329aa8a507dded5dcd8d39c7653447328e4235
i-melnyk <i.melnyk1985@gmail.com>
2025-03-26 21:28:43 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  tree.querySelectorAll('li:has(> ul)').forEach((li) => {
+    const textNode = [...li.childNodes].find(
+      (node) => node.nodeType === Node.TEXT_NODE && node.nodeValue.trim(),
+    );
+
+    if (textNode) {
+      const span = document.createElement('span');
+
+      span.textContent = textNode.nodeValue.trim();
+      textNode.replaceWith(span);
+    }
+  });
+
+  tree.addEventListener('click', (e) => {
+    if (e.target.tagName === 'SPAN') {
+      const childUl = e.target.nextElementSibling;
+
+      if (childUl && childUl.tagName === 'UL') {
+        childUl.hidden = !childUl.hidden;
+      }
+    }
+  });
+});
