742305fd74ab0e9a24d3850538820aec8c4f9f05
Anton Donchenko <donch.a080@gmail.com>
2025-03-27 13:41:49 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const listOfLi = document.querySelectorAll('.tree li');
+
+for (const li of listOfLi) {
+  if (li.firstElementChild && li.firstElementChild.tagName === 'UL') {
+    const nestedUl = li.firstElementChild;
+
+    let directText = '';
+
+    for (const node of li.childNodes) {
+      if (node.nodeType === Node.TEXT_NODE) {
+        directText += node.textContent;
+      }
+    }
+
+    const savedUl = li.removeChild(nestedUl);
+
+    li.textContent = '';
+
+    const span = document.createElement('span');
+
+    span.textContent = directText.trim();
+    li.appendChild(span);
+
+    li.appendChild(savedUl);
+
+    span.addEventListener('click', () => {
+      if (savedUl.style.display === 'none') {
+        savedUl.style.display = '';
+      } else {
+        savedUl.style.display = 'none';
+      }
+    });
+  }
+}
