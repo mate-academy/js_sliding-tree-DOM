ce250f51d4dc185bfd7a75f7058298482e9a59a1
George <george.boujhenitsa@gmail.com>
2025-04-05 18:46:40 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+Array.from(document.querySelectorAll('li'))
+  .filter((headling) => {
+    return headling.querySelector('ul') !== null;
+  })
+  .forEach((headling) => {
+    const newSpan = document.createElement('span');
+
+    newSpan.textContent = headling.childNodes[0].textContent.trim();
+    headling.replaceChild(newSpan, headling.childNodes[0]);
+  });
+
+tree.addEventListener('click', (e) => {
+  if (e.target.closest('span')) {
+    const ulToHide = e.target.closest('li').childNodes[1];
+
+    if (ulToHide.style.display === 'none') {
+      ulToHide.style.display = 'block';
+    } else {
+      ulToHide.style.display = 'none';
+    }
+  }
+});
