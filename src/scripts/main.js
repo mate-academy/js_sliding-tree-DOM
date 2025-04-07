6939061c121bcaf2e5b3aa564aa95795656c4420
sirserg <sirserg05@gmail.com>
2025-04-07 18:37:45 +0300

    feat: add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.querySelectorAll('ul').forEach((list) => {
+  const listHeader = list.previousSibling;
+  const spanListHeader = document.createElement('span');
+
+  spanListHeader.textContent = listHeader.textContent;
+  listHeader.replaceWith(spanListHeader);
+
+  spanListHeader.addEventListener('click', () => {
+    Array.from(list.children).forEach((item) => {
+      if (item.style.display === 'none') {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  });
+});
