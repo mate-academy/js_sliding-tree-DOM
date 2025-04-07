27078f3c65ed35d138fecae682172c8ec0832e75
dimon <80713na@gmail.com>
2025-04-07 16:00:59 +0300

    tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const treeItems = document.querySelectorAll('.tree li');
+
+treeItems.forEach((li) => {
+  const firstChild = li.firstChild;
+
+  if (firstChild && firstChild.nodeType === Node.TEXT_NODE) {
+    const span = document.createElement('span');
+
+    span.textContent = firstChild.textContent;
+    li.insertBefore(span, firstChild);
+    li.removeChild(firstChild);
+  }
+});
+
+document.querySelector('.tree').addEventListener('click', (even) => {
+  if (even.target.tagName === 'SPAN') {
+    const li = even.target.closest('li');
+    const childUl = li.querySelector('ul');
+
+    if (childUl) {
+      childUl.style.display = childUl.style.display === 'none' ? '' : 'none';
+    }
+  }
+});
