625452e004ba4ae227d5eba47dd3d280f8fda8bd
Pavlo Stelmashchuk <stelmaschukpavlo3@gmail.com>
2025-04-10 19:53:32 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const treeList = document.querySelector('.tree');
+
+treeList.querySelectorAll('li').forEach((li) => {
+  const childUl = li.querySelector('ul');
+
+  if (childUl) {
+    const span = document.createElement('span');
+    const text = li.firstChild.textContent.trim();
+
+    span.textContent = text;
+
+    li.insertBefore(span, childUl);
+
+    li.firstChild.remove();
+  }
+});
+
+treeList.addEventListener('click', (e) => {
+  if (!e.target.matches('li > span')) {
+    return;
+  }
+
+  const clickedElement = e.target.closest('li');
+
+  if (clickedElement) {
+    const nestedList = clickedElement.querySelector('ul');
+
+    if (nestedList) {
+      if (nestedList.style.display === 'none') {
+        nestedList.style.display = 'block';
+      } else {
+        nestedList.style.display = 'none';
+      }
+    }
+  }
+});
