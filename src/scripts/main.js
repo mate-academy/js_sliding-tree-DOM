04d33f5433dcb0936f43c62923527545f83b5c1f
Hanna <hannachachacha7@gmail.com>
2025-04-11 15:59:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = Array.from(tree.querySelectorAll('li'));
+let span;
+
+for (const item of listItems) {
+  const childNodes = Array.from(item.childNodes);
+
+  for (const node of childNodes) {
+    if (node.nodeType === 3) {
+      span = document.createElement('span');
+      span.textContent = node.textContent;
+      item.replaceChild(span, node);
+    }
+  }
+}
+
+const style = document.createElement('style');
+
+style.innerHTML = `
+  .hidden {
+    display: none;
+  }
+`;
+
+document.head.appendChild(style);
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const tarLi = e.target.parentElement;
+    const tarUl = tarLi.querySelector('ul');
+
+    if (tarUl) {
+      tarUl.classList.toggle('hidden');
+    }
+  }
+});
