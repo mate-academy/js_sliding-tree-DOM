ca91c1957dbad360c873d05034e5355dd5a9a23c
AnoriFrell <anori.fjord@gmail.com>
2025-04-14 20:24:37 +0300

    sliding-tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const list = document.querySelectorAll('li');
+
+list.forEach((item) => {
+  const itemWrapper = document.createElement('span');
+  const unit = item.firstChild;
+
+  item.prepend(itemWrapper);
+  itemWrapper.append(unit);
+});
+
+tree.addEventListener('click', (ev) => {
+  const target = ev.target;
+
+  if (target.tagName === 'SPAN') {
+    const sublist = target.closest('li').querySelector('ul');
+
+    if (sublist) {
+      sublist.hidden = !sublist.hidden;
+    }
+  }
+});
