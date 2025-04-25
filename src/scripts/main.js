3cab3a01e78707f136d583f457bf64d0ccd7b3d0
Yuliia Babych <jl.babych29@gmail.com>
2025-04-25 15:14:02 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+  const listItems = tree.querySelectorAll('li');
+
+  listItems.forEach((li) => {
+    const childUl = li.querySelector('ul');
+
+    if (childUl) {
+      const textNode = Array.from(li.childNodes).find(
+        (node) =>
+          node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '',
+      );
+
+      if (textNode) {
+        const span = document.createElement('span');
+
+        span.textContent = textNode.textContent.trim();
+        textNode.replaceWith(span);
+
+        span.style.cursor = 'pointer';
+
+        span.addEventListener('click', () => {
+          childUl.hidden = !childUl.hidden;
+        });
+      }
+    }
+  });
+});
