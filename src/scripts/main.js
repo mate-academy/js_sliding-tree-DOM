c1cce6b43117e38991524abb20187214340f0145
Lidiia Shvets <lepoki051003@gmail.com>
2025-05-19 18:11:28 +0200

    tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  // Обгортаємо текст у <span>
+  const items = tree.querySelectorAll('li');
+
+  items.forEach((li) => {
+    const firstChild = li.firstChild;
+
+    if (firstChild && firstChild.nodeType === Node.TEXT_NODE) {
+      const span = document.createElement('span');
+
+      span.textContent = firstChild.textContent.trim();
+      li.insertBefore(span, firstChild);
+      li.removeChild(firstChild);
+    }
+  });
+
+  // Додаємо клік
+  tree.addEventListener('click', (ev) => {
+    if (ev.target.tagName !== 'SPAN') {
+      return;
+    }
+
+    const li = ev.target.closest('li');
+    const childUl = li.querySelector('ul');
+
+    if (childUl) {
+      childUl.hidden = !childUl.hidden;
+    }
+  });
+});
