d3be3cc3e809680d1758574198140e192835b8e3
Oleksandr Voronkin <94705289+Alexraiv@users.noreply.github.com>
2025-05-22 20:04:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write code here
+const liWithUl = document.querySelectorAll('li:has(> ul)');
+
+liWithUl.forEach((li) => {
+  const textNodes = Array.from(li.childNodes).filter(
+    (node) =>
+      node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '',
+  );
+
+  textNodes.forEach((textNode) => {
+    const span = document.createElement('span');
+
+    span.textContent = textNode.textContent.trim();
+    li.replaceChild(span, textNode);
+
+    span.onclick = () => {
+      const ul = li.querySelector('ul');
+
+      ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+    };
+  });
+});
