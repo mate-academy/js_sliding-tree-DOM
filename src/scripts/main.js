1e36451cdb1b3565033764fa0c42d26517ba8281
Oleksandr Voronkin <94705289+Alexraiv@users.noreply.github.com>
2025-05-22 20:19:41 +0300

    fix1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,21 +4,19 @@
 const liWithUl = document.querySelectorAll('li:has(> ul)');
 
 liWithUl.forEach((li) => {
-  const textNodes = Array.from(li.childNodes).filter(
+  const textNode = Array.from(li.childNodes).find(
     (node) =>
       node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '',
   );
 
-  textNodes.forEach((textNode) => {
-    const span = document.createElement('span');
+  const span = document.createElement('span');
 
-    span.textContent = textNode.textContent.trim();
-    li.replaceChild(span, textNode);
+  span.textContent = textNode.textContent.trim();
+  li.replaceChild(span, textNode);
 
-    span.onclick = () => {
-      const ul = li.querySelector('ul');
+  span.onclick = () => {
+    const ul = li.querySelector('ul');
 
-      ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
-    };
-  });
+    ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+  };
 });
