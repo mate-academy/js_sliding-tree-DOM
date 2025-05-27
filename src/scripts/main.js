635d027408d6dd8fe1a65fb60225fb046faeca96
z3rt <z3rt@MacBook-Pro-Aleksandr.local>
2025-05-27 12:06:20 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const items = tree.querySelectorAll('li');
+
+items.forEach((li) => {
+  const firstLi = li.firstChild;
+
+  if (li.children.length && firstLi.nodeType === Node.TEXT_NODE) {
+    const span = document.createElement('span');
+
+    span.textContent = firstLi.textContent.trim();
+
+    li.insertBefore(span, firstLi);
+    li.removeChild(firstLi);
+
+    span.addEventListener('click', (e) => {
+      if (e.target.tagName !== 'SPAN') {
+        return;
+      }
+
+      const clickLi = e.target.closest('li');
+      const childUl = clickLi.querySelector('ul');
+
+      if (childUl) {
+        childUl.hidden = !childUl.hidden;
+      }
+    });
+  }
+});
