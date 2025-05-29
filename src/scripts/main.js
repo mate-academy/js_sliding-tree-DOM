62bc8a0453ca46bd6a09293b58194bc8fc178384
combine143 <combine142@gmail.com>
2025-05-29 20:51:32 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,20 +3,23 @@
 document.querySelectorAll('.tree li').forEach((li) => {
   const span = document.createElement('span');
 
-  span.textContent = li.firstChild.textContent.trim();
-
-  li.insertBefore(span, li.firstChild);
-  li.firstChild.nextSibling.remove();
+  if (li.firstChild && li.firstChild.nodeType === 3) {
+    span.textContent = li.firstChild.textContent.trim();
+    li.insertBefore(span, li.firstChild);
+    li.firstChild.nextSibling.remove();
+  }
 });
 
 document.querySelectorAll('.tree span').forEach((span) => {
   const nextUl = span.nextElementSibling;
 
-  span.addEventListener('click', () => {
-    if (nextUl.style.display === 'none') {
-      nextUl.style.display = 'block';
-    } else {
-      nextUl.style.display = 'none';
-    }
-  });
+  if (nextUl && nextUl.tagName === 'UL') {
+    span.addEventListener('click', () => {
+      if (nextUl.style.display === 'none') {
+        nextUl.style.display = 'block';
+      } else {
+        nextUl.style.display = 'none';
+      }
+    });
+  }
 });
