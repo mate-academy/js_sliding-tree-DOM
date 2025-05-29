423ff9a8a6d6b8d3d7072ef74fe3275393744ada
combine143 <combine142@gmail.com>
2025-05-29 20:39:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('.tree li').forEach((li) => {
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent.trim();
+
+  li.insertBefore(span, li.firstChild);
+  li.firstChild.nextSibling.remove();
+});
+
+document.querySelectorAll('.tree span').forEach((span) => {
+  const nextUl = span.nextElementSibling;
+
+  span.addEventListener('click', () => {
+    if (nextUl.style.display === 'none') {
+      nextUl.style.display = 'block';
+    } else {
+      nextUl.style.display = 'none';
+    }
+  });
+});
