7f143de6a59d1a8360748fae8afc9e639d13098e
Viktor Kyshenchuk <viktor.kyshenchuk@gmail.com>
2025-06-05 17:35:59 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listTree = tree.querySelectorAll('li');
+
+listTree.forEach((li) => {
+  const firstTextNode = li.firstChild;
+  const nestedList = li.querySelector('ul');
+
+  if (nestedList) {
+    nestedList.style.display = 'none';
+  }
+
+  if (firstTextNode && firstTextNode.nodeType === Node.TEXT_NODE) {
+    const span = document.createElement('span');
+
+    span.textContent = firstTextNode.textContent;
+    firstTextNode.remove();
+    li.insertBefore(span, li.firstChild);
+
+    span.addEventListener('click', (e) => {
+      e.stopImmediatePropagation();
+
+      if (nestedList) {
+        if (nestedList.style.display === 'none') {
+          nestedList.style.display = 'block';
+        } else {
+          nestedList.style.display = 'none';
+          hideAllNestedLists(nestedList);
+        }
+      }
+    });
+  }
+});
+
+function hideAllNestedLists(ul) {
+  const nestedUls = ul.querySelectorAll('ul');
+
+  nestedUls.forEach((subUl) => {
+    subUl.style.display = 'none';
+  });
+}
