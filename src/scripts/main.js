62376024e16ed790824474e464e76015ed490f50
Oleg Nepiuk <gtailde@gmail.com>
2025-06-12 22:49:35 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liElements = tree.querySelectorAll('li');
+
+liElements.forEach((li) => {
+  if (li.querySelector('ul')) {
+    const textNode = li.childNodes[0];
+    const span = document.createElement('span');
+
+    span.innerText = textNode.textContent.trim();
+    li.replaceChild(span, textNode);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  const ul = e.target.parentElement.querySelector('ul');
+
+  if (ul) {
+    ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+  }
+});
