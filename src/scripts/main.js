5890d1ba0d5b5909a11b9918b8bf8122532d2072
Valentyna Moskvinova <tina.moskwinova@gmail.com>
2025-06-17 14:49:43 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.tree li').forEach((li) => {
+    if (li.querySelector('ul')) {
+      const span = document.createElement('span');
+
+      span.textContent = li.firstChild.textContent.trim();
+      li.firstChild.replaceWith(span);
+
+      span.addEventListener('click', () => {
+        const childUl = li.querySelector('ul');
+
+        childUl.hidden = !childUl.hidden;
+      });
+    }
+  });
+});
