256538eb358da96e31e83e74f4f92583c889553c
Oleksii Myronenko <oleksiimrnko@gmail.com>
2025-06-19 23:55:18 +0300

    add test solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  for (const li of tree.querySelectorAll('li')) {
+    const firstChild = li.firstChild;
+
+    if (firstChild.nodeType === Node.TEXT_NODE) {
+      const span = document.createElement('span');
+
+      span.textContent = firstChild.textContent.trim();
+      li.insertBefore(span, firstChild);
+      li.removeChild(firstChild);
+    }
+  }
+
+  tree.addEventListener('click', (e) => {
+    if (e.target.tagName !== 'SPAN') {
+      return;
+    }
+
+    const li = e.target.closest('li');
+    const nestedUl = li.querySelector('ul');
+
+    if (nestedUl) {
+      nestedUl.hidden = !nestedUl.hidden;
+    }
+  });
+});
