d356da8b0cef7ff4b0dfd72f83be5693783bac4f
YuliaSabitova <yuliasab2017@mail.com>
2025-06-26 08:50:17 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const tree = document.querySelectorAll('.tree > li > ul > li');
+
+tree.forEach((li) => {
+  const ul = li.querySelector('ul');
+
+  const firstNode = Array.from(li.childNodes).find(
+    (node) =>
+      node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '',
+  );
+
+  if (ul && firstNode) {
+    const span = document.createElement('span');
+
+    span.textContent = firstNode.textContent;
+    li.insertBefore(span, firstNode);
+    li.removeChild(firstNode);
+  }
+
+  // if (ul) {
+  // ul.style.display = 'none';
+  // }
+});
+
+document.querySelector('.tree').addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const next = e.target.nextElementSibling;
+
+    if (next && next.tagName === 'UL') {
+      next.style.display = next.style.display === 'none' ? 'block' : 'none';
+    }
+    e.stopPropagation();
+  }
+});
