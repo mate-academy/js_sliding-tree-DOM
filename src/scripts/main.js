a0b811079bbbab9e306724c38a3934a064c7de30
Daniel-spec-dell <fvgdanil@gmail.com>
2025-06-26 17:22:19 +0300

    add task soliton

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const li = e.target.closest('li');
+  const childrenUl = li.querySelector('ul');
+
+  if (li && e.target.tagName !== 'UL') {
+    if (childrenUl) {
+      childrenUl.style.display =
+        childrenUl.style.display === 'none' ? 'block' : 'none';
+    }
+  }
+});
