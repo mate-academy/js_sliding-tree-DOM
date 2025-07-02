363b5ddfee23a730423474ae56143910e0c1f580
Anatoliy Korotenko <tolik.korotenko@21gmail.com>
2025-07-02 17:33:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
 // write code here
+
+const treeItems = document.querySelectorAll('.tree li');
+
+treeItems.forEach((li) => {
+  const text = li.firstChild;
+
+  if (text.nodeType === 3) {
+    const span = document.createElement('span');
+
+    span.textContent = text.textContent.trim();
+
+    li.insertAdjacentElement('afterbegin', span);
+    text.parentNode.removeChild(text);
+  }
+});
+
+document.querySelector('.tree').addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const li = e.target.parentElement;
+  const childUl = li.querySelector('ul');
+
+  if (childUl) {
+    childUl.hidden = !childUl.hidden;
+  }
+});
