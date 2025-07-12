6330bf12e13080b5c68532ece087d4b49d1192b8
Ruslan <gnatisin.ruslan1988@gmail.com>
2025-07-12 13:08:13 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write code here
+const liItems = document.querySelectorAll('li');
+
+liItems.forEach((item) => {
+  const nestedUl = item.querySelector('ul');
+
+  if (nestedUl) {
+    const titleText = item.firstChild?.textContent?.trim();
+
+    if (titleText) {
+      const spanTag = document.createElement('span');
+
+      spanTag.textContent = titleText;
+
+      item.removeChild(item.firstChild);
+      item.prepend(spanTag);
+
+      spanTag.addEventListener('click', (e) => {
+        nestedUl.style.display =
+          nestedUl.style.display === 'none' ? 'block' : 'none';
+      });
+    }
+  }
+});
