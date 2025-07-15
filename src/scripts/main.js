cb5c7812295cb1de29561b3456dd1d021a8a2064
Drsgon <oksanaleschenko.ol@gmail.com>
2025-07-15 11:34:34 +0300

    add sliding tree dom solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+document.addEventListener('DOMContentLoaded', function () {
+  const tree = document.querySelector('.tree');
+  const allLi = tree.querySelectorAll('li');
+
+  for (const li of allLi) {
+    const childUl = li.querySelector('ul');
+
+    if (!childUl) {
+      continue;
+    }
+
+    const text = li.firstChild;
+    const span = document.createElement('span');
+
+    span.textContent = text.textContent.trim();
+    li.insertBefore(span, text);
+    li.removeChild(text);
+
+    span.style.cursor = 'pointer';
+
+    span.addEventListener('click', () => {
+      childUl.hidden = !childUl.hidden;
+    });
+  }
+});
