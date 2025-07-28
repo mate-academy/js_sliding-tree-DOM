d8646255d4f04c64b75ce8784bb5309b4024bd3f
Denys Herych <fojjof2@gmail.com>
2025-07-28 08:27:53 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+
+for (const li of listItems) {
+  const span = document.createElement('span');
+
+  const text = Array.from(li.childNodes).find(
+    (node) =>
+      node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '',
+  );
+
+  span.textContent = text.textContent.trim();
+  li.insertBefore(span, text);
+  li.removeChild(text);
+}
+
+const spanItems = document.querySelectorAll('span');
+
+spanItems.forEach((span) => {
+  span.addEventListener('click', () => {
+    const li = span.parentNode;
+    const nestedUl = li.querySelector('ul');
+
+    if (nestedUl) {
+      nestedUl.hidden = !nestedUl.hidden;
+    }
+  });
+});
