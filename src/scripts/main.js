b053996f0e687d563602f0b5e0fffe2e07324b05
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-08-02 15:13:58 +0300

    list open close

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
 // write code here
+const list = document.querySelectorAll('.tree li');
+
+list.forEach((li) => {
+  const childN = li.childNodes;
+
+  for (const node of childN) {
+    if (node.nodeType === 3 && node.nodeValue.trim() !== '') {
+      const span = document.createElement('span');
+
+      span.textContent = node.nodeValue.trim();
+      li.replaceChild(span, node);
+      break;
+    }
+  }
+});
+
+document.querySelector('.tree').addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const parentLi = e.target.closest('li');
+  const subList = parentLi.querySelector('ul');
+
+  if (subList) {
+    subList.hidden = !subList.hidden;
+  }
+});
