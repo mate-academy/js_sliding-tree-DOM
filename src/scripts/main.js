5d5880c74cbb0fbaa4ef7fbbedc8bea2e87c0b09
Ivan Denys <denisivan06007@gmail.com>
2025-08-02 17:03:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const li = e.target;
+  const childUL = li.querySelector('ul');
+
+  childUL.style.display = childUL.style.display === 'none' ? 'block' : 'none';
+});
