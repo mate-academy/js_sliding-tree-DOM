9002f9f2c712581a3c3913e4564fcff4dcf25d5f
Maxim <makson202007@gmail.com>
2025-08-07 14:44:13 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const ulCollections = document.querySelectorAll('ul:not(.tree)');
+
+ulCollections.forEach((oneUl) => {
+  const header = oneUl.parentElement.firstChild;
+
+  const $collapsableSpan = document.createElement('span');
+
+  $collapsableSpan.addEventListener('click', (e) => {
+    oneUl.hidden = !oneUl.hidden;
+  });
+
+  $collapsableSpan.textContent = header.textContent.trim();
+  oneUl.parentElement.replaceChild($collapsableSpan, header);
+});
