21079e40dca69458e44e949a25733fc25d250133
Dmytro Nykorovych <dev.dmytronykorovych@gmail.com>
2025-08-08 13:51:27 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+li.forEach((item) => {
+  const span = document.createElement('span');
+
+  item.childNodes.forEach((node) => {
+    if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '') {
+      span.textContent = node.textContent.trim();
+      item.replaceChild(span, node);
+    }
+  });
+
+  span.addEventListener('click', () => {
+    if (span.nextElementSibling.style.display === 'none') {
+      span.nextElementSibling.style.display = 'block';
+    } else {
+      span.nextElementSibling.style.display = 'none';
+    }
+  });
+});
