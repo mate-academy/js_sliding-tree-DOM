5107498d9ce5512363ac6344882e559222aee57f
Nikita Chupahin <nik123chupahin@gmail.com>
2025-08-21 10:50:07 +0300

    Added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const items = tree.querySelectorAll('li');
+
+items.forEach((item) => {
+  const list = item.querySelector('ul');
+  const span = document.createElement('span');
+
+  span.textContent = item.firstChild.textContent;
+
+  item.firstChild.replaceWith(span);
+
+  span.addEventListener('click', () => {
+    list.toggleAttribute('hidden');
+  });
+});
