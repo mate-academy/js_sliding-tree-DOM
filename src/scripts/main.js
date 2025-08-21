534808252b1322e916b7cf72b4bb5b19b00f04df
Mykola <nyr3mvneov@gmail.com>
2025-08-21 15:12:29 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const listItems = document.querySelectorAll('.tree li');
+
+  for (const item of listItems) {
+    const submenu = item.querySelector('ul');
+
+    if (submenu) {
+      const titleNode = item.childNodes[0];
+
+      const span = document.createElement('span');
+
+      span.textContent = titleNode.textContent;
+
+      span.style.cursor = 'pointer';
+      span.style.userSelect = 'none';
+
+      item.replaceChild(span, titleNode);
+
+      span.addEventListener('click', (ev) => {
+        ev.stopPropagation();
+
+        const isHidden = submenu.style.display === 'none';
+
+        submenu.style.display = isHidden ? '' : 'none';
+      });
+    }
+  }
+});
