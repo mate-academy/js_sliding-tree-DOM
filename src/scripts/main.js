77734bdb2b129754ff126b5a588e103b40066c18
BadOfficer <tarasbondarenko2005@gmail.com>
2025-08-28 00:41:46 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  const liChilds = tree.querySelectorAll('li');
+
+  liChilds.forEach((li) => {
+    const ulChild = li.querySelector('ul');
+
+    if (ulChild) {
+      const headNode = li.childNodes[0];
+
+      if (headNode.data.trim() !== '') {
+        const span = document.createElement('span');
+
+        span.textContent = headNode.data.trim();
+
+        li.prepend(span);
+        headNode.remove();
+      }
+    }
+  });
+
+  tree.addEventListener('click', (e) => {
+    const targetEl = e.target;
+
+    if (targetEl.tagName === 'SPAN') {
+      const siblingUl = targetEl.parentNode.querySelector('ul');
+
+      if (siblingUl) {
+        siblingUl.toggleAttribute('hidden');
+      }
+    }
+  });
+});
