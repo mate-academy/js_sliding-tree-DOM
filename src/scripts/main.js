f78960ca325ea90c6f388689cbb08486ece9c04c
Denys Piddubnyi <iforgotmypincode@gmail.com>
2025-08-28 18:07:09 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+li.forEach((n) => {
+  const first = n.firstChild;
+  const span = document.createElement('span');
+
+  span.textContent = first.textContent;
+
+  n.replaceChild(span, first);
+});
+
+const forSpan = document.querySelectorAll('span');
+
+forSpan.forEach((n) => {
+  n.addEventListener('click', (el) => {
+    const ul = el.currentTarget.nextElementSibling;
+
+    if (ul && ul.tagName === 'UL') {
+      const show = ul.style.display;
+
+      if (show === 'none') {
+        ul.style.display = 'block';
+      } else {
+        ul.style.display = 'none';
+      }
+    }
+  });
+});
