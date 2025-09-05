8c361cb2c55be62af8df68b5b4f95235f8d9d449
Gennadiy99 <login841@gmail.com>
2025-09-05 23:36:18 +0200

    finished project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const treeUl = document.querySelector('.tree');
+
+treeUl.addEventListener('click', (e) => {
+  const li = e.target.closest('li');
+
+  if (!li) {
+    return;
+  }
+
+  const nested = li.querySelector('ul');
+
+  if (nested) {
+    nested.hidden = !nested.hidden;
+  }
+});
