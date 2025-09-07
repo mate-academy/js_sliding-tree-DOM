3b81fe610e7265eaf5c7780f70e7e6491131fe66
Gennadiy99 <login841@gmail.com>
2025-09-07 14:28:04 +0200

    finished project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,19 @@
 
 const treeUl = document.querySelector('.tree');
 
+treeUl.querySelectorAll('li').forEach((l) => {
+  const span = document.createElement('span');
+
+  const textNode = Array.from(l.childNodes).find((node) => {
+    return node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '';
+  });
+
+  if (textNode) {
+    span.textContent = textNode.textContent;
+    textNode.replaceWith(span);
+  }
+});
+
 treeUl.addEventListener('click', (e) => {
   const li = e.target.closest('li');
 
