21a0561b9120404f4c9c98aa06722e3aaaf53a88
Gennadiy99 <login841@gmail.com>
2025-09-07 18:32:11 +0200

    add tagName !== 'SPAN'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,19 +3,23 @@
 const treeUl = document.querySelector('.tree');
 
 treeUl.querySelectorAll('li').forEach((l) => {
-  const span = document.createElement('span');
-
   const textNode = Array.from(l.childNodes).find((node) => {
     return node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '';
   });
 
   if (textNode) {
+    const span = document.createElement('span');
+
     span.textContent = textNode.textContent;
     textNode.replaceWith(span);
   }
 });
 
 treeUl.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
   const li = e.target.closest('li');
 
   if (!li) {
