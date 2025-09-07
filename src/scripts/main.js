e03edfea8da28d658574f667ea7e128e3441b7d6
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-09-08 01:06:03 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('ul.tree');
+
+tree.querySelectorAll('li').forEach((li) => {
+  li.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    const nested = li.querySelector(':scope > ul');
+
+    if (nested && e.target !== 'null') {
+      nested.style.display = nested.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
