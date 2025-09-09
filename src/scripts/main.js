99f079833cc3890e4a20f74df005b06b75af7f49
Ivan Vaniuk <ivanvanuk77@gmail.com>
2025-09-09 21:28:23 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+  tree.querySelectorAll('li').forEach(item => {
+    let span = document.createElement('span');
+    item.prepend(span);
+    span.append(span.nextSibling);
+  });
+
+  tree.addEventListener('click', (e) => {
+    if (e.target.tagName != 'SPAN') {
+      return;
+    }
+
+    let childrenContainer = e.target.parentNode.querySelector('ul');
+
+    if (!childrenContainer) return; 
+
+    childrenContainer.hidden = !childrenContainer.hidden;
+  });
+});
