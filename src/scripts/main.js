d8c9af08582bec8cd88dad77593f4e03f79b714d
DropPredator <ionchiccc@gmail.com>
2025-09-12 15:11:09 +0300

    ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('body .tree');
+
+tree.addEventListener('click', (e) => {
+  for (const child of e.target.children) {
+    if (child.style.display === 'none') {
+      child.style.display = 'block';
+    } else {
+      child.style.display = 'none';
+    }
+  }
+});
