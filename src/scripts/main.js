f079a77f69770096b7d7ecac541ff0b3fc81d5fd
Mykola Kuzmenko <kolua.kuzmenko@gmail.com>
2025-09-12 15:41:55 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const allLi = tree.querySelectorAll('li');
+const parents = Array.from(allLi).filter((li) => li.querySelector('ul'));
+
+if (parents.length) {
+  for (const li of parents) {
+    const textNode = li.firstChild;
+
+    if (textNode && textNode.nodeType === Node.TEXT_NODE) {
+      const span = document.createElement('span');
+
+      span.textContent = textNode.textContent.trim();
+      li.insertBefore(span, textNode);
+      li.removeChild(textNode);
+    }
+  }
+}
+
+tree.addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (!(target instanceof HTMLElement) || target.tagName !== 'SPAN') {
+    return undefined;
+  }
+
+  const li = target.parentElement;
+
+  if (!li) {
+    return undefined;
+  }
+
+  const childUl = li.querySelector(':scope > ul');
+
+  childUl.hidden = !childUl.hidden;
+});
