5730ed3ff857f42002d0b2e556e84809f3097338
DropPredator <ionchiccc@gmail.com>
2025-09-12 15:35:36 +0300

    add changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,24 @@
 'use strict';
 
-const tree = document.querySelector('body .tree');
+const headers = document.querySelectorAll('li');
 
-tree.addEventListener('click', (e) => {
+for (const header of headers) {
+  if (header.childNodes.length > 1) {
+    for (const node of header.childNodes) {
+      if (node.nodeType === 3 && node.textContent.trim().length !== 0) {
+        const span = document.createElement('span');
+
+        span.textContent = node.textContent;
+
+        node.replaceWith(span);
+      }
+    }
+  }
+}
+
+const spans = document.querySelectorAll('span');
+
+spans.addEventListener('click', (e) => {
   for (const child of e.target.children) {
     if (child.style.display === 'none') {
       child.style.display = 'block';
