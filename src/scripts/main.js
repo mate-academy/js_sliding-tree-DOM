2dc51adf421fd650c2f3817be597a2b37d0269f3
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-09-24 19:31:22 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const lister = [...list];
+
+lister.forEach((element) => {
+  const l = element.childNodes;
+
+  l.forEach((node) => {
+    if (node.nodeType === 3) {
+      const spaner = document.createElement('span');
+
+      spaner.textContent = node.nodeValue;
+      element.replaceChild(spaner, node);
+    }
+  });
+});
+
+const sp = document.querySelectorAll('span');
+
+sp.forEach((item) => {
+  item.addEventListener('click', () => {
+    const parentLi = item.parentElement;
+
+    const childUl = parentLi.querySelector('ul');
+
+    if (childUl) {
+      childUl.style.display =
+        childUl.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
