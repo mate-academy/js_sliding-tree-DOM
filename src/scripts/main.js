775156b8e1f17c13ecee72abd80400fe2ae1d493
Oleksandr <kozyrev.sania@gmail.com>
2025-09-25 21:34:26 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listLi = tree.querySelectorAll('li');
+
+listLi.forEach((li) => {
+  if (li.querySelector('ul')) {
+    const title = li.firstChild;
+
+    const span = document.createElement('span');
+
+    span.innerText = title.textContent.trim();
+
+    title.replaceWith(span);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const target = e.target;
+
+    if (target.className === 'off') {
+      target.className = 'on';
+    } else {
+      target.className = 'off';
+    }
+
+    const child = target.nextElementSibling;
+
+    switch (target.className) {
+      case 'on':
+        child.style.display = 'block';
+        break;
+
+      case 'off':
+        child.style.display = 'none';
+        break;
+    }
+  }
+});
