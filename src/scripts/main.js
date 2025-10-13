571c1e23c2f39fecf9f76586587f3a098410a273
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-10-13 22:31:50 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+  const listOfLi = tree.querySelectorAll('li');
+
+  if (!tree || !listOfLi) {
+    return;
+  }
+
+  for (const li of listOfLi) {
+    const span = document.createElement('span');
+
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+
+  tree.addEventListener('click', (e) => {
+    if (e.target.tagName !== 'SPAN') {
+      return;
+    }
+
+    const childerUL = e.target.parentNode.querySelector('ul');
+
+    if (!childerUL) {
+      return;
+    }
+
+    childerUL.hidden = !childerUL.hidden;
+  });
+});
