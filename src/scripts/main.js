126c1c27b191574f0afed0ad956e5b5afa991118
Dima <dima2003strik@gmail.com>
2025-10-22 01:14:57 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+
+function wrapHeadersWithSpan(node) {
+  const children = Array.from(node.children);
+
+  children.forEach(child => {
+    if (child.tagName === 'LI') {
+      const nestedUl = child.querySelector(':scope > ul'); // шукаємо прямого нащадка <ul>
+      if (nestedUl) {
+        const textNode = Array.from(child.childNodes).find(n => n.nodeType === Node.TEXT_NODE && n.textContent.trim() !== '');
+
+        if (textNode) {
+          const span = document.createElement('span');
+          span.textContent = textNode.textContent.trim();
+          textNode.replaceWith(span);
+        }
+        wrapHeadersWithSpan(nestedUl);
+      }
+    }
+  });
+}
+
+wrapHeadersWithSpan(tree);
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') return;
+
+  const ul = e.target.parentElement.querySelector(':scope > ul');
+  if (!ul) return;
+
+  ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+});
