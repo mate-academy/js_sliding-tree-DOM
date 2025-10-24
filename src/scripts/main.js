dec4105a0ec0d56cd549348a1f78593f88cf855d
Iryna Knyzh <iryna.knyzh@gmail.com>
2025-10-24 12:53:00 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+
+const tree = document.querySelectorAll('.tree li');
+
+tree.forEach((element) => {
+  const text = element.firstChild;
+
+  const span = document.createElement('span');
+
+  span.textContent = text.textContent;
+
+  text.remove();
+
+  element.prepend(span);
+
+  span.addEventListener('click', (e) => {
+    const ul = span.nextSibling;
+
+    ul.hidden = !ul.hidden;
+  });
+});
