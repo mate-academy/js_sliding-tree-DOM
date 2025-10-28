a21339fff636fbe64c3997eab4e12dd4d821ccd3
yahohulia <yaroslavhohulia@gmail.com>
2025-10-28 16:19:30 +0200

    added js solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+
+for (const li of items) {
+  for (const node of Array.from(li.childNodes)) {
+    if (node.nodeType === Node.TEXT_NODE && node.textContent.trim()) {
+      const span = document.createElement('span');
+
+      span.textContent = node.textContent.trim();
+      span.classList.add('title');
+
+      li.replaceChild(span, node);
+    }
+  }
+}
+
+document.addEventListener('click', (e) => {
+  if (!e.target.matches('.title')) {
+    return;
+  }
+
+  const li = e.target.closest('li');
+  const sub = li.querySelector(':scope > ul');
+
+  sub.style.display = sub.style.display ? '' : 'none';
+});
