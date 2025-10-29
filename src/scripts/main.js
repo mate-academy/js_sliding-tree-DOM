325abaf785de3bc8886b44cc1187cebff2feb59a
Luna <yanka16247@mail.com>
2025-10-29 15:49:08 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+for (const li of document.querySelectorAll('li')) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+document.querySelector('.tree').addEventListener('click', (ev) => {
+  const target = ev.target;
+
+  if (target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const ul = target.parentNode.querySelector('ul');
+
+  if (!ul) {
+    return;
+  }
+
+  ul.hidden = !ul.hidden;
+});
