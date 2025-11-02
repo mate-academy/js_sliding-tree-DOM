8d090c3e0e78a57030ea261ce1d23367088c82f8
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2025-11-02 19:28:03 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const items = tree.querySelectorAll('li');
+
+items.forEach((li) => {
+  const subList = li.querySelector('ul');
+
+  if (!subList) {
+    return;
+  }
+
+  const text = li.firstChild.textContent.trim();
+  const span = document.createElement('span');
+
+  span.textContent = text;
+
+  li.firstChild.replaceWith(span);
+
+  span.addEventListener('click', () => {
+    subList.hidden = !subList.hidden;
+  });
+});
