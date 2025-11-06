0efdcb95a5fd27e1620904f89446611b6959e1b8
Oksana Rubel <okhymych480@gmail.com>
2025-11-06 14:56:22 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('.tree li');
+
+headers.forEach((li) => {
+  const elem = li.querySelector('ul');
+
+  if (elem) {
+    elem.style.display = 'block';
+
+    const childNodes = li.childNodes;
+
+    childNodes.forEach((node) => {
+      if (node.nodeType === 3 && node.textContent.trim() !== '') {
+        const span = document.createElement('span');
+
+        span.textContent = node.textContent;
+        li.replaceChild(span, node);
+
+        span.addEventListener('click', onClick);
+      }
+    });
+  }
+});
+
+function onClick(eve) {
+  const elem = eve.target.nextElementSibling;
+
+  if (elem) {
+    if (elem.style.display === 'none' || elem.style.display === '') {
+      elem.style.display = 'block';
+    } else {
+      elem.style.display = 'none';
+    }
+  }
+}
