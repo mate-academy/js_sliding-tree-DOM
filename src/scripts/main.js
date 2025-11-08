cfffcad48236686b38f662069e68df824f9b912a
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-11-08 10:57:21 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const headLines = [...tree.querySelectorAll('li')];
+
+headLines
+  .filter((li) => li.querySelector('ul'))
+  .forEach((li) => {
+    const span = document.createElement('span');
+    const textNode = li.childNodes[0];
+
+    span.textContent = textNode.textContent.trim();
+    li.replaceChild(span, textNode);
+
+    li.addEventListener('click', (ev) => {
+      ev.stopPropagation();
+
+      if (li.querySelector('ul').style.display === 'none') {
+        li.querySelector('ul').style.display = 'block';
+      } else {
+        li.querySelector('ul').style.display = 'none';
+      }
+    });
+  });
