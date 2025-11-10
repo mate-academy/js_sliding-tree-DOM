3c1eb1e50eb3f15983445e42414f276180107853
Vasyl Levitskyi <leva1990vale@gmail.com>
2025-11-10 12:57:56 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const li = tree.querySelectorAll('li');
+
+li.forEach((liItem) => {
+  const span = document.createElement('span');
+  const text = liItem.firstChild;
+
+  liItem.prepend(span);
+  span.append(text);
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const liItem = e.target.closest('li');
+  const childUl = liItem.querySelector('ul');
+
+  if (!childUl) {
+    return;
+  }
+
+  if (childUl.style.display === 'none') {
+    childUl.style.display = 'block';
+  } else {
+    childUl.style.display = 'none';
+  }
+});
