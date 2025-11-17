3fa094a724f30e61d008b3c6e3244ce3f82d6cb7
Yurii Drutsul <druculurij@gmail.com>
2025-11-17 13:06:44 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = document.querySelectorAll('.tree li');
+
+for (const li of listItems) {
+  const firstChild = li.firstChild;
+
+  if (firstChild.nodeType === 3 && firstChild.textContent.trim() !== '') {
+    const span = document.createElement('span');
+    const text = firstChild.textContent;
+
+    li.insertBefore(span, firstChild);
+    span.textContent = text;
+    li.removeChild(firstChild);
+  }
+}
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const li = e.target.closest('li');
+  const childUl = li.querySelector(':scope > ul');
+
+  if (!childUl) {
+    return;
+  }
+
+  childUl.hidden = !childUl.hidden;
+});
