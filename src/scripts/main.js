5092a35c5765ec5efe9b88a26fcc44ffa48f7ee4
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2025-11-25 22:02:48 +0200

    Tree-dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const allLi = document.querySelectorAll('.tree li');
+
+allLi.forEach((liElem) => {
+  const headLiElem = liElem.querySelector('ul');
+
+  if (headLiElem) {
+    const spanEl = document.createElement('span');
+    const headLi = liElem.firstChild.textContent.trim();
+
+    liElem.firstChild.textContent = '';
+    spanEl.textContent = headLi;
+    liElem.prepend(spanEl);
+
+    spanEl.addEventListener('click', () => {
+      if (headLiElem.style.display === 'none') {
+        headLiElem.style.display = 'block';
+      } else {
+        headLiElem.style.display = 'none';
+      }
+    });
+  }
+});
