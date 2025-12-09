a283b3abaa45320fedb269fe1e584531d1d2ae36
Alex <1inboxna@gmail.com>
2025-12-10 00:12:37 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const liList = document.querySelectorAll('li');
+
+liList.forEach((el) => {
+  if (el.querySelector('ul')) {
+    el.childNodes.forEach((node) => {
+      if (node.nodeType === 3 && node.textContent.trim() !== '') {
+        const span = document.createElement('span');
+
+        el.insertBefore(span, node);
+        span.append(node);
+      }
+    });
+  }
+});
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const span = e.target.closest('span');
+
+  if (!span) {
+    return;
+  }
+
+  const li = span.closest('li');
+  const nestedUl = li.querySelector(':scope > ul');
+
+  if (!nestedUl) {
+    return;
+  }
+
+  nestedUl.hidden = !nestedUl.hidden;
+});
