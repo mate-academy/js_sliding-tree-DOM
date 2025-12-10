a9100ddc7edafc5384a4518fa1f292af415e0048
Ruslan Kasinets <rulik.357@gmail.com>
2025-12-10 16:12:46 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const liElements = document.querySelectorAll('li');
+
+liElements.forEach((li) => {
+  const text = li.firstChild.textContent.trim();
+  const span = document.createElement('span');
+
+  span.textContent = text;
+
+  li.firstChild.remove();
+  li.prepend(span);
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const li = e.target.closest('li');
+  const childUl = li.querySelector(':scope > ul');
+
+  if (!childUl) {
+    return;
+  }
+
+  childUl.style.display = childUl.style.display === 'none' ? '' : 'none';
+});
