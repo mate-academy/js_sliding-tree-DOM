83a08596da4081e74d423699b8714028a9eab3d8
SageUA <panchyshyn33@gmail.com>
2025-12-15 17:40:24 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const nav = document.querySelector('.tree');
+const li = document.querySelectorAll('li');
+
+li.forEach((element) => {
+  const span = document.createElement('span');
+  const text = element.firstChild.textContent.trim();
+
+  span.textContent = text;
+
+  element.firstChild.replaceWith(span);
+});
+
+nav.addEventListener('click', (e) => {
+  const span = e.target.closest('span');
+  const li2 = span.closest('li');
+  const ul = li2.querySelector(':scope > ul');
+
+  if (!ul) {
+    return;
+  }
+
+  ul.style.display = ul.style.display === 'none' ? '' : 'none';
+});
