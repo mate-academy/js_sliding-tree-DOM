ca311ffe851a169e203dd4716784d0dc8c5a862c
Andrey <kagamlykas@gmail.com>
2025-12-17 15:52:45 +0200

    Solved Task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const lists = tree.querySelectorAll('ul');
+
+if (lists) {
+  for (const list of lists) {
+    const head = list.previousSibling;
+    const newHead = document.createElement('span');
+
+    newHead.textContent = head.textContent;
+    head.replaceWith(newHead);
+  }
+
+  tree.addEventListener('click', (ev) => {
+    const header = ev.target.closest('span');
+    const innerList = header.nextElementSibling;
+
+    if (innerList.style.display === 'none') {
+      innerList.style.display = '';
+    } else {
+      innerList.style.display = 'none';
+    }
+  });
+}
