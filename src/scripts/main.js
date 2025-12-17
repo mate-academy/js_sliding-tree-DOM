d3eca9b4cedf8a18ff4e73d27553ba5c18c6c3e9
Natalia Yakovleva <yakovlevane29@gmail.com>
2025-12-17 11:50:13 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.querySelectorAll('li').forEach((el) => {
+  const textNode = [...el.childNodes].find(
+    (node) => node.nodeType === Node.TEXT_NODE && node.textContent.trim(),
+  );
+
+  const span = document.createElement('span');
+
+  span.textContent = textNode.textContent.trim();
+
+  textNode.replaceWith(span);
+});
+
+tree.addEventListener('click', (e) => {
+  const parentLi = e.target.parentElement;
+  const childUl = parentLi.querySelector('ul');
+
+  if (childUl) {
+    childUl.hidden = !childUl.hidden;
+  }
+});
