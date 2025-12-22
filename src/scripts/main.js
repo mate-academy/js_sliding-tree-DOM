b4f6977015628d90090be2149472d86797908f44
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2025-12-22 14:01:07 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,21 +6,27 @@ listItems.forEach((el) => {
   const childList = el.querySelector('ul');
 
   if (childList) {
-    const span = document.createElement('span');
-    const text = el.firstChild;
+    const text = Array.from(el.childNodes).find(
+      (node) =>
+        node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '',
+    );
 
-    span.appendChild(text);
-    el.prepend(span);
+    if (text) {
+      const span = document.createElement('span');
 
-    span.addEventListener('click', () => {
-      if (
-        childList.style.display === '' ||
-        childList.style.display === 'block'
-      ) {
-        childList.style.display = 'none';
-      } else {
-        childList.style.display = 'block';
-      }
-    });
+      span.appendChild(text);
+      el.prepend(span);
+
+      span.addEventListener('click', () => {
+        if (
+          childList.style.display === '' ||
+          childList.style.display === 'block'
+        ) {
+          childList.style.display = 'none';
+        } else {
+          childList.style.display = 'block';
+        }
+      });
+    }
   }
 });
