c45811c6b1635fad3293b804914c6c70b34da93b
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2025-12-22 13:22:04 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach((el) => {
+  const childList = el.querySelector('ul');
+
+  if (childList) {
+    const span = document.createElement('span');
+    const text = el.firstChild;
+
+    span.appendChild(text);
+    el.prepend(span);
+
+    span.addEventListener('click', () => {
+      if (
+        childList.style.display === '' ||
+        childList.style.display === 'block'
+      ) {
+        childList.style.display = 'none';
+      } else {
+        childList.style.display = 'block';
+      }
+    });
+  }
+});
