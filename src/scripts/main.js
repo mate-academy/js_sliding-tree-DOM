b943ec110f3ce6efba42829da444f407760fc5d0
vladiksanl <vladislav.liashenko1@gmail.com>
2026-01-08 19:56:39 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const titles = document.querySelectorAll('li:has(ul)');
+const arrayOfTitles = [...titles].reverse();
+
+arrayOfTitles.forEach((el) => {
+  el.innerHTML = el.innerHTML.trim();
+
+  const { nameOfTitle, newHTML } = cutAndCopy(el.innerHTML);
+
+  el.innerHTML = newHTML;
+
+  const spanTitle = document.createElement('span');
+
+  spanTitle.textContent = nameOfTitle;
+
+  el.prepend(spanTitle);
+});
+
+function cutAndCopy(str) {
+  const check = /^[a-zA-Z]$/;
+  let result = '';
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (check.test(str[i]) || str[i].includes(' ')) {
+      result += str[i];
+      count++;
+    } else {
+      break;
+    }
+  }
+
+  return { nameOfTitle: result, newHTML: str.slice(count) };
+}
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  if (e.target.matches('span')) {
+    e.target.nextElementSibling.toggleAttribute('hidden');
+  }
+});
