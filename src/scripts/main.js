66c01d35355096a57492ddfc5ac3e5b5eefdb2c9
Yar14k <tabachuk1406@gmail.com>
2026-01-12 08:52:57 +0100

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,9 @@ const list = document.querySelectorAll('li');
 
 list.forEach((li) => {
   const text = Array.from(li.childNodes).find((node) => node.nodeType === 3);
+  if (!text) {
+    return;
+  }
   const span = document.createElement('span');
 
   span.textContent = text.textContent.trim();
@@ -14,10 +17,19 @@ list.forEach((li) => {
 const ul = document.querySelector('ul');
 
 ul.addEventListener('click', (e) => {
+
+  if (e.target.tagName !== 'SPAN')
+    {
+       return;
+    }
+
   const li = e.target.parentElement;
   const childUl = li.querySelector(':scope > ul');
 
-  if (childUl) {
-    childUl.style.display = childUl.style.display === 'none' ? 'block' : 'none';
+  if (!childUl) {
+    return;
   }
+
+  childUl.style.display = childUl.style.display === 'none' ? 'block' : 'none';
 });
+
