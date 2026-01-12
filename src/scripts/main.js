2a3a665dab819785f72cf5077064fd1cce7bf03e
Yar14k <tabachuk1406@gmail.com>
2026-01-12 08:42:42 +0100

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+list.forEach((li) => {
+  const text = Array.from(li.childNodes).find((node) => node.nodeType === 3);
+  const span = document.createElement('span');
+
+  span.textContent = text.textContent.trim();
+  li.insertBefore(span, text);
+  li.removeChild(text);
+});
+
+const ul = document.querySelector('ul');
+
+ul.addEventListener('click', (e) => {
+  const li = e.target.parentElement;
+  const childUl = li.querySelector(':scope > ul');
+
+  if (childUl) {
+    childUl.style.display = childUl.style.display === 'none' ? 'block' : 'none';
+  }
+});
