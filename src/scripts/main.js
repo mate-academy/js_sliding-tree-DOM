82654d44581f302c263713fdb4a8d206ef26674b
Nazarii Korol <nazarkorol3684@gmail.com>
2026-01-16 18:44:43 +0200

    task js-sliding-tree-DOM done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+li.forEach((item) => {
+  if (item.children.length > 0) {
+    const span = document.createElement('span');
+
+    span.innerText = item.firstChild.textContent.trim().replace('\n', '');
+    item.firstChild.replaceWith(span);
+  }
+});
+
+const togglers = document.querySelectorAll('span');
+
+togglers.forEach((item) => {
+  item.addEventListener('click', () => {
+    item.nextElementSibling.style.display =
+      item.nextElementSibling.style.display === 'none' ? 'block' : 'none';
+  });
+});
