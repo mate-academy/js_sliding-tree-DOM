29bc2a8dcff72eb74ee28366e034b72ed377a48b
shymkivvasyl <shymkiv.vasyl333@gmail.com>
2026-01-20 18:22:06 +0100

    add eventtListener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const allLi = tree.querySelectorAll('li');
+
+allLi.forEach((li) => {
+  const ul = li.querySelector('ul');
+
+  if (ul) {
+    const liChild = li.firstChild;
+
+    if (liChild.nodeType === Node.TEXT_NODE) {
+      const textInSpan = document.createElement('span');
+
+      textInSpan.textContent = liChild.textContent;
+      li.replaceChild(textInSpan, liChild);
+    }
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const parents = e.target.closest('li');
+  const ulParents = parents.querySelector('ul');
+
+  ulParents.hidden = !ulParents.hidden;
+});
