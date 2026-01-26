50cd2480eec3a219e6258df4aa14c9bafabea711
Andriy <andriit35@gmail.com>
2026-01-26 19:54:45 +0200

    implement required logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const lis = document.querySelectorAll('li');
+
+lis.forEach((li) => {
+  const span = document.createElement('span');
+
+  while (li.firstChild) {
+    span.appendChild(li.firstChild);
+  }
+
+  li.appendChild(span);
+});
+
+lis.forEach((li) => {
+  li.addEventListener('click', (ev) => {
+    const childrenContainer = li.querySelector('ul');
+
+    if (childrenContainer) {
+      childrenContainer.hidden = !childrenContainer.hidden;
+      li.classList.toggle('closed');
+      li.classList.toggle('open');
+    }
+
+    ev.stopPropagation();
+  });
+});
