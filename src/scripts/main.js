95c1a8d924772925ae7e650463ebca36ea9d1166
unknown <dkuz6323@gmail.com>
2026-01-27 15:36:50 +0200

    completed tasks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write code here
+
+const tree = document.querySelector('.tree');
+
+const list = document.querySelectorAll('li');
+
+list.forEach((el) => {
+  if (el.firstChild.nodeType === Node.TEXT_NODE) {
+    const span = document.createElement('span');
+
+    span.textContent = el.firstChild.textContent;
+
+    el.firstChild.remove();
+
+    el.insertBefore(span, el.firstChild);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const li = e.target.parentElement;
+  const ul = li.querySelector('ul');
+
+  if (!ul) {
+    return;
+  }
+
+  ul.hidden = !ul.hidden;
+});
