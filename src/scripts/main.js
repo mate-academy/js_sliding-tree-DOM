ef9f38b70bdbe93e09bb6534f09c94b568117c00
Taras Borshch <tarasborshch4@gmail.com>
2026-02-13 18:44:43 +0200

    Develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector(".tree")
+
+tree.addEventListener("click", (event) => {
+    const element = event.target
+
+    if (element.hasChildNodes()) {
+        for (const child of element.children) {
+            if (child.style.display === 'none') {
+                child.style.display = '';
+            } else {
+                child.style.display = 'none';
+            }
+        } 
+    }
+})
