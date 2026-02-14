2c2fb7058d6a19965e665c1d3d9b982a2a318a46
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-02-14 21:20:21 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const items = tree.querySelectorAll('li');
+
+[...items].forEach((item) => {
+  const childList = item.querySelector(':scope > ul');
+
+  if (!childList) {
+    return;
+  }
+
+  const textNode = [...item.childNodes].find(
+    (node) => node.nodeType === Node.TEXT_NODE && node.textContent.trim(),
+  );
+
+  if (!textNode) {
+    return;
+  }
+
+  const span = document.createElement('span');
+
+  span.textContent = textNode.textContent.trim();
+
+  item.replaceChild(span, textNode);
+});
+
+tree.addEventListener('click', (e) => {
+  const span = e.target.closest('span');
+
+  if (!span) {
+    return;
+  }
+
+  const list = span.closest('li');
+
+  const subTree = list.querySelector('ul');
+
+  if (!subTree) {
+    return;
+  }
+
+  subTree.hidden = !subTree.hidden;
+});
