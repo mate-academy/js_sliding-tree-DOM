14127ae3c1cb3226ec0f66c182f3e74360796de5
Anatolii Lutai <toliklutai@gmail.com>
2026-02-26 17:10:45 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
-'use strict';
+document.addEventListener('click', (e) => {
+  const span = e.target.closest('span');
 
-// write code here
+  if (!span) {
+    return;
+  }
+
+  const headline = span.closest('h1, h2, h3, h4, h5, h6');
+
+  if (!headline) {
+    return;
+  }
+
+  const nextElement = headline.nextElementSibling;
+
+  if (nextElement) {
+    nextElement.classList.toggle('hidden');
+  }
+});
