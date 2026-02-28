173834c01dfa018cce07fe9ee81ef728558948f7
Natalia Humenyk <gumenuknatalia53@gmail.com>
2026-02-28 20:16:34 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const li = e.target.closest('li');
+
+  if (!li) {
+    return;
+  }
+
+  const childUl = li.querySelector(':scope > ul');
+
+  if (!childUl) {
+    return;
+  }
+
+  childUl.hidden = !childUl.hidden;
+});
