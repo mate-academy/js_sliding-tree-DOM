4ad06bc322de810543d26eddd4aa83b3926b9f0c
Artem <muha13artem@gmail.com>
2026-03-21 11:54:55 +0200

    finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+li.forEach((one) => {
+  const textNode = one.firstChild;
+  const span = document.createElement('span');
+
+  one.insertBefore(span, textNode);
+
+  span.appendChild(textNode);
+});
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const foundUl = e.target.parentNode.querySelector('ul');
+
+    if (foundUl) {
+      foundUl.hidden = !foundUl.hidden;
+    }
+  }
+});
