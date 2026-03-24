ee858148645b0c1f753295678cbb0df31f30878f
Ivadriil <petrivi51@gmail.com>
2026-03-24 18:49:56 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const allLi = document.querySelectorAll('.tree li');
+
+allLi.forEach((li) => {
+  const textNode = [...li.childNodes].find((node) => {
+    return node.nodeType === Node.TEXT_NODE && node.textContent.trim();
+  });
+
+  if (textNode) {
+    const span = document.createElement('span');
+
+    span.textContent = textNode.textContent.trim();
+
+    textNode.replaceWith(span);
+  }
+});
+
+document.addEventListener('click', menu);
+
+function menu(e) {
+  const span = e.target.closest('span');
+
+  if (!span) {
+    return;
+  }
+
+  const li = span.parentElement;
+  const nestedUl = li.querySelector('ul');
+
+  if (!nestedUl) {
+    return;
+  }
+  nestedUl.hidden = !nestedUl.hidden;
+}
