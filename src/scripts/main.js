c630c3886dae03474d6426021572ed602b085f9d
Yurii Leniv <supusman32@gmail.com>
2026-03-29 19:23:14 +0300

    sdtgsdgtsgt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const m = document.querySelectorAll('.tree li');
+
+function spanCreate(node) {
+  const pureText = node.childNodes[0];
+  const newSpan = document.createElement('span');
+
+  newSpan.textContent = pureText.textContent;
+  pureText.replaceWith(newSpan);
+}
+
+for (let i = 0; i < m.length; i++) {
+  if (m[i].children.length > 0) {
+    spanCreate(m[i]);
+  }
+}
+
+const spanList = document.querySelectorAll('span');
+
+for (const el of spanList) {
+  el.addEventListener('click', () => {
+    const sibling = el.nextElementSibling;
+
+    if (sibling) {
+      if (sibling.style.display === 'none') {
+        sibling.style.display = 'block';
+      } else {
+        sibling.style.display = 'none';
+      }
+    }
+  });
+}
