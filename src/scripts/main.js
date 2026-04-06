09965dab9b5bcc18ba9545d523f8e7bd87de3998
Banderos14 <anton.shyshenko@gmail.com>
2026-04-06 18:28:31 +0200

    feat: task_solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const treeItems = tree.querySelectorAll('li');
+
+for (const li of treeItems) {
+  const textNode = li.firstChild;
+
+  if (textNode.nodeType === Node.TEXT_NODE) {
+    const span = document.createElement('span');
+
+    span.textContent = textNode.textContent.trim();
+
+    textNode.textContent = '';
+    li.prepend(span);
+  }
+}
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const span = e.target;
+  const li = span.parentElement;
+  const ul = li.querySelector('ul');
+
+  if (!ul) {
+    return;
+  }
+
+  ul.hidden = !ul.hidden;
+});
