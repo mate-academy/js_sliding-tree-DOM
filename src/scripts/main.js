291bcb835fd1340ac8dc69a709e92cdaa18aee16
Ruslan <kryzhanskiyruslan@gmail.com>
2026-04-06 22:14:57 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const headerItems = document.querySelectorAll('li:has(ul)');
+const tree = document.querySelector('.tree');
+
+for (const header of headerItems) {
+  const span = document.createElement('span');
+  const liText = header.childNodes[0].textContent;
+
+  span.textContent = liText;
+
+  header.replaceChild(span, header.childNodes[0]);
+}
+
+tree.addEventListener('click', (eve) => {
+  const currentTarget = eve.target;
+
+  if (currentTarget.tagName === 'SPAN') {
+    const nestedUI = currentTarget.parentNode.querySelector('ul');
+
+    if (nestedUI.style.display === 'none') {
+      nestedUI.style.display = '';
+    } else {
+      nestedUI.style.display = 'none';
+    }
+  }
+});
