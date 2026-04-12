99f523cd9e1689d10c29be578e9622b168ff74ed
Bohdan <felbohdan@gmail.com>
2026-04-13 01:04:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const allLists = tree.querySelectorAll('li');
+
+allLists.forEach((list) => {
+  const firstChild = list.firstChild;
+
+  if (firstChild && firstChild.nodeType === Node.TEXT_NODE) {
+    const span = document.createElement('span');
+
+    span.textContent = firstChild.textContent;
+    list.insertBefore(span, firstChild);
+    list.removeChild(firstChild);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const parentLi = e.target.closest('li');
+  const targertUl = parentLi.querySelector('ul');
+
+  if (targertUl) {
+    const isHidden = targertUl.style.display === 'none';
+
+    targertUl.style.display = isHidden ? 'block' : 'none';
+  }
+});
