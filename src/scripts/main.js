9f12ee71f0319e82637cf8a02aba2bfbedf125c3
Andrew <a.tupko98>
2026-04-24 10:18:45 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+const li = document.querySelectorAll('li');
+
+li.forEach((e) => {
+  const textNode = e.firstChild;
+
+  if (!textNode || textNode.nodeType !== Node.TEXT_NODE) return;
+
+  const span = document.createElement('span');
+  span.textContent = textNode.textContent;
+
+  e.replaceChild(span, textNode);
+
+  span.addEventListener('click', () => {
+    const nested = e.querySelector('ul');
+
+    if (nested) {
+      nested.hidden = !nested.hidden;
+    }
+  });
+});
