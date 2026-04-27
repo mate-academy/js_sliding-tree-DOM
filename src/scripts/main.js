637ab706f48ed1e3a424f0dcb07430f8d83d45b8
Yaroslav Kravchuk <Yaroslav.kravchyk@gmail.com>
2026-04-27 16:54:03 +0300

    add task solution 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const listItems = tree.querySelectorAll('li');
+
+for (const item of listItems) {
+  const textNote = [...item.childNodes].find(
+    (node) =>
+      node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '',
+  );
+
+  if (textNote) {
+    const span = document.createElement('span');
+
+    span.textContent = textNote.textContent.trim();
+    item.insertBefore(span, textNote);
+    item.removeChild(textNote);
+  }
+
+  tree.addEventListener('click', (eve) => {
+    if (eve.target.tagName !== 'SPAN') {
+      return;
+    }
+
+    const li = eve.target.closest('li');
+    const nestedList = li.querySelector('ul');
+
+    if (nestedList) {
+      nestedList.hidden = !nestedList.hidden;
+    }
+  });
+}
