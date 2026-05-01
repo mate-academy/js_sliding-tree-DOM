51d9dd4e1fc752c6543db00e75fd15435ccf5636
Alibobick <dozenkoalex6@gmail.com>
2026-05-01 21:29:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('.tree li span');
+
+headers.forEach((header) => {
+  header.addEventListener('click', () => {
+    const ul = header.nextElementSibling;
+
+    if (ul) {
+      ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
