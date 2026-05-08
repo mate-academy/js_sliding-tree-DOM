c5733f1346a536f2cf6990e8058ad6b985f9b640
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-05-08 13:07:09 +0300

    finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach((li) => {
+  li.childNodes.forEach((node) => {
+    if (node.nodeType === 3 && node.nodeValue.trim() !== '') {
+      const span = document.createElement('span');
+
+      span.textContent = node.nodeValue.trim();
+      li.insertBefore(span, node);
+      node.remove();
+    }
+  });
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const li = e.target.closest('li');
+  const nestedUl = li.querySelector('ul');
+
+  if (nestedUl) {
+    nestedUl.hidden = !nestedUl.hidden;
+  }
+});
