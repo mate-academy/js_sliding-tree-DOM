c20d153b6d3883ae981407826846992e1ca80a9a
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-05-12 18:13:18 +0200

    sliding tree-dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const items = document.querySelectorAll('.tree li');
+
+for (const li of items) {
+  const span = document.createElement('span');
+
+  const text = li.firstChild;
+
+  if (text && text.textContent.trim() !== '') {
+    span.append(text);
+    li.prepend(span);
+  }
+}
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const subTree = e.target.parentNode.querySelector('ul');
+
+  if (subTree) {
+    subTree.hidden = !subTree.hidden;
+  }
+});
