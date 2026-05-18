40f9ad9009b7856d5f11405822f02771b833a126
Mariia Madarash <mariiamadarash@gmail.com>
2026-05-18 12:17:36 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+
+items.forEach((item) => {
+  const nestedList = item.querySelector('ul');
+
+  if (nestedList) {
+    const span = document.createElement('span');
+
+    span.textContent = item.firstChild.textContent.trim();
+
+    item.firstChild.replaceWith(span);
+
+    span.addEventListener('click', () => {
+      nestedList.hidden = !nestedList.hidden;
+    });
+  }
+});
