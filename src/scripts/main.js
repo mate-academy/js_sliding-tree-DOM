a99518799733e105e248dae75a926964faa18be0
Yana Chernenko <yanamyrna@gmail.com>
2026-05-19 10:59:37 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.querySelectorAll('li').forEach((li) => {
+  const ul = li.querySelector(':scope > ul');
+
+  if (!ul) {
+    return;
+  }
+
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent.trim();
+
+  li.firstChild.remove();
+
+  li.prepend(span);
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const ul = e.target.parentElement.querySelector(':scope > ul');
+
+  if (ul) {
+    ul.hidden = !ul.hidden;
+  }
+});
