f0595ef15bd2c1f9d0a022ecc097bc717f376018
Danylo Skepskyi <skepskyidanylo@gmail.com>
2026-05-25 17:51:40 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+
+function replaceSpan(tr) {
+  for (const child of tr.querySelectorAll('li')) {
+    const ul = child.querySelectorAll(':scope > ul');
+
+    if (!ul) {
+      return;
+    }
+
+    const span = document.createElement('span');
+
+    span.textContent = child.firstChild.textContent.trim();
+
+    child.firstChild.remove();
+
+    child.prepend(span);
+  }
+}
+
+replaceSpan(tree);
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const ul = e.target.parentElement.querySelector(':scope > ul');
+
+  if (ul) {
+    ul.hidden = !ul.hidden;
+  }
+});
