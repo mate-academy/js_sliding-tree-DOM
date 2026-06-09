d5f3f3683d3bfebe4355fa476fc7cb36e08eb442
ILLYA SMYK <smykillya@gmail.com>
2026-06-09 12:27:49 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+
+for (const item of items) {
+  const nestedList = item.querySelector('ul');
+
+  if (!nestedList) {
+    continue;
+  }
+
+  const span = document.createElement('span');
+
+  span.textContent = item.childNodes[0].textContent;
+  item.childNodes[0].replaceWith(span);
+
+  const list = span.nextElementSibling;
+
+  span.addEventListener('click', () => {
+    list.hidden = !list.hidden;
+  });
+}
