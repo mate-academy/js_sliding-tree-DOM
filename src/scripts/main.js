97ed351298c3a3b7afa283271a2f405d4621049d
Denys Boiko <big4ever6996@gmail.com>
2026-06-17 19:53:50 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const hidingTree = document.querySelector('ul');
+const allLiTags = document.querySelectorAll('li');
+
+for (const item of allLiTags) {
+  const liItem = item.firstChild;
+  const spanElement = document.createElement('span');
+
+  spanElement.append(liItem);
+  item.prepend(spanElement);
+}
+
+hidingTree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const parentLi = e.target.parentNode;
+  const childrenContainer = parentLi.querySelector('ul');
+
+  if (childrenContainer) {
+    childrenContainer.hidden = !childrenContainer.hidden;
+  }
+});
