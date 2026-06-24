5fca8c62656f3dd43c2fead67a5a2fccbd557a5c
Ihor Balakhmei <balakhmei.dev@gmail.com>
2026-06-24 15:08:35 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li:has(ul)');
+
+items.forEach((li) => {
+  const text = li.firstChild.textContent;
+
+  li.firstChild.remove();
+
+  const span = document.createElement('span');
+
+  span.textContent = text;
+
+  li.prepend(span);
+
+  span.addEventListener('click', () => {
+    const ul = li.querySelector('ul');
+
+    ul.style.display = ul.style.display === 'none' ? '' : 'none';
+  });
+});
