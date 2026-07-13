0f739a84efbc97fe18562489854fb42f3e221758
Anna Pryimak <sapfira03@gmail.com>
2026-07-13 19:45:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const titles = list.querySelectorAll('li:has(ul)');
+
+titles.forEach((title) => {
+  const titleText = title.firstChild.textContent;
+  const wrappedTitle = document.createElement('span');
+
+  wrappedTitle.textContent = titleText;
+
+  title.firstChild.replaceWith(wrappedTitle);
+
+  const listElem = title.querySelector('ul');
+
+  wrappedTitle.addEventListener('click', (e) => {
+    listElem.style.display = listElem.style.display === 'none' ? '' : 'none';
+  });
+});
