3ae235feedfa84730c829c2f2d86b55dcc97d907
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-18 18:10:16 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const curr = e.target;
+
+  if (curr.children[0].children.length > 1) {
+    // curr.children.style.display = 'none';
+    if (curr.style.fontWeight !== 'bold') {
+      curr.style.fontWeight = 'bold';
+    } else {
+      curr.style.fontWeight = '';
+    }
+
+    for (const item of [...curr.querySelectorAll('li')]) {
+      // item.style.display = 'none';
+      if (item.hidden) {
+        item.removeAttribute('hidden');
+      } else {
+        item.setAttribute('hidden', 'true');
+      }
+    }
+  }
+});
