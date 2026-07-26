849ca2c45e08ab68bbaf9b66824b63e5c4f5c9a6
darvexon <darvexon.w@proton.me>
2026-07-26 20:38:40 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const parentList = document.querySelector('.tree');
+const childLists = parentList.getElementsByTagName('ul');
+
+function wrapHeadingWithSpan(list) {
+  const oldHeading = list.previousSibling;
+  const newHeading = document.createElement('span');
+
+  newHeading.textContent = oldHeading.textContent;
+
+  oldHeading.remove();
+
+  list.before(newHeading);
+}
+
+function addCloseFeature(list) {
+  const heading = list.previousElementSibling;
+
+  heading.addEventListener('click', (e) => {
+    if (list.style.display === 'none') {
+      list.style.display = '';
+    } else {
+      list.style.display = 'none';
+    }
+  });
+}
+
+for (const item of childLists) {
+  wrapHeadingWithSpan(item);
+  addCloseFeature(item);
+}
