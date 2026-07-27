10956b4a4343677e478122d0a56550b1e3d0f893
Yehor Mishchenko <130080322+myehor@users.noreply.github.com>
2026-07-27 19:58:53 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write code here
+const allLiElements = [...document.querySelectorAll('li')];
+
+for (const element of allLiElements) {
+  if (element.querySelector('ul') === null) {
+    continue;
+  }
+
+  const elementChilds = [...element.childNodes];
+  const span = document.createElement('span');
+
+  span.textContent = elementChilds[0].textContent;
+  elementChilds[0].replaceWith(span);
+}
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const span = e.target;
+  const li = span.parentElement;
+  const ul = li.querySelector('ul');
+
+  if (ul.style.display === 'none') {
+    ul.style.display = '';
+  } else {
+    ul.style.display = 'none';
+  }
+});
