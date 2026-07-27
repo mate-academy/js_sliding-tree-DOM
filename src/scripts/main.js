3c88f03927cffcc207ded4fb9526e49d33bfb966
ptrodion <rdnpetrenko@gmail.com>
2026-07-27 22:08:46 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write code here
+const refs = {
+  listTitleEl: document.querySelectorAll('.tree li:has(ul)'),
+};
+
+refs.listTitleEl.forEach((li) => {
+  const nestedList = li.querySelector('ul');
+
+  if (!nestedList) {
+    return;
+  }
+
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent.trim();
+  li.firstChild.replaceWith(span);
+
+  let displayList = nestedList.style.display;
+
+  span.addEventListener('click', () => {
+    displayList = displayList === 'none' ? '' : 'none';
+  });
+});
