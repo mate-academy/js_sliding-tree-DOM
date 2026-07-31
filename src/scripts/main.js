c1d86e1936955221eabf0ea30f52ca0565b3e6f8
Alexander Okhotnychenko <shashaohota@gmail.com>
2026-07-31 15:51:16 +0300

    solution: change tree DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+function changeTree() {
+  const liElements = document.querySelectorAll('li');
+
+  liElements.forEach(processTreeItem);
+}
+
+const processTreeItem = (element) => {
+  const nestedList = element.querySelector('ul');
+
+  if (!nestedList) {
+    return;
+  }
+
+  const textNode = element.childNodes[0];
+  const text = textNode.textContent.trim();
+
+  const span = document.createElement('span');
+
+  span.textContent = text;
+
+  element.insertBefore(span, nestedList);
+  textNode.remove();
+
+  span.addEventListener('click', handleToggleNode(span));
+};
+
+const handleToggleNode = (span) => {
+  const li = span.closest('li');
+  const nestedList = li.querySelector('ul');
+
+  nestedList.style.display =
+    nestedList.style.display === 'none' ? 'block' : 'none';
+};
+
+changeTree();
