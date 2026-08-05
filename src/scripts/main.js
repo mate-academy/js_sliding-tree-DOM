0e91f232158c235298f97291539d018dc975b5a0
Maksym Babalich <17033001m@gmail.com>
2026-08-05 17:35:35 +0300

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', (e) => {
+  const clicked = e.target.closest('li');
+
+  if (!clicked) {
+    return 0;
+  }
+
+  const ul = clicked.querySelectorAll(':scope > ul');
+
+  if (!ul) {
+    return;
+  }
+
+  for (const u of ul) {
+    u.style.display = u.style.display === 'none' ? 'block' : 'none';
+  }
+});
