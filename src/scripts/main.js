074567f2e4fe5659a72f8d719dcebe0333effb63
pparys93 <pparys93@gmail.com>
2026-08-05 22:16:26 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('.tree li:has(ul)');
+
+headers.forEach((header) => {
+  const textNode = header.firstChild;
+
+  const button = document.createElement('button');
+
+  button.setAttribute('aria-label', 'Toggle');
+  button.classList.add('toggle-button');
+  button.innerHTML = `<span class ="visually-hidden">Toggle</span>`;
+  header.prepend(button);
+
+  const span = document.createElement('span');
+
+  span.textContent = textNode.textContent;
+
+  button.addEventListener('click', () => {
+    const childUl = header.querySelector('ul');
+
+    childUl.hidden = !childUl.hidden;
+  });
+
+  span.addEventListener('click', () => {
+    const childUl = header.querySelector('ul');
+
+    childUl.hidden = !childUl.hidden;
+  });
+  header.replaceChild(span, textNode);
+});
