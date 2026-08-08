e18a9e79dff56e005513b9ea3d17d0018c77986b
Nik200309 <nikitapidlypnij@gmail.com>
2026-08-08 09:07:04 +0300

    First attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
 // write code here
+const table = document.querySelectorAll('.tree');
+
+function wrapper(item) {
+  const wrapperSpan = document.createElement('span');
+
+  wrapperSpan.classList.add('tree-label');
+
+  item.parentNode.insertBefore(wrapperSpan, item);
+  wrapperSpan.appendChild(item);
+}
+
+table.forEach((tree) => {
+  const items = tree.querySelectorAll('li');
+
+  items.forEach((li) => {
+    let textNode = null;
+
+    for (const node of li.childNodes) {
+      if (node.nodeType === 3 && node.textContent.trim() !== '') {
+        textNode = node;
+        break;
+      }
+    }
+
+    if (textNode) {
+      wrapper(textNode);
+    }
+  });
+});
+
+table.forEach((el) => {
+  el.addEventListener('click', (ev) => {
+    const label = ev.target.closest('.tree-label');
+
+    if (!label) {
+      return;
+    }
+
+    const clickedLi = label.closest('li');
+    const childLi = clickedLi.querySelector(':scope > ul');
+
+    if (!childLi) {
+      return;
+    }
+
+    childLi.style.display = childLi.style.display === '' ? 'none' : '';
+
+    ev.stopPropagation();
+  });
+});
