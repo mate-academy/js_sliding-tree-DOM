3fb2966c9987ab3825d3e579da2f8a0b3d3e9360
Victoria Neustroeva <neustrvk@gmail.com>
2026-08-09 08:59:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const ulEl = document.querySelector('ul.tree');
+
+const allLi = [...ulEl.querySelectorAll('li')];
+const headersLi = allLi.filter((li) => li.querySelector('ul'));
+
+headersLi.forEach((li) => {
+  const nestedUl = li.querySelector('ul');
+
+  const textNode = [...li.childNodes].find(
+    (node) =>
+      node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '',
+  );
+
+  const span = document.createElement('span');
+
+  span.textContent = textNode.textContent.trim();
+  textNode.replaceWith(span);
+
+  span.addEventListener('click', () => {
+    nestedUl.toggleAttribute('hidden');
+  });
+});
