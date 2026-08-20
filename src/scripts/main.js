5804831129ff126081d0388a16d24b2ad7c50f49
HiljSV <hiljsv@gmail.com>
2026-08-20 22:35:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+for (const item of tree.querySelectorAll('li')) {
+  const nested = item.querySelector('ul');
+
+  if (!nested) {
+    continue;
+  }
+
+  const title = document.createElement('span');
+
+  title.append(item.firstChild);
+  item.prepend(title);
+}
+
+tree.addEventListener('click', (e) => {
+  const title = e.target.closest('span');
+
+  if (!title) {
+    return;
+  }
+
+  const nested = title.parentElement.querySelector('ul');
+
+  nested.hidden = !nested.hidden;
+});
