767405b6a0df3eba71778915c198fa4acf2284ee
Vira Shmyhelska <vira.zykhor@gmail.com>
2026-08-21 22:02:44 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('li');
+
+headers.forEach((header) => {
+  const span = document.createElement('span');
+
+  span.textContent = header.firstChild.textContent;
+  header.firstChild.replaceWith(span);
+
+  const list = header.querySelector('ul');
+
+  span.addEventListener('click', () => {
+    list.hidden = !list.hidden;
+  });
+});
