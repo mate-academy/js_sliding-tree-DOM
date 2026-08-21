409a78568a7a38dd6736c1795947d233c58a265a
PrinceValue <princeossoa@gmail.com>
2026-08-21 16:20:25 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const treeItems = tree.querySelectorAll('li');
+
+treeItems.forEach((n) => {
+  const span = document.createElement('span');
+
+  span.append(n.firstChild);
+
+  n.prepend(span);
+});
+
+tree.addEventListener('click', (e) => {
+  const nestedUl = e.target.closest('li').querySelector('ul');
+
+  if (!tree.contains(e.target)) {
+    return;
+  }
+
+  if (nestedUl) {
+    nestedUl.hidden = !nestedUl.hidden;
+  }
+});
