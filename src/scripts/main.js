1dcf1d3397014782e9ec56116173301f3a20d578
ALex <sasha.hivuk@gmail.com>
2026-08-27 11:32:14 +0300

    sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+const headLines = [...document.querySelectorAll('.tree li')];
+
+headLines.forEach((line) => {
+  const nestedUl = line.querySelector('ul');
+
+  if (nestedUl) {
+    const span = document.createElement('span');
+
+    span.append(line.firstChild);
+
+    line.prepend(span);
+
+    span.addEventListener('click', (e) => {
+      nestedUl.hidden = !nestedUl.hidden;
+    });
+  }
+});
