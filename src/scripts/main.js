a65b46a15f72109dd9e9bc967551792719f07022
Vitalii <vitaliy.doronin4@gmail.com>
2026-08-27 13:57:21 +0300

    Add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const liItems = document.querySelectorAll('li');
+
+liItems.forEach((li) => {
+  const firstChild = li.firstChild;
+  const text = firstChild.textContent.trim();
+  const span = document.createElement('span');
+
+  span.textContent = text;
+
+  firstChild.replaceWith(span);
+});
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (eventClick) => {
+  const sp = eventClick.target.closest('span');
+
+  if (!sp) {
+    return;
+  }
+
+  const subTree = sp.parentElement.querySelector('ul');
+
+  if (!subTree) {
+    return;
+  }
+
+  subTree.hidden = !subTree.hidden;
+});
