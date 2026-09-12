509171ee6f3e419614b8c07b719ad8e1c5cd26b3
Your Name <you@example.com>
2026-09-12 21:03:46 +0300

    Solution_v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const uls = tree.querySelectorAll('ul');
+
+const headers = [];
+
+for (const elem of uls) {
+  headers.push(elem.parentNode);
+}
+
+headers.forEach((header) => {
+  header.addEventListener('click', () => {
+    const content = header.nextElementSibling;
+
+    content.style.display = content.style.display === 'none' ? '' : 'none';
+  });
+});
