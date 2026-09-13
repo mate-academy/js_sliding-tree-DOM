ff553c9a36474a1b6799d4d496c5e2694746aa06
Mykola Averchenko <kolya.averchenko@gmail.com>
2026-09-13 13:04:01 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
 // write code here
+const list = document.querySelector('.tree');
+
+wrapChildrenHeaders(list);
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const targetParent = e.target.parentNode;
+
+    for (const sibling of targetParent.children) {
+      if (sibling.tagName === 'UL') {
+        sibling.style.display = sibling.style.display === 'none' ? '' : 'none';
+      }
+    }
+  }
+});
+
+function wrapChildrenHeaders(element) {
+  const childNodes = element.childNodes;
+
+  for (const child of childNodes) {
+    if (child.nodeType === 3) {
+      const childTrimmedText = child.textContent.trim();
+
+      if (childTrimmedText.length > 0) {
+        const headerSpan = document.createElement('span');
+
+        headerSpan.textContent = child.textContent;
+        child.replaceWith(headerSpan);
+      }
+    } else {
+      wrapChildrenHeaders(child);
+    }
+  }
+}
