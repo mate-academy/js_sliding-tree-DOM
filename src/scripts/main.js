6ccfa9382a091b283074ccc2e10813aaca8bc51a
kateryna Habchak <kvizovapidtrimka@gmail.com>
2026-09-15 21:10:12 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+
+items.forEach((item) => {
+  const span = document.createElement('span');
+
+  span.textContent = item.childNodes[0].textContent;
+
+  item.childNodes[0].remove();
+
+  item.append(span);
+
+  const ul = item.querySelector('ul');
+
+  if (ul) {
+    span.addEventListener('click', () => {
+      if (ul.style.display === 'none') {
+        ul.style.display = 'block';
+      } else {
+        ul.style.display = 'none';
+      }
+    });
+  }
+});
